737f5009e50df45950ccfa4b297e198dca8e9024
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-14 14:46:34 +0800

    Update linenotify.js

--- LineNotify_20181014/linenotify.js
+++ LineNotify_20181014/linenotify.js
@@ -4,22 +4,22 @@
 
   'use strict';
 
-  function createIframe(ifttt_event,ifttt_key,ifttt_value1,ifttt_value2,ifttt_value3) {
+  function createIframe(notify_token,notify_userid,ifttt_msg) {
     
-    var input_url="https://maker.ifttt.com/trigger/"+ifttt_event+"/with/key/"+ifttt_key+"?value1="+ifttt_value1+"&value2="+ifttt_value2+"&value3="+ifttt_value3;
+    var input_url="https://script.google.com/macros/s/AKfycbwNu63z3ZFHo38wp9LBAwDGyG8tI46-5d-TpFLYFiOHDVOvmgN0/exec?USERID="+notify_userid+"&MESSAGE="+ifttt_msg+"&TOKEN="+notify_token;
     var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
-    if (document.getElementById("ifttt_iframe"))
+    if (document.getElementById("notify_iframe"))
     {
-          document.getElementById("ifttt_iframe").style.left = '0px';
-          document.getElementById("ifttt_iframe").style.top = '0px';
-          document.getElementById("ifttt_iframe").style.display = 'block';
-          document.getElementById("ifttt_iframe").innerHTML = src;
+          document.getElementById("notify_iframe").style.left = '0px';
+          document.getElementById("notify_iframe").style.top = '0px';
+          document.getElementById("notify_iframe").style.display = 'block';
+          document.getElementById("notify_iframe").innerHTML = src;
     }
     else
     {
         var div = document.createElement('div');
-        div.id = "ifttt_iframe";
+        div.id = "notify_iframe";
         div.style.position = 'absolute';      
         div.style.left = '0px';
         div.style.top = '0px';
