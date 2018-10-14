40c939dcb39533da43ad0cd83334cba1f7a6bf70
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-14 14:54:50 +0800

    Update javascript.js

--- LineNotify_20181014/blockly/javascript.js
+++ LineNotify_20181014/blockly/javascript.js
@@ -1,9 +1,7 @@
-Blockly.JavaScript['ifttt_open'] = function (block) {
-  var ifttt_event = Blockly.JavaScript.valueToCode(block, 'ifttt_event', Blockly.JavaScript.ORDER_ATOMIC);  
-  var ifttt_key = Blockly.JavaScript.valueToCode(block, 'ifttt_key', Blockly.JavaScript.ORDER_ATOMIC);
-  var ifttt_value1 = Blockly.JavaScript.valueToCode(block, 'ifttt_value1', Blockly.JavaScript.ORDER_ATOMIC);
-  var ifttt_value2 = Blockly.JavaScript.valueToCode(block, 'ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
-  var ifttt_value3 = Blockly.JavaScript.valueToCode(block, 'ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'createIframe('+ifttt_event+','+ifttt_key+','+ifttt_value1+','+ifttt_value2+','+ifttt_value3+');\n';
+Blockly.JavaScript['line_notify'] = function (block) {
+  var notify_token = Blockly.JavaScript.valueToCode(block, 'notify_token', Blockly.JavaScript.ORDER_ATOMIC);  
+  var notify_userid = Blockly.JavaScript.valueToCode(block, 'notify_userid', Blockly.JavaScript.ORDER_ATOMIC);
+  var notify_msg = Blockly.JavaScript.valueToCode(block, 'notify_msg', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'createIframe('+notify_token+','+notify_userid+','+notify_msg+');\n';
   return code; 
 };
