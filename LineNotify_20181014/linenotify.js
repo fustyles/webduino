4aa359d3c2c8f5b29e04bda8ceda671ddae1b40d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-14 14:55:28 +0800

    Update linenotify.js

--- LineNotify_20181014/linenotify.js
+++ LineNotify_20181014/linenotify.js
@@ -4,9 +4,9 @@
 
   'use strict';
 
-  function createIframe(notify_token,notify_userid,ifttt_msg) {
+  function createIframe(notify_token,notify_userid,notify_msg) {
     
-    var input_url="https://script.google.com/macros/s/AKfycbwNu63z3ZFHo38wp9LBAwDGyG8tI46-5d-TpFLYFiOHDVOvmgN0/exec?USERID="+notify_userid+"&MESSAGE="+ifttt_msg+"&TOKEN="+notify_token;
+    var input_url="https://script.google.com/macros/s/AKfycbwNu63z3ZFHo38wp9LBAwDGyG8tI46-5d-TpFLYFiOHDVOvmgN0/exec?USERID="+notify_userid+"&MESSAGE="+notify_msg+"&TOKEN="+notify_token;
     var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
     if (document.getElementById("notify_iframe"))
