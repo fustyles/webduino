bf4c028677d6521421cd9e3893e60f66be168a0e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-14 14:50:47 +0800

    Update blocks.js

--- LineNotify_20181014/blockly/blocks.js
+++ LineNotify_20181014/blockly/blocks.js
@@ -1,30 +1,21 @@
-Blockly.Blocks['ifttt_open'] = {
+Blockly.Blocks['line_notify'] = {
   init: function() {
-  this.appendValueInput("ifttt_event")
+  this.appendValueInput("notify_token")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField("IFTTT   ")
+      .appendField("CHANNEL ACCESS TOKEN")
       .appendField("Event");    
-  this.appendValueInput("ifttt_key")
+  this.appendValueInput("notify_userid")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField("Key");
-  this.appendValueInput("ifttt_value1")
+      .appendField("USERID");
+  this.appendValueInput("ifttt_msg")
       .setCheck(null)
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField("Value1");
-  this.appendValueInput("ifttt_value2")
-      .setCheck(null)
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField("Value2");
-  this.appendValueInput("ifttt_value3")
-      .setCheck(null)
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField("Value3");  
+      .appendField("MESSAGE");
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setTooltip('');
   this.setColour(65);
-  this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
   }
 };
