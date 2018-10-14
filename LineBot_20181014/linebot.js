039c2128b77dd6f35457b4acfbb320f13212e4ad
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-14 15:57:11 +0800

    Update linebot.js

--- LineBot_20181014/linebot.js
+++ LineBot_20181014/linebot.js
@@ -9,17 +9,17 @@
     var input_url="https://script.google.com/macros/s/AKfycbwNu63z3ZFHo38wp9LBAwDGyG8tI46-5d-TpFLYFiOHDVOvmgN0/exec?USERID="+bot_userid+"&MESSAGE="+bot_msg+"&TOKEN="+bot_token;
     var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
-    if (document.getElementById("notify_iframe"))
+    if (document.getElementById("bot_iframe"))
     {
-          document.getElementById("notify_iframe").style.left = '0px';
-          document.getElementById("notify_iframe").style.top = '0px';
-          document.getElementById("notify_iframe").style.display = 'block';
-          document.getElementById("notify_iframe").innerHTML = src;
+          document.getElementById("bot_iframe").style.left = '0px';
+          document.getElementById("bot_iframe").style.top = '0px';
+          document.getElementById("bot_iframe").style.display = 'block';
+          document.getElementById("bot_iframe").innerHTML = src;
     }
     else
     {
         var div = document.createElement('div');
-        div.id = "notify_iframe";
+        div.id = "bot_iframe";
         div.style.position = 'absolute';      
         div.style.left = '0px';
         div.style.top = '0px';
