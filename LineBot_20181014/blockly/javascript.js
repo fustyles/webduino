0c255535a15bea2c5413ad67122cd8ae32b08552
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-14 16:21:31 +0800

    Update javascript.js

--- LineBot_20181014/blockly/javascript.js
+++ LineBot_20181014/blockly/javascript.js
@@ -1,4 +1,4 @@
-Blockly.JavaScript['line_notify'] = function (block) {
+Blockly.JavaScript['linebot_notify'] = function (block) {
   var bot_token = Blockly.JavaScript.valueToCode(block, 'bot_token', Blockly.JavaScript.ORDER_ATOMIC);  
   var bot_userid = Blockly.JavaScript.valueToCode(block, 'bot_userid', Blockly.JavaScript.ORDER_ATOMIC);
   var bot_msg = Blockly.JavaScript.valueToCode(block, 'bot_msg', Blockly.JavaScript.ORDER_ATOMIC);
