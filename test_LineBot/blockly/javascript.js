6c0c01fb24e049e62d785aeb4a22a36ef7cf53f4
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-15 18:06:38 +0800

    Update javascript.js

--- test_LineBot/blockly/javascript.js
+++ test_LineBot/blockly/javascript.js
@@ -5,3 +5,23 @@ Blockly.JavaScript['linebot_notify'] = function (block) {
   var code = 'createIframe('+bot_token+','+bot_userid+','+bot_msg+');\n';
   return code; 
 };
+
+Blockly.JavaScript['linebot_type'] = function(block) {
+  var value_type = block.getFieldValue('value_type');
+  var value_paramter1 = Blockly.JavaScript.valueToCode(block, 'value_paramter1', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_paramter2 = Blockly.JavaScript.valueToCode(block, 'value_paramter2', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_paramter3 = Blockly.JavaScript.valueToCode(block, 'value_paramter3', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_paramter4 = Blockly.JavaScript.valueToCode(block, 'value_paramter4', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_paramter5 = Blockly.JavaScript.valueToCode(block, 'value_paramter5', Blockly.JavaScript.ORDER_ATOMIC);
+  if (value_type=='text')
+    var code = '';
+  else if (value_type=='sticker')
+    var code = '';
+  else if (value_type=='image')
+    var code = '';
+  else if (value_type=='video')
+    var code = '';
+  else if (value_type=='audio')
+    var code = '';  
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
