9ebffdfca69cfd5df74bd26a134782b6ef9e194f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-15 21:30:04 +0800

    Add files via upload

---
+++ LineBot_20181015/blockly/javascript.js
@@ -0,0 +1,32 @@
+Blockly.JavaScript['linebot_notify'] = function (block) {
+  var bot_token = Blockly.JavaScript.valueToCode(block, 'bot_token', Blockly.JavaScript.ORDER_ATOMIC);  
+  var bot_userid = Blockly.JavaScript.valueToCode(block, 'bot_userid', Blockly.JavaScript.ORDER_ATOMIC);
+  var bot_msg = Blockly.JavaScript.valueToCode(block, 'bot_msg', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'linebot_notify('+bot_token+','+bot_userid+','+bot_msg.replace(/\'/g,"")+');\n';
+  return code; 
+};
+
+Blockly.JavaScript['linebot_type'] = function(block) {
+  var value_type = block.getFieldValue('value_type');
+  var value_paramter1 = Blockly.JavaScript.valueToCode(block, 'value_paramter1', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_paramter2 = Blockly.JavaScript.valueToCode(block, 'value_paramter2', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if (value_type=='text')
+    var code = '"&type='+value_type+'&text='+value_paramter1+'"';
+  else if (value_type=='sticker')
+    var code = '"&type='+value_type+'&packageId='+value_paramter1+"&stickerId="+value_paramter2+'"';
+  else if (value_type=='image')
+    var code = '"&type='+value_type+'&originalContentUrl='+value_paramter1+"&previewImageUrl="+value_paramter2+'"';
+  else if (value_type=='video')
+    var code = '"&type='+value_type+'&originalContentUrl='+value_paramter1+"&previewImageUrl="+value_paramter2+'"';
+  else if (value_type=='audio')
+    var code = '"&type='+value_type+'&originalContentUrl='+value_paramter1+"&duration="+value_paramter2+'"';  
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['linenotify'] = function (block) {
+  var notify_token = Blockly.JavaScript.valueToCode(block, 'notify_token', Blockly.JavaScript.ORDER_ATOMIC);  
+  var notify_msg = Blockly.JavaScript.valueToCode(block, 'notify_msg', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'linenotify('+notify_token+','+notify_msg+');\n';
+  return code; 
+};
