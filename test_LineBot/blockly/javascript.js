b850606c63f662b768f05fd8d8c0513fcd112b6d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-15 17:19:37 +0800

    Add files via upload

---
+++ test_LineBot/blockly/javascript.js
@@ -0,0 +1,7 @@
+Blockly.JavaScript['linebot_notify'] = function (block) {
+  var bot_token = Blockly.JavaScript.valueToCode(block, 'bot_token', Blockly.JavaScript.ORDER_ATOMIC);  
+  var bot_userid = Blockly.JavaScript.valueToCode(block, 'bot_userid', Blockly.JavaScript.ORDER_ATOMIC);
+  var bot_msg = Blockly.JavaScript.valueToCode(block, 'bot_msg', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'createIframe('+bot_token+','+bot_userid+','+bot_msg+');\n';
+  return code; 
+};
---
+++ test_LineBot/linebot.js
@@ -0,0 +1,68 @@
+// Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
+
++(function (window, document) {
+
+  'use strict';
+
+  function createIframe(bot_token,bot_userid,bot_msg) {
+    
+    var input_url="https://script.google.com/macros/s/AKfycbwNu63z3ZFHo38wp9LBAwDGyG8tI46-5d-TpFLYFiOHDVOvmgN0/exec?TOKEN="+bot_token+"&USERID="+bot_userid+"&MESSAGE="+bot_msg;
+    var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
+    
+    if (document.getElementById("bot_iframe"))
+    {
+          document.getElementById("bot_iframe").style.left = '0px';
+          document.getElementById("bot_iframe").style.top = '0px';
+          document.getElementById("bot_iframe").style.display = 'block';
+          document.getElementById("bot_iframe").innerHTML = src;
+    }
+    else
+    {
+        var div = document.createElement('div');
+        div.id = "bot_iframe";
+        div.style.position = 'absolute';      
+        div.style.left = '0px';
+        div.style.top = '0px';
+        div.style.zindex='9999';      
+        div.innerHTML = src;
+        document.body.appendChild(div);
+    }
+  }
+
+  window.createIframe = createIframe;
+
+}(window, window.document));
+
+/*
+// Google Apps Script
+// https://script.google.com/macros/s/xxxxx/exec?USERID=xxxxx&MESSAGE=xxxxx&TOKEN=xxxxx
+
+function doGet(e) {
+  var token = e.parameter.TOKEN.replace(/\s+/g,"+");
+  var userid = e.parameter.USERID;
+  var msg = e.parameter.MESSAGE;
+  
+  //console.log("token="+token);
+  //console.log("userid="+userid);
+  //console.log("msg="+msg);    
+  
+  var url = 'https://api.line.me/v2/bot/message/push';
+  UrlFetchApp.fetch(url, {
+    'headers': {
+      'Content-Type': 'application/json; charset=UTF-8',
+      'Authorization': 'Bearer ' + token,
+    },
+    'method': 'post',
+    'payload': JSON.stringify({
+      'to':  userid,
+      'messages': [{
+        type:'text',
+        text: msg
+      }]
+    }),
+  });  
+  
+  var textOutput = ContentService.createTextOutput(msg)
+  return textOutput
+}
+*/
