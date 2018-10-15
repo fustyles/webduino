60b7509ee9cd4f8204a321cddbb0fc3c78d238b6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-15 18:07:32 +0800

    Update javascript.js

--- test_LineBot/blockly/javascript.js
+++ test_LineBot/blockly/javascript.js
@@ -2,7 +2,7 @@ Blockly.JavaScript['linebot_notify'] = function (block) {
   var bot_token = Blockly.JavaScript.valueToCode(block, 'bot_token', Blockly.JavaScript.ORDER_ATOMIC);  
   var bot_userid = Blockly.JavaScript.valueToCode(block, 'bot_userid', Blockly.JavaScript.ORDER_ATOMIC);
   var bot_msg = Blockly.JavaScript.valueToCode(block, 'bot_msg', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'createIframe('+bot_token+','+bot_userid+','+bot_msg+');\n';
+  var code = 'linebot_notify('+bot_token+','+bot_userid+','+bot_msg+');\n';
   return code; 
 };
 
@@ -24,4 +24,11 @@ Blockly.JavaScript['linebot_type'] = function(block) {
   else if (value_type=='audio')
     var code = '';  
   return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['linenotify'] = function (block) {
+  var bot_token = Blockly.JavaScript.valueToCode(block, 'bot_token', Blockly.JavaScript.ORDER_ATOMIC);  
+  var bot_msg = Blockly.JavaScript.valueToCode(block, 'bot_msg', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'linenotify('+bot_token+','+bot_msg+');\n';
+  return code; 
 };
