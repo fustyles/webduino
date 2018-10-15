339790ba756ae2239acbe07a4f3df61a585e21e6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-16 00:58:23 +0800

    Update javascript.js

--- test_LineBot/blockly/javascript.js
+++ test_LineBot/blockly/javascript.js
@@ -12,7 +12,7 @@ Blockly.JavaScript['linebot_type'] = function(block) {
   var value_paramter2 = Blockly.JavaScript.valueToCode(block, 'value_paramter2', Blockly.JavaScript.ORDER_ATOMIC);
   
   if (value_type=='text')
-    var code = '"&type='+value_type+'&text="+(typeof('+value_paramter1+')=="undefined"?'+'"'+value_paramter1+'")';
+    var code = '"&type='+value_type+'&text="+(typeof('+value_paramter1+')=="undefined"?('+'"'+value_paramter1+'"):'+value_paramter1+')';
   else if (value_type=='sticker')
     var code = '"&type='+value_type+'&packageId="+'+value_paramter1+'+"&stickerId="+'+value_paramter2;
   else if (value_type=='image')
