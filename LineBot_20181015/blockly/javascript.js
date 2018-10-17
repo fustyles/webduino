d5a8b875f739b4c19d0babc41a739986597e8771
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-17 11:30:43 +0800

    Update javascript.js

--- LineBot_20181015/blockly/javascript.js
+++ LineBot_20181015/blockly/javascript.js
@@ -13,10 +13,10 @@ Blockly.JavaScript['linebot_type'] = function(block) {
   var value_parameter3 = Blockly.JavaScript.valueToCode(block, 'value_parameter3', Blockly.JavaScript.ORDER_ATOMIC);
   var value_parameter4 = Blockly.JavaScript.valueToCode(block, 'value_parameter4', Blockly.JavaScript.ORDER_ATOMIC);  
   
-  if (!value_parameter1) value_parameter1="";
-  if (!value_parameter2) value_parameter2="";
-  if (!value_parameter3) value_parameter3="";
-  if (!value_parameter4) value_parameter4="";
+  if (!value_parameter1) value_parameter1='""';
+  if (!value_parameter2) value_parameter2='""';
+  if (!value_parameter3) value_parameter3='""';
+  if (!value_parameter4) value_parameter4='""';
   
   if ((value_parameter1.indexOf(":")!=-1)||(value_parameter1.indexOf("/")!=-1)||(value_parameter1.indexOf("+")!=-1))
     value_parameter1 = '"'+value_parameter1+'"';
