437fb91db4ff6fd39a6a978062bcdf5d4d5459e5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-17 10:17:44 +0800

    Delete en.js

--- RGB_LED_20180219/blockly/msg/blocks/en.js
+++
@@ -1,7 +0,0 @@
-Blockly.Msg.TITLE_SHOW = "RGB LED   ";
-Blockly.Msg.RED_SHOW = "RED";
-Blockly.Msg.VALE_RED_SHOW = "VALUE";
-Blockly.Msg.GREEN_SHOW = "GREEN";
-Blockly.Msg.VALUE_GREEN_SHOW = "VALUE";
-Blockly.Msg.BLUE_SHOW = "BLUE";
-Blockly.Msg.VALUE_BLUE_SHOW = "VALUE";
