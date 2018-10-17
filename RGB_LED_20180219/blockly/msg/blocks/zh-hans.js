155c8e3b1fe007819d16e2aa4db9a34dea3346a9
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-17 10:17:48 +0800

    Delete zh-hans.js

--- RGB_LED_20180219/blockly/msg/blocks/zh-hans.js
+++
@@ -1,7 +0,0 @@
-Blockly.Msg.TITLE_SHOW = "RGB LED   ";
-Blockly.Msg.RED_SHOW = "红";
-Blockly.Msg.VALE_RED_SHOW = "数值";
-Blockly.Msg.GREEN_SHOW = "绿";
-Blockly.Msg.VALUE_GREEN_SHOW = "数值";
-Blockly.Msg.BLUE_SHOW = "蓝";
-Blockly.Msg.VALUE_BLUE_SHOW = "数值";
