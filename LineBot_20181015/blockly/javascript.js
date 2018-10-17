1f53a098279ba0e609a7da7265b685939ebd2e40
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-17 23:23:28 +0800

    Update javascript.js

--- LineBot_20181015/blockly/javascript.js
+++ LineBot_20181015/blockly/javascript.js
@@ -20,10 +20,10 @@ Blockly.JavaScript['linebot_type'] = function(block) {
   
   console.log(value_parameter1);
   
-  value_parameter1 = value_parameter1.replace(/\\\'/g,"'");
-  value_parameter2 = value_parameter2.replace(/\\\'/g,"'");
-  value_parameter3 = value_parameter3.replace(/\\\'/g,"'");
-  value_parameter4 = value_parameter4.replace(/\\\'/g,"'");
+  value_parameter1 = value_parameter1.replace(/\\\'/g,"'").replace(/\"/g,"'");
+  value_parameter2 = value_parameter2.replace(/\\\'/g,"'").replace(/\"/g,"'");
+  value_parameter3 = value_parameter3.replace(/\\\'/g,"'").replace(/\"/g,"'");
+  value_parameter4 = value_parameter4.replace(/\\\'/g,"'").replace(/\"/g,"'");
   
   console.log(value_parameter1);
   
@@ -38,10 +38,10 @@ Blockly.JavaScript['linebot_type'] = function(block) {
   
   console.log(value_parameter1);
   
-  value_parameter1 = escape(value_parameter1).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
-  value_parameter2 = escape(value_parameter2).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
-  value_parameter3 = escape(value_parameter3).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
-  value_parameter4 = escape(value_parameter4).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
+  value_parameter1 = escape(value_parameter1).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"').replace(/\+/g,"%2B");
+  value_parameter2 = escape(value_parameter2).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"').replace(/\+/g,"%2B");
+  value_parameter3 = escape(value_parameter3).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"').replace(/\+/g,"%2B");
+  value_parameter4 = escape(value_parameter4).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"').replace(/\+/g,"%2B");
   
   if (value_type=='text')
     var code = '"&type='+value_type+'&text="+'+value_parameter1;
