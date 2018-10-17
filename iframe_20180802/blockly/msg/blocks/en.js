363312686c29d773b29a4ea25917c9d5b65d47ea
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-17 21:31:44 +0800

    Delete en.js

--- iframe_20180802/blockly/msg/blocks/en.js
+++
@@ -1,9 +0,0 @@
-Blockly.Msg.OPEN_SHOW = "Web Page";
-Blockly.Msg.CLOSE_SHOW = "Close Web Page";
-Blockly.Msg.OPENID_SHOW = "ID";
-Blockly.Msg.CLOSEID_SHOW = "ID";
-Blockly.Msg.URL_SHOW = "Url";
-Blockly.Msg.WIDTH_SHOW = "Width";
-Blockly.Msg.LEFT_SHOW = "Left";
-Blockly.Msg.TOP_SHOW = "Top";
-Blockly.Msg.ZINDEX_SHOW = "Z-index";
