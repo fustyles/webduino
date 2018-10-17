48826e5271b999d554226fae6319efa834ee825c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-17 22:55:57 +0800

    Update javascript.js

--- LineBot_20181015/blockly/javascript.js
+++ LineBot_20181015/blockly/javascript.js
@@ -22,15 +22,20 @@ Blockly.JavaScript['linebot_type'] = function(block) {
   console.log(escape(value_parameter1).indexOf("%27"));
   console.log(escape(value_parameter1).lastIndexOf("%27"));
   console.log(escape(value_parameter1).length);
-  if ((escape(value_parameter1).indexOf("%27")==0)&&(escape(value_parameter1).lastIndexOf("%27")==escape(value_parameter1).length-3)) {
-    value_parameter1 = '"'+value_parameter1.substr(1,value_parameter1.length-2)+'"';
-    console.log(value_parameter1);
-  }
-  value_parameter1 = escape(value_parameter1).replace(/\%26/g," ").replace(/\%23/g," ");
   
-  value_parameter2 = escape(value_parameter2).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%27/g,'"');
-  value_parameter3 = escape(value_parameter3).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%27/g,'"');
-  value_parameter4 = escape(value_parameter4).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%27/g,'"');
+  if ((escape(value_parameter1).indexOf("%27")==0)&&(escape(value_parameter1).lastIndexOf("%27")==escape(value_parameter1).length-3))
+    value_parameter1 = '"'+value_parameter1.substr(1,value_parameter1.length-2)+'"';
+  if ((escape(value_parameter2).indexOf("%27")==0)&&(escape(value_parameter2).lastIndexOf("%27")==escape(value_parameter2).length-3))
+    value_parameter2 = '"'+value_parameter2.substr(1,value_parameter2.length-2)+'"';
+  if ((escape(value_parameter3).indexOf("%27")==0)&&(escape(value_parameter3).lastIndexOf("%27")==escape(value_parameter3).length-3))
+    value_parameter3 = '"'+value_parameter3.substr(1,value_parameter3.length-2)+'"';
+  if ((escape(value_parameter4).indexOf("%27")==0)&&(escape(value_parameter4).lastIndexOf("%27")==escape(value_parameter4).length-3))
+    value_parameter4 = '"'+value_parameter4.substr(1,value_parameter4.length-2)+'"';
+  
+  value_parameter1 = escape(value_parameter1).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
+  value_parameter2 = escape(value_parameter2).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
+  value_parameter3 = escape(value_parameter3).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
+  value_parameter4 = escape(value_parameter4).replace(/\%26/g," ").replace(/\%23/g," ").replace(/\%22/g,'"');
   
   if (value_type=='text')
     var code = '"&type='+value_type+'&text="+'+value_parameter1;
