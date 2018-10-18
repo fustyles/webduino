07ee4fa3cfd2ee7d193ddf3105b871213ad78e97
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-18 23:52:54 +0800

    Update javascript.js

--- test_LineBot/blockly/javascript.js
+++ test_LineBot/blockly/javascript.js
@@ -12,16 +12,16 @@ Blockly.JavaScript['linebot_notify'] = function (block) {
 };
 
 Blockly.JavaScript['linebot_type'] = function(block) {
-  var value_type = block.getFieldValue('value_type');
-  var value_parameter1 = Blockly.JavaScript.valueToCode(block, 'value_parameter1', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_parameter2 = Blockly.JavaScript.valueToCode(block, 'value_parameter2', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_parameter3 = Blockly.JavaScript.valueToCode(block, 'value_parameter3', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_parameter4 = Blockly.JavaScript.valueToCode(block, 'value_parameter4', Blockly.JavaScript.ORDER_ATOMIC);  
+  var type = block.getFieldValue('value_type');
+  var parameter1 = Blockly.JavaScript.valueToCode(block, 'value_parameter1', Blockly.JavaScript.ORDER_ATOMIC);
+  var parameter2 = Blockly.JavaScript.valueToCode(block, 'value_parameter2', Blockly.JavaScript.ORDER_ATOMIC);
+  var parameter3 = Blockly.JavaScript.valueToCode(block, 'value_parameter3', Blockly.JavaScript.ORDER_ATOMIC);
+  var parameter4 = Blockly.JavaScript.valueToCode(block, 'value_parameter4', Blockly.JavaScript.ORDER_ATOMIC);  
   
-  if (!value_parameter1) value_parameter1='';
-  if (!value_parameter2) value_parameter2='';
-  if (!value_parameter3) value_parameter3='';
-  if (!value_parameter4) value_parameter4='';
+  if (!parameter1) parameter1='';
+  if (!parameter2) parameter2='';
+  if (!parameter3) parameter3='';
+  if (!parameter4) parameter4='';
   
   var code = 'linebot_url_escape('+type+','+parameter1+','+parameter2+','+parameter3+','+parameter4+')';
   return [code, Blockly.JavaScript.ORDER_NONE];
