3b08950547c7d1a842c6fd064f462adaa01aec0a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-20 17:52:20 +0800

    Update javascript.js

--- IFTTT_20180312/blockly/javascript.js
+++ IFTTT_20180312/blockly/javascript.js
@@ -4,6 +4,13 @@ Blockly.JavaScript['ifttt_open'] = function (block) {
   var ifttt_value1 = Blockly.JavaScript.valueToCode(block, 'ifttt_value1', Blockly.JavaScript.ORDER_ATOMIC);
   var ifttt_value2 = Blockly.JavaScript.valueToCode(block, 'ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
   var ifttt_value3 = Blockly.JavaScript.valueToCode(block, 'ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if (!ifttt_event) ifttt_event='""';
+  if (!ifttt_key) ifttt_key='""';
+  if (!ifttt_value1) ifttt_value1='""';
+  if (!ifttt_value2) ifttt_value2='""';
+  if (!ifttt_value3) ifttt_value3='""';
+  
   var code = 'createIframe('+ifttt_event+','+ifttt_key+','+ifttt_value1+','+ifttt_value2+','+ifttt_value3+');\n';
   return code; 
 };
