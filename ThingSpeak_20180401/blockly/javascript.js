4b19ab3d5e6105df75366683f1952502e0b5e30d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-20 19:34:55 +0800

    Update javascript.js

--- ThingSpeak_20180401/blockly/javascript.js
+++ ThingSpeak_20180401/blockly/javascript.js
@@ -8,6 +8,17 @@ Blockly.JavaScript['thingspeak_update'] = function (block) {
   var field6 = Blockly.JavaScript.valueToCode(block, 'field6', Blockly.JavaScript.ORDER_ATOMIC);
   var field7 = Blockly.JavaScript.valueToCode(block, 'field7', Blockly.JavaScript.ORDER_ATOMIC);
   var field8 = Blockly.JavaScript.valueToCode(block, 'field8', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if (!key) key='""';
+  if (!field1) field1='""';
+  if (!field2) field2='""';
+  if (!field3) field3='""';
+  if (!field4) field4='""';
+  if (!field5) field5='""';
+  if (!field6) field6='""';
+  if (!field7) field7='""';
+  if (!field8) field8='""';
+  
   var code = 'createIframe('+key+','+field1+','+field2+','+field3+','+field4+','+field5+','+field6+','+field7+','+field8+');\n';
   return code; 
 };
