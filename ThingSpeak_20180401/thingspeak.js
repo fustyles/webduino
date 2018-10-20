b49840af7806a509432e6919d78d9694827c1412
fustyles <34397289+fustyles@users.noreply.github.com>
2018-10-20 19:32:51 +0800

    Update thingspeak.js

--- ThingSpeak_20180401/thingspeak.js
+++ ThingSpeak_20180401/thingspeak.js
@@ -6,6 +6,16 @@
 
   function createIframe(key,field1,field2,field3,field4,field5,field6,field7,field8) {
     
+    key = escape(key+"");
+    field1 = escape(field1+"");
+    field2 = escape(field2+"");
+    field3 = escape(field3+"");
+    field4 = escape(field4+"");
+    field5 = escape(field5+"");
+    field6 = escape(field6+"");
+    field7 = escape(field7+"");
+    field8 = escape(field8+"");
+    
     var input_url="https://api.thingspeak.com/update?api_key="+key+"&field1="+field1+"&field2="+field2+"&field3="+field3+"&field4="+field4+"&field5="+field5+"&field6="+field6+"&field7="+field7+"&field8="+field8;
     var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
     
