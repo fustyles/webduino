f7178c44e647cc88917acea6a8a5d2bcd959b7d9
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-09 23:24:50 +0800

    Update javascript.js

--- DeviceStatus_20181109/blockly/javascript.js
+++ DeviceStatus_20181109/blockly/javascript.js
@@ -3,6 +3,6 @@ Blockly.JavaScript['boardevent_ready'] = function (block) {
   var do_ready = Blockly.JavaScript.statementToCode(block, 'do_ready');
   var do_error = Blockly.JavaScript.statementToCode(block, 'do_error');
   var do_message = Blockly.JavaScript.statementToCode(block, 'do_message');
-  var code = 'board.on(BoardEvent.READY,async function() {\nsetInterval(async function(){\n    board.send([0xF0, 0x0E, 0x07, 0xF7]);\n    }, ' + value_intervals + ');\n' + do_ready + '\n});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '\n});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '\n});\n';
+  var code = 'board.on(BoardEvent.READY,async function() {\nsetInterval(async function(){\n    board.send([0xF0, 0x0E, 0x07, 0xF7]);\n    }, ' + value_intervals + ');\n' + do_ready + '\n});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\n';
   return code;
 };
