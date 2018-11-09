3742cb5280d7cfa564f093ee6d35878f72ce6cc3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-09 23:06:43 +0800

    Update blocks.js

--- DeviceStatus_20181109/blockly/blocks.js
+++ DeviceStatus_20181109/blockly/blocks.js
@@ -4,9 +4,13 @@ Blockly.Blocks['boardevent_ready'] = {
         .setAlign(Blockly.ALIGN_RIGHT)
         .setCheck("Number")
         .appendField("BoardEvent.READY    Time Intervals");
-    this.appendStatementInput("do_");
-    this.appendStatementInput("do2_");
-    this.appendStatementInput("do3_");
+    this.appendStatementInput("do_ready");
+    this.appendDummyInput()
+      .appendField("BoardEvent.ERROR");
+    this.appendStatementInput("do_error");
+    this.appendDummyInput()
+      .appendField("BoardEvent.STRING_MESSAGE");
+    this.appendStatementInput("do_message");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(60);
