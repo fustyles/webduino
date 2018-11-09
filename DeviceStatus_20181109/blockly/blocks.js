41b25f423dd829e8a01a05b1837357b21514e802
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-09 23:22:24 +0800

    Update blocks.js

--- DeviceStatus_20181109/blockly/blocks.js
+++ DeviceStatus_20181109/blockly/blocks.js
@@ -16,24 +16,3 @@ Blockly.Blocks['boardevent_ready'] = {
     this.setColour(60);
   }
 };
-
-Blockly.Blocks['boardevent_error'] = {
-  init: function () { 
-    this.appendDummyInput()
-      .appendField("BoardEvent.ERROR");
-    this.appendStatementInput("do_");
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(60);
-  }
-};
-
-Blockly.Blocks['boardevent_message'] = {
-  init: function () {
-    this.appendDummyInput()
-      .appendField("BoardEvent.STRING_MESSAGE");
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(60);
-  }
-};
