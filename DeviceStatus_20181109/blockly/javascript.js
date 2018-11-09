93d6261309496cd0049e1d0323069a2181617a06
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-09 22:57:14 +0800

    Update javascript.js

--- DeviceStatus_20181109/blockly/javascript.js
+++ DeviceStatus_20181109/blockly/javascript.js
@@ -1,17 +1,17 @@
 Blockly.JavaScript['boardevent_ready'] = function (block) {
-  var value_intervals = Blockly.JavaScript.valueToCode(block, 'intervals_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'board.on(BoardEvent.READY, function() {setInterval(function(){board.send([0xF0, 0x0E, 0x07, 0xF7]);}, ' + value_intervals + ');});\n';
+  var value_intervals = Blockly.JavaScript.valueToCode(block, 'intervals', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'board.on(BoardEvent.READY, function() {\nsetInterval(function(){\nboard.send([0xF0, 0x0E, 0x07, 0xF7]);\n}, ' + value_intervals + ');\n});\n';
   return code;
 };
 
 Blockly.JavaScript['boardevent_error'] = function (block) {
   var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = 'board.on(BoardEvent.ERROR, function() {' + statements_do_ + '});\n';
+  var code = 'board.on(BoardEvent.ERROR, function() {\n' + statements_do_ + '\n});\n';
   return code;
 };
 
 Blockly.JavaScript['boardevent_message'] = function (block) {
   var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = 'board.on(webduino.BoardEvent.STRING_MESSAGE, function(event) {' + statements_do_ + '});\n';
+  var code = 'board.on(webduino.BoardEvent.STRING_MESSAGE, function(event) {\n' + statements_do_ + '\n});\n';
   return code;
 };
