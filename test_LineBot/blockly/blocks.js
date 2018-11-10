7678173c097d04b96287810f87b4eee1663a73a6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 20:26:30 +0800

    Update blocks.js

--- test_LineBot/blockly/blocks.js
+++ test_LineBot/blockly/blocks.js
@@ -70,7 +70,7 @@ Blockly.Blocks['linenotify_type'] = {
         .appendField("Message (Line Notify)");    
     this.appendDummyInput()
         .appendField("Type")
-        .appendField(new Blockly.FieldDropdown([["text (text)","text"], ["sticker (packageId, stickerId, text)","sticker"], ["image (originalContentUrl, previewImageUrl, text)","image"]]), "value_type");
+        .appendField(new Blockly.FieldDropdown([["text (text)","text"], ["sticker (text, packageId, stickerId)","sticker"], ["image (text, originalContentUrl, previewImageUrl)","image"]]), "value_type");
     this.appendValueInput("value_parameter1")
         .setCheck(null)
         .appendField("Parameter1","p1");
