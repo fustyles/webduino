6d44e5158dfe0caf0cc5d78c77e2fbf498da91cb
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 14:41:11 +0800

    Update javascript.js

--- DeviceStatus_20181109/blockly/javascript.js
+++ DeviceStatus_20181109/blockly/javascript.js
@@ -3,14 +3,14 @@ Blockly.JavaScript['boardevent'] = function (block) {
   var do_ready = Blockly.JavaScript.statementToCode(block, 'do_ready');
   var do_error = Blockly.JavaScript.statementToCode(block, 'do_error');
   var do_message = Blockly.JavaScript.statementToCode(block, 'do_message');
-  var code = 'var board1 = new webduino.WebArduino('+device+');\nvar BoardEvent = webduino.BoardEvent;\nboard1.on(BoardEvent.READY,async function() {\n'+do_ready+'});\nboard1.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard1.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\nawait delay(1);\n';
+  var code = 'var board = new webduino.WebArduino('+device+');\nvar BoardEvent = webduino.BoardEvent;\nboard.on(BoardEvent.READY,async function() {\n'+do_ready+'});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\nawait delay(1);\n';
   return code;
 };
 
 Blockly.JavaScript['boardevent_sendmessage'] = function (block) {
   var cmd = Blockly.JavaScript.valueToCode(block, 'cmd', Blockly.JavaScript.ORDER_ATOMIC);
   cmd = cmd.replace(/\'/g,"");
-  var code = 'board1.send('+cmd+');\n';
+  var code = 'board.send('+cmd+');\n';
   return code;
 };
 
