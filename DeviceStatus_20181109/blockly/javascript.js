c379d8e9b167c36fedf9e68f16d9eb9c3ab6cb10
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 13:26:39 +0800

    Update javascript.js

--- DeviceStatus_20181109/blockly/javascript.js
+++ DeviceStatus_20181109/blockly/javascript.js
@@ -2,7 +2,7 @@ Blockly.JavaScript['boardevent'] = function (block) {
   var do_ready = Blockly.JavaScript.statementToCode(block, 'do_ready');
   var do_error = Blockly.JavaScript.statementToCode(block, 'do_error');
   var do_message = Blockly.JavaScript.statementToCode(block, 'do_message');
-  var code = 'var BoardEvent = webduino.BoardEvent;\nboard.on(BoardEvent.READY,async function() {'+do_ready+'});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\n';
+  var code = 'var BoardEvent = webduino.BoardEvent;\nboard.on(BoardEvent.READY,async function() {\n'+do_ready+'});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\n';
   return code;
 };
 
