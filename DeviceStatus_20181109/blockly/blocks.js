1f84a246f728bf77276ffc95e44c4e1a59dcd3ae
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 16:57:07 +0800

    Update blocks.js

--- DeviceStatus_20181109/blockly/blocks.js
+++ DeviceStatus_20181109/blockly/blocks.js
@@ -18,7 +18,7 @@ Blockly.Blocks['boardevent'] = {
     this.appendStatementInput("do_ready");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(90);
+    this.setColour(190);
     this.getField('samplingInterval').setValue('250');
   }
 };
@@ -30,7 +30,7 @@ Blockly.Blocks['boardevent_sendmessage'] = {
       .appendField("Send Command");    
     this.setPreviousStatement(true);
     this.setNextStatement(true);
-    this.setColour(90);
+    this.setColour(190);
   }
 };
 
