c246ef2561f7f25d0f4c82f55e55bd0b82f0483a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 21:25:30 +0800

    Update blocks.js

--- test_LineBot/blockly/blocks.js
+++ test_LineBot/blockly/blocks.js
@@ -91,13 +91,13 @@ Blockly.Blocks['linenotify_type'] = {
   },
     onchange: function() {
     if (this.getField('value_type').getValue() == 'text') {
-      if (this.getField('p2').visible==true) this.getField('p2').setVisible(false);
-      if (this.getField('p3').visible==true) this.getField('p3').setVisible(false);
+      this.getField('p2').setVisible(false);
+      this.getField('p3').setVisible(false);
       this.getInput('value_parameter2').setVisible(false);
       this.getInput('value_parameter3').setVisible(false);
     } else {
-      if (this.getField('p2').visible==false) this.getField('p2').setVisible(true);
-      if (this.getField('p3').visible==false) this.getField('p3').setVisible(true);
+      this.getField('p2').setVisible(true);
+      this.getField('p3').setVisible(true);
       this.getInput('value_parameter2').setVisible(true);
       this.getInput('value_parameter3').setVisible(true);
     }
