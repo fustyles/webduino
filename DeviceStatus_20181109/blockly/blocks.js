d358b44ed30d85e1330939ced64eeeabcdf30a61
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 15:56:55 +0800

    Update blocks.js

--- DeviceStatus_20181109/blockly/blocks.js
+++ DeviceStatus_20181109/blockly/blocks.js
@@ -2,7 +2,10 @@ Blockly.Blocks['boardevent'] = {
   init: function () {
     this.appendValueInput("device")
       .setCheck("String")
-      .appendField("BoardEvent.READY   Device ID");    
+      .appendField("BoardEvent.READY   Device ID"); 
+    this.appendDummyInput()  
+      .appendField("Sampling Interval")
+      .appendField(new Blockly.FieldDropdown([["20","20"], ["50","50"], ["75","75"], ["100","100"], ["250","250"], ["500","500"], ["1000","1000"]]), "samplingInterval");    
     this.appendStatementInput("do_ready");
     this.appendDummyInput()
       .appendField("BoardEvent.ERROR");
