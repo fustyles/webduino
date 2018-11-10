59448582074f57ff0bba0678f1f93894587e00fd
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-10 13:13:42 +0800

    Update javascript.js

--- DeviceStatus_20181109/blockly/javascript.js
+++ DeviceStatus_20181109/blockly/javascript.js
@@ -1,16 +1,22 @@
 Blockly.JavaScript['boardevent'] = function (block) {
   var value_intervals = Blockly.JavaScript.valueToCode(block, 'intervals', Blockly.JavaScript.ORDER_ATOMIC);
   var device = Blockly.JavaScript.valueToCode(block, 'device', Blockly.JavaScript.ORDER_ATOMIC);
-  var cmd = Blockly.JavaScript.valueToCode(block, 'cmd', Blockly.JavaScript.ORDER_ATOMIC);
+  
   cmd = cmd.replace(/\'/g,"");
   var do_ready = Blockly.JavaScript.statementToCode(block, 'do_ready');
   var do_error = Blockly.JavaScript.statementToCode(block, 'do_error');
   var do_message = Blockly.JavaScript.statementToCode(block, 'do_message');
-  var code = 'var BoardEvent = webduino.BoardEvent;\nboard.on(BoardEvent.READY,async function() {\n    setInterval(async function(){\n        board.send('+cmd+');\n    }, ' + value_intervals + ');\n'+do_ready+'});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\n';
+  var code = 'var BoardEvent = webduino.BoardEvent;\nboard.on(BoardEvent.READY,async function() {\n    setInterval(async function(){\n}, ' + value_intervals + ');\n'+do_ready+'});\nboard.on(BoardEvent.ERROR,async function() {\n' + do_error + '});\nboard.on(webduino.BoardEvent.STRING_MESSAGE,async function(event) {\n' + do_message + '});\n';
   return code;
 };
 
-Blockly.JavaScript['boardevent_message'] = function (block) {
+Blockly.JavaScript['boardevent_sendmessage'] = function (block) {
+  var cmd = Blockly.JavaScript.valueToCode(block, 'cmd', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'board.send('+cmd+');\n';
+  return code;
+};
+
+Blockly.JavaScript['boardevent_getmessage'] = function (block) {
   var code = 'event.message.split(",")';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
