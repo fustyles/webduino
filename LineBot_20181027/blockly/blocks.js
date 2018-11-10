e605bda073332acd013b61114e14e6f821c21c36
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-11 02:01:43 +0800

    Update blocks.js

--- LineBot_20181027/blockly/blocks.js
+++ LineBot_20181027/blockly/blocks.js
@@ -25,7 +25,6 @@ Blockly.Blocks['linebot_type'] = {
     this.appendDummyInput()
         .appendField("Message (Line Bot)");    
     this.appendDummyInput()
-        .appendField("Type")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField(new Blockly.FieldDropdown([["text (text)","text"], ["sticker (packageId, stickerId)","sticker"], ["image (originalContentUrl, previewImageUrl)","image"], ["video (originalContentUrl, previewImageUrl)","video"], ["audio (originalContentUrl, duration)","audio"], ["location (title, address, latitude, longitude)","location"]]), "value_type");
     this.appendValueInput("value_parameter1")
@@ -158,7 +157,6 @@ Blockly.Blocks['linenotify_type'] = {
     this.appendDummyInput()
         .appendField("Message (Line Notify)");    
     this.appendDummyInput()
-        .appendField("Type")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField(new Blockly.FieldDropdown([["text (text)","text"], ["sticker (text, packageId, stickerId)","sticker"], ["image (text, originalContentUrl, previewImageUrl)","image"]]), "value_type");
     this.appendValueInput("value_parameter1")
