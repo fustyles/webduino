9ca266426e764cdbafd61183a0d816491166d598
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-13 22:54:21 +0800

    Update blocks.js

--- LineBot_20181027/blockly/blocks.js
+++ LineBot_20181027/blockly/blocks.js
@@ -132,9 +132,9 @@ Blockly.Blocks['linebot_type'] = {
     //}
   },
   mutationToDom : function() {
-      var container = document.createElement('mutation');
-      container.setAttribute('bot_type', this.getField('value_type').getValue());
-      return container;
+    var container = document.createElement('mutation');
+    container.setAttribute('bot_type', this.getField('value_type').getValue());
+    return container;
   },
   domToMutation : function(xmlElement) {
     this.getField('value_type').setValue(xmlElement.getAttribute('bot_type'));
@@ -214,9 +214,9 @@ Blockly.Blocks['linenotify_type'] = {
     //}
   },
   mutationToDom : function() {
-      var container = document.createElement('mutation');
-      container.setAttribute('notify_type', this.getField('value_type').getValue());
-      return container;
+    var container = document.createElement('mutation');
+    container.setAttribute('notify_type', this.getField('value_type').getValue());
+    return container;
   },
   domToMutation : function(xmlElement) {
     this.getField('value_type').setValue(xmlElement.getAttribute('notify_type'));
