ae9cb1de6080ca4b719a21f183f9bef9db1f4f25
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-16 23:03:41 +0800

    Update zh-hans.js

--- MatrixLed_20180810/blockly/msg/blocks/zh-hans.js
+++ MatrixLed_20180810/blockly/msg/blocks/zh-hans.js
@@ -41,6 +41,6 @@ Blockly.Msg.MATRIXLED_GETCOLOR_SHOW = "指示灯 色码值";
 Blockly.Msg.MATRIXLED_BACKCOLOR_SHOW = "指示灯(单色) 背景颜色";
 Blockly.Msg.MATRIXLED_MATRIXCODE_COLOR_SHOW = "指示灯(全彩) 编码";
 Blockly.Msg.MATRIXLED_MARQUEE_COLOR_SHOW = "跑马灯(全彩) 循环";
-Blockly.Msg.MATRIXLED_MARQUEE_COLOR_ONCE_SHOW = "跑马灯(全彩) 单次";
+Blockly.Msg.MATRIXLED_MARQUEE_COLOR_ONCE_SHOW = "跑马灯(全彩) 次数";
 Blockly.Msg.MATRIXLED_MATRIXCODE_LINE_SHOW = "指示灯(单色) 新增纵行";
 Blockly.Msg.MATRIXLED_MATRIXCODE_LINE_COLOR_SHOW = "指示灯(全彩) 新增纵行";
