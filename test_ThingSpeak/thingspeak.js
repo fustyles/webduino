44eb438a298d6c70a3ef6b89b84edb6b4d0b5597
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-26 17:34:04 +0800

    Update thingspeak.js

--- test_ThingSpeak/thingspeak.js
+++ test_ThingSpeak/thingspeak.js
@@ -16,27 +16,30 @@
     field7 = escape(field7+"");
     field8 = escape(field8+"");
     
-    var input_url="https://api.thingspeak.com/update?api_key="+key+"&field1="+field1+"&field2="+field2+"&field3="+field3+"&field4="+field4+"&field5="+field5+"&field6="+field6+"&field7="+field7+"&field8="+field8;
-    var src = '<iframe src="' + input_url + '" style="width:0px;height:0px"></iframe>';
-    
-    if (document.getElementById("thingspeak_iframe"))
-    {
-          document.getElementById("thingspeak_iframe").style.left = '0px';
-          document.getElementById("thingspeak_iframe").style.top = '0px';
-          document.getElementById("thingspeak_iframe").style.display = 'block';
-          document.getElementById("thingspeak_iframe").innerHTML = src;
-    }
-    else
-    {
-        var div = document.createElement('div');
-        div.id = "thingspeak_iframe";
-        div.style.position = 'absolute';      
-        div.style.left = '0px';
-        div.style.top = '0px';
-        div.style.zindex='9999';      
-        div.innerHTML = src;
-        document.body.appendChild(div);
-    }
+    var input_url =" api.thingspeak.com/update.json";
+    var data = $.ajax({
+        type: "POST",
+        dataType: "application/json",
+        url: input_url,
+        api_key: key,
+        created_at: "DATETIME_STAMP",
+        field1: field1, 
+        field2: field2, 
+        field3: field3, 
+        field4: field4, 
+        field5: field5, 
+        field6: field6, 
+        field7: field7, 
+        field8: field8,
+        success: function(json)
+        {
+          console.log(json);
+        },
+        error: function(exception)
+        {
+          console.log("Sorry");
+        }
+     });
   }
 
   window.createIframe = createIframe;
