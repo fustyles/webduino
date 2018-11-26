1ed16385cf2bc546f848096e50afc625f5077ebf
fustyles <34397289+fustyles@users.noreply.github.com>
2018-11-26 20:18:47 +0800

    Update ifttt.js

--- test_IFTTT_20180312/ifttt.js
+++ test_IFTTT_20180312/ifttt.js
@@ -22,7 +22,7 @@
         },
         error: function(exception)
         {
-          console.log(jQuery.parseJSON( jqXHR.responseText ).Message);
+          console.log(jQuery.parseJSON(exception.responseText ).Message);
         }
      });
   }
