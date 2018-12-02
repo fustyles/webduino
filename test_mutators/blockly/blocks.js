f1a29828e5d702bb913e9b2d9f6fecedffaec895
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 17:37:46 +0800

    Update blocks.js

--- test_mutators/blockly/blocks.js
+++ test_mutators/blockly/blocks.js
@@ -1,8 +1,6 @@
-Blockly.Blocks['demo_text_add'] = {
+Blockly.Blocks['mutators_test'] = {
   init: function() {
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.DEMO_TEXT_ADD_SHOW);
+    this.appendDummyInput();
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour(65);
