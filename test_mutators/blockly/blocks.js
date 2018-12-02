99eb3059b685220f8ba8c087e04277bce5f5b4a8
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 19:33:47 +0800

    Update blocks.js

--- test_mutators/blockly/blocks.js
+++ test_mutators/blockly/blocks.js
@@ -5,50 +5,68 @@ Blockly.Blocks["mutators_test"] = {
     this.setColour(100);
     this.inputcount = 1;
     this.updateShape_();    
-    this.setMutator(new Blockly.Mutator(['input_value_with_item']));
+    this.setMutator(new Blockly.Mutator(['input_with_item','list_with_item']));
   },
   mutationToDom: function (workspace) {
     var container = document.createElement('mutation');
     container.setAttribute('inputcount', this.inputcount);
+    container.setAttribute('listcount', this.listcount);
     return container;
   },
   domToMutation: function (xmlElement) {
     this.inputcount = parseInt(xmlElement.getAttribute('inputcount'), 10);
+    this.listcount = parseInt(xmlElement.getAttribute('listcount'), 10);
     this.updateShape_();
   },
   decompose: function (workspace) {
-    var containerBlock = workspace.newBlock('input_value_with_container');
+    var containerBlock = workspace.newBlock('mutation_container');
     containerBlock.initSvg();
     var connection = containerBlock.getInput('STACK').connection;
     for (var i = 0; i < this.inputcount ; i++) {
-      var itemBlock = workspace.newBlock('input_value_with_item');
-      itemBlock.initSvg();
-      connection.connect(itemBlock.previousConnection);
-      connection = itemBlock.nextConnection;
+      var itemBlock_input = workspace.newBlock('input_with_item');
+      itemBlock_input.initSvg();
+      connection.connect(itemBlock_input.previousConnection);
+      connection = itemBlock_input.nextConnection;
     }
+    for (var j = 0; j < this.listcount ; j++) {
+      var itemBlock_list = workspace.newBlock('list_with_item');
+      itemBlock_list.initSvg();
+      connection.connect(itemBlock_list.previousConnection);
+      if (j < this.listcount-1) connection = itemBlock_list.nextConnection;
+    }    
     return containerBlock;
   },
   compose: function(containerBlock) {
-    var itemBlock = containerBlock.getInputTargetBlock('STACK');
+    
+    var clauseBlock = containerBlock.nextConnection.targetBlock();
     // Count number of inputs.
-    var connections = [];
-    while (itemBlock) {
-      connections.push(itemBlock.valueConnection_);
-      itemBlock = itemBlock.nextConnection &&
-          itemBlock.nextConnection.targetBlock();
-    }
-    // Disconnect any children that don't belong.
-    for (var i = 0; i < this.inputcount ; i++) {
-      var connection = this.getInput('input_value' + i).connection.targetConnection;
-      if (connection && connections.indexOf(connection) == -1) {
-        connection.disconnect();
+    this.inputcount_ = 0;
+    this.listcount_ = 0;
+    var inputConnections = [null];
+    var listConnections = [null];
+    while (clauseBlock) {
+      switch (clauseBlock.type) {
+        case 'input_with_item':
+          this.inputcount++;
+          inputConnections.push(clauseBlock.inputConnection_);
+          break;
+        case 'list_with_item':
+          this.listcount++;
+          listConnections.push(clauseBlock.listConnection_);
+          break;
+        default:
+          throw TypeError('Unknown block type: ' + clauseBlock.type);
       }
+      clauseBlock = clauseBlock.nextConnection &&
+          clauseBlock.nextConnection.targetBlock();
     }
-    this.inputcount = connections.length;
     this.updateShape_();
-    // Reconnect any child blocks.
-    for (var i = 0; i < this.inputcount ; i++) {
-      Blockly.Mutator.reconnect(connections[i], this, 'input_value' + i);
+    
+    for (var i = 1; i <= this.inputcount_; i++) {
+      Blockly.Mutator.reconnect(inputConnections[i], this, 'AAA' + i);
+    }
+    for (var j = 1; j <= this.listcount_; j++) {
+      Blockly.Mutator.reconnect(listConnections[i], this, 'BBB' + j);
     }
   },
   saveConnections: function(containerBlock) {
@@ -107,6 +125,21 @@ Blockly.Blocks["input_value_with_item"] = {
     this.setColour(Blockly.Blocks.lists.HUE);
     this.appendDummyInput()
         .appendField("Input_value");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.contextMenu = false;
+  }
+};
+
+Blockly.Blocks["list_with_item"] = {
+  /**
+   * Mutator bolck for adding items.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.setColour(Blockly.Blocks.lists.HUE);
+    this.appendDummyInput()
+        .appendField("List");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.contextMenu = false;
