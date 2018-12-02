3d1c913075500e84c2191a0c8d5c5b7f8121af3b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 09:57:41 +0800

    Update javascript.js

--- test_xmlHTTP_20180206/blockly/javascript.js
+++ test_xmlHTTP_20180206/blockly/javascript.js
@@ -1,6 +1,4 @@
-Blockly.JavaScript['xmlHTTP_ResponseData'] = function(block) {
-  var value_format_ = block.getFieldValue('value_format_'); 
-  var value_url_ = Blockly.JavaScript.valueToCode(block, 'value_url_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'getResponse('+value_url_+',"'+value_format_+'")';
+Blockly.JavaScript['en'] = function(block) {
+  var code = '';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
