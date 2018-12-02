75aa8a7fae031c26d7c0812bc5b7926bbc7076b7
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 13:02:39 +0800

    Update blocks.js

--- test_LineBot_20181027/blockly/blocks.js
+++ test_LineBot_20181027/blockly/blocks.js
@@ -191,7 +191,7 @@ Blockly.Blocks['linenotify_type'] = {
       this.getField('p2').setVisible(false);
       this.getField('p3').setVisible(false); 
       console.log(this.getInput('value_parameter2').isVisible);
-      if (this.getInput('value_parameter2').isVisible==true||this.getInput('value_parameter3').isVisible==true) this.unplugBlock();
+      if (this.getInput('value_parameter2').Visible==true||this.getInput('value_parameter3').Visible==true) this.unplugBlock();
       this.getInput('value_parameter2').setVisible(false);
       this.getInput('value_parameter3').setVisible(false);
     } else if (this.getField('value_type').getValue() == "sticker") {
