c62e3e38669a2e5b51fecf0b67024079a05edaac
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 09:57:16 +0800

    Update blocks.js

--- test_xmlHTTP_20180206/blockly/blocks.js
+++ test_xmlHTTP_20180206/blockly/blocks.js
@@ -1,21 +1,63 @@
-Blockly.Blocks['xmlHTTP_ResponseData'] = {
+//**************************************************************************
+//Definition of the enable block (optional connection depending on checkbox)
+//**************************************************************************
+Blockly.Blocks['en'] = {
   init: function() {
+    var checkbox = new Blockly.FieldCheckbox("TRUE", function(pxchecked) {
+      this.sourceBlock_.updateShape_(pxchecked);
+    });
+    this.setInputsInline(false);
     this.appendDummyInput()
-        .appendField(Blockly.Msg.XMLHTTP_RESPONSEDATA)
-        .appendField(new Blockly.FieldDropdown([
-          ["JSON","JSON"],
-          ["HTML","HTML"],    
-          ["XML","XML"]
-        ]), "value_format_");          
-    this.appendValueInput("value_url_")
-        .setCheck("String");
-    this.setInputsInline(true);
-    this.setOutput(true, null);
-    this.setColour(300);
-    this.setTooltip("");
-    this.setHelpUrl("");
-  }  
+       .appendField("Unit")
+       .appendField(new Blockly.FieldTextInput("C01"), "ENName");
+    this.appendDummyInput()
+       .appendField("PX")
+       .appendField(checkbox, 'HasPX');
+    this.setPreviousStatement(true, ["C", "EN"]);
+    this.setNextStatement(true, ["C", "EN"]);
+    this.setColour(40);
+  },
+  /**
+   * Create XML to represent whether the 'pxchecked' should be present.
+   * @return {Element} XML storage element.
+   * @this Blockly.Block
+   */
+  mutationToDom: function() {
+    var container = document.createElement('mutation');
+    var pxchecked = (this.getFieldValue('HasPX') == 'TRUE');
+    container.setAttribute('pxchecked', pxchecked);
+    return container;
+  },
+  /**
+   * Parse XML to restore the 'pxchecked'.
+   * @param {!Element} xmlElement XML storage element.
+   * @this Blockly.Block
+   */
+  domToMutation: function(xmlElement) {
+    var pxchecked = (xmlElement.getAttribute('pxchecked') == 'true');
+    this.updateShape_(pxchecked);
+  },
+  /**
+   * Modify this block to have (or not have) an input for 'HasPX'.
+   * @param {boolean} pxchecked True if this block HasPX is pxchecked.
+   * @private
+   * @this Blockly.Block
+   */
+  updateShape_: function(pxchecked) {
+    // Add or remove a Value Input.
+    if (pxchecked) {
+      this.appendValueInput("PX")
+          .setCheck('PX');
+    } else {
+      if (this.childBlocks_.length > 0) {
+        for (var i = 0; i < this.childBlocks_.length; i++) {
+          if (this.childBlocks_[i].type == 'px') {
+            this.childBlocks_[i].unplug();
+            break;
+          }
+        }
+      }
+      this.removeInput('PX');
+    }
+  }
 };
-
-
-
