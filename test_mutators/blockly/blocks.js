25838a239424c98cdeecf5deb08ded5fc9a63fd6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 18:05:18 +0800

    Update blocks.js

--- test_mutators/blockly/blocks.js
+++ test_mutators/blockly/blocks.js
@@ -99,7 +99,7 @@ Blockly.Blocks['input_value_with_container'] = {
   }
 };
 
-Blockly.Blocks["input_value_with_item'] = {
+Blockly.Blocks["input_value_with_item"] = {
   /**
    * Mutator bolck for adding items.
    * @this Blockly.Block
