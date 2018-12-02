2da750b9f61874323e4cd4eb431fe51010e147c7
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-02 10:52:43 +0800

    Update blocks.js

--- test_xmlHTTP_20180206/blockly/blocks.js
+++ test_xmlHTTP_20180206/blockly/blocks.js
@@ -1,61 +1,311 @@
-//**************************************************************************
-//Definition of the enable block (optional connection depending on checkbox)
-//**************************************************************************
-Blockly.Blocks['en'] = {
-  init: function() {
-    this.setInputsInline(false);
-    this.appendDummyInput()
-       .appendField("Unit")
-       .appendField(new Blockly.FieldTextInput("C01"), "ENName");
-    this.appendDummyInput()
-       .appendField("PX")
-       .appendField(new Blockly.FieldCheckbox("TRUE",function(pxchecked) {
-      this.sourceBlock_.updateShape_(pxchecked);
-    }), 'HasPX');
-    this.setPreviousStatement(true, ["C", "EN"]);
-    this.setNextStatement(true, ["C", "EN"]);
-    this.setColour(40);
+'use strict';
+
+goog.provide('Blockly.Blocks.logic');  // Deprecated
+goog.provide('Blockly.Constants.Logic');
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly');
+
+Blockly.Constants.Logic.HUE = 210;
+
+Blockly.defineBlocksWithJsonArray([  // BEGIN JSON EXTRACT
+  // Block for if/elseif/else condition.
+  {
+    "type": "controls_if",
+    "message0": "%{BKY_CONTROLS_IF_MSG_IF} %1",
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "IF0",
+        "check": "Boolean"
+      }
+    ],
+    "message1": "%{BKY_CONTROLS_IF_MSG_THEN} %1",
+    "args1": [
+      {
+        "type": "input_statement",
+        "name": "DO0"
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": "%{BKY_LOGIC_HUE}",
+    "helpUrl": "%{BKY_CONTROLS_IF_HELPURL}",
+    "mutator": "controls_if_mutator",
+    "extensions": ["controls_if_tooltip"]
+  }
+]);  // END JSON EXTRACT (Do not delete this comment.)
+
+Blockly.defineBlocksWithJsonArray([ // Mutator blocks. Do not extract.
+  // Block representing the if statement in the controls_if mutator.
+  {
+    "type": "controls_if_if",
+    "message0": "%{BKY_CONTROLS_IF_IF_TITLE_IF}",
+    "nextStatement": null,
+    "enableContextMenu": false,
+    "colour": "%{BKY_LOGIC_HUE}",
+    "tooltip": "%{BKY_CONTROLS_IF_IF_TOOLTIP}"
   },
-  /**
-   * Create XML to represent whether the 'pxchecked' should be present.
-   * @return {Element} XML storage element.
-   * @this Blockly.Block
-   */
+  // Block representing the else-if statement in the controls_if mutator.
+  {
+    "type": "controls_if_elseif",
+    "message0": "%{BKY_CONTROLS_IF_ELSEIF_TITLE_ELSEIF}",
+    "previousStatement": null,
+    "nextStatement": null,
+    "enableContextMenu": false,
+    "colour": "%{BKY_LOGIC_HUE}",
+    "tooltip": "%{BKY_CONTROLS_IF_ELSEIF_TOOLTIP}"
+  },
+  // Block representing the else statement in the controls_if mutator.
+  {
+    "type": "controls_if_else",
+    "message0": "%{BKY_CONTROLS_IF_ELSE_TITLE_ELSE}",
+    "previousStatement": null,
+    "enableContextMenu": false,
+    "colour": "%{BKY_LOGIC_HUE}",
+    "tooltip": "%{BKY_CONTROLS_IF_ELSE_TOOLTIP}"
+  }
+]);
+
+Blockly.Constants.Logic.TOOLTIPS_BY_OP = {
+  // logic_compare
+  'EQ': '%{BKY_LOGIC_COMPARE_TOOLTIP_EQ}',
+  'NEQ': '%{BKY_LOGIC_COMPARE_TOOLTIP_NEQ}',
+  'LT': '%{BKY_LOGIC_COMPARE_TOOLTIP_LT}',
+  'LTE': '%{BKY_LOGIC_COMPARE_TOOLTIP_LTE}',
+  'GT': '%{BKY_LOGIC_COMPARE_TOOLTIP_GT}',
+  'GTE': '%{BKY_LOGIC_COMPARE_TOOLTIP_GTE}',
+
+  // logic_operation
+  'AND': '%{BKY_LOGIC_OPERATION_TOOLTIP_AND}',
+  'OR': '%{BKY_LOGIC_OPERATION_TOOLTIP_OR}'
+};
+
+Blockly.Extensions.register('logic_op_tooltip',
+    Blockly.Extensions.buildTooltipForDropdown(
+        'OP', Blockly.Constants.Logic.TOOLTIPS_BY_OP));
+
+Blockly.Constants.Logic.CONTROLS_IF_MUTATOR_MIXIN = {
+  elseifCount_: 0,
+  elseCount_: 0,
+  
   mutationToDom: function() {
+    if (!this.elseifCount_ && !this.elseCount_) {
+      return null;
+    }
     var container = document.createElement('mutation');
-    var pxchecked = (this.getFieldValue('HasPX') == 'TRUE');
-    container.setAttribute('pxchecked', pxchecked);
+    if (this.elseifCount_) {
+      container.setAttribute('elseif', this.elseifCount_);
+    }
+    if (this.elseCount_) {
+      container.setAttribute('else', 1);
+    }
     return container;
   },
-  /**
-   * Parse XML to restore the 'pxchecked'.
-   * @param {!Element} xmlElement XML storage element.
-   * @this Blockly.Block
-   */
   domToMutation: function(xmlElement) {
-    var pxchecked = (xmlElement.getAttribute('pxchecked') == 'true');
-    this.updateShape_(pxchecked);
+    this.elseifCount_ = parseInt(xmlElement.getAttribute('elseif'), 10) || 0;
+    this.elseCount_ = parseInt(xmlElement.getAttribute('else'), 10) || 0;
+    this.updateShape_();
   },
-  /**
-   * Modify this block to have (or not have) an input for 'HasPX'.
-   * @param {boolean} pxchecked True if this block HasPX is pxchecked.
-   * @private
-   * @this Blockly.Block
-   */
-  updateShape_: function(pxchecked) {
-    // Add or remove a Value Input.
-    if (pxchecked) {
-      this.appendValueInput("PX");
-    } else {
-      if (this.childBlocks_.length > 0) {
-        for (var i = 0; i < this.childBlocks_.length; i++) {
-          if (this.childBlocks_[i].type == 'px') {
-            this.childBlocks_[i].unplug();
-            break;
-          }
-        }
+  decompose: function(workspace) {
+    var containerBlock = workspace.newBlock('controls_if_if');
+    containerBlock.initSvg();
+    var connection = containerBlock.nextConnection;
+    for (var i = 1; i <= this.elseifCount_; i++) {
+      var elseifBlock = workspace.newBlock('controls_if_elseif');
+      elseifBlock.initSvg();
+      connection.connect(elseifBlock.previousConnection);
+      connection = elseifBlock.nextConnection;
+    }
+    if (this.elseCount_) {
+      var elseBlock = workspace.newBlock('controls_if_else');
+      elseBlock.initSvg();
+      connection.connect(elseBlock.previousConnection);
+    }
+    return containerBlock;
+  },
+  compose: function(containerBlock) {
+    var clauseBlock = containerBlock.nextConnection.targetBlock();
+    // Count number of inputs.
+    this.elseifCount_ = 0;
+    this.elseCount_ = 0;
+    var valueConnections = [null];
+    var statementConnections = [null];
+    var elseStatementConnection = null;
+    while (clauseBlock) {
+      switch (clauseBlock.type) {
+        case 'controls_if_elseif':
+          this.elseifCount_++;
+          valueConnections.push(clauseBlock.valueConnection_);
+          statementConnections.push(clauseBlock.statementConnection_);
+          break;
+        case 'controls_if_else':
+          this.elseCount_++;
+          elseStatementConnection = clauseBlock.statementConnection_;
+          break;
+        default:
+          throw TypeError('Unknown block type: ' + clauseBlock.type);
       }
-      this.removeInput('PX');
+      clauseBlock = clauseBlock.nextConnection &&
+          clauseBlock.nextConnection.targetBlock();
+    }
+    this.updateShape_();
+    // Reconnect any child blocks.
+    for (var i = 1; i <= this.elseifCount_; i++) {
+      Blockly.Mutator.reconnect(valueConnections[i], this, 'IF' + i);
+      Blockly.Mutator.reconnect(statementConnections[i], this, 'DO' + i);
+    }
+    Blockly.Mutator.reconnect(elseStatementConnection, this, 'ELSE');
+  },
+  saveConnections: function(containerBlock) {
+    var clauseBlock = containerBlock.nextConnection.targetBlock();
+    var i = 1;
+    while (clauseBlock) {
+      switch (clauseBlock.type) {
+        case 'controls_if_elseif':
+          var inputIf = this.getInput('IF' + i);
+          var inputDo = this.getInput('DO' + i);
+          clauseBlock.valueConnection_ =
+              inputIf && inputIf.connection.targetConnection;
+          clauseBlock.statementConnection_ =
+              inputDo && inputDo.connection.targetConnection;
+          i++;
+          break;
+        case 'controls_if_else':
+          var inputDo = this.getInput('ELSE');
+          clauseBlock.statementConnection_ =
+              inputDo && inputDo.connection.targetConnection;
+          break;
+        default:
+          throw TypeError('Unknown block type: ' + clauseBlock.type);
+      }
+      clauseBlock = clauseBlock.nextConnection &&
+          clauseBlock.nextConnection.targetBlock();
+    }
+  },
+  updateShape_: function() {
+    // Delete everything.
+    if (this.getInput('ELSE')) {
+      this.removeInput('ELSE');
+    }
+    var i = 1;
+    while (this.getInput('IF' + i)) {
+      this.removeInput('IF' + i);
+      this.removeInput('DO' + i);
+      i++;
+    }
+    // Rebuild block.
+    for (var i = 1; i <= this.elseifCount_; i++) {
+      this.appendValueInput('IF' + i)
+          .setCheck('Boolean')
+          .appendField(Blockly.Msg['CONTROLS_IF_MSG_ELSEIF']);
+      this.appendStatementInput('DO' + i)
+          .appendField(Blockly.Msg['CONTROLS_IF_MSG_THEN']);
+    }
+    if (this.elseCount_) {
+      this.appendStatementInput('ELSE')
+          .appendField(Blockly.Msg['CONTROLS_IF_MSG_ELSE']);
     }
   }
 };
+
+Blockly.Extensions.registerMutator('controls_if_mutator',
+    Blockly.Constants.Logic.CONTROLS_IF_MUTATOR_MIXIN, null,
+    ['controls_if_elseif', 'controls_if_else']);
+
+Blockly.Constants.Logic.CONTROLS_IF_TOOLTIP_EXTENSION = function() {
+
+  this.setTooltip(function() {
+    if (!this.elseifCount_ && !this.elseCount_) {
+      return Blockly.Msg['CONTROLS_IF_TOOLTIP_1'];
+    } else if (!this.elseifCount_ && this.elseCount_) {
+      return Blockly.Msg['CONTROLS_IF_TOOLTIP_2'];
+    } else if (this.elseifCount_ && !this.elseCount_) {
+      return Blockly.Msg['CONTROLS_IF_TOOLTIP_3'];
+    } else if (this.elseifCount_ && this.elseCount_) {
+      return Blockly.Msg['CONTROLS_IF_TOOLTIP_4'];
+    }
+    return '';
+  }.bind(this));
+};
+
+Blockly.Extensions.register('controls_if_tooltip',
+    Blockly.Constants.Logic.CONTROLS_IF_TOOLTIP_EXTENSION);
+
+Blockly.Constants.Logic.LOGIC_COMPARE_ONCHANGE_MIXIN = {
+  onchange: function(e) {
+    if (!this.prevBlocks_) {
+      this.prevBlocks_ = [null, null];
+    }
+
+    var blockA = this.getInputTargetBlock('A');
+    var blockB = this.getInputTargetBlock('B');
+    // Disconnect blocks that existed prior to this change if they don't match.
+    if (blockA && blockB &&
+        !blockA.outputConnection.checkType_(blockB.outputConnection)) {
+      // Mismatch between two inputs.  Revert the block connections,
+      // bumping away the newly connected block(s).
+      Blockly.Events.setGroup(e.group);
+      var prevA = this.prevBlocks_[0];
+      if (prevA !== blockA) {
+        blockA.unplug();
+        if (prevA && !prevA.isShadow()) {
+          // The shadow block is automatically replaced during unplug().
+          this.getInput('A').connection.connect(prevA.outputConnection);
+        }
+      }
+      var prevB = this.prevBlocks_[1];
+      if (prevB !== blockB) {
+        blockB.unplug();
+        if (prevB && !prevB.isShadow()) {
+          // The shadow block is automatically replaced during unplug().
+          this.getInput('B').connection.connect(prevB.outputConnection);
+        }
+      }
+      this.bumpNeighbours_();
+      Blockly.Events.setGroup(false);
+    }
+    this.prevBlocks_[0] = this.getInputTargetBlock('A');
+    this.prevBlocks_[1] = this.getInputTargetBlock('B');
+  }
+};
+
+Blockly.Constants.Logic.LOGIC_COMPARE_EXTENSION = function() {
+  // Add onchange handler to ensure types are compatible.
+  this.mixin(Blockly.Constants.Logic.LOGIC_COMPARE_ONCHANGE_MIXIN);
+};
+
+Blockly.Extensions.register('logic_compare',
+    Blockly.Constants.Logic.LOGIC_COMPARE_EXTENSION);
+
+Blockly.Constants.Logic.LOGIC_TERNARY_ONCHANGE_MIXIN = {
+  prevParentConnection_: null,
+
+  onchange: function(e) {
+    var blockA = this.getInputTargetBlock('THEN');
+    var blockB = this.getInputTargetBlock('ELSE');
+    var parentConnection = this.outputConnection.targetConnection;
+    // Disconnect blocks that existed prior to this change if they don't match.
+    if ((blockA || blockB) && parentConnection) {
+      for (var i = 0; i < 2; i++) {
+        var block = (i == 1) ? blockA : blockB;
+        if (block && !block.outputConnection.checkType_(parentConnection)) {
+          // Ensure that any disconnections are grouped with the causing event.
+          Blockly.Events.setGroup(e.group);
+          if (parentConnection === this.prevParentConnection_) {
+            this.unplug();
+            parentConnection.getSourceBlock().bumpNeighbours_();
+          } else {
+            block.unplug();
+            block.bumpNeighbours_();
+          }
+          Blockly.Events.setGroup(false);
+        }
+      }
+    }
+    this.prevParentConnection_ = parentConnection;
+  }
+};
+
+Blockly.Extensions.registerMixin('logic_ternary',
+    Blockly.Constants.Logic.LOGIC_TERNARY_ONCHANGE_MIXIN);
