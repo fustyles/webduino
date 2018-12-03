03867fbb4314a5b95337f973c2bdd37c1dd12bd2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-03 19:00:40 +0800

    Add files via upload

---
+++ example_mutation/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,1 @@
+Blockly.Msg.DEMO_TEXT_ADD_SHOW = "显示文字附加";
---
+++ test_DeviceStatus_20181109/blockly/blocks.js
@@ -0,0 +1,45 @@
+Blockly.Blocks['boardevent'] = {
+  init: function () {
+    this.appendValueInput("device")
+      .setCheck("String")
+      .appendField("Sampling Interval (ms)")
+      .appendField(new Blockly.FieldDropdown([["20","20"], ["50","50"], ["75","75"], ["100","100"], ["250","250"], ["500","500"], ["1000","1000"]]), "samplingInterval")
+      .appendField("    Device ID");
+    this.appendDummyInput()
+      .appendField("");
+    this.appendDummyInput()
+      .appendField("BoardEvent.ERROR");
+    this.appendStatementInput("do_error");
+    this.appendDummyInput()
+      .appendField("BoardEvent.STRING_MESSAGE");
+    this.appendStatementInput("do_message");
+    this.appendDummyInput()
+      .appendField("BoardEvent.READY");
+    this.appendStatementInput("do_ready");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(340);
+    this.getField('samplingInterval').setValue('250');
+  }
+};
+
+Blockly.Blocks['boardevent_sendmessage'] = {
+  init: function () {
+    this.appendValueInput("cmd")
+      .setCheck("String")
+      .appendField("Send Command");    
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(210);
+  }
+};
+
+Blockly.Blocks['boardevent_getmessage'] = {
+  init: function () {
+    this.appendDummyInput()
+      .appendField("Get Message");
+    this.setInputsInline(true);
+    this.setOutput(true, null);
+    this.setColour(45);
+  }
+};
