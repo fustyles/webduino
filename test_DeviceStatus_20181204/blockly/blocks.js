821296bb1feac8c5ac8361d163ab510e18f55f3b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-04 21:53:01 +0800

    Update blocks.js

--- test_DeviceStatus_20181204/blockly/blocks.js
+++ test_DeviceStatus_20181204/blockly/blocks.js
@@ -1,31 +1,16 @@
-Blockly.Blocks["mutation_container"] = {
+Blockly.Blocks["boardevent_container"] = {
   init: function() {
-    this.setColour(Blockly.Blocks.lists.HUE);
     this.appendDummyInput()
-        .appendField("BoardEvent");
-    this.appendStatementInput('STACK');
-    this.contextMenu = false;
-  }
-};
-
-Blockly.Blocks["error_with_item"] = {
-  init: function() {
-    this.setColour(Blockly.Blocks.lists.HUE);
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("BoardEvent.STRING_MESSAGE")
+      .appendField(new Blockly.FieldCheckbox("FALSE"), "chkmessage");
     this.appendDummyInput()
-        .appendField("event.Error");
-    this.setPreviousStatement(true);
-    this.contextMenu = false;
-  }
-};
-
-Blockly.Blocks["message_with_item"] = {
-  init: function() {
-    this.setColour(Blockly.Blocks.lists.HUE);
-    this.appendDummyInput()
-        .appendField("Message");
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.contextMenu = false;
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("BoardEvent.ERROR")
+      .appendField(new Blockly.FieldCheckbox("FALSE"), "chkerror");
+    this.setColour(20);
+    this.setTooltip("");
+    this.setHelpUrl("");
   }
 };
 
@@ -36,133 +21,69 @@ Blockly.Blocks['boardevent'] = {
       .appendField("Sampling Interval (ms)")
       .appendField(new Blockly.FieldDropdown([["20","20"], ["50","50"], ["75","75"], ["100","100"], ["250","250"], ["500","500"], ["1000","1000"]]), "samplingInterval")
       .appendField("    Device ID");
-    this.appendDummyInput()
-      .appendField("BoardEvent.READY");
-    this.appendStatementInput("do_ready"); 
-    this.appendDummyInput()
+    this.appendStatementInput("do_ready")
+        .appendField("BoardEvent.READY");   
+    this.appendStatementInput("do_message")
         .appendField("BoardEvent.STRING_MESSAGE","title_message");
-    this.appendStatementInput("do_message");      
-    this.appendDummyInput()
+    this.appendStatementInput("do_error")
         .appendField("BoardEvent.ERROR","title_error");
-    this.appendStatementInput("do_error");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
     this.getField('samplingInterval').setValue('250');
-    this.list = [];
+    this.messageVisible_=false;
+    this.errorVisible_=false;
     this.updateShape_();    
-    this.setMutator(new Blockly.Mutator(['message_with_item','error_with_item']));
+    this.jsonInit({"mutator": "boardevent_container"});
   },
   mutationToDom: function (workspace) {
+    if (!this.messageVisible_ && !this.errorVisible_) {
+      return null;
+    }
     var container = document.createElement('mutation');
-    container.setAttribute('list', this.list);
+    if (this.messageVisible_) {
+      container.setAttribute('message', this.messageVisible_);
+    }
+    if (this.errorVisible_) {
+      container.setAttribute('error', this.errorVisible_);
+    }
     return container;
   },
   domToMutation: function (xmlElement) {
-    this.list = xmlElement.getAttribute('list').split(",");
+    this.messageVisible_ = xmlElement.getAttribute('message') || false;
+    this.errorVisible_ = xmlElement.getAttribute('error') || false;
     this.updateShape_();
   },
   decompose: function (workspace) {
-    var containerBlock = workspace.newBlock('mutation_container');
+    var containerBlock = workspace.newBlock('border_mutator');
+    containerBlock.setFieldValue(new Blockly.FieldCheckbox(this.messageVisible_), "chkmessage");
+    containerBlock.setFieldValue(new Blockly.FieldCheckbox(this.errorVisible_), "chkerror");
     containerBlock.initSvg();
-    var connection = containerBlock.getInput('STACK').connection;
-    
-    for (var i = 0; i < this.list.length; i++) {
-      if (this.list[i]=="message") {
-        var itemBlock_list = workspace.newBlock('message_with_item');
-        itemBlock_list.initSvg();
-        connection.connect(itemBlock_list.previousConnection);
-        connection = itemBlock_list.nextConnection;
-      } else if (this.list[i]=="error") {
-        var itemBlock_input = workspace.newBlock('error_with_item');
-        itemBlock_input.initSvg();
-        connection.connect(itemBlock_input.previousConnection);
-        connection = itemBlock_input.nextConnection;
-      } 
-    }
-    this.updateShape_();
     return containerBlock;
   },
   compose: function(containerBlock) {
-    var clauseBlock = containerBlock.getInputTargetBlock('STACK');
-    var errorCount = 0;
-    var messageCount = 0;
-    this.list = [];
-    var errorConnections = [null];
-    var messageConnections = [null];
-    while (clauseBlock) {
-      switch (clauseBlock.type) {
-        case 'message_with_item':
-          if (messageCount==0) {   // limit to 1
-            messageCount++;
-            this.list.unshift("message");
-            messageConnections.push(clauseBlock.listConnection_);
-          }
-          break;          
-        case 'error_with_item':
-          if (errorCount==0) {   // limit to 1
-            errorCount++;
-            this.list.push("error");
-            errorConnections.push(clauseBlock.inputConnection_);
-          }
-          break;
-        default:
-          throw TypeError('Unknown block type: ' + clauseBlock.type);
-      }
-      clauseBlock = clauseBlock.nextConnection &&
-          clauseBlock.nextConnection.targetBlock();
-    }
-    
+    this.messageVisible_ = (containerBlock.getFieldValue("chkmessage") == "TRUE");
+    this.widthVisible_ = (containerBlock.getFieldValue("chkerror") == "TRUE");
     this.updateShape_();
-    
-    if (messageCount>0) 
-      Blockly.Mutator.reconnect(messageConnections[1], this, 'do_message');    
-    if (errorCount>0)
-      Blockly.Mutator.reconnect(errorConnections[1], this, 'do_error');
+    Blockly.Mutator.reconnect(containerBlock.valueConnection_, this, 'chkmessage');
+    //Blockly.Mutator.reconnect(containerBlock.valueConnection_[1], this, 'chkerror');
   },
   saveConnections: function(containerBlock) {
-    var clauseBlock = containerBlock.getInputTargetBlock('STACK');
-    var errorCount = 0;
-    var messageCount = 0;
-    while (clauseBlock) {
-      switch (clauseBlock.type) {
-        case 'message_with_item':
-          if (messageCount==0) {
-            messageCount++;
-            var list = this.getInput('do_message');
-            clauseBlock.listConnection_ =
-                list && list.connection.targetConnection;
-          }
-          break;          
-        case 'error_with_item':
-          if (errorCount==0) {
-            errorCount++;
-            var input = this.getInput('do_error');
-            clauseBlock.inputConnection_ =
-                input && input.connection.targetConnection;
-          }
-          break;
-        default:
-          throw TypeError('Unknown block type: ' + clauseBlock.type);
-      }
-      clauseBlock = clauseBlock.nextConnection &&
-          clauseBlock.nextConnection.targetBlock();
-    }
+    var message = this.getInput('chkmessage');
+    containerBlock.valueConnection_ = message && message.connection.targetConnection;
+    var error = this.getInput('chkerror');
+    // containerBlock.valueConnection_[1] = error && error.connection.targetConnection;
   },
   updateShape_: function() {
-    this.getField('title_message').setVisible(false);
-    this.getInput('do_message').setVisible(false);    
-    this.getField('title_error').setVisible(false);
-    this.getInput('do_error').setVisible(false);
-      
-    for (var i = 0; i < this.list.length; i++) {
-      if (this.list[i]=="message") {
-        this.getField('title_message').setVisible(true);
-        this.getInput('do_message').setVisible(true);
-      } else if (this.list[i]=="error") {
-        this.getField('title_error').setVisible(true);
-        this.getInput('do_error').setVisible(true);
-      } 
+    if (this.getInput('do_message')) this.removeInput('do_message');
+    if (this.getInput('do_error')) this.removeInput('do_error');
+    if (this.messageVisible_) {
+      this.appendStatementInput("do_message")
+          .appendField("BoardEvent.STRING_MESSAGE","title_message");
+    }
+    if (this.errorVisible_) {
+      this.appendStatementInput("do_error")
+          .appendField("BoardEvent.ERROR","title_error");
     }
   }
 };
