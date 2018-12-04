4d8fc44b1e886f7034a7b94af2c85c790117e962
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-04 20:03:00 +0800

    Update blocks.js

--- test_DeviceStatus_20181109/blockly/blocks.js
+++ test_DeviceStatus_20181109/blockly/blocks.js
@@ -63,11 +63,7 @@ Blockly.Blocks['boardevent'] = {
     return container;
   },
   domToMutation: function (xmlElement) {
-    console.log(xmlElement.getAttribute('list'));
-    if (xmlElement.getAttribute('list'))
-      this.list = xmlElement.getAttribute('list').split(",");
-    else
-      this.list = "message,error".split(",");
+    this.list = xmlElement.getAttribute('list').split(",");
     this.updateShape_();
   },
   decompose: function (workspace) {
