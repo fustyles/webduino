c0cf17ad6fa318b99c7d5731cbec479ecb23ba5b
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-06 00:45:38 +0800

    Add files via upload

--- DeviceStatus_20181109/blockly/blocks.js
+++ DeviceStatus_20181109/blockly/blocks.js
@@ -1,3 +1,19 @@
+Blockly.Blocks["boardevent_mutator"] = {
+  init: function () {
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("BoardEvent.STRING_MESSAGE")
+      .appendField(new Blockly.FieldCheckbox("FALSE"), "chkmessage");
+    this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("BoardEvent.ERROR")
+      .appendField(new Blockly.FieldCheckbox("FALSE"), "chkerror");
+    this.setColour(20);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+
 Blockly.Blocks['boardevent'] = {
   init: function () {
     this.appendValueInput("device")
@@ -5,21 +21,65 @@ Blockly.Blocks['boardevent'] = {
       .appendField("Sampling Interval (ms)")
       .appendField(new Blockly.FieldDropdown([["20","20"], ["50","50"], ["75","75"], ["100","100"], ["250","250"], ["500","500"], ["1000","1000"]]), "samplingInterval")
       .appendField("    Device ID");
-    this.appendDummyInput()
-      .appendField("");
-    this.appendDummyInput()
-      .appendField("BoardEvent.ERROR");
-    this.appendStatementInput("do_error");
-    this.appendDummyInput()
-      .appendField("BoardEvent.STRING_MESSAGE");
-    this.appendStatementInput("do_message");
-    this.appendDummyInput()
-      .appendField("BoardEvent.READY");
-    this.appendStatementInput("do_ready");
+    this.appendStatementInput("do_ready")
+        .appendField("BoardEvent.READY");   
+    this.appendStatementInput("do_message")
+        .appendField("BoardEvent.STRING_MESSAGE","title_message");
+    this.appendStatementInput("do_error")
+        .appendField("BoardEvent.ERROR","title_error");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
     this.getField('samplingInterval').setValue('250');
+    this.messageVisible_= "TRUE";
+    this.errorVisible_= "TRUE";
+    this.updateShape_();
+    this.setMutator(new Blockly.Mutator([]));
+  },
+  mutationToDom: function (workspace) {
+    var container = document.createElement('mutation');
+    if (this.messageVisible_)
+      container.setAttribute('message', this.messageVisible_);
+    if (this.errorVisible_)
+      container.setAttribute('error', this.errorVisible_);
+    return container;
+  },
+  domToMutation: function (xmlElement) {
+    this.messageVisible_ = xmlElement.getAttribute('message');
+    this.errorVisible_ = xmlElement.getAttribute('error');
+    this.updateShape_();
+  },
+  decompose: function (workspace) {      
+    var containerBlock = workspace.newBlock('boardevent_mutator');
+    containerBlock.setFieldValue(this.messageVisible_, "chkmessage");
+    containerBlock.setFieldValue(this.errorVisible_, "chkerror");
+    containerBlock.initSvg();
+    return containerBlock;
+  },
+  compose: function(containerBlock) {
+    this.messageVisible_ = containerBlock.getFieldValue("chkmessage");
+    this.errorVisible_ = containerBlock.getFieldValue("chkerror"); 
+    this.updateShape_();
+    Blockly.Mutator.reconnect(containerBlock.messageConnection_, this, 'chkmessage');
+    Blockly.Mutator.reconnect(containerBlock.errorConnection_, this, 'chkerror');
+  },
+  saveConnections: function(containerBlock) {
+    var message = this.getInput('chkmessage');
+    containerBlock.messageConnection_ = message && message.connection.targetConnection;
+    var error = this.getInput('chkerror');
+    containerBlock.errorConnection_ = error && error.connection.targetConnection; 
+  },
+  updateShape_: function() {
+    if (this.getInput('do_message')) this.removeInput('do_message');
+    if (this.getInput('do_error')) this.removeInput('do_error');
+    if (this.messageVisible_=="TRUE") {
+      this.appendStatementInput("do_message")
+          .appendField("BoardEvent.STRING_MESSAGE","title_message");
+    }
+    if (this.errorVisible_=="TRUE") {
+      this.appendStatementInput("do_error")
+          .appendField("BoardEvent.ERROR","title_error");
+    }
   }
 };
 
