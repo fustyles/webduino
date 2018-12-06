085c74011ae82b51f0dac1b4c739e9cbf44cc106
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-06 18:36:22 +0800

    Update blocks.js

--- example_mutator_4/blockly/blocks.js
+++ example_mutator_4/blockly/blocks.js
@@ -69,9 +69,9 @@ Blockly.Blocks['boardevent'] = {
         connection.connect(itemBlock_input.previousConnection);
         connection = itemBlock_input.nextConnection;
       }
-      this.updateShape_();
-      return containerBlock;
     }
+    this.updateShape_();
+    return containerBlock;
   },
   compose: function(containerBlock) {
     var clauseBlock = containerBlock.nextConnection.targetBlock();
