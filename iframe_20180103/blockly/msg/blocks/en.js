297f6bd5044f86f1d9ccde16111d0e83ac5f9091
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-07 20:11:10 +0800

    Delete en.js

--- iframe_20180103/blockly/msg/blocks/en.js
+++
@@ -1,6 +0,0 @@
-Blockly.Msg.TEXT_SHOW = "Web Page";
-Blockly.Msg.URL_SHOW = "url";
-Blockly.Msg.WIDTH_SHOW = "width";
-Blockly.Msg.LEFT_SHOW = "LEFT";
-Blockly.Msg.TOP_SHOW = "TOP";
-Blockly.Msg.CLOSE_SHOW = "Close Web Page";
