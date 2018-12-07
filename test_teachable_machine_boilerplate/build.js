08d897034f7d2b59c7492c0e77976e028e73baa2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-07 22:08:40 +0800

    Update build.js

--- test_teachable_machine_boilerplate/build.js
+++ test_teachable_machine_boilerplate/build.js
@@ -26,7 +26,7 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
+console.log(document.cookie);
 // Number of classes to classify
 var NUM_CLASSES = 4;
 // Webcam Image size. Must be 227. 
