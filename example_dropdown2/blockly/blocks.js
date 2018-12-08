1ee24fa22c0e05e1fec0a0fd4caa2474082b8afd
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-08 21:53:19 +0800

    Update blocks.js

--- example_dropdown2/blockly/blocks.js
+++ example_dropdown2/blockly/blocks.js
@@ -5,8 +5,10 @@ Blockly.Blocks['dropdown'] = {
     this.Dropdown1="A";
     this.listA = [["A1","A1"], ["A2","A2"]];
     this.listB = [["B1","B1"], ["B2","B2"]];
-    this.appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown1");
-    this.appendField(new Blockly.FieldDropdown(this.listA), "Dropdown2");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown1");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown(this.listA), "Dropdown2");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
@@ -24,9 +26,13 @@ Blockly.Blocks['dropdown'] = {
   },
   updateShape_: function() {
     if (this.getField('Dropdown2')) this.removeField('Dropdown2');
-    if (this.Dropdown1=="A")
-      this.appendField(new Blockly.FieldDropdown([this.listA]), "Dropdown2");
-    else if (this.Dropdown1=="B")
-      this.appendField(new Blockly.FieldDropdown([this.listB]), "Dropdown2");
+    if (this.Dropdown1=="A") {
+      this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([this.listA]), "Dropdown2");
+    }
+    else if (this.Dropdown1=="B") {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldDropdown([this.listB]), "Dropdown2");
+    }
   }
 };
