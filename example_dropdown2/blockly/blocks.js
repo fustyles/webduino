93b29d95555ceca78723933c42feb287a6dc175e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-08 21:50:51 +0800

    Update blocks.js

--- example_dropdown2/blockly/blocks.js
+++ example_dropdown2/blockly/blocks.js
@@ -3,10 +3,10 @@
 Blockly.Blocks['dropdown'] = {
   init: function () {
     this.Dropdown1="A";
-    this.listA = ["A1","A1"], ["A2","A2"];
-    this.listA = ["B1","B1"], ["B2","B2"];
+    this.listA = [["A1","A1"], ["A2","A2"]];
+    this.listB = [["B1","B1"], ["B2","B2"]];
     this.appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown1");
-    this.appendField(new Blockly.FieldDropdown([this.listA]), "Dropdown2");
+    this.appendField(new Blockly.FieldDropdown(this.listA), "Dropdown2");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
@@ -18,7 +18,8 @@ Blockly.Blocks['dropdown'] = {
     return container;
   },
   domToMutation: function (xmlElement) {
-    this.Dropdown1 = xmlElement.getAttribute('Dropdown1');
+    if (xmlElement.getAttribute('Dropdown1'))
+      this.Dropdown1 = xmlElement.getAttribute('Dropdown1');
     this.updateShape_();
   },
   updateShape_: function() {
