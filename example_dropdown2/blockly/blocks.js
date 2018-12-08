8467ddffd24c4efb69f0d3b7db21cf0f0b57bfc6
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-08 22:21:28 +0800

    Update blocks.js

--- example_dropdown2/blockly/blocks.js
+++ example_dropdown2/blockly/blocks.js
@@ -12,7 +12,7 @@ Blockly.Blocks['dropdown'] = {
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
-    //this.updateShape_();
+    this.updateShape_();
   },
   mutationToDom: function (workspace) {
     var container = document.createElement('mutation');
@@ -23,7 +23,7 @@ Blockly.Blocks['dropdown'] = {
     if (xmlElement.getAttribute('Dropdown'))
       this.Dropdown = xmlElement.getAttribute('Dropdown');
     console.log(this.Dropdown);
-    //this.updateShape_();
+    this.updateShape_();
   },
   updateShape_: function() {
     if (this.getInput('b')) this.removeInput('b');
