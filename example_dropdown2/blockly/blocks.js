c1b6e15d72f5c776f95a123aae2cb0fd983bf5ea
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-09 01:09:44 +0800

    Update blocks.js

--- example_dropdown2/blockly/blocks.js
+++ example_dropdown2/blockly/blocks.js
@@ -33,14 +33,14 @@ Blockly.Blocks['dropdown'] = {
   },
   updateShape_: function() {  
     if (this.Dropdown=="A") {
-      this.getInput('Dropdown2').setVisible(true);
-      this.getInput('Dropdown3').setValue("");
-      this.getInput('Dropdown3').setVisible(false);
+      this.getInput('d2').setVisible(true);
+      this.getInput('d3').setValue("");
+      this.getInput('d3').setVisible(false);
     }
     else if (this.Dropdown=="B") {
-      this.getInput('Dropdown2').setValue("");
-      this.getInput('Dropdown2').setVisible(false);
-      this.getInput('Dropdown3').setVisible(true);
+      this.getInput('d2').setValue("");
+      this.getInput('d2').setVisible(false);
+      this.getInput('d3').setVisible(true);
     }
   }
 };
