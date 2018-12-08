d49213a1af2b7f1a3cff79a3a6c6cb793cd16246
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-09 01:56:23 +0800

    Update blocks.js

--- example_dropdown2/blockly/blocks.js
+++ example_dropdown2/blockly/blocks.js
@@ -2,13 +2,13 @@
 
 Blockly.Blocks['dropdown'] = {
   init: function () {
-    this.d1="A";
+    this.Dropdown="A";
     this.appendDummyInput()
         .appendField("List1")
-        .appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown1")
+        .appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown")
         .appendField("List2")
-        .appendField(new Blockly.FieldDropdown([["A1","A1"], ["A2","A2"]]), "Dropdown2")
-        .appendField(new Blockly.FieldDropdown([["B1","B1"], ["B2","B2"]]), "Dropdown3");
+        .appendField(new Blockly.FieldDropdown([["A1","A1"], ["A2","A2"]]), "Dropdown1")
+        .appendField(new Blockly.FieldDropdown([["B1","B1"], ["B2","B2"]]), "Dropdown2");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
@@ -16,41 +16,40 @@ Blockly.Blocks['dropdown'] = {
   },
   mutationToDom: function (workspace) {
     var container = document.createElement('mutation');
+    container.setAttribute('d', this.getFieldValue('Dropdown'));
     container.setAttribute('d1', this.getFieldValue('Dropdown1'));
     container.setAttribute('d2', this.getFieldValue('Dropdown2'));
-    container.setAttribute('d3', this.getFieldValue('Dropdown3'));
     return container;
   },
   domToMutation: function (xmlElement) {
+    if (xmlElement.getAttribute('d'))
+      this.getField('Dropdown').setValue(xmlElement.getAttribute('d'));
     if (xmlElement.getAttribute('d1'))
       this.getField('Dropdown1').setValue(xmlElement.getAttribute('d1'));
     if (xmlElement.getAttribute('d2'))
       this.getField('Dropdown2').setValue(xmlElement.getAttribute('d2'));
-    if (xmlElement.getAttribute('d3'))
-      this.getField('Dropdown3').setValue(xmlElement.getAttribute('d3'));
     this.updateShape_();
   },
   onchange: function (event) {
-    console.log(event);
     if (event.element=="field") {
-      if (event.name=="Dropdown1") {
-        this.Dropdown = this.getFieldValue('Dropdown1');
+      if (event.name=="Dropdown") {
+        this.Dropdown = this.getFieldValue('Dropdown');
         this.updateShape_();
       }
     }
   },
   updateShape_: function() {  
-    if (this.getFieldValue('Dropdown1')=="A") {
-      this.getField('Dropdown2').setValue("A1");
-      this.getField('Dropdown2').setVisible(true);
-      this.getField('Dropdown3').setValue("");
-      this.getField('Dropdown3').setVisible(false);
-    }
-    else if (this.getFieldValue('Dropdown1')=="B") {
+    if (this.getFieldValue('Dropdown')=="A") {
+      this.getField('Dropdown1').setValue("A1");
+      this.getField('Dropdown1').setVisible(true);
       this.getField('Dropdown2').setValue("");
       this.getField('Dropdown2').setVisible(false);
-      this.getField('Dropdown3').setValue("B1");
-      this.getField('Dropdown3').setVisible(true);
+    }
+    else if (this.getFieldValue('Dropdown')=="B") {
+      this.getField('Dropdown1').setValue("");
+      this.getField('Dropdown1').setVisible(false);
+      this.getField('Dropdown2').setValue("B1");
+      this.getField('Dropdown2').setVisible(true);
     }
     this.setNextStatement(true);
   }
