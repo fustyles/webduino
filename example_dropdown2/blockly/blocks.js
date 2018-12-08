35308079d60f72ed5b47b70070499c159ae50728
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-08 21:40:25 +0800

    Update blocks.js

--- example_dropdown2/blockly/blocks.js
+++ example_dropdown2/blockly/blocks.js
@@ -5,10 +5,8 @@ Blockly.Blocks['boardevent'] = {
     this.Dropdown1="A";
     this.listA = ["A1","A1"], ["A2","A2"];
     this.listA = ["B1","B1"], ["B2","B2"];
-    this.appendValueInput("device")
-      .appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown1");
-    this.appendValueInput("device")
-      .appendField(new Blockly.FieldDropdown([this.listA]), "Dropdown2");
+    this.appendField(new Blockly.FieldDropdown([["A","A"], ["B","B"]]), "Dropdown1");
+    this.appendField(new Blockly.FieldDropdown([this.listA]), "Dropdown2");
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(340);
@@ -23,53 +21,11 @@ Blockly.Blocks['boardevent'] = {
     this.Dropdown1 = xmlElement.getAttribute('Dropdown1');
     this.updateShape_();
   },
-  decompose: function (workspace) {      
-    var containerBlock = workspace.newBlock('boardevent_mutator');
-    containerBlock.setFieldValue(this.messageVisible_, "chkmessage");
-    containerBlock.setFieldValue(this.errorVisible_, "chkerror");
-    containerBlock.initSvg();
-    return containerBlock;
-  },
   updateShape_: function() {
-    if (this.getInput('do_message')) this.removeInput('do_message');
-    if (this.getInput('do_error')) this.removeInput('do_error');
-    if (this.messageVisible_=="TRUE") {
-      this.appendStatementInput("do_message")
-          .appendField("BoardEvent.STRING_MESSAGE","title_message");
-    }
-    if (this.errorVisible_=="TRUE") {
-      this.appendStatementInput("do_error")
-          .appendField("BoardEvent.ERROR","title_error");
-    }
-    /*
-    this.getInput('do_message').setVisible(false);    
-    this.getInput('do_error').setVisible(false);
-      
-    if (this.messageVisible_=="TRUE")
-        this.getInput('do_message').setVisible(true);
-    if (this.errorVisible_=="TRUE")
-        this.getInput('do_error').setVisible(true);
-    */
-  }
-};
-
-Blockly.Blocks['boardevent_sendmessage'] = {
-  init: function () {
-    this.appendValueInput("cmd")
-      .setCheck("String")
-      .appendField("Send Command");    
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(210);
-  }
-};
-
-Blockly.Blocks['boardevent_getmessage'] = {
-  init: function () {
-    this.appendDummyInput()
-      .appendField("Get Message");
-    this.setInputsInline(true);
-    this.setOutput(true, null);
-    this.setColour(45);
+    if (this.getField('Dropdown2')) this.removeField('Dropdown2');
+    if (this.Dropdown1=="A")
+      this.appendField(new Blockly.FieldDropdown([this.listA]), "Dropdown2");
+    else if (this.Dropdown1=="B")
+      this.appendField(new Blockly.FieldDropdown([this.listB]), "Dropdown2");
   }
 };
