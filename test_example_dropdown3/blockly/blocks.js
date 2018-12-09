6d1a309b80bd6e889ba1e3ad7d1d39c92da5565f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-09 22:33:39 +0800

    Update blocks.js

--- test_example_dropdown3/blockly/blocks.js
+++ test_example_dropdown3/blockly/blocks.js
@@ -36,7 +36,6 @@ Blockly.Blocks['dropdown'] = {
     this.getField('DropdownB').setValue(xmlElement.getAttribute('dB'));
     this.getField('DropdownB1').setValue(xmlElement.getAttribute('dB1'));
     this.getField('DropdownB2').setValue(xmlElement.getAttribute('dB2')); 
-    this.updateShape_("refresh");
   },
   onchange: function (event) {
     if (event.element=="field") {
