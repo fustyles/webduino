8b3d38aec98057ce52dbf0ba52c532b4131147e3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-11 00:02:14 +0800

    Update blocks.js

--- test_example_dropdown3/blockly/blocks.js
+++ test_example_dropdown3/blockly/blocks.js
@@ -30,6 +30,7 @@ Blockly.Blocks['dropdown'] = {
     return container;
   },
   domToMutation: function (xmlElement) {
+    this.updateShape_("refresh",xmlElement.getAttribute('d')+","+xmlElement.getAttribute('dA')+","+xmlElement.getAttribute('dA1')+","+xmlElement.getAttribute('dA2')+","+xmlElement.getAttribute('dB')+","+xmlElement.getAttribute('dB1')+","+xmlElement.getAttribute('dB2'));
     this.getField('Dropdown').setValue(xmlElement.getAttribute('d'));
     this.getField('DropdownA').setValue(xmlElement.getAttribute('dA'));
     this.getField('DropdownA1').setValue(xmlElement.getAttribute('dA1'));
@@ -37,7 +38,6 @@ Blockly.Blocks['dropdown'] = {
     this.getField('DropdownB').setValue(xmlElement.getAttribute('dB'));
     this.getField('DropdownB1').setValue(xmlElement.getAttribute('dB1'));
     this.getField('DropdownB2').setValue(xmlElement.getAttribute('dB2'));
-    this.updateShape_("refresh",xmlElement.getAttribute('d')+","+xmlElement.getAttribute('dA')+","+xmlElement.getAttribute('dA1')+","+xmlElement.getAttribute('dA2')+","+xmlElement.getAttribute('dB')+","+xmlElement.getAttribute('dB1')+","+xmlElement.getAttribute('dB2'));
   },
   onchange: function (event) {
     if (event.element=="field") {
