510ccb901164ea00b6df512e41cbc28eed1f26f2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-11 19:24:29 +0800

    Update blocks.js

--- example_dropdown3/blockly/blocks.js
+++ example_dropdown3/blockly/blocks.js
@@ -80,8 +80,7 @@ Blockly.Blocks['dropdown'] = {
   },
     changelist: function (id_display,id_hide) {
     for (var i=0;i<id_display.length;i++) {
-      console.log(this.getOptions(id_display[i]));
-      console.log(this.getOptions(id_display[i])[0]);
+      console.log(this.getField(id_display[i]).getOptions);
       
       this.getField(id_display[i]).setValue("");
       this.getField(id_display[i]).setVisible(true);
