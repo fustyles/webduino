475d1d9cc093bfec0f2b71932a6934c4fd243d82
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 17:36:02 +0800

    Add files via upload

---
+++ Instruction_20181213/instruction.js
@@ -0,0 +1,32 @@
+// Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
+
++(function (window, document) {
+
+  'use strict';
+
+  function ifttt(event,key,value1,value2,value3) {
+    
+    var input_url="https://maker.ifttt.com/trigger/"+event+"/with/key/"+key;
+    var data = $.ajax({
+        "type": "POST",
+        "dataType": "jsonp",
+        "url": input_url,
+        "data":{
+          "value1": value1, 
+          "value2": value2, 
+          "value3": value3, 
+        },
+        success: function(jsonp)
+        {
+          console.log(jsonp);
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+          //console.log(errorThrown);
+        }
+     });
+  }
+
+  window.ifttt = ifttt;
+
+}(window, window.document));
