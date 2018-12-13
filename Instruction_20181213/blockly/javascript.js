1ead8ac967fd360990e085f026c2715eab3ed9a1
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 18:07:10 +0800

    Update javascript.js

--- Instruction_20181213/blockly/javascript.js
+++ Instruction_20181213/blockly/javascript.js
@@ -1,10 +1,10 @@
 Blockly.JavaScript['cmd1'] = function (block) {
-  var cmd1_1 = Blockly.JavaScript.valueToCode(block, 'cmd1_1', Blockly.JavaScript.ORDER_ATOMIC);  
+  var cmd1_0 = Blockly.JavaScript.valueToCode(block, 'cmd1_0', Blockly.JavaScript.ORDER_ATOMIC);  
   
-  if (!cmd1_1) cmd1_1='""';
-  if ((cmd1_1.indexOf("'")==0)&&(cmd1_1.lastIndexOf("'")==cmd1_1.length-1))
-    cmd1_1 = cmd1_1.substring(1,cmd1_1.length-1);
+  if (!cmd1_0) cmd1_0='""';
+  if ((cmd1_0.indexOf("'")==0)&&(cmd1_0.lastIndexOf("'")==cmd1_0.length-1))
+    cmd1_0 = cmd1_0.substring(1,cmd1_0.length-1);
     
-  var code = cmd1_1 + '\n';
+  var code = cmd1_0 + '\n';
   return [code, Blockly.JavaScript.ORDER_NONE]; 
 };
