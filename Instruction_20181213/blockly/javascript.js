87e8d9e298f40275e2962c9a0d41544f88644067
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 17:57:50 +0800

    Update javascript.js

--- Instruction_20181213/blockly/javascript.js
+++ Instruction_20181213/blockly/javascript.js
@@ -1,9 +1,12 @@
 Blockly.JavaScript['ifttt_open'] = function (block) {
   var cmd1_1 = Blockly.JavaScript.valueToCode(block, 'cmd1_1', Blockly.JavaScript.ORDER_ATOMIC);  
   
+  console.log(cmd1_1);
   if (!cmd1_1) cmd1_1='""';
   if ((cmd1_1.indexOf("'")==0)&&(cmd1_1.lastIndexOf("'")==cmd1_1.length-1))
-  cmd1_1 = cmd1_1.substring(1,cmd1_1.length-2);
+  console.log(cmd1_1.substring(1,cmd1_1.length-2));
+    
+  //cmd1_1 = cmd1_1.substring(1,cmd1_1.length-2);
     
   var code = cmd1_1 + '\n';
   return [code, Blockly.JavaScript.ORDER_NONE]; 
