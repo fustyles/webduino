e739df094537f4f7cd3c6076dd9cfac4746d9942
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 19:41:44 +0800

    Update blocks.js

--- Instruction_20181213/blockly/blocks.js
+++ Instruction_20181213/blockly/blocks.js
@@ -100,3 +100,60 @@ Blockly.Blocks['cmd5'] = {
   }
 };
 
+
+Blockly.Blocks['cmd6'] = {
+  init: function() {
+  this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown([
+        ["Math.E","Math.E"], 
+        ["Math.LN2","Math.LN2"], 
+        ["Math.LN10","Math.LN10"], 
+        ["Math.LOG2E","Math.LOG2E"], 
+        ["Math.LOG10E","Math.LOG10E"], 
+        ["Math.PI","Math.PI"], 
+        ["Math.SQRT1_2","Math.SQRT1_2"], 
+        ["Math.SQRT2","Math.SQRT2"], 
+        ["Math.abs","Math.abs"],
+        ["Math.acos","Math.acos"],
+        ["Math.acosh","Math.acosh"],
+        ["Math.asin","Math.asin"],
+        ["Math.asinh","Math.asinh"],
+        ["Math.atan","Math.atan"],
+        ["Math.atan2","Math.atan2"],
+        ["Math.atanh","Math.atanh"],
+        ["Math.cbrt","Math.cbrt"],
+        ["Math.ceil","Math.ceil"],
+        ["Math.clz32","Math.clz32"],
+        ["Math.cos","Math.cos"],
+        ["Math.cosh","Math.cosh"],
+        ["Math.exp","Math.exp"],
+        ["Math.expm1","Math.expm1"],
+        ["Math.floor","Math.floor"],
+        ["Math.fround","Math.fround"],
+        ["Math.hypot","Math.hypot"],
+        ["Math.imul","Math.imul"],
+        ["Math.log","Math.log"],
+        ["Math.log1p","Math.log1p"],
+        ["Math.log10","Math.log10"],
+        ["Math.log2","Math.log2"],
+        ["Math.max","Math.max"],
+        ["Math.min","Math.min"],
+        ["Math.pow","Math.pow"],
+        ["Math.random","Math.random"],
+        ["Math.round","Math.round"],
+        ["Math.sign","Math.sign"],
+        ["Math.sin","Math.sin"],
+        ["Math.sinh","Math.sinh"],
+        ["Math.sqrt","Math.sqrt"],
+        ["Math.tan","Math.tan"],
+        ["Math.tanh","Math.tanh"],
+        ["Math.trunc","Math.trunc"],  
+        ["null","null"]
+      ]), "math");    
+  this.setOutput(true, null);
+  this.setInputsInline(true);
+  this.setTooltip('');
+  this.setColour(20);
+  }
+};
+
