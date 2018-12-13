81f1930f51d4d70579a1710400b62bc69f9fd3a0
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 18:25:49 +0800

    Update blocks.js

--- Instruction_20181213/blockly/blocks.js
+++ Instruction_20181213/blockly/blocks.js
@@ -6,7 +6,7 @@ Blockly.Blocks['cmd1'] = {
   this.setOutput(true, null);
   this.setInputsInline(true);
   this.setTooltip('');
-  this.setColour(65);
+  this.setColour(80);
   }
 };
 
@@ -14,15 +14,38 @@ Blockly.Blocks['cmd2'] = {
   init: function() {
   this.appendValueInput("cmd2_0")
       .setCheck("String")
-      .appendField("Javascript: ");    
+      .appendField("Javascript: ");   
+  this.appendDummyInput()
+      .appendField("( ");      
   this.appendValueInput("cmd2_1")
-      .setCheck(null)
-      .appendField("( ");  
+      .setCheck(null);  
   this.appendDummyInput()
       .appendField(" )");      
   this.setOutput(true, null);
   this.setInputsInline(true);
   this.setTooltip('');
-  this.setColour(65);
+  this.setColour(80);
+  }
+};
+
+Blockly.Blocks['cmd3'] = {
+  init: function() {
+  this.appendValueInput("cmd2_0")
+      .setCheck("String")
+      .appendField("Javascript: ");
+  this.appendDummyInput()
+      .appendField("( ");    
+  this.appendValueInput("cmd2_1")
+      .setCheck(null);  
+  this.appendDummyInput()
+      .appendField(", ");       
+  this.appendValueInput("cmd2_2")
+      .setCheck(null);     
+  this.appendDummyInput()
+      .appendField(" )");      
+  this.setOutput(true, null);
+  this.setInputsInline(true);
+  this.setTooltip('');
+  this.setColour(80);
   }
 };
