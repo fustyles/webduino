a852783cac265f49c557b947d18d177b294e388c
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 17:50:35 +0800

    Update javascript.js

--- Instruction_20181213/blockly/javascript.js
+++ Instruction_20181213/blockly/javascript.js
@@ -1,16 +1,10 @@
 Blockly.JavaScript['ifttt_open'] = function (block) {
-  var ifttt_event = Blockly.JavaScript.valueToCode(block, 'ifttt_event', Blockly.JavaScript.ORDER_ATOMIC);  
-  var ifttt_key = Blockly.JavaScript.valueToCode(block, 'ifttt_key', Blockly.JavaScript.ORDER_ATOMIC);
-  var ifttt_value1 = Blockly.JavaScript.valueToCode(block, 'ifttt_value1', Blockly.JavaScript.ORDER_ATOMIC);
-  var ifttt_value2 = Blockly.JavaScript.valueToCode(block, 'ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
-  var ifttt_value3 = Blockly.JavaScript.valueToCode(block, 'ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd1_1 = Blockly.JavaScript.valueToCode(block, 'cmd1_1', Blockly.JavaScript.ORDER_ATOMIC);  
   
-  if (!ifttt_event) ifttt_event='""';
-  if (!ifttt_key) ifttt_key='""';
-  if (!ifttt_value1) ifttt_value1='""';
-  if (!ifttt_value2) ifttt_value2='""';
-  if (!ifttt_value3) ifttt_value3='""';
-  
-  var code = 'ifttt('+ifttt_event+','+ifttt_key+','+ifttt_value1+','+ifttt_value2+','+ifttt_value3+');\n';
-  return code; 
+  if (!cmd1_1) cmd1_1='""';
+  if ((cmd1_1.indexOf("'")==0)&&(cmd1_1.lastIndexOf("'")==cmd1_1.length-1))
+  cmd1_1 = cmd1_1.substring(1,cmd1_1.length-2);
+    
+  var code = cmd1_1 + '\n';
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
 };
