0c5ecc8f3aa72f6e24dedaa9897555943742b9ea
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-13 18:06:42 +0800

    Update blocks.js

--- Instruction_20181213/blockly/blocks.js
+++ Instruction_20181213/blockly/blocks.js
@@ -1,9 +1,25 @@
 Blockly.Blocks['cmd1'] = {
   init: function() {
-  this.appendValueInput("cmd1_1")
+  this.appendValueInput("cmd1_0")
       .setCheck("String")
-      .setAlign(Blockly.ALIGN_RIGHT)
       .appendField("Code ");    
+  this.setOutput(true, null);
+  this.setInputsInline(true);
+  this.setTooltip('');
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['cmd2'] = {
+  init: function() {
+  this.appendValueInput("cmd2_0")
+      .setCheck("String")
+      .appendField("Code ");    
+  this.appendValueInput("cmd2_1")
+      .setCheck("String")
+      .appendField("( ");  
+  this.appendDummyInput()
+      .appendField(" )");      
   this.setOutput(true, null);
   this.setInputsInline(true);
   this.setTooltip('');
