764ab866a2eff1ebc3fc6cc83071dae3f30bf72e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-16 02:30:45 +0800

    Update javascript.js

--- Instruction_20181213/blockly/javascript.js
+++ Instruction_20181213/blockly/javascript.js
@@ -1,11 +1,3 @@
-var workspace = Blockly.inject('blocklyDiv',
-      {toolbox: document.getElementById('toolbox')});
-function myUpdateFunction(event) {
-  var code = Blockly.JavaScript.workspaceToCode(workspace);
-  document.getElementById('demo-area-01-show').innerHTML = code;
-}
-workspace.addChangeListener(myUpdateFunction);
-
 Blockly.JavaScript['cmd1'] = function (block) {
   var cmd1_0 = Blockly.JavaScript.valueToCode(block, 'cmd1_0', Blockly.JavaScript.ORDER_ATOMIC);  
   
