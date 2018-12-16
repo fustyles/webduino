9a3f62766d8af42f9cfd7ed511dd67738754e412
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-16 17:36:51 +0800

    Update blocks.js

--- ShowCode_20181216/blockly/blocks.js
+++ ShowCode_20181216/blockly/blocks.js
@@ -18,12 +18,5 @@ Blockly.Blocks['showcode'] = {
       console.clear();
       console.log(Blockly.JavaScript.workspaceToCode(workspace));
     }
-  },
-  mutationToDom: function (workspace) {
-    var container = document.createElement('mutation');
-    return container;
-  },
-  domToMutation: function (xmlElement) {
-    this.setFieldValue("FALSE","continued");
   }
 };    
