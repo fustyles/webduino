5a2cdbe110014558e99722834d51c57a40b0b242
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-16 13:39:03 +0800

    Update blocks.js

--- ShowCode_20181216/blockly/blocks.js
+++ ShowCode_20181216/blockly/blocks.js
@@ -1,19 +1,25 @@
 Blockly.Blocks['showcode'] = {
   init: function() {
   this.appendDummyInput()
-      .appendField("Show Code");
+      .appendField("Show Code  ")
+      .appendField(new Blockly.FieldCheckbox("FALSE"), "check");
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setTooltip('');
   this.setColour(20);
   },
   onchange: function (event) {
-    if (event.element=="click") {
+    //console.log(this.getFieldValue("check")=="TRUE");
+    if (event.element=="click"&&this.getFieldValue("check")=="FALSE") {
       if (this.id==event.blockId) {
         var workspace = Blockly.getMainWorkspace('blocklyDiv');
         console.clear();
         console.log(Blockly.JavaScript.workspaceToCode(workspace));
       }
+    } else if (this.getFieldValue("check")=="TRUE") {
+      var workspace = Blockly.getMainWorkspace('blocklyDiv');
+      console.clear();
+      console.log(Blockly.JavaScript.workspaceToCode(workspace));
     }
   }
 };    
