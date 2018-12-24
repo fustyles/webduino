47b5d331f224027b5a5f8d18efcfc45552eecf8f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-24 20:58:55 +0800

    Add files via upload

---
+++ myBlockly/Instruction/javascript_Instruction.js
@@ -0,0 +1,214 @@
+Blockly.JavaScript['cmd1'] = function (block) {
+
+  var cmd1_0 = Blockly.JavaScript.valueToCode(block, 'cmd1_0', Blockly.JavaScript.ORDER_ATOMIC);  
+  
+  if ((cmd1_0.indexOf("'")==0)&&(cmd1_0.lastIndexOf("'")==cmd1_0.length-1))
+    cmd1_0 = cmd1_0.substring(1,cmd1_0.length-1);
+  if ((cmd1_0.indexOf("(")==0)&&(cmd1_0.lastIndexOf(")")==cmd1_0.length-1))
+    cmd1_0 = cmd1_0.substring(1,cmd1_0.length-1);
+    console.log(cmd1_0);
+  
+  var code = cmd1_0;
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd2'] = function (block) {
+  var cmd2_0 = Blockly.JavaScript.valueToCode(block, 'cmd2_0', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd2_1 = Blockly.JavaScript.valueToCode(block, 'cmd2_1', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((cmd2_0.indexOf("'")==0)&&(cmd2_0.lastIndexOf("'")==cmd2_0.length-1))
+    cmd2_0 = cmd2_0.substring(1,cmd2_0.length-1);
+  if ((cmd2_0.indexOf("(")==0)&&(cmd2_0.lastIndexOf(")")==cmd2_0.length-1))
+    cmd2_0 = cmd2_0.substring(1,cmd2_0.length-1);
+    
+  var code = cmd2_0 + "(" + cmd2_1 + ")";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd3'] = function (block) {
+  var cmd3_0 = Blockly.JavaScript.valueToCode(block, 'cmd3_0', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd3_1 = Blockly.JavaScript.valueToCode(block, 'cmd3_1', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd3_2 = Blockly.JavaScript.valueToCode(block, 'cmd3_2', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((cmd3_0.indexOf("'")==0)&&(cmd3_0.lastIndexOf("'")==cmd3_0.length-1))
+    cmd3_0 = cmd3_0.substring(1,cmd3_0.length-1);
+  if ((cmd3_0.indexOf("(")==0)&&(cmd3_0.lastIndexOf(")")==cmd3_0.length-1))
+    cmd3_0 = cmd3_0.substring(1,cmd3_0.length-1);
+    
+  var code = cmd3_0 + "(" + cmd3_1 + ", " + cmd3_2 + ")";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd4'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd4_0 = Blockly.JavaScript.valueToCode(block, 'cmd4_0', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd4_1 = Blockly.JavaScript.valueToCode(block, 'cmd4_1', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd4_2 = Blockly.JavaScript.valueToCode(block, 'cmd4_2', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((cmd4_0.indexOf("'")==0)&&(cmd4_0.lastIndexOf("'")==cmd4_0.length-1))
+    cmd4_0 = cmd4_0.substring(1,cmd4_0.length-1);
+  if ((cmd4_0.indexOf("(")==0)&&(cmd4_0.lastIndexOf(")")==cmd4_0.length-1))
+    cmd4_0 = cmd4_0.substring(1,cmd4_0.length-1);
+    
+  var code = myVar + "." + cmd4_0 + "(" + cmd4_1 + ", " + cmd4_2 + ")";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd5'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd5_0 = Blockly.JavaScript.valueToCode(block, 'cmd5_0', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd5_1 = Blockly.JavaScript.valueToCode(block, 'cmd5_1', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((cmd5_0.indexOf("'")==0)&&(cmd5_0.lastIndexOf("'")==cmd5_0.length-1))
+    cmd5_0 = cmd5_0.substring(1,cmd5_0.length-1);
+  if ((cmd5_0.indexOf("(")==0)&&(cmd5_0.lastIndexOf(")")==cmd5_0.length-1))
+    cmd5_0 = cmd5_0.substring(1,cmd5_0.length-1);
+    
+  var code = myVar + "." + cmd5_0 + "(" + cmd5_1 + ")";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd6'] = function (block) {
+  var cmd_math = block.getFieldValue('cmd_math');
+  
+  if ((cmd_math.indexOf("'")==0)&&(cmd_math.lastIndexOf("'")==cmd_math.length-1))
+    cmd_math = cmd_math.substring(1,cmd_math.length-1);
+  if ((cmd_math.indexOf("(")==0)&&(cmd_math.lastIndexOf(")")==cmd_math.length-1))
+    cmd_math = cmd_math.substring(1,cmd_math.length-1);
+    
+  var code = cmd_math;
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd7'] = function (block) {
+  var cmd_string = block.getFieldValue('cmd_string');
+  
+  if ((cmd_string.indexOf("'")==0)&&(cmd_string.lastIndexOf("'")==cmd_string.length-1))
+    cmd_string = cmd_string.substring(1,cmd_string.length-1);
+  if ((cmd_string.indexOf("(")==0)&&(cmd_string.lastIndexOf(")")==cmd_string.length-1))
+    cmd_string = cmd_string.substring(1,cmd_string.length-1);
+    
+  var code = cmd_string;
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd8'] = function (block) {
+  var cmd_array = block.getFieldValue('cmd_array');
+  
+  if ((cmd_array.indexOf("'")==0)&&(cmd_array.lastIndexOf("'")==cmd_array.length-1))
+    cmd_array = cmd_array.substring(1,cmd_array.length-1);
+  if ((cmd_array.indexOf("(")==0)&&(cmd_array.lastIndexOf(")")==cmd_array.length-1))
+    cmd_array = cmd_array.substring(1,cmd_array.length-1);
+    
+  var code = cmd_array;
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd9'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd9_0 = Blockly.JavaScript.valueToCode(block, 'cmd9_0', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((cmd9_0.indexOf("'")==0)&&(cmd9_0.lastIndexOf("'")==cmd9_0.length-1))
+    cmd9_0 = cmd9_0.substring(1,cmd9_0.length-1);
+  if ((cmd9_0.indexOf("(")==0)&&(cmd9_0.lastIndexOf(")")==cmd9_0.length-1))
+    cmd9_0 = cmd9_0.substring(1,cmd9_0.length-1);
+    
+  var code = myVar + "." + cmd9_0;
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd10'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd10_0 = Blockly.JavaScript.valueToCode(block, 'cmd10_0', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+    
+  var code = myVar + "[" + cmd10_0 + "]";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd11'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd11_0 = Blockly.JavaScript.valueToCode(block, 'cmd11_0', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd11_1 = Blockly.JavaScript.valueToCode(block, 'cmd11_1', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd11_2 = Blockly.JavaScript.valueToCode(block, 'cmd11_2', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd11_3 = Blockly.JavaScript.valueToCode(block, 'cmd11_3', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((cmd11_0.indexOf("'")==0)&&(cmd11_0.lastIndexOf("'")==cmd11_0.length-1))
+    cmd11_0 = cmd11_0.substring(1,cmd11_0.length-1);
+  if ((cmd11_0.indexOf("(")==0)&&(cmd11_0.lastIndexOf(")")==cmd11_0.length-1))
+    cmd11_0 = cmd11_0.substring(1,cmd11_0.length-1);
+    
+  var code = myVar + "." + cmd11_0 + "(" + cmd11_1 + ", " + cmd11_2 + ", " + cmd11_3 + ")";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd12'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd12_0 = Blockly.JavaScript.valueToCode(block, 'cmd12_0', Blockly.JavaScript.ORDER_ATOMIC);
+  var cmd12_1 = Blockly.JavaScript.valueToCode(block, 'cmd12_1', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+    
+  var code = myVar + "[" + cmd12_0 + "][" + cmd12_1 + "]";
+  return [code, Blockly.JavaScript.ORDER_NONE]; 
+};
+
+Blockly.JavaScript['cmd13'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+    
+  var code = "var " + myVar + ";\n";
+  return code;
+};
+
+Blockly.JavaScript['cmd14'] = function (block) {
+  var myVar = Blockly.JavaScript.valueToCode(block, 'myVar', Blockly.JavaScript.ORDER_ATOMIC);
+  var variableValue = Blockly.JavaScript.valueToCode(block, 'variableValue', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((myVar.indexOf("'")==0)&&(myVar.lastIndexOf("'")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+  if ((myVar.indexOf("(")==0)&&(myVar.lastIndexOf(")")==myVar.length-1))
+    myVar = myVar.substring(1,myVar.length-1);
+    
+  var code = myVar + " = " + variableValue +";\n";
+  return code;
+};
+
+Blockly.JavaScript['cmd15'] = function (block) {
+  var Instruction = Blockly.JavaScript.valueToCode(block, 'Instruction', Blockly.JavaScript.ORDER_ATOMIC);
+  
+  if ((Instruction.indexOf("'")==0)&&(Instruction.lastIndexOf("'")==Instruction.length-1))
+    Instruction = Instruction.substring(1,Instruction.length-1);
+  if ((Instruction.indexOf("(")==0)&&(Instruction.lastIndexOf(")")==Instruction.length-1))
+    Instruction = Instruction.substring(1,Instruction.length-1);
+    
+  var code = Instruction + ";\n";
+  return code;
+};
---
+++ myBlockly/Instruction/toolbox_Instruction.js
@@ -0,0 +1,177 @@
+var catInstruction = '<xml><category name="%{BKY_INSTRUCTION}" colour="%{BKY_INSTRUCTION_HUE}">'+
+'  <block type="cmd1">'+
+'    <value name="cmd1_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+
+'  <block type="cmd2">'+
+'    <value name="cmd2_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd2_1">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+
+'  <block type="cmd3">'+
+'    <value name="cmd3_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd3_1">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd3_2">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+
+'  <block type="cmd9">'+
+'    <value name="myVar">'+
+'      <block type="variables_get">'+ 
+'        <field name="VAR">myVar</field>'+ 
+'      </block>'+
+'    </value>'+    
+'    <value name="cmd9_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+      
+'  </block>'+   
+'  <block type="cmd5">'+
+'    <value name="myVar">'+
+'      <block type="variables_get">'+
+'        <field name="VAR">myVar</field>'+ 
+'     </block>'+
+'    </value>'+
+'    <value name="cmd5_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd5_1">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+
+'  <block type="cmd4">'+
+'    <value name="myVar">'+
+'      <block type="variables_get">'+
+'        <field name="VAR">myVar</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd4_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd4_1">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+   
+'    <value name="cmd4_2">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+
+'  <block type="cmd11">'+
+'    <value name="myVar">'+
+'      <block type="variables_get">'+ 
+'        <field name="VAR">myVar</field>'+ 
+'      </block>'+
+'    </value>'+
+'    <value name="cmd11_0">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+    
+'    <value name="cmd11_1">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+   
+'    <value name="cmd11_2">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+   
+'    <value name="cmd11_3">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+    
+'  <block type="cmd10">'+
+'    <value name="myVar">'+
+'      <block type="variables_get">'+ 
+'        <field name="VAR">myArr</field>'+ 
+'      </block>'+
+'    </value>'+
+'    <value name="cmd10_0">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+
+'  <block type="cmd12">'+
+'    <value name="myVar">'+
+'      <block type="variables_get">'+
+'        <field name="VAR">myArr</field>'+ 
+'      </block>'+
+'    </value>'+
+'    <value name="cmd12_0">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="cmd12_1">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+  
+'  </block>'+
+'  <block type="cmd13">'+
+'    <value name="myVar">'+
+'      <block type="text">'+
+'        <field name="TEXT">myVar</field>'+
+'      </block>'+
+'    </value>'+      
+'  </block>'+    
+'  <block type="cmd14">'+
+'    <value name="myVar">'+
+'      <block type="text">'+
+'        <field name="TEXT">myVar</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="variableValue">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+   
+'  <block type="cmd15">'+
+'    <value name="Instruction">'+
+'      <block type="text">'+
+'        <field name="TEXT">break</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+ 
+'  <block type="cmd6">'+   
+'  </block>'+
+'  <block type="cmd7">'+   
+'  </block>'+
+'  <block type="cmd8">'+   
+'  </block>'+
+' </category></xml>';
