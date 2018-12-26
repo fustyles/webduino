22d025209c91759e0c4cb8f350e26739373f797e
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-27 00:23:16 +0800

    Delete msg_ExecuteURL.js

--- myBlockly/ExecuteURL/msg_ExecuteURL.js
+++
@@ -1,9 +0,0 @@
-Blockly.Msg.OPEN_SHOW = "Open Link";
-Blockly.Msg.CLOSE_SHOW = "Close Link";
-Blockly.Msg.OPENID_SHOW = "ID";
-Blockly.Msg.CLOSEID_SHOW = "ID";
-Blockly.Msg.URL_SHOW = "Url";
-Blockly.Msg.WIDTH_SHOW = "Width";
-Blockly.Msg.LEFT_SHOW = "Left";
-Blockly.Msg.TOP_SHOW = "Top";
-Blockly.Msg.ZINDEX_SHOW = "Z-index";
