f06a5c50292ee2f4814df48a908931a58c3f4ce3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-27 18:26:25 +0800

    Update javascript_Keyboard.js

--- myBlockly/Keyboard/javascript_Keyboard.js
+++ myBlockly/Keyboard/javascript_Keyboard.js
@@ -8,6 +8,6 @@ Blockly.JavaScript['keydown_listener'] = function (block) {
 
 Blockly.JavaScript['keydown_keycode'] = function(block) {
   var keycode = block.getFieldValue('keycode'); 
-  var code = 'event.keyCode = ' + keycode;
+  var code = 'event.keyCode == ' + keycode;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
