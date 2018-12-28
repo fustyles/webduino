e45fce49c77433e045cc9dd28c4f6a17ecf15619
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-28 22:03:18 +0800

    Add files via upload

--- myBlockly/ShowText/blocks_ShowText.js
+++ myBlockly/ShowText/blocks_ShowText.js
@@ -1,5 +1,7 @@
 Blockly.Blocks['showtext'] = {
   init: function() {
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.SHOWTEXT_SHOW);  
   this.appendValueInput("size")
       .setCheck("Number")
       .appendField(Blockly.Msg.SHOWTEXT_SIZE_SHOW); 
@@ -8,6 +10,7 @@ Blockly.Blocks['showtext'] = {
   this.appendValueInput("text")
       .setCheck("String")
       .appendField(Blockly.Msg.SHOWTEXT_TEXT_SHOW);   
+  this.setInputsInline(true);
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setColour(65);
--- myBlockly/ShowText/msg_ShowText_zh-hant.js
+++ myBlockly/ShowText/msg_ShowText_zh-hant.js
@@ -1,3 +1,4 @@
-Blockly.Msg.SHOWTEXT_SIZE_SHOW = "文字大小";
-Blockly.Msg.SHOWTEXT_COLOR_SHOW = "文字顏色";
-Blockly.Msg.SHOWTEXT_TEXT_SHOW = "顯示文字";
+Blockly.Msg.SHOWTEXT_SHOW = "顯示文字";
+Blockly.Msg.SHOWTEXT_SIZE_SHOW = "大小";
+Blockly.Msg.SHOWTEXT_COLOR_SHOW = "顏色";
+Blockly.Msg.SHOWTEXT_TEXT_SHOW = "文字";
