f7cfb882689128bdaa63ddb12800241e0c652555
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-28 22:14:25 +0800

    Update javascript_ShowText.js

--- myBlockly/ShowText/javascript_ShowText.js
+++ myBlockly/ShowText/javascript_ShowText.js
@@ -1,5 +1,6 @@
 Blockly.JavaScript['showtext'] = function (block) {
   var size = Blockly.JavaScript.valueToCode(block, 'size', Blockly.JavaScript.ORDER_ATOMIC);
+  size = "'"+size+"px'";
   var color = Blockly.JavaScript.valueToCode(block, 'color', Blockly.JavaScript.ORDER_ATOMIC);
   var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);  
   if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
