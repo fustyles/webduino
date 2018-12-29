5eafdf045fa10c84b0424a240226378a5fffb593
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-29 17:53:57 +0800

    Update msg_ShowText.js

--- myBlockly/ShowText/msg_ShowText.js
+++ myBlockly/ShowText/msg_ShowText.js
@@ -3,6 +3,7 @@ Blockly.Msg.SHOWTEXT_SIZE_SHOW = "size";
 Blockly.Msg.SHOWTEXT_COLOR_SHOW = "color";
 Blockly.Msg.SHOWTEXT_TEXT_SHOW = "Text";
 Blockly.Msg.LINEBREAK_SHOW = "Line Break";
+Blockly.Msg.SETSCREEN_SHOW = "Set Screen";
 Blockly.Msg.SETSCREEN_LEFT_SHOW = "LEFT";
 Blockly.Msg.SETSCREEN_TOP_SHOW = "TOP";
 Blockly.Msg.SETSCREEN_WIDTH_SHOW = "WIDTH";
