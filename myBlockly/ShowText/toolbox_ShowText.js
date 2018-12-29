cee03b0bb218500bb5f8ec94126af135587f55e3
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-29 18:10:58 +0800

    Update toolbox_ShowText.js

--- myBlockly/ShowText/toolbox_ShowText.js
+++ myBlockly/ShowText/toolbox_ShowText.js
@@ -1,24 +1,5 @@
 var catShowText = '<xml>'+
 '<category name="%{BKY_SHOWTEXT}" colour="%{BKY_SHOWTEXT_HUE}">'+
-'  <block type="showtext">'+
-'    <value name="size">'+
-'      <block type="math_number">'+
-'        <field name="NUM">12</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="color">'+
-'      <block type="colour_picker">'+
-'        <field name="COLOUR">#000000</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="text">'+
-'      <block type="text">'+
-'        <field name="TEXT">Hello</field>'+
-'      </block>'+
-'    </value>'+
-'  </block>'+
-'  <block type="linebreak">'+
-'  </block>'+
 '  <block type="setscreen">'+
 '    <value name="left">'+
 '      <block type="math_number">'+
@@ -40,6 +21,25 @@ var catShowText = '<xml>'+
 '        <field name="NUM">500</field>'+
 '      </block>'+
 '    </value>'+
+'  </block>'+
+'  <block type="showtext">'+
+'    <value name="size">'+
+'      <block type="math_number">'+
+'        <field name="NUM">12</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="color">'+
+'      <block type="colour_picker">'+
+'        <field name="COLOUR">#000000</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="text">'+
+'      <block type="text">'+
+'        <field name="TEXT">Hello</field>'+
+'      </block>'+
+'    </value>'+
+'  </block>'+
+'  <block type="linebreak">'+
 '  </block>'+
 '</category>'+
 '</xml>';
