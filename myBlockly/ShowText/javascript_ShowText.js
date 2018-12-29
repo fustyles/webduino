68a419e3acf8d0b0daeea5d205d0cd38d1b66d1f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-29 18:07:15 +0800

    Update javascript_ShowText.js

--- myBlockly/ShowText/javascript_ShowText.js
+++ myBlockly/ShowText/javascript_ShowText.js
@@ -14,17 +14,13 @@ Blockly.JavaScript['linebreak'] = function (block) {
 
 Blockly.JavaScript['setscreen'] = function (block) {
   var left = Blockly.JavaScript.valueToCode(block, 'left', Blockly.JavaScript.ORDER_ATOMIC);
-  left = "'"+left+"px'";
   var top = Blockly.JavaScript.valueToCode(block, 'top', Blockly.JavaScript.ORDER_ATOMIC);
-  top = "'"+top+"px'";
   var width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
-  width = "'"+width+"px'";
   var height = Blockly.JavaScript.valueToCode(block, 'height', Blockly.JavaScript.ORDER_ATOMIC);
-  height = "'"+height+"px'";
-  document.getElementById("runArea").style.left=left;
-  document.getElementById("runArea").style.top=top;
-  document.getElementById("iframe_run").style.width=width;
-  document.getElementById("iframe_run").style.height=height;
+  document.getElementById("ifrLeft").value=left;
+  document.getElementById("ifrTop").value=top;
+  document.getElementById("ifrWidth").value=width;
+  document.getElementById("ifrHeight").value=height;
   
   var code = '//document.getElementById("runArea").style.left='+left+';\n//document.getElementById("runArea").style.top='+top+';\n//document.getElementById("iframe_run").style.width='+width+';\n//document.getElementById("iframe_run").style.height='+height+';\n';
   return code;
