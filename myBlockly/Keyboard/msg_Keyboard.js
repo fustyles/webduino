a9d36812c1d17813c6056676f56eb88be81d35cd
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 02:44:47 +0800

    Update msg_Keyboard.js

--- myBlockly/Keyboard/msg_Keyboard.js
+++ myBlockly/Keyboard/msg_Keyboard.js
@@ -4,5 +4,6 @@ Blockly.Msg["KEYBOARD_HUE"] = "280";
 Blockly.Msg.KEYDOWN_LISTENER_SHOW = "Keydown Listener";
 Blockly.Msg.KEYUP_LISTENER_SHOW = "Keyup Listener";
 Blockly.Msg.KEYPRESS_LISTENER_SHOW = "Keypress Listener";
+Blockly.Msg.GET_KEYCODE_SHOW = "Get Keycode";
 Blockly.Msg.KEYDOWN_KEYCODE_SHOW = "Key is";
 Blockly.Msg.KEYPRESS_KEY_SHOW = "Key is";
