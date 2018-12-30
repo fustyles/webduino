0a039a33ac06bac32cc344d622ba0637b33b323a
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 02:55:12 +0800

    Update msg_Keyboard_zh-hant.js

--- myBlockly/Keyboard/msg_Keyboard_zh-hant.js
+++ myBlockly/Keyboard/msg_Keyboard_zh-hant.js
@@ -6,4 +6,4 @@ Blockly.Msg.KEYUP_LISTENER_SHOW = "開始偵測鍵盤放開";
 Blockly.Msg.KEYPRESS_LISTENER_SHOW = "開始偵測鍵盤按壓";
 Blockly.Msg.GET_KEYCODE_SHOW = "取得鍵碼值";
 Blockly.Msg.KEYDOWN_KEYCODE_SHOW = "鍵碼值為";
-Blockly.Msg.KEYPRESS_KEY_SHOW = "鍵碼值為";
+Blockly.Msg.HOTKEY_SHOW = "組合鍵碼值(keydown)為";
