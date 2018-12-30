ed962e4a7b09af20d82ffe164b93abd20154d93f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 08:25:01 +0800

    Update msg_ShowText.js

--- myBlockly/ShowText/msg_ShowText.js
+++ myBlockly/ShowText/msg_ShowText.js
@@ -1,4 +1,7 @@
-﻿Blockly.Msg.SHOWTEXT_SHOW = "Show Text";
+Blockly.Msg["SHOWTEXT"] = "ShowText";
+Blockly.Msg["SHOWTEXT_HUE"] = "280";
+
+Blockly.Msg.SHOWTEXT_SHOW = "Show Text";
 Blockly.Msg.SHOWTEXT_SIZE_SHOW = "size";
 Blockly.Msg.SHOWTEXT_COLOR_SHOW = "color";
 Blockly.Msg.SHOWTEXT_TEXT_SHOW = "Text";
