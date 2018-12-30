cfb75afc6c66f97736cde7831378f700543e7215
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 09:13:06 +0800

    Add files via upload

---
+++ myBlockly/ShowText/msg_ShowText_zh-hans.js
@@ -0,0 +1,9 @@
+Blockly.Msg["SHOWTEXT"] = "顯示文字";
+Blockly.Msg["SHOWTEXT_HUE"] = "280";
+
+Blockly.Msg.SHOWTEXT_SHOW = "顯示文字";
+Blockly.Msg.SHOWTEXT_SIZE_SHOW = "大小";
+Blockly.Msg.SHOWTEXT_COLOR_SHOW = "顏色";
+Blockly.Msg.SHOWTEXT_TEXT_SHOW = "文字";
+Blockly.Msg.LINEBREAK_SHOW = "換行";
+Blockly.Msg.COMMENT_SHOW = "註解";
