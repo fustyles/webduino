b0be471d769efd8a27dfe96213dae72c7130da91
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 08:02:28 +0800

    Update javascript_ShowText.js

--- myBlockly/ShowText/javascript_ShowText.js
+++ myBlockly/ShowText/javascript_ShowText.js
@@ -11,3 +11,13 @@ Blockly.JavaScript['linebreak'] = function (block) {
   var code = "'<br>'";
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['comment'] = function (block) {
+  var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
+  if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
+    text = text.substring(1,text.length-1);
+  if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
+    text = text.substring(1,text.length-1);
+  var code = '//' + text+'\n';
+  return code;
+};
