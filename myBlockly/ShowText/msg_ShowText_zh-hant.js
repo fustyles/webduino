4580c9a35175db1189ce7c6798ba29a79079de71
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 08:02:38 +0800

    Update msg_ShowText_zh-hant.js

--- myBlockly/ShowText/msg_ShowText_zh-hant.js
+++ myBlockly/ShowText/msg_ShowText_zh-hant.js
@@ -3,3 +3,4 @@ Blockly.Msg.SHOWTEXT_SIZE_SHOW = "大小";
 Blockly.Msg.SHOWTEXT_COLOR_SHOW = "顏色";
 Blockly.Msg.SHOWTEXT_TEXT_SHOW = "文字";
 Blockly.Msg.LINEBREAK_SHOW = "換行";
+Blockly.Msg.COMMENT_SHOW = "註解";
