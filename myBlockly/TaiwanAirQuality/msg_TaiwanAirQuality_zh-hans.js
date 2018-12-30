f668baaa47f2cae51dc4e25d962ceb7c6e89c746
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 10:17:36 +0800

    Update msg_TaiwanAirQuality_zh-hans.js

--- myBlockly/TaiwanAirQuality/msg_TaiwanAirQuality_zh-hans.js
+++ myBlockly/TaiwanAirQuality/msg_TaiwanAirQuality_zh-hans.js
@@ -1,4 +1,4 @@
-Blockly.Msg["TAIWANAIRQUALITY"] = "TaiwanAirQuality";
+Blockly.Msg["TAIWANAIRQUALITY"] = "台湾空气品质";
 Blockly.Msg["TAIWANAIRQUALITY_HUE"] = "280";
 
 Blockly.Msg.PM_LINK_SHOW = "取得空气资料";
