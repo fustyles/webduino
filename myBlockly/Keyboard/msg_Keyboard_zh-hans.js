30cd579a8cddb138edf5376640a0749141cf6678
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 02:28:30 +0800

    Update msg_Keyboard_zh-hans.js

--- myBlockly/Keyboard/msg_Keyboard_zh-hans.js
+++ myBlockly/Keyboard/msg_Keyboard_zh-hans.js
@@ -5,4 +5,4 @@ Blockly.Msg.KEYDOWN_LISTENER_SHOW = "开始侦测键盘按下";
 Blockly.Msg.KEYUP_LISTENER_SHOW = "开始侦测键盘放开";
 Blockly.Msg.KEYPRESS_LISTENER_SHOW = "开始侦测键盘按压";
 Blockly.Msg.KEYDOWN_KEYCODE_SHOW = "键码值为";
-Blockly.Msg.KEYPRESS_KEY_SHOW = "键码值(keypress)为";
+Blockly.Msg.KEYPRESS_KEY_SHOW = "键码值为";
