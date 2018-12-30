d2cb480667bbde91ea53f331b20ce43d48e2d3c5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 08:02:26 +0800

    Update javascript_SetScreen.js

--- myBlockly/SetScreen/javascript_SetScreen.js
+++ myBlockly/SetScreen/javascript_SetScreen.js
@@ -11,13 +11,3 @@ Blockly.JavaScript['setscreen'] = function (block) {
   var code = '';
   return code;
 };
-
-Blockly.JavaScript['comment'] = function (block) {
-  var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
-  if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
-    text = text.substring(1,text.length-1);
-  if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
-    text = text.substring(1,text.length-1);
-  var code = '//' + text+'\n';
-  return code;
-};
