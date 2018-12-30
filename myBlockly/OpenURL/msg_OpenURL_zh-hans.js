21ae93bd575b7189330a81674c59b3b57864371d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-30 09:14:56 +0800

    Update msg_OpenURL_zh-hans.js

--- myBlockly/OpenURL/msg_OpenURL_zh-hans.js
+++ myBlockly/OpenURL/msg_OpenURL_zh-hans.js
@@ -1,13 +1,13 @@
-Blockly.Msg["OPENURL"] = "執行網址";
+Blockly.Msg["OPENURL"] = "执行网址";
 Blockly.Msg["OPENURL_HUE"] = "280";
 
-Blockly.Msg.OPEN_SHOW = "開啟連結";
-Blockly.Msg.CLOSE_SHOW = "關閉連結";
-Blockly.Msg.OPENID_SHOW = "代碼";
-Blockly.Msg.CLOSEID_SHOW = "代碼";
-Blockly.Msg.URL_SHOW = "來源網址";
-Blockly.Msg.WIDTH_SHOW = "寬度";
+Blockly.Msg.OPEN_SHOW = "开启连结";
+Blockly.Msg.CLOSE_SHOW = "关闭连结";
+Blockly.Msg.OPENID_SHOW = "代码";
+Blockly.Msg.CLOSEID_SHOW = "代码";
+Blockly.Msg.URL_SHOW = "来源网址";
+Blockly.Msg.WIDTH_SHOW = "宽度";
 Blockly.Msg.HEIGHT_SHOW = "高度";
-Blockly.Msg.LEFT_SHOW = "靠左距離";
-Blockly.Msg.TOP_SHOW = "靠上距離";
-Blockly.Msg.ZINDEX_SHOW = "層次";
+Blockly.Msg.LEFT_SHOW = "靠左距离";
+Blockly.Msg.TOP_SHOW = "靠上距离";
+Blockly.Msg.ZINDEX_SHOW = "层次";
