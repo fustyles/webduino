6dad2e3ff4ca6ed3eaaa33232ead66f15f980def
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 08:26:06 +0800

    Update javascript_Keyboard.js

--- myBlockly/Keyboard/javascript_Keyboard.js
+++ myBlockly/Keyboard/javascript_Keyboard.js
@@ -1,30 +1,13 @@
 Blockly.JavaScript['keydown_listener'] = function (block) {
   var statement = Blockly.JavaScript.statementToCode(block, 'statement');  
-  var code = 'window.addEventListener("keydown", function (event) {\n' + statement + '}, true);\n';
+  var event = block.getFieldValue('event');
+  if (event=="keydown")
+    var code = 'window.addEventListener("keydown", function (event) {\n' + statement + '}, true);\n';
+  else if (event=="keyup")
+    var code = 'window.addEventListener("keyup", function (event) {\n' + statement + '}, true);\n';
+  else if (event=="keypress")
+    var code = 'window.addEventListener("keypress", function (event) {\n' + statement + '}, true);\n';
   return code;
-};
-
-Blockly.JavaScript['keyup_listener'] = function (block) {
-  var statement = Blockly.JavaScript.statementToCode(block, 'statement');  
-  var code = 'window.addEventListener("keyup", function (event) {\n' + statement + '}, true);\n';
-  return code;
-};
-
-Blockly.JavaScript['keypress_listener'] = function (block) {
-  var statement = Blockly.JavaScript.statementToCode(block, 'statement');  
-  var code = 'window.addEventListener("keypress", function (event) {\n' + statement + '}, true);\n';
-  return code;
-};
-
-Blockly.JavaScript['get_keycode'] = function(block) {
-  var code = 'event.keyCode';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['keydown_keycode'] = function(block) {
-  var keycode = block.getFieldValue('keycode'); 
-  var code = 'event.keyCode == ' + keycode;
-  return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['hotkey'] = function(block) {
