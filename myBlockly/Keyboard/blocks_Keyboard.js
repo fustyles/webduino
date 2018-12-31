c2d4fc2c723ee71c452bd0486311df15d437c1f2
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 09:08:26 +0800

    Update blocks_Keyboard.js

--- myBlockly/Keyboard/blocks_Keyboard.js
+++ myBlockly/Keyboard/blocks_Keyboard.js
@@ -1,20 +1,3 @@
-Blockly.Blocks['keydown_listener'] = {
-	init: function() {
-	this.appendDummyInput()
-	    .appendField(Blockly.Msg.KEYBOARD_LISTENER_SHOW)
-	    .appendField(new Blockly.FieldDropdown([
-	      ["keydown","keydown_old"],
-	      ["keyup","keyup_old"],
-	      ["keypress","keypress_old"]
-	      ]), "event");
-	this.appendStatementInput("statement");
-	this.setPreviousStatement(true);
-	this.setNextStatement(true);
-	this.setTooltip('');
-	this.setColour(65);
-	}
-};
-
 Blockly.Blocks['keyboard_listener'] = {
 	init: function() {
 	this.appendDummyInput()
@@ -57,80 +40,6 @@ Blockly.Blocks['get_keycode'] = {
 	this.setColour(65);
   }
 };
-
-Blockly.Blocks['keydown_keycode'] = {
-	init: function() {
-    	this.appendDummyInput()
-		.appendField(Blockly.Msg.KEYCODE_SHOW)
-		.appendField(new Blockly.FieldDropdown([
-			["BackSpace_old","8"],
-			["Tab","9"],
-			["Clear","12"],
-			["Enter","13"],
-			["Shift_L","16"],
-			["Control_L","17"],
-			["Alt_L","18"],
-			["Pause","19"],
-			["Caps_Lock","20"],
-			["Escape","27"],
-			["space","32"],
-			["Prior","33"],
-			["Next","34"],
-			["End","35"],
-			["Home","36"],
-			["Left","37"],
-			["Up","38"],
-			["Right","39"],
-			["Down","40"],
-			["Select","41"],
-			["Print","42"],
-			["Execute","43"],
-			["Insert","45"],
-			["Delete","46"],
-			["Help","47"],
-			["0","48"],
-			["1","49"],
-			["2","50"],
-			["3","51"],
-			["4","52"],
-			["5","53"],
-			["6","54"],
-			["7","55"],
-			["8","56"],
-			["9","57"],
-			["A(a)","65"],
-			["B(b)","66"],
-			["C(c)","67"],
-			["D(d)","68"],
-			["E(e)","69"],
-			["F(f)","70"],
-			["G(g)","71"],
-			["H(h)","72"],
-			["I(i)","73"],
-			["J(j)","74"],
-			["K(k)","75"],
-			["L(l)","76"],
-			["M(m)","77"],
-			["N(n)","78"],
-			["O(o)","79"],
-			["P(p)","80"],
-			["Q(q)","81"],
-			["R(r)","82"],
-			["S(s)","83"],
-			["T(t)","84"],
-			["U(u)","85"],
-			["V(v)","86"],
-			["W(w)","87"],
-			["X(x)","88"],
-			["Y(y)","89"],
-			["Z(z)","90"]
-			]), "keycode");
-	this.setInputsInline(true);
-	this.setOutput(true, null); 
-	this.setColour(65);
-  }
-};
-
 
 Blockly.Blocks['keyboard_keycode'] = {
 	init: function() {
