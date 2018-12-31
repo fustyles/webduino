88f03bb19b56cc6dd9c2e5e2c662af84f4fb777d
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 08:52:40 +0800

    Update msg_Keyboard_zh-hans.js

--- myBlockly/Keyboard/msg_Keyboard_zh-hans.js
+++ myBlockly/Keyboard/msg_Keyboard_zh-hans.js
@@ -1,7 +1,8 @@
 Blockly.Msg["KEYBOARD"] = "键盘事件"
 Blockly.Msg["KEYBOARD_HUE"] = "280";
 
-Blockly.Msg.KEYBOARD_LISTENER_SHOW = "开始侦测键盘";
+Blockly.Msg.KEYBOARD_LISTENER_SHOW = "开始监听键盘";
+Blockly.Msg.STOP_KEYBOARD_LISTENER_SHOW = "停止监听键盘";
 Blockly.Msg.GET_KEYCODE_SHOW = "取得键码值";
 Blockly.Msg.KEYCODE_SHOW = "键码值为";
 Blockly.Msg.HOTKEY_SHOW = "组合键码值(keydown)为";
