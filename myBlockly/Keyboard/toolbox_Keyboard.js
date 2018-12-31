5f415906ae428e1bd7901069c2f3929463907d98
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 08:51:10 +0800

    Update toolbox_Keyboard.js

--- myBlockly/Keyboard/toolbox_Keyboard.js
+++ myBlockly/Keyboard/toolbox_Keyboard.js
@@ -1,11 +1,13 @@
 var catKeyboard = '<xml>'+
 '<category name="%{BKY_KEYBOARD}" colour="%{BKY_KEYBOARD_HUE}">'+
-'  <block type="keydown_listener">'+
+'  <block type="keyboard_listener">'+
 '  </block>'+
+'  <block type="stop_keyboard_listener">'+
+'  </block>'+    
 '  <block type="get_keycode">'+
 '  </block>'+    
-'  <block type="keydown_keycode">'+
-'  </block>'+
+'  <block type="keyboard_keycode">'+
+'  </block>'+    
 '  <block type="hotkey">'+
 '  </block>'+    
 '</category>'+
