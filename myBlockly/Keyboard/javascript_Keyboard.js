02d8ae2496f283ca910f7eeece063d08ccc89b5f
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 09:08:42 +0800

    Update javascript_Keyboard.js

--- myBlockly/Keyboard/javascript_Keyboard.js
+++ myBlockly/Keyboard/javascript_Keyboard.js
@@ -1,15 +1,3 @@
-Blockly.JavaScript['keydown_listener'] = function (block) {
-  var statement = Blockly.JavaScript.statementToCode(block, 'statement');  
-  var event = block.getFieldValue('event');
-  if (event=="keydown")
-    var code = 'window.addEventListener("keydown", function (event) {\n' + statement + '}, true);\n';
-  else if (event=="keyup")
-    var code = 'window.addEventListener("keyup", function (event) {\n' + statement + '}, true);\n';
-  else if (event=="keypress")
-    var code = 'window.addEventListener("keypress", function (event) {\n' + statement + '}, true);\n';
-  return code;
-};
-
 Blockly.JavaScript['keyboard_listener'] = function (block) {
   var statement = Blockly.JavaScript.statementToCode(block, 'statement');  
   var event = block.getFieldValue('event');
@@ -35,12 +23,6 @@ Blockly.JavaScript['stop_keyboard_listener'] = function (block) {
 
 Blockly.JavaScript['get_keycode'] = function(block) {
   var code = 'event.keyCode';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['keydown_keycode'] = function(block) {
-  var keycode = block.getFieldValue('keycode'); 
-  var code = 'event.keyCode == ' + keycode;
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
