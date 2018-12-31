4454c6167f64c5a4a16278a667f114f59db329d5
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 09:36:59 +0800

    Update showtext.js

--- myBlockly/ShowText/showtext.js
+++ myBlockly/ShowText/showtext.js
@@ -1,5 +1,5 @@
   function showtext(input_size, input_color, input_text){
-    document.getElementById("showText").style.fontSize = input_size;
+    document.getElementById("showText").style.fontSize = input_size+"px";
     document.getElementById("showText").style.color = input_color;
     document.getElementById("showText").innerHTML = input_text;
   }
