9098e92eb24af441425b270a5f6b539dbfe05b65
fustyles <34397289+fustyles@users.noreply.github.com>
2018-12-31 09:36:28 +0800

    Update javascript_ShowText.js

--- myBlockly/ShowText/javascript_ShowText.js
+++ myBlockly/ShowText/javascript_ShowText.js
@@ -3,7 +3,7 @@ Blockly.JavaScript['showtext'] = function (block) {
   size = "'"+size+"px'";
   var color = Blockly.JavaScript.valueToCode(block, 'color', Blockly.JavaScript.ORDER_ATOMIC);
   var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'document.getElementById("showText").style.fontSize='+size+';\ndocument.getElementById("showText").style.color='+color+';\ndocument.getElementById("showText").innerHTML='+text+';\n';
+  var code = 'showtext('+size+', '+color+', '+text+');\n';
   return code;
 };
 
