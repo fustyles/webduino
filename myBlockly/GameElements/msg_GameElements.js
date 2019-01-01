5ee24b1681710a5be7531741889cf50270a79c43
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-01 17:46:05 +0800

    Update msg_GameElements.js

--- myBlockly/GameElements/msg_GameElements.js
+++ myBlockly/GameElements/msg_GameElements.js
@@ -102,3 +102,4 @@ Blockly.Msg.IMAGE_RESIZE_SHOW = "Resize";
 Blockly.Msg.PERCENTAGE_SHOW = "%";
 Blockly.Msg.IMAGE_ONCLICK_DO_SHOW = "is Clicked ";
 Blockly.Msg.BOUNDARY_COLLISION_DO_SHOW = "If collision with sides of screen";
+Blockly.TABLE_TD_IMAGE_MOVE_SHOW = "Move image to";
