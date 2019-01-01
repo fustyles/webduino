4e2720af3a7b2912a6836d0344bf61189825be71
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-01 09:47:41 +0800

    Update javascript_GameElements.js

--- myBlockly/GameElements/javascript_GameElements.js
+++ myBlockly/GameElements/javascript_GameElements.js
@@ -371,7 +371,7 @@ Blockly.JavaScript['function_return'] = function (block) {
 
 Blockly.JavaScript['time_delay'] = function (block) {
   var seconds = Blockly.JavaScript.valueToCode(block, 'seconds', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'await sleep(' + seconds + ');\n';
+  var code = 'await delay(' + seconds + ');\n';
   return code;
 };
 
