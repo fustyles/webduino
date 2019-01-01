71527c83158507bb47533b8f9b7003605d44b8ff
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-01 16:40:08 +0800

    Update msg_SetScreen_zh-hans.js

--- myBlockly/SetScreen/msg_SetScreen_zh-hans.js
+++ myBlockly/SetScreen/msg_SetScreen_zh-hans.js
@@ -6,3 +6,4 @@ Blockly.Msg.SETSCREEN_LEFT_SHOW = "靠左";
 Blockly.Msg.SETSCREEN_TOP_SHOW = "靠上";
 Blockly.Msg.SETSCREEN_WIDTH_SHOW = "宽度";
 Blockly.Msg.SETSCREEN_HEIGHT_SHOW = "高度";
+Blockly.Msg.BODY_SET_SHOW = "设定执行视窗背景";
