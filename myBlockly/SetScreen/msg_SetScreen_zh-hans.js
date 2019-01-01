7a61b1c03a04ea18245f443db9236109552aad74
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-01 12:53:39 +0800

    Update msg_SetScreen_zh-hans.js

--- myBlockly/SetScreen/msg_SetScreen_zh-hans.js
+++ myBlockly/SetScreen/msg_SetScreen_zh-hans.js
@@ -1,7 +1,7 @@
 Blockly.Msg["SETSCREEN"] = "设定视窗";
 Blockly.Msg["SETSCREEN_HUE"] = "280";
 
-Blockly.Msg.SETSCREEN_SHOW = "设定视窗";
+Blockly.Msg.SETSCREEN_SHOW = "设定执行视窗";
 Blockly.Msg.SETSCREEN_LEFT_SHOW = "靠左";
 Blockly.Msg.SETSCREEN_TOP_SHOW = "靠上";
 Blockly.Msg.SETSCREEN_WIDTH_SHOW = "宽度";
