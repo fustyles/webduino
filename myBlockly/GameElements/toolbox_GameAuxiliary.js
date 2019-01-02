58e12a5eee38f22d9a59122840ba9c72c2890374
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-02 23:03:50 +0800

    Update toolbox_GameAuxiliary.js

--- myBlockly/GameElements/toolbox_GameAuxiliary.js
+++ myBlockly/GameElements/toolbox_GameAuxiliary.js
@@ -58,4 +58,46 @@ var catGameAuxiliary = '<xml><category name="%{BKY_GAMEAUXILIARY}" colour="%{BKY
 '  </block>'+  
 '  <block type="music_delete">'+ 
 '  </block>'+   
+'  <block type="button_create">'+ 
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="left_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="top_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">100</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="width_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">60</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="height_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">30</field>'+
+'      </block>'+
+'    </value>'+    
+'    <value name="value_">'+
+'      <block type="text">'+
+'        <field name="TEXT">button</field>'+
+'      </block>'+
+'    </value>'+       
+'    <value name="zindex_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="display_">'+
+'      <block type="logic_boolean">'+
+'        <field name="BOOL">TRUE</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+
 ' </category></xml>';
