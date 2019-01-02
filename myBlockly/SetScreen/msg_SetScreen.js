00e70393c57124dd789bcefdcc2fc1e259b4d684
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-02 22:21:20 +0800

    Update msg_SetScreen.js

--- myBlockly/SetScreen/msg_SetScreen.js
+++ myBlockly/SetScreen/msg_SetScreen.js
@@ -1,7 +1,7 @@
 Blockly.Msg["SETSCREEN"] = "SetScreen";
 Blockly.Msg["SETSCREEN_HUE"] = "280";
 
-Blockly.Msg.SETSCREEN_SHOW = "Set R Screen";
+Blockly.Msg.SETSCREEN_SHOW = "Set Run Screen";
 Blockly.Msg.SETSCREEN_LEFT_SHOW = "left";
 Blockly.Msg.SETSCREEN_TOP_SHOW = "top";
 Blockly.Msg.SETSCREEN_WIDTH_SHOW = "width";
