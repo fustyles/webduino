0fe66a965929dd5882b6a09c903d20b8f458e04f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 19:43:55 +0800

    Update msg_Instruction.js

--- myBlockly/Instruction/msg_Instruction.js
+++ myBlockly/Instruction/msg_Instruction.js
@@ -1,6 +1,7 @@
-Blockly.Msg["INSTRUCTION"] = "Instruction"
+Blockly.Msg["INSTRUCTION"] = "Instruction";
 Blockly.Msg["INSTRUCTION_HUE"] = "280";
 
+Blockly.Msg.INSTRUCTION_SHOW = "Instruction";
 Blockly.Msg.INSTRUCTION_GET_SHOW = "Get";
 Blockly.Msg.INSTRUCTION_SHOW = "Instruction";
 Blockly.Msg.INSTRUCTION_GET_ARRAY_SHOW = "Get Array";
