be3f7a9f8a1f4b6b5e3fea9c8ff55766615fbe63
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 18:25:18 +0800

    Update blocks_SetWindow.js

--- myBlockly/SetWindow/blocks_SetWindow.js
+++ myBlockly/SetWindow/blocks_SetWindow.js
@@ -1,3 +1,26 @@
+Blockly.Blocks['setscreen'] = {
+  init: function() {
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.SETWINDOW_SHOW);  
+  this.appendValueInput("left")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.SETWINDOW_LEFT_SHOW); 
+  this.appendValueInput("top")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.SETWINDOW_TOP_SHOW);   
+  this.appendValueInput("width")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.SETWINDOW_WIDTH_SHOW); 
+  this.appendValueInput("height")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.SETWINDOW_HEIGHT_SHOW);   
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(90);
+  }
+}; 
+
 Blockly.Blocks['setwindow'] = {
   init: function() {
   this.appendDummyInput()
