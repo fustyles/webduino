88b386c2e9f41715b360e83eb0a1b3ec5734de42
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 16:42:11 +0800

    Add files via upload

---
+++ myBlockly/GameElements/toolbox_GameForm.js
@@ -0,0 +1,77 @@
+var catGameForm = '<xml><category name="%{BKY_GAMEAUXILIARY}" colour="%{BKY_GAMEAUXILIARY_HUE}">'+
+'  <block type="button_create">'+ 
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="left_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="top_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">100</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="width_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">60</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="height_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">30</field>'+
+'      </block>'+
+'    </value>'+    
+'    <value name="value_">'+
+'      <block type="text">'+
+'        <field name="TEXT">button</field>'+
+'      </block>'+
+'    </value>'+       
+'    <value name="zindex_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="display_">'+
+'      <block type="logic_boolean">'+
+'        <field name="BOOL">TRUE</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+
+'  <block type="button_set">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="value_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+    
+'  </block>'+  
+'  <block type="button_get">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+     
+'  <block type="button_delete">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+    
+'  <block type="button_onclick_do">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+         
+'  </block>'+           
+' </category></xml>';
