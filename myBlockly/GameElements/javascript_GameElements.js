5b4174067c1887e5735df3a4217d165732b3a817
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 22:03:04 +0800

    Update javascript_GameElements.js

--- myBlockly/GameElements/javascript_GameElements.js
+++ myBlockly/GameElements/javascript_GameElements.js
@@ -428,6 +428,12 @@ Blockly.JavaScript['rotatez_degrees'] = function (block) {
   return code;
 };
 
+Blockly.JavaScript['rotateㄗ_degrees'] = function (block) {
+  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'if (image_get('+value_id_+',"rotateY")<=90) \n\timage_set('+value_id_+',"rotateY",180); else \n\timage_set('+value_id_+',"rotateY",0);\n';
+  return code;
+};
+
 Blockly.JavaScript['button_create'] = function (block) {
   var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_left_ = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
