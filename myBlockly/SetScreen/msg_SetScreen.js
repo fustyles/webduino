d6b323fb1cd283c59b8d94d3bce2d8c38afc4dfe
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 17:02:44 +0800

    Update msg_SetScreen.js

--- myBlockly/SetScreen/msg_SetScreen.js
+++ myBlockly/SetScreen/msg_SetScreen.js
@@ -6,3 +6,4 @@ Blockly.Msg.SETSCREEN_LEFT_SHOW = "left";
 Blockly.Msg.SETSCREEN_TOP_SHOW = "top";
 Blockly.Msg.SETSCREEN_WIDTH_SHOW = "width";
 Blockly.Msg.SETSCREEN_HEIGHT_SHOW = "height";
+Blockly.Msg.SCREEN_RELOAD_SHOW = "Reload Page";
