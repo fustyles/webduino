aff590a6156e7ee01a48218951a2f3c7ae1ee6f8
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 17:58:33 +0800

    Delete setscreen.js

--- myBlockly/SetScreen/setscreen.js
+++
@@ -1,6 +0,0 @@
-   function body_set(input_property,input_value) {
-    if (input_property=="backgroundColor")
-      document.body.style.backgroundColor = input_value;
-    else if (input_property=="backgroundImage") 
-      document.body.style.backgroundImage = "url('"+input_value+"')";
-  }
