4579c1d525d923a6cdb69f0d5ba875e7403d291f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-03 19:41:33 +0800

    Update msg_Instruction_zh-hans.js

--- myBlockly/Instruction/msg_Instruction_zh-hans.js
+++ myBlockly/Instruction/msg_Instruction_zh-hans.js
@@ -3,3 +3,7 @@ Blockly.Msg["INSTRUCTION_HUE"] = "280";
 
 Blockly.Msg.INSTRUCTION_GET_SHOW = "取得";
 Blockly.Msg.INSTRUCTION_SHOW = "指令";
+Blockly.Msg.INSTRUCTION_GET_ARRAY_SHOW = "取得阵列";
+Blockly.Msg.INSTRUCTION_DECLARE_ARRAY_SHOW = "宣告阵列";
+Blockly.Msg.INSTRUCTION_SET_VARIABLE_SHOW = "设定变数";
+Blockly.Msg.INSTRUCTION_DECLARE_VARIABLE_SHOW = "宣告变数";
