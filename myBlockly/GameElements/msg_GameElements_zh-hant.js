de321b37e9f037d09d3acc97e2e0dafcb761a540
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-04 18:19:18 +0800

    Update msg_GameElements_zh-hant.js

--- myBlockly/GameElements/msg_GameElements_zh-hant.js
+++ myBlockly/GameElements/msg_GameElements_zh-hant.js
@@ -114,4 +114,5 @@ Blockly.Msg.BUTTON_DELETE_SHOW = "刪除按鈕";
 Blockly.Msg.BUTTON_ONCLICK_SHOW = "按鈕點擊後執行";
 Blockly.Msg.BUTTON_GET_SHOW = "取得按鈕";
 Blockly.Msg.ROTATEZ_DEGREES_SHOW = "旋轉角度";
-Blockly.Msg.ROTATEY_DEGREES_SHOW = "左右翻轉";
+Blockly.Msg.ROTATE_VERTICAL_FLIP_SHOW = "上下翻轉";
+Blockly.Msg.ROTATE_HORIZONTAL_FLIP_SHOW = "左右翻轉";
