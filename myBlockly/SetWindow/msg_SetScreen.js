00314709cd7e2042b42f1f276430f23760aef203
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-07 00:22:49 +0800

    Delete msg_SetScreen.js

--- myBlockly/SetWindow/msg_SetScreen.js
+++
@@ -1,9 +0,0 @@
-Blockly.Msg["SETWINDOW"] = "SetWindow";
-Blockly.Msg["SETWINDOW_HUE"] = "280";
-
-Blockly.Msg.SETWINDOW_SHOW = "Set Run Window";
-Blockly.Msg.SETWINDOW_LEFT_SHOW = "left";
-Blockly.Msg.SETWINDOW_TOP_SHOW = "top";
-Blockly.Msg.SETWINDOW_WIDTH_SHOW = "width";
-Blockly.Msg.SETWINDOW_HEIGHT_SHOW = "height";
-Blockly.Msg.WINDOW_RELOAD_SHOW = "Reload Page";
