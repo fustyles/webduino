6d1de94d97d6b6f62a1482f5cf020d3d1ba9b710
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-08 22:26:05 +0800

    Update javascript_GameElements.js

--- myBlockly/GameElements/javascript_GameElements.js
+++ myBlockly/GameElements/javascript_GameElements.js
@@ -532,8 +532,7 @@ Blockly.JavaScript['call_async_function'] = function (block) {
 };
 
 Blockly.JavaScript['transform_async_function'] = function (block) {
-  Blockly.getMainWorkspace('blocklyDiv').getBlocksByType('procedures_defnoreturn').setPreviousStatement(true, null);
-  Blockly.getMainWorkspace('blocklyDiv').getBlocksByType('procedures_defnoreturn').setNextStatement(true, null);
+  Blockly.getMainWorkspace('blocklyDiv').getBlocksByType('procedures_defnoreturn').setPreviousStatement(true, json['previousStatement']);
   var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
   var code = statements_do_.replace(/function/g,"async function").replace(/async async/g,"async");
   return code;
