973d5e027045b49ef7735eb0c958da06967a2109
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-08 21:25:55 +0800

    Update javascript_GameElements.js

--- myBlockly/GameElements/javascript_GameElements.js
+++ myBlockly/GameElements/javascript_GameElements.js
@@ -530,3 +530,9 @@ Blockly.JavaScript['call_async_function'] = function (block) {
   var code = value_name_ + '('+value_parameter_+');\n';
   return code;
 };
+
+Blockly.JavaScript['transform_async_function'] = function (block) {
+  var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
+  var code = statements_do_.replace(/function/g,"async function").replace(/async async/g,"async");
+  return code;
+};
