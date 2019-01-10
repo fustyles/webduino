fdcc9c045e0e86bc9e178843cb9fb127e2d6178d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-10 20:05:10 +0800

    Update msg_Instruction_zh-hant.js

--- myBlockly/Instruction/msg_Instruction_zh-hant.js
+++ myBlockly/Instruction/msg_Instruction_zh-hant.js
@@ -5,6 +5,5 @@ Blockly.Msg.INSTRUCTION_SHOW = "指令";
 Blockly.Msg.INSTRUCTION_GET_SHOW = "取得";
 Blockly.Msg.INSTRUCTION_SHOW = "指令";
 Blockly.Msg.INSTRUCTION_GET_ARRAY_SHOW = "取得陣列";
-Blockly.Msg.INSTRUCTION_DECLARE_ARRAY_SHOW = "宣告陣列";
 Blockly.Msg.INSTRUCTION_SET_VARIABLE_SHOW = "設定變數";
 Blockly.Msg.INSTRUCTION_DECLARE_VARIABLE_SHOW = "宣告變數";
