0b0a7912e03d845a1e1a4385c6038eae1e4d7438
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-11 19:25:18 +0800

    Update toolbox_GameElements.js

--- myBlockly/GameElements/toolbox_GameElements.js
+++ myBlockly/GameElements/toolbox_GameElements.js
@@ -39,7 +39,9 @@ var catGameElements = '<xml><category name="%{BKY_GAMEELEMENTS}" colour="%{BKY_G
 '    </value>'+
 '    <value name="parameter_">'+  
 '    </value>'+     
-'  </block>'+      
+'  </block>'+ 
+'  <block type="transform_async_function">'+      
+'  </block>'+ 
 '  <block type="text_to_number">'+
 '    <value name="value_text_">'+
 '    </value>'+    
@@ -150,7 +152,24 @@ var catGameElements = '<xml><category name="%{BKY_GAMEELEMENTS}" colour="%{BKY_G
 '    </value>'+    
 '  </block>'+  
 '  <block type="image_onclick_listener">'+
-'  </block>'+     
+'  </block>'+  
+'  <block type="table_td_onclick_do">'+  
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+         
+'    <value name="x_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="y_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+  
+'  </block>'+   
 '  <block type="table_get">'+   
 '    <value name="id_">'+
 '      <block type="text">'+
@@ -409,6 +428,204 @@ var catGameElements = '<xml><category name="%{BKY_GAMEELEMENTS}" colour="%{BKY_G
 '      </block>'+
 '    </value>'+    
 '  </block>'+
+'  <block type="image_create">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+    
+'    <value name="url_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="width_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">200</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="height_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">300</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="left_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="top_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+  
+'    <value name="zindex_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="display_">'+
+'      <block type="logic_boolean">'+
+'        <field name="BOOL">TRUE</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+
+'  <block type="image_boundary">'+
+'    <value name="left_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">500</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="top_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">500</field>'+
+'      </block>'+
+'    </value>'+    
+'  </block>'+  
+'  <block type="image_sys_get">'+
+'  </block>'+      
+'  <block type="image_collision">'+
+'    <value name="id1_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="id2_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+       
+'  <block type="image_set">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="value_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+    
+'  </block>'+
+'  <block type="image_get">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'  </block>'+
+'  <block type="image_onclick_listener">'+
+'  </block>'+   
+'  <block type="image_onclick_do">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+ 
+'  <block type="image_onclick_get">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+  
+'  <block type="image_boundary_collision">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+  
+'  </block>'+   
+'  <block type="image_boundary_collision_do">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+  
+'  <block type="image_delete">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'  </block>'+
+'  <block type="image_move">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="step_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">10</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+      
+'  <block type="move_to_mouse">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+     
+'  <block type="move_to_coordinate">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="left_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="top_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+      
+'  </block>'+        
+'  <block type="image_resize">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="percentage_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">100</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+      
+'  <block type="rotatez_degrees">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+   
+'    <value name="degrees_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">30</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+   
+'  <block type="rotate_vertical_flip">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+       
+'  </block>'+ 
+'  <block type="rotate_horizontal_flip">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+       
+'  </block>'+      
 '  <block type="canvas_create">'+ 
 '    <value name="id_">'+
 '      <block type="text">'+
@@ -680,204 +897,6 @@ var catGameElements = '<xml><category name="%{BKY_GAMEELEMENTS}" colour="%{BKY_G
 '      </block>'+
 '    </value>'+      
 '  </block>'+  
-'  <block type="image_create">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+    
-'    <value name="url_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="width_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">200</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="height_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">300</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="left_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="top_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+  
-'    <value name="zindex_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="display_">'+
-'      <block type="logic_boolean">'+
-'        <field name="BOOL">TRUE</field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+
-'  <block type="image_boundary">'+
-'    <value name="left_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">500</field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="top_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">500</field>'+
-'      </block>'+
-'    </value>'+    
-'  </block>'+  
-'  <block type="image_sys_get">'+
-'  </block>'+      
-'  <block type="image_collision">'+
-'    <value name="id1_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="id2_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+       
-'  <block type="image_set">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="value_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+    
-'  </block>'+
-'  <block type="image_get">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'  </block>'+
-'  <block type="image_onclick_listener">'+
-'  </block>'+   
-'  <block type="image_onclick_do">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+ 
-'  <block type="image_onclick_get">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+  
-'  <block type="image_boundary_collision">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+  
-'  </block>'+   
-'  <block type="image_boundary_collision_do">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+  
-'  <block type="image_delete">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'  </block>'+
-'  <block type="image_move">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="step_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">10</field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+      
-'  <block type="move_to_mouse">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+     
-'  <block type="move_to_coordinate">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="left_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+     
-'    <value name="top_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+      
-'  </block>'+        
-'  <block type="image_resize">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+ 
-'    <value name="percentage_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">100</field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+      
-'  <block type="rotatez_degrees">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+   
-'    <value name="degrees_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">30</field>'+
-'      </block>'+
-'    </value>'+     
-'  </block>'+   
-'  <block type="rotate_vertical_flip">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+       
-'  </block>'+ 
-'  <block type="rotate_horizontal_flip">'+
-'    <value name="id_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+       
-'  </block>'+      
 '  <block type="button_create">'+ 
 '    <value name="id_">'+
 '      <block type="text">'+
