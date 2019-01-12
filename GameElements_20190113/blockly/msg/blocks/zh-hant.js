40af87cbf782ed6615c9c15f250977ac1670d45e
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-13 00:33:18 +0800

    Update zh-hant.js

--- GameElements_20190113/blockly/msg/blocks/zh-hant.js
+++ GameElements_20190113/blockly/msg/blocks/zh-hant.js
@@ -111,3 +111,4 @@ Blockly.Msg.CALL_ASYNC_FUNCTION_SHOW = "執行 async function ";
 Blockly.Msg.TRANSFORM_ASYNC_FUNCTION_SHOW = "轉換成 async function";
 Blockly.Msg.TABLE_TD_ONCLICK_DO_SHOW = "儲存格點擊後執行";
 Blockly.Msg.OPACITY_SHOW = "透明度";
+Blockly.Msg.SETWINDOW_SHOW = "設定執行視窗";
