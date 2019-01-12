a761e8d7b9b0a09b2b3e5f7c7021d9252fbb254f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-12 21:01:51 +0800

    Update msg_GameElements_zh-hans.js

--- myBlockly/GameElements/msg_GameElements_zh-hans.js
+++ myBlockly/GameElements/msg_GameElements_zh-hans.js
@@ -123,3 +123,4 @@ Blockly.Msg.ASYNC_FUNCTION_SHOW = "async function ";
 Blockly.Msg.CALL_ASYNC_FUNCTION_SHOW = "执行 async function ";
 Blockly.Msg.TRANSFORM_ASYNC_FUNCTION_SHOW = "转换成 async function";
 Blockly.Msg.TABLE_TD_ONCLICK_DO_SHOW = "储存格点击后执行";
+Blockly.Msg.OPACITY_SHOW = "透明度";
