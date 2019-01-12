00fdf8402f694c49da6c6a8133c3e0088e717c09
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-12 18:31:07 +0800

    Update gameelements.js

--- GameElements_20190111/gameelements.js
+++ GameElements_20190111/gameelements.js
@@ -40,6 +40,8 @@
           td.style.width = input_width + 'px';
           td.style.height = input_height + 'px';
           td.setAttribute("onclick", "javascript:image_onclickid_set(this);");
+          td.setAttribute("ondrop","javascript:var obj=document.getElementById(event.dataTransfer.getData('text'));obj.style.position='static';obj.style.left=null;obj.style.top=null;event.preventDefault();event.target.appendChild(obj);");
+          td.setAttribute("ondragover","javascript:event.preventDefault();"); 
         }
        }
       document.body.appendChild(tbl);
@@ -280,6 +282,8 @@
       img.style.width = input_width + 'px';
       img.style.height = input_height + 'px';
       img.setAttribute("onclick", "javascript:image_onclickid_set(this);");
+      img.draggable="true";
+      img.setAttribute("ondragstart", "javascript:event.dataTransfer.setData('text',event.target.id);");
       document.getElementById("gametable_td_"+input_id+"_"+input_y+"_"+input_x).appendChild(img);
     }
   }
@@ -292,6 +296,8 @@
         img.style.width = document.getElementById("gameimg_"+input_img_id).style.width;
         img.style.height = document.getElementById("gameimg_"+input_img_id).style.height;
         img.setAttribute("onclick", "javascript:image_onclickid_set(this);");
+        img.draggable="true";
+        img.setAttribute("ondragstart", "javascript:event.dataTransfer.setData('text',event.target.id);");
         document.getElementById("gameimg_"+input_img_id).parentNode.removeChild(document.getElementById("gameimg_"+input_img_id));
         img.id = "gameimg_"+input_img_id;
         document.getElementById("gametable_td_"+input_id+"_"+input_y+"_"+input_x).appendChild(img);
@@ -495,6 +501,8 @@
     else
       img.style.display = "block";
     img.setAttribute("onclick", "javascript:image_onclickid_set(this);");
+    img.draggable="true";
+    img.setAttribute("ondragstart", "javascript:event.dataTransfer.setData('text',event.target.id);");
     document.body.appendChild(img);
   }
   
