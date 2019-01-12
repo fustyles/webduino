7780ba438cd4769be8cd05d5e41e3faed260e4f5
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-12 18:19:39 +0800

    Update gameelements.js

--- myBlockly/GameElements/gameelements.js
+++ myBlockly/GameElements/gameelements.js
@@ -35,7 +35,7 @@
           td.style.width = input_width + 'px';
           td.style.height = input_height + 'px';
           td.setAttribute("onclick", "javascript:image_onclickid_set(this);");
-          td.setAttribute("ondrop","javascript:event.preventDefault();var obj=document.getElementById(event.dataTransfer.getData('text'));event.target.appendChild(obj);obj.style.left=null;obj.style.top=null;");
+          td.setAttribute("ondrop","javascript:obj.style.left=null;obj.style.top=null;event.preventDefault();var obj=document.getElementById(event.dataTransfer.getData('text'));event.target.appendChild(obj);");
           td.setAttribute("ondragover","javascript:event.preventDefault();"); 
         }
        }
