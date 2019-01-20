362847df197922ab7de814003cc041a9ac44d76d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-20 20:04:02 +0800

    Update blocks_SetWindow.js

--- myBlockly/SetWindow/blocks_SetWindow.js
+++ myBlockly/SetWindow/blocks_SetWindow.js
@@ -72,7 +72,7 @@ Blockly.Blocks['window_event'] = {
   init: function () {
   this.appendDummyInput()  
       .appendField(Blockly.Msg.WINDOW_EVENT_SHOW)
-      .appendField(new Blockly.FieldDropdown([["blur","blur"],["focus","focus"],["change","change"],["mouseover","mouseover"],["mouseout","mouseout"],["mousemove","mousemove"],["mousedown","mousedown"],["mouseup","mouseup"],["click","click"],["dblclick","dblclick"],["keydown","keydown"],["keyup","keyup"],["keypress","keypress"],["submit","submit"],["load","load"],["unload","unload"]]), "property_");
+      .appendField(new Blockly.FieldDropdown([["blur","blur"],["focus","focus"],["change","change"],["mouseover","mouseover"],["mouseout","mouseout"],["mousemove","mousemove"],["mousedown","mousedown"],["mouseup","mouseup"],["click","click"],["dblclick","dblclick"],["keydown","keydown"],["keyup","keyup"],["keypress","keypress"],["submit","submit"],["load","load"],["unload","unload"]]), "event");
   this.setInputsInline(true);
   this.appendStatementInput("do_");
   this.setPreviousStatement(true);
