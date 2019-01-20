5f8842e415568008095fe3a32ff558dcbf5c2ca0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-20 19:58:28 +0800

    Update msg_SetWindow_zh-hans.js

--- myBlockly/SetWindow/msg_SetWindow_zh-hans.js
+++ myBlockly/SetWindow/msg_SetWindow_zh-hans.js
@@ -7,3 +7,4 @@ Blockly.Msg.SETWINDOW_TOP_SHOW = "靠上";
 Blockly.Msg.SETWINDOW_WIDTH_SHOW = "宽度";
 Blockly.Msg.SETWINDOW_HEIGHT_SHOW = "高度";
 Blockly.Msg.WINDOW_RELOAD_SHOW = "视窗重新载入网页";
+Blockly.Msg.WINDOW_EVENT_SHOW = "视窗监听到事件后执行";
