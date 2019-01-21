1988f818ee3d2ec0b2965c084aa52d40a50fb57d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-21 10:18:14 +0800

    Update javascript_SetWindow.js

--- myBlockly/SetWindow/javascript_SetWindow.js
+++ myBlockly/SetWindow/javascript_SetWindow.js
@@ -13,12 +13,12 @@ Blockly.JavaScript['setwindow'] = function (block) {
   var top = Blockly.JavaScript.valueToCode(block, 'top', Blockly.JavaScript.ORDER_ATOMIC);
   var width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
   var height = Blockly.JavaScript.valueToCode(block, 'height', Blockly.JavaScript.ORDER_ATOMIC);
+  var overflow = Blockly.JavaScript.valueToCode(block, 'overflow', Blockly.JavaScript.ORDER_ATOMIC);
   document.getElementById("ifrLeft").value=left;
   document.getElementById("ifrTop").value=top;
   document.getElementById("ifrWidth").value=width;
   document.getElementById("ifrHeight").value=height;
-  
-  var code = '';
+  var code = 'body_set("overflow",' + overflow + ');\n';
   return code;
 };
 
