104ad9df1dc8c928b0ba5bffdb5b9465e6d949b2
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-21 18:54:13 +0800

    Update toolbox_GameTable.js

--- myBlockly/GameElements/toolbox_GameTable.js
+++ myBlockly/GameElements/toolbox_GameTable.js
@@ -170,7 +170,29 @@ var catGameTable = '<xml><category name="%{BKY_GAMETABLE}" colour="%{BKY_GAMETAB
 '        <field name="NUM">0</field>'+
 '      </block>'+
 '    </value>'+  
-'  </block>'+    
+'  </block>'+   
+'  <block type="table_td_canvas_move">'+  
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="canvasid_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+      
+'    <value name="x_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="y_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+  
+'  </block>'+     
 '  <block type="table_td_copy_move">'+  
 '    <value name="id_">'+
 '      <block type="text">'+
