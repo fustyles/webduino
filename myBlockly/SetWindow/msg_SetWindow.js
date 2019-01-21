37697e03456f739c6fdb821dcc2f864840413a61
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-21 10:22:34 +0800

    Update msg_SetWindow.js

--- myBlockly/SetWindow/msg_SetWindow.js
+++ myBlockly/SetWindow/msg_SetWindow.js
@@ -6,5 +6,6 @@ Blockly.Msg.SETWINDOW_LEFT_SHOW = "left";
 Blockly.Msg.SETWINDOW_TOP_SHOW = "top";
 Blockly.Msg.SETWINDOW_WIDTH_SHOW = "width";
 Blockly.Msg.SETWINDOW_HEIGHT_SHOW = "height";
+Blockly.Msg.SETWINDOW_OVERFLOW_SHOW = "overflow";
 Blockly.Msg.WINDOW_RELOAD_SHOW = "Reload Page";
 Blockly.Msg.WINDOW_EVENT_SHOW = "window.addEventListener";
