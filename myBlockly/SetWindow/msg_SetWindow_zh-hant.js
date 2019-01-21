ac60f6baa83f7fe54b5ca93e0a947df89489c0d0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-21 10:21:37 +0800

    Update msg_SetWindow_zh-hant.js

--- myBlockly/SetWindow/msg_SetWindow_zh-hant.js
+++ myBlockly/SetWindow/msg_SetWindow_zh-hant.js
@@ -6,5 +6,6 @@ Blockly.Msg.SETWINDOW_LEFT_SHOW = "靠左";
 Blockly.Msg.SETWINDOW_TOP_SHOW = "靠上";
 Blockly.Msg.SETWINDOW_WIDTH_SHOW = "寬度";
 Blockly.Msg.SETWINDOW_HEIGHT_SHOW = "高度";
+Blockly.Msg.SETWINDOW_OVERFLOW_SHOW = "捲軸";
 Blockly.Msg.WINDOW_RELOAD_SHOW = "視窗重新載入網頁";
 Blockly.Msg.WINDOW_EVENT_SHOW = "視窗監聽到事件後執行";
