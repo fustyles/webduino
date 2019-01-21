f29d60f0a11712132c36cf9ea7697da34e537d25
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-21 21:15:06 +0800

    Update setwindow.js

--- myBlockly/SetWindow/setwindow.js
+++ myBlockly/SetWindow/setwindow.js
@@ -6,7 +6,12 @@
     else if (input_property=="overflow") 
       document.body.style.overflow = input_value; 
     else if (input_property=="droppable") {
-      document.body.setAttribute("ondrop","javascript:var obj=document.getElementById(event.dataTransfer.getData('text/plain'));obj.style.position='static';obj.style.left=null;obj.style.top=null;event.preventDefault();if(event.target.tagName!='TD') {return false;} else {event.target.appendChild(obj);}");
-      document.body.setAttribute("ondragover","javascript:event.preventDefault();"); 
+      if (input_value==1) {
+        document.body.setAttribute("ondrop","javascript:var obj=document.getElementById(event.dataTransfer.getData('text/plain'));obj.style.position='static';obj.style.left=null;obj.style.top=null;event.preventDefault();if(event.target.tagName!='TD') {return false;} else {event.target.appendChild(obj);}");
+        document.body.setAttribute("ondragover","javascript:event.preventDefault();"); 
+      } else {
+        document.body.setAttribute("ondrop","");
+        document.body.setAttribute("ondragover","");  
+      }
     }
   }
