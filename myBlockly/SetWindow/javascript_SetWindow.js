8fff192f05e9b5e2cfaf5bd736060786539b2492
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-21 21:35:45 +0800

    Update javascript_SetWindow.js

--- myBlockly/SetWindow/javascript_SetWindow.js
+++ myBlockly/SetWindow/javascript_SetWindow.js
@@ -1,13 +1,3 @@
-Blockly.JavaScript['setscreen'] = function (block) {
-  var left = Blockly.JavaScript.valueToCode(block, 'left', Blockly.JavaScript.ORDER_ATOMIC);
-  var top = Blockly.JavaScript.valueToCode(block, 'top', Blockly.JavaScript.ORDER_ATOMIC);
-  var width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
-  var height = Blockly.JavaScript.valueToCode(block, 'height', Blockly.JavaScript.ORDER_ATOMIC);
-  
-  var code = '';
-  return code;
-};
-
 Blockly.JavaScript['setwindow'] = function (block) {
   var left = Blockly.JavaScript.valueToCode(block, 'left', Blockly.JavaScript.ORDER_ATOMIC);
   var top = Blockly.JavaScript.valueToCode(block, 'top', Blockly.JavaScript.ORDER_ATOMIC);
