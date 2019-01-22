23cd0bb8f5019e44591d70dd1a61e31d897cce82
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-22 23:26:55 +0800

    Update javascript_GameElements.js

--- myBlockly/GameElements/javascript_GameElements.js
+++ myBlockly/GameElements/javascript_GameElements.js
@@ -537,8 +537,10 @@ Blockly.JavaScript['button_delete'] = function (block) {
 
 Blockly.JavaScript['button_onclick_do'] = function (block) {
   var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC); 
+  if ((value_id_.indexOf("'")==0)&&(value_id_.lastIndexOf("'")==value_id_.length-1))
+    value_id_ = value_id_.substring(1,value_id_.length-1);
   var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = 'if (button_onclick_get(' + value_id_ + ')==1) {\n' + statements_do_ + '};\n';
+  var code = 'gamebutton_'+value_id_+'.addEventListener("click", gamebutton_'+value_id_+'_onclick, true);\nasync function gamebutton_'+value_id_+'_onclick (event) {\n' + statements_do_ + '};\n';
   return code;
 };
 
