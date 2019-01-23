03c99d88290083e10a96486b250f7ad99a2122aa
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-23 12:29:32 +0800

    Update gameelements.js

--- myBlockly/GameElements/gameelements.js
+++ myBlockly/GameElements/gameelements.js
@@ -492,15 +492,40 @@
   } 
 
   function canvas_quadraticcurve(input_id,input_x0,input_y0,input_cp1x,input_cp1y,input_x,input_y,input_linewidth,input_color,input_fill) {
-
+    if (document.getElementById("gamecanvas_"+input_id)) {
+      var context = document.getElementById("gamecanvas_"+input_id).getContext("2d");
+      context.strokeStyle = input_color;
+      context.lineWidth = input_linewidth;
+      context.beginPath();
+      context.moveTo(input_x0,input_y0);
+      context.quadraticCurveTo(input_cp1x,input_cp1y,input_x,input_y);
+      if (input_fill==0)
+        context.stroke();
+      else
+        context.fill();
+    }
   }
 
   function canvas_beziercurve(input_id,input_x0,input_y0,input_cp1x,input_cp1y,input_cp2x,input_cp2y,input_x,input_y,input_linewidth,input_color,input_fill) {
-
+    if (document.getElementById("gamecanvas_"+input_id)) {
+      var context = document.getElementById("gamecanvas_"+input_id).getContext("2d");
+      context.strokeStyle = input_color;
+      context.lineWidth = input_linewidth;
+      context.beginPath();
+      context.moveTo(input_x0,input_y0);
+      context.bezierCurveTo(input_cp1x,input_cp1y,input_cp2x,input_cp2y,input_x,input_y);
+      if (input_fill==0)
+        context.stroke();
+      else
+        context.fill();
+    }
   }
 
   function canvas_clearrect(input_id,input_x,input_y,input_width,input_height) {
-
+    if (document.getElementById("gamecanvas_"+input_id)) {
+      var context = document.getElementById("gamecanvas_"+input_id).getContext("2d");
+      context.clearRect(input_x,input_y,input_width,input_height);
+    }
   }
 
   function canvas_clear(input_id) {
