1f8c24aa03ab02292a3b42b947d57212adf16dba
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-23 13:43:54 +0800

    Update javascript.js

--- GameElements_20190121/blockly/javascript.js
+++ GameElements_20190121/blockly/javascript.js
@@ -639,6 +639,10 @@ Blockly.JavaScript['element_event'] = function (block) {
     var obj="document.getElementById('gamecanvas_"+value_id_+"')"; 
   else if (element=="image")
     var obj="document.getElementById('gameimg_"+value_id_+"')";
+  else if (element=="button")
+    var obj="document.getElementById('gamebutton_"+value_id_+"')"; 
+  else if (element=="color")
+    var obj="document.getElementById('gamecolor_"+value_id_+"')";  
   if ((element.indexOf("'")==0)&&(element.lastIndexOf("'")==element.length-1))
     element = element.substring(1,element.length-1);
   var event = block.getFieldValue('event');
@@ -664,12 +668,60 @@ Blockly.JavaScript['element_event_stop'] = function (block) {
     var obj="document.getElementById('gamecanvas_"+value_id_+"')"; 
   else if (element=="image")
     var obj="document.getElementById('gameimg_"+value_id_+"')";
+  else if (element=="button")
+    var obj="document.getElementById('gamebutton_"+value_id_+"')"; 
+  else if (element=="color")
+    var obj="document.getElementById('gamecolor_"+value_id_+"')";
   if ((element.indexOf("'")==0)&&(element.lastIndexOf("'")==element.length-1))
     element = element.substring(1,element.length-1);
   var event = block.getFieldValue('event');
   if ((event.indexOf("'")==0)&&(event.lastIndexOf("'")==event.length-1))
     event = event.substring(1,event.length-1);
   var code = obj+'.removeEventListener("'+event+'", '+element+'_'+value_id_+'_'+event+', true);\n';
+  return code;
+};
+
+Blockly.JavaScript['colorpicker_create'] = function (block) {
+  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var value_left_ = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_top_ = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC); 
+  var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var value_opacity_ = Blockly.JavaScript.valueToCode(block, 'opacity_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var value_zindex_ = Blockly.JavaScript.valueToCode(block, 'zindex_', Blockly.JavaScript.ORDER_ATOMIC);    
+  var value_display_ = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'colorpicker_create(' + value_id_ + ','+ value_width_ + ',' + value_height_ + ',' + value_left_ + ',' + value_top_ + ',' + value_opacity_ + ',' + value_value_ + ',' + value_zindex_ + ',' + value_display_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['colorpicker_set'] = function (block) {
+  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var value_property_ = block.getFieldValue('property_');
+  var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'colorpicker_set(' + value_id_ + ',"' + value_property_ + '",' + value_value_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['colorpicker_get'] = function (block) {
+  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var value_property_ = block.getFieldValue('property_');
+  var code = 'colorpicker_get(' + value_id_ + ',"' + value_property_ + '")';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['colorpicker_delete'] = function (block) {
+  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'colorpicker_delete(' + value_id_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['colorpicker_onchange_do'] = function (block) {
+  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC); 
+  if ((value_id_.indexOf("'")==0)&&(value_id_.lastIndexOf("'")==value_id_.length-1))
+    value_id_ = value_id_.substring(1,value_id_.length-1);
+  var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
+  var code = 'gamecolor_'+value_id_+'.addEventListener("change", gamecolor_'+value_id_+'_onchange, true);\nasync function gamecolor_'+value_id_+'_onchange (event) {\n' + statements_do_ + '};\n';
   return code;
 };
 
