127c9145ce7596d2090b7ddfd7f5ae1d15f9a915
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 00:55:34 +0800

    Update msg_SpeechRecognition_zh-hant.js

--- myBlockly/SpeechRecognition/msg_SpeechRecognition_zh-hant.js
+++ myBlockly/SpeechRecognition/msg_SpeechRecognition_zh-hant.js
@@ -1,3 +1,6 @@
+Blockly.Msg["SPEECHRECOGNITION"] = "語音辨識";
+Blockly.Msg["SPEECHRECOGNITION_HUE"] = "280";
+
 Blockly.Msg.SPEECHRECOGNITION_SHOW = "開始語音辨識";
 Blockly.Msg.SPEECHRECOGNITION_LANGUAGE_SHOW = "語言";
 Blockly.Msg.SPEECHRECOGNITION_GET_SHOW = "取得語音辨識文字";
