dbc04b6db2103c687ae2c647051dd80889a48174
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 01:23:46 +0800

    Update javascript_SpeechRecognition.js

--- myBlockly/SpeechRecognition/javascript_SpeechRecognition.js
+++ myBlockly/SpeechRecognition/javascript_SpeechRecognition.js
@@ -7,7 +7,7 @@ Blockly.JavaScript['SpeechRecognition_run'] = function (block) {
 Blockly.JavaScript['SpeechRecognition_get'] = function (block) {
   var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_property_ = block.getFieldValue('property_');
-  var code = 'Recognition_final_get();';
+  var code = 'Recognition_final_get()';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
