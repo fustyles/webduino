47d9083de816ed5271db9953cd9e1fcdd2f84dca
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 00:10:40 +0800

    Update javascript_SpeechRecognition.js

--- myBlockly/SpeechRecognition/javascript_SpeechRecognition.js
+++ myBlockly/SpeechRecognition/javascript_SpeechRecognition.js
@@ -1,20 +1,5 @@
-Blockly.JavaScript['setscreen'] = function (block) {
-  var left = Blockly.JavaScript.valueToCode(block, 'left', Blockly.JavaScript.ORDER_ATOMIC);
-  var top = Blockly.JavaScript.valueToCode(block, 'top', Blockly.JavaScript.ORDER_ATOMIC);
-  var width = Blockly.JavaScript.valueToCode(block, 'width', Blockly.JavaScript.ORDER_ATOMIC);
-  var height = Blockly.JavaScript.valueToCode(block, 'height', Blockly.JavaScript.ORDER_ATOMIC);
-  document.getElementById("ifrLeft").value=left;
-  document.getElementById("ifrTop").value=top;
-  document.getElementById("ifrWidth").value=width;
-  document.getElementById("ifrHeight").value=height;
-  
-  var code = '';
-  return code;
-};
-
-Blockly.JavaScript['body_set'] = function (block) {
-  var value_property_ = block.getFieldValue('property_');
-  var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'body_set("' + value_property_ + '",' + value_value_ + ');\n';
+Blockly.JavaScript['SpeechRecognition_run'] = function (block) {
+  var value_language_ = block.getFieldValue('language_');
+  var code = 'recognition.lang="'+value_language_+'";\nstartButton(event);\n';
   return code;
 };
