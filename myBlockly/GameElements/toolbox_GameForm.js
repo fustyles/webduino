0f8eb7f56acc3a2a71e804d77f0864ab4f40d611
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-24 10:13:11 +0800

    Update toolbox_GameForm.js

--- myBlockly/GameElements/toolbox_GameForm.js
+++ myBlockly/GameElements/toolbox_GameForm.js
@@ -162,5 +162,90 @@ var catGameForm = '<xml><category name="%{BKY_GAMEFORM}" colour="%{BKY_GAMEFORM_
 '        <field name="TEXT"></field>'+
 '      </block>'+
 '    </value>'+         
+'  </block>'+                
+'  <block type="select_create">'+ 
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="left_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="top_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">100</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="width_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">60</field>'+
+'      </block>'+
+'    </value>'+
+'    <value name="height_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">30</field>'+
+'      </block>'+
+'    </value>'+    
+'    <value name="opacity_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">1</field>'+
+'      </block>'+
+'    </value>'+      
+'    <value name="option_">'+  
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+    
+'    </value>'+       
+'    <value name="value_">'+  
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+    
+'    </value>'+          
+'    <value name="zindex_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+ 
+'    <value name="display_">'+
+'      <block type="logic_boolean">'+
+'        <field name="BOOL">TRUE</field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+
+'  <block type="select_set">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'    <value name="value_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+    
+'  </block>'+  
+'  <block type="select_get">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+     
+'  <block type="select_delete">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+     
+'  </block>'+    
+'  <block type="select_onchange_do">'+
+'    <value name="id_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+
+'    </value>'+         
 '  </block>'+               
 ' </category></xml>';
