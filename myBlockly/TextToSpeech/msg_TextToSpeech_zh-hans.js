a973e88c301021fb8da24a9f4735f643d1b7508d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 12:44:13 +0800

    Update msg_TextToSpeech_zh-hans.js

--- myBlockly/TextToSpeech/msg_TextToSpeech_zh-hans.js
+++ myBlockly/TextToSpeech/msg_TextToSpeech_zh-hans.js
@@ -1,7 +1,4 @@
-Blockly.Msg["SPEECHRECOGNITION"] = "语音辨识";
-Blockly.Msg["SPEECHRECOGNITION_HUE"] = "280";
+Blockly.Msg["TEXTTOSPEECH"] = "语音朗读";
+Blockly.Msg["TEXTTOSPEECH_HUE"] = "280";
 
-Blockly.Msg.SPEECHRECOGNITION_SHOW = "开始语音辨识";
-Blockly.Msg.SPEECHRECOGNITION_LANGUAGE_SHOW = "语言";
-Blockly.Msg.SPEECHRECOGNITION_GET_SHOW = "取得语音辨识文字";
-Blockly.Msg.SPEECHRECOGNITION_LISTENER_SHOW = "开始监听语音辨识";
+Blockly.Msg.TEXTTOSPEECH_SHOW = "开始朗读文字";
