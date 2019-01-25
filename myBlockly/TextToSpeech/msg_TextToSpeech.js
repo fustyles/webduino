91b03f6dca6371332bc7603a127e218c709d5093
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 12:42:56 +0800

    Update msg_TextToSpeech.js

--- myBlockly/TextToSpeech/msg_TextToSpeech.js
+++ myBlockly/TextToSpeech/msg_TextToSpeech.js
@@ -1,7 +1,4 @@
-Blockly.Msg["SPEECHRECOGNITION"] = "Speech Recognition";
-Blockly.Msg["SPEECHRECOGNITION_HUE"] = "280";
+Blockly.Msg["TEXTTOSPEECH"] = "Speech Recognition";
+Blockly.Msg["TEXTTOSPEECH_HUE"] = "280";
 
-Blockly.Msg.SPEECHRECOGNITION_SHOW = "Start Speech Recognition";
-Blockly.Msg.SPEECHRECOGNITION_LANGUAGE_SHOW = "Language";
-Blockly.Msg.SPEECHRECOGNITION_GET_SHOW = "Get text of Speech Recognition ";
-Blockly.Msg.SPEECHRECOGNITION_LISTENER_SHOW = "Speech Recognition Listener";
+Blockly.Msg.TEXTTOSPEECH_SHOW = "Text to Speech";
