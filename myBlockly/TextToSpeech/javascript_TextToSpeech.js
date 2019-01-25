3e4d8b986b5bcc4204aa2a5c89bfe3b855ff3c0e
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 14:03:16 +0800

    Update javascript_TextToSpeech.js

--- myBlockly/TextToSpeech/javascript_TextToSpeech.js
+++ myBlockly/TextToSpeech/javascript_TextToSpeech.js
@@ -3,6 +3,7 @@ Blockly.JavaScript['TextToSpeech_run'] = function (block) {
   var value_rate_ = Blockly.JavaScript.valueToCode(block, 'rate_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_pitch_ = Blockly.JavaScript.valueToCode(block, 'pitch_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_lang_ = block.getFieldValue('lang_');
+  console.log(value_lang_);
   var value_voice_ = Blockly.JavaScript.valueToCode(block, 'voice_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_text_ = Blockly.JavaScript.valueToCode(block, 'text_', Blockly.JavaScript.ORDER_ATOMIC);
   var code = 'var tts = new SpeechSynthesisUtterance('+value_text_+');\n'+
