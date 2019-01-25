52daf98c5b087f826209ed272880d11f485d4818
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 20:00:25 +0800

    Update javascript_TextToSpeech.js

--- myBlockly/TextToSpeech/javascript_TextToSpeech.js
+++ myBlockly/TextToSpeech/javascript_TextToSpeech.js
@@ -5,15 +5,16 @@ Blockly.JavaScript['TextToSpeech_run'] = function (block) {
   var value_voice_ = Blockly.JavaScript.valueToCode(block, 'voice_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_lang_ = block.getFieldValue('lang_');
   var value_text_ = Blockly.JavaScript.valueToCode(block, 'text_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'var tts = new SpeechSynthesisUtterance('+value_text_+');\n'+
+  var code = 'var synth = window.speechSynthesis;\n'+
+      'var tts = new SpeechSynthesisUtterance('+value_text_+');\n'+
       'tts.volume='+value_volume_+';\n'+
       'tts.rate='+value_rate_+';\n'+
       'tts.pitch='+value_pitch_+';\n'+
       'tts.lang="'+value_lang_+'";\n'+
-      'var voices = window.speechSynthesis.getVoices();\n'+
+      'var voices = synth.getVoices();\n'+
       'if (voices.length==0) {\n'+
       '  await delay(5);\n'+        
-      '  var voices = window.speechSynthesis.getVoices();\n'+      
+      '  var voices = synth.getVoices();\n'+      
       '}\n'+
       'if (voices.length>0) {\n'+ 
       '  for(i = 0; i < voices.length ; i++) {\n'+
@@ -22,18 +23,18 @@ Blockly.JavaScript['TextToSpeech_run'] = function (block) {
       '    }\n'+
       '  }\n'+
       '}\n'+
-      'window.speechSynthesis.speak(tts);\n';
+      'synth.speak(tts);\n';
   return code;
 };
 
 Blockly.JavaScript['TextToSpeech_control'] = function(block) {
   var control_ = block.getFieldValue('control_'); 
   if (control_=="paused")
-    var code = 'window.speechSynthesis.paused();\n';
+    var code = 'synth.paused();\n';
   else if (control_=="resume")
-    var code = 'window.speechSynthesis.resume();\n';
+    var code = 'synth.resume();\n';
   else if (control_=="cancel")
-    var code = 'window.speechSynthesis.cancel();\n';  
+    var code = 'synth.cancel();\n';  
   return code;
 };
 
