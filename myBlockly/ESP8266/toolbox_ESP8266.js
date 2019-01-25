5d3d0e55543377fd4e6b5fbeb6781faafb6508af
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 20:53:42 +0800

    Update toolbox_ESP8266.js

--- myBlockly/ESP8266/toolbox_ESP8266.js
+++ myBlockly/ESP8266/toolbox_ESP8266.js
@@ -17,49 +17,6 @@ var catESP8266 = '<xml><category name="%{BKY_ESP8266}" colour="%{BKY_ESP8266_HUE
 '          </block>'+
 '        </value>'+
 '    </block>'+
-'    <block type="esp8266_car">'+
-'        <field name="str5">F</field>'+
-'        <value name="cmd">'+
-'          <block type="text">'+
-'            <field name="TEXT">car</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str1">'+
-'          <block type="math_number">'+
-'            <field name="NUM">3</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str2">'+
-'          <block type="math_number">'+
-'            <field name="NUM">5</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str3">'+
-'          <block type="math_number">'+
-'            <field name="NUM">6</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str4">'+
-'          <block type="math_number">'+
-'            <field name="NUM">9</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str5">'+
-'          <block type="math_number">'+
-'            <field name="NUM">200</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str6">'+
-'          <block type="math_number">'+
-'            <field name="NUM">200</field>'+
-'          </block>'+
-'        </value>'+
-'        <value name="str7">'+
-'          <block type="math_number">'+
-'            <field name="NUM">200</field>'+
-'          </block>'+
-'        </value>'+
-'    </block>'+
 '    <block type="esp8266_custom">'+
 '        <value name="cmd">'+
 '          <block type="text">'+
@@ -112,4 +69,47 @@ var catESP8266 = '<xml><category name="%{BKY_ESP8266}" colour="%{BKY_ESP8266_HUE
 '          </block>'+
 '        </value>'+
 '    </block>'+
+'    <block type="esp8266_car">'+
+'        <field name="str5">F</field>'+
+'        <value name="cmd">'+
+'          <block type="text">'+
+'            <field name="TEXT">car</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str1">'+
+'          <block type="math_number">'+
+'            <field name="NUM">3</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str2">'+
+'          <block type="math_number">'+
+'            <field name="NUM">5</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str3">'+
+'          <block type="math_number">'+
+'            <field name="NUM">6</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str4">'+
+'          <block type="math_number">'+
+'            <field name="NUM">9</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str5">'+
+'          <block type="math_number">'+
+'            <field name="NUM">200</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str6">'+
+'          <block type="math_number">'+
+'            <field name="NUM">200</field>'+
+'          </block>'+
+'        </value>'+
+'        <value name="str7">'+
+'          <block type="math_number">'+
+'            <field name="NUM">200</field>'+
+'          </block>'+
+'        </value>'+
+'    </block>'+    
 '</category></xml>';
