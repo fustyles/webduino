4f406d97e373e323392a72d7a45599eedf19e6d6
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-26 01:52:23 +0800

    Update esp8266.js

--- myBlockly/ESP8266/esp8266.js
+++ myBlockly/ESP8266/esp8266.js
@@ -25,7 +25,6 @@ function esp8266_sendCommand(url,cmd,str1,str2,str3,str4,str5,str6,str7,str8,str
 function esp8266_getResponse() {
  console.log("respone="+Response);
  var res = Response;
- Response.length=0;
- console.log("res"+res);
+ console.log("res="+res);
  return res;
 }
