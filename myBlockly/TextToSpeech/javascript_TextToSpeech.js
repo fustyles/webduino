46cc5ffd9d4c541d3a9baa5c9d8962b082fca16c
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 13:00:53 +0800

    Update javascript_TextToSpeech.js

--- myBlockly/TextToSpeech/javascript_TextToSpeech.js
+++ myBlockly/TextToSpeech/javascript_TextToSpeech.js
@@ -1,5 +1,5 @@
 Blockly.JavaScript['TextToSpeech_run'] = function (block) {
   var value_text_ = Blockly.JavaScript.valueToCode(block, 'text_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'var msg = new SpeechSynthesisUtterance('+value_text_+');\nwindow.speechSynthesis.speak(msg);\n';
+  var code = 'var msg = new SpeechSynthesisUtterance('+value_text_+');\nwindow.speechSynthesis.speak(msg);\nvar voices = speechSynthesis.getVoices();\nconsole.log(voices);\n';
   return code;
 };
