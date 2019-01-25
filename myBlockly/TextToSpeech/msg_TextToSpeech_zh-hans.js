b5e9f2ea4fbcdad8926b6505217856bf0348a080
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 14:23:03 +0800

    Update msg_TextToSpeech_zh-hans.js

--- myBlockly/TextToSpeech/msg_TextToSpeech_zh-hans.js
+++ myBlockly/TextToSpeech/msg_TextToSpeech_zh-hans.js
@@ -2,9 +2,9 @@ Blockly.Msg["TEXTTOSPEECH"] = "语音朗读";
 Blockly.Msg["TEXTTOSPEECH_HUE"] = "280";
 
 Blockly.Msg.TEXTTOSPEECH_SHOW = "朗读文字";
-Blockly.Msg.TEXTTOSPEECH_VOLUME_SHOW = "音量";  
-Blockly.Msg.TEXTTOSPEECH_RATE_SHOW = "速度";  
-Blockly.Msg.TEXTTOSPEECH_PITCH_SHOW = "语调";      
+Blockly.Msg.TEXTTOSPEECH_VOLUME_SHOW = "音量(0~1)";  
+Blockly.Msg.TEXTTOSPEECH_RATE_SHOW = "速度(0.1~10)";  
+Blockly.Msg.TEXTTOSPEECH_PITCH_SHOW = "语调(0~2)";      
 Blockly.Msg.TEXTTOSPEECH_LANG_SHOW = "语系";      
 Blockly.Msg.TEXTTOSPEECH_VOICE_SHOW = "语音"; 
 Blockly.Msg.TEXTTOSPEECH_TEXT_SHOW = "文字";
