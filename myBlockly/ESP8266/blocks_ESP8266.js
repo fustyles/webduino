a1381d873a14dcfb8888bb575f8a65234c344f51
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 23:19:02 +0800

    Update blocks_ESP8266.js

--- myBlockly/ESP8266/blocks_ESP8266.js
+++ myBlockly/ESP8266/blocks_ESP8266.js
@@ -29,57 +29,6 @@ Blockly.Blocks['esp8266_system'] = {
   }
 };
 
-Blockly.Blocks['esp8266_car'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("ESP8266 Car");  
-    this.appendValueInput("url")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("url");       
-    this.appendValueInput("cmd")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("cmd");    
-    this.appendValueInput("str1")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("pinL1");
-    this.appendValueInput("str2")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("pinL2");
-    this.appendValueInput("str3")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("pinR1");
-    this.appendValueInput("str4")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("pinR2");
-    this.appendValueInput("str5")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("L speed");
-    this.appendValueInput("str6")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("R speed");
-    this.appendValueInput("str7")
-        .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("Delay(ms)");
-    this.appendDummyInput()
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("State")
-        .appendField(new Blockly.FieldDropdown([["FORWARD","F"], ["BACKWARD","B"], ["LEFT","L"], ["RIGHT","R"], ["STOP","S"]]), "str8");    
-    this.setInputsInline(false);
-    this.setPreviousStatement(true);
-    this.setNextStatement(true);
-    this.setColour(60);
-  }
-};
-
 Blockly.Blocks['esp8266_custom'] = {
   init: function() {
     this.appendDummyInput()
@@ -128,6 +77,57 @@ Blockly.Blocks['esp8266_custom'] = {
         .setCheck(null)
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("str9");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
+
+Blockly.Blocks['esp8266_car'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("ESP8266 Car");  
+    this.appendValueInput("url")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("url");       
+    this.appendValueInput("cmd")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("cmd");    
+    this.appendValueInput("str1")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinL1");
+    this.appendValueInput("str2")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinL2");
+    this.appendValueInput("str3")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinR1");
+    this.appendValueInput("str4")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinR2");
+    this.appendValueInput("str5")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("L speed");
+    this.appendValueInput("str6")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("R speed");
+    this.appendValueInput("str7")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Delay(ms)");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("State")
+        .appendField(new Blockly.FieldDropdown([["FORWARD","F"], ["BACKWARD","B"], ["LEFT","L"], ["RIGHT","R"], ["STOP","S"]]), "str8");    
     this.setInputsInline(false);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
