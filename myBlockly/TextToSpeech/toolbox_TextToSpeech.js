e6d58c0d979d9678900e328c6bf2d448f0731467
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 13:23:44 +0800

    Update toolbox_TextToSpeech.js

--- myBlockly/TextToSpeech/toolbox_TextToSpeech.js
+++ myBlockly/TextToSpeech/toolbox_TextToSpeech.js
@@ -1,6 +1,31 @@
 var catTextToSpeech = '<xml>'+
 '<category name="%{BKY_TEXTTOSPEECH}" colour="%{BKY_TEXTTOSPEECH_HUE}">'+
 '  <block type="TextToSpeech_run">'+  
+'    <value name="volume_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+  
+'    </value>'+    
+'    <value name="rate_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+  
+'    </value>'+  
+'    <value name="pitch_">'+
+'      <block type="math_number">'+
+'        <field name="NUM">0</field>'+
+'      </block>'+
+'    </value>'+      
+'    <value name="lang_">'+
+'      <block type="text">'+
+'        <field name="TEXT">en-us</field>'+
+'      </block>'+    
+'    </value>'+  
+'    <value name="voice_">'+
+'      <block type="text">'+
+'        <field name="TEXT">en-us</field>'+
+'      </block>'+    
+'    </value>'+  
 '    <value name="text_">'+
 '      <block type="text">'+
 '        <field name="TEXT"></field>'+
