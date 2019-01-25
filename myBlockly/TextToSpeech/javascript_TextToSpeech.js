8d35d230ef8649907aed64cefbbe0aba5146ade7
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 12:31:54 +0800

    Update javascript_TextToSpeech.js

--- myBlockly/TextToSpeech/javascript_TextToSpeech.js
+++ myBlockly/TextToSpeech/javascript_TextToSpeech.js
@@ -1,18 +1,5 @@
-Blockly.JavaScript['SpeechRecognition_run'] = function (block) {
-  var value_language_ = block.getFieldValue('language_');
-  var code = 'recognition.lang="'+value_language_+'";\nstartButton(event);\n';
-  return code;
-};
-
-Blockly.JavaScript['SpeechRecognition_get'] = function (block) {
-  var value_id_ = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_property_ = block.getFieldValue('property_');
-  var code = 'Recognition_final_get()';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['SpeechRecognition_listener'] = function (block) {
-  var statements_do_ = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = 'setInterval(async function(){\n' + statements_do_ + '},10);\n';
+Blockly.JavaScript['TextToSpeech_run'] = function (block) {
+  var value_text_ = Blockly.JavaScript.valueToCode(block, 'text_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'var msg = new SpeechSynthesisUtterance("'+value_text_+'");\nwindow.speechSynthesis.speak(msg);\n';
   return code;
 };
