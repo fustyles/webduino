85f036a8288dea90f582ca4cb2269279483bbc1c
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 13:17:39 +0800

    Update javascript_TextToSpeech.js

--- myBlockly/TextToSpeech/javascript_TextToSpeech.js
+++ myBlockly/TextToSpeech/javascript_TextToSpeech.js
@@ -1,5 +1,16 @@
 Blockly.JavaScript['TextToSpeech_run'] = function (block) {
+  var value_volume_ = Blockly.JavaScript.valueToCode(block, 'volume_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_rate_ = Blockly.JavaScript.valueToCode(block, 'rate_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_pitch_ = Blockly.JavaScript.valueToCode(block, 'pitch_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_lang_ = Blockly.JavaScript.valueToCode(block, 'lang_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_voice_ = Blockly.JavaScript.valueToCode(block, 'voice_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_text_ = Blockly.JavaScript.valueToCode(block, 'text_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'var msg = new SpeechSynthesisUtterance('+value_text_+');\nwindow.speechSynthesis.speak(msg);\nvar voices = speechSynthesis.getVoices();\nconsole.log(voices.length);\n';
+  var code = 'var tts = new SpeechSynthesisUtterance('+value_text_+');\n'+
+      'tts.volume='+value_volume_+';\n'+
+      'tts.rate='+value_rate_+';\n'+
+      'tts.pitch='+value_pitch_+';\n'+
+      'tts.lang='+value_lang_+';\n'+
+      'tts.voice='+value_voice_+';\n'+     
+      'window.speechSynthesis.speak(tts);\n';
   return code;
 };
