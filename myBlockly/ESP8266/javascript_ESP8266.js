726e21c353971b5c0667e5a8947613ee1393c844
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 21:37:32 +0800

    Update javascript_ESP8266.js

--- myBlockly/ESP8266/javascript_ESP8266.js
+++ myBlockly/ESP8266/javascript_ESP8266.js
@@ -4,7 +4,7 @@ Blockly.JavaScript['esp8266_system'] = function(block) {
   var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "esp8266_system("+value_ip+",'"+dropdown_cmd+"',"+value_str1+","+value_str2+","+value_str3+");\n";
+  var code = "esp8266_SendCommand("+value_ip+",'"+dropdown_cmd+"',"+value_str1+","+value_str2+","+value_str3+",'','','','','','');\n";
   return code;
 };
 
@@ -18,7 +18,7 @@ Blockly.JavaScript['esp8266_car'] = function(block) {
   var value_str6 = Blockly.JavaScript.valueToCode(block, 'str6', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);  
   var dropdown_str8 = block.getFieldValue('str8');
-  var code = "esp8266_car("+value_ip+","+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+",'"+dropdown_str8+"');\n";
+  var code = "esp8266_SendCommand("+value_ip+","+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+",'"+dropdown_str8+"','');\n";
   return code;
 };
 
@@ -33,6 +33,6 @@ Blockly.JavaScript['esp8266_custom'] = function(block) {
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str8 = Blockly.JavaScript.valueToCode(block, 'str8', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str9 = Blockly.JavaScript.valueToCode(block, 'str9', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "esp8266_custom("+value_ip+","+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+","+value_str8+","+value_str9+");\n";
+  var code = "esp8266_SendCommand("+value_ip+","+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+","+value_str8+","+value_str9+");\n";
   return code;
 };
