b77ff8dc9acc5b1493ddb6eccd00a189ffc3c0c7
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 23:36:08 +0800

    Update esp8266.js

--- myBlockly/ESP8266/esp8266.js
+++ myBlockly/ESP8266/esp8266.js
@@ -7,8 +7,7 @@ function esp8266_SendCommand(url,cmd,str1,str2,str3,str4,str5,str6,str7,str8,str
   var data = $.ajax({
       "type": "POST",
       "dataType": "html",
-      "url": url,
-      "data": cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9,
+      "url": url+"?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9,
       success: function(html)
       {
         FeedBack=html;
