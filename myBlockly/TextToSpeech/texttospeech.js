9553bdb2ef08df72b86c4cfc9beb7e91dcaff9f6
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 18:16:52 +0800

    Update texttospeech.js

--- myBlockly/TextToSpeech/texttospeech.js
+++ myBlockly/TextToSpeech/texttospeech.js
@@ -1,1 +1,9 @@
-
+var voices = window.speechSynthesis.getVoices();
+if (voices.length==0) {
+  var myVoices = setInterval(
+      function() {
+        var voices = window.speechSynthesis.getVoices();
+        if (voices.length>0) clearInterval(myVoices);
+      }
+  , 1000);
+}
