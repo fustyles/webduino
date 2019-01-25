4f74c645eb6d3d391b803967f19bad71bfb4c1c1
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 14:23:01 +0800

    Update msg_TextToSpeech.js

--- myBlockly/TextToSpeech/msg_TextToSpeech.js
+++ myBlockly/TextToSpeech/msg_TextToSpeech.js
@@ -2,9 +2,9 @@ Blockly.Msg["TEXTTOSPEECH"] = "Speech Synthesis";
 Blockly.Msg["TEXTTOSPEECH_HUE"] = "280";
 
 Blockly.Msg.TEXTTOSPEECH_SHOW = "Text to Speech";
-Blockly.Msg.TEXTTOSPEECH_VOLUME_SHOW = "Volume";  
-Blockly.Msg.TEXTTOSPEECH_RATE_SHOW = "Rate";  
-Blockly.Msg.TEXTTOSPEECH_PITCH_SHOW = "Pitch";      
+Blockly.Msg.TEXTTOSPEECH_VOLUME_SHOW = "Volume(0~1)";  
+Blockly.Msg.TEXTTOSPEECH_RATE_SHOW = "Rate(0.1~10)";  
+Blockly.Msg.TEXTTOSPEECH_PITCH_SHOW = "Pitch(0~2)";      
 Blockly.Msg.TEXTTOSPEECH_LANG_SHOW = "Language";      
 Blockly.Msg.TEXTTOSPEECH_VOICE_SHOW = "Voice"; 
 Blockly.Msg.TEXTTOSPEECH_TEXT_SHOW = "Text";
