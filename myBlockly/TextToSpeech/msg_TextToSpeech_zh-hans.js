ca66c80a02a43ba697520b50d881846a96c59f13
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 19:49:26 +0800

    Update msg_TextToSpeech_zh-hans.js

--- myBlockly/TextToSpeech/msg_TextToSpeech_zh-hans.js
+++ myBlockly/TextToSpeech/msg_TextToSpeech_zh-hans.js
@@ -1,14 +1,14 @@
 Blockly.Msg["TEXTTOSPEECH"] = "语音朗读";
 Blockly.Msg["TEXTTOSPEECH_HUE"] = "280";
 
-Blockly.Msg.TEXTTOSPEECH_SHOW = "朗读文字";
+Blockly.Msg.TEXTTOSPEECH_SHOW = "文字";
 Blockly.Msg.TEXTTOSPEECH_VOLUME_SHOW = "音量(0~1)";  
 Blockly.Msg.TEXTTOSPEECH_RATE_SHOW = "速度(0.1~10)";  
 Blockly.Msg.TEXTTOSPEECH_PITCH_SHOW = "语调(0~2)";      
 Blockly.Msg.TEXTTOSPEECH_LANG_SHOW = "语系";      
 Blockly.Msg.TEXTTOSPEECH_VOICE_SHOW = "语音"; 
 Blockly.Msg.TEXTTOSPEECH_TEXT_SHOW = "文字";
-
+Blockly.Msg.TEXTTOSPEECH_CONTROL_SHOW = "语音朗读执行";
 Blockly.Msg.TEXTTOSPEECH_LANG_af_ZA = "Afrikaans (South Africa)";
 Blockly.Msg.TEXTTOSPEECH_LANG_am_ET = "Amharic (Ethiopia)";
 Blockly.Msg.TEXTTOSPEECH_LANG_hy_AM = "Armenian (Armenia)";
