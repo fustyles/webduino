218f49417f8dbbc295c9b86381cb93d6a60332d8
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 20:48:58 +0800

    Update javascript_ESP8266.js

--- myBlockly/ESP8266/javascript_ESP8266.js
+++ myBlockly/ESP8266/javascript_ESP8266.js
@@ -3,8 +3,8 @@ Blockly.JavaScript['esp8266_system'] = function(block) {
   var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "esp8266_system('"+dropdown_cmd+"',"+value_str1+","+value_str2+","+value_str3+")";
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = "esp8266_system('"+dropdown_cmd+"',"+value_str1+","+value_str2+","+value_str3+");\n";
+  return code;
 };
 
 Blockly.JavaScript['esp8266_car'] = function(block) {
@@ -17,8 +17,8 @@ Blockly.JavaScript['esp8266_car'] = function(block) {
   var value_str6 = Blockly.JavaScript.valueToCode(block, 'str6', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);  
   var dropdown_str8 = block.getFieldValue('str8');
-  var code = "esp8266_car("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+",'"+dropdown_str8+"')";
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = "esp8266_car("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+",'"+dropdown_str8+"');\n";
+  return code;
 };
 
 Blockly.JavaScript['esp8266_custom'] = function(block) {
@@ -32,6 +32,6 @@ Blockly.JavaScript['esp8266_custom'] = function(block) {
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str8 = Blockly.JavaScript.valueToCode(block, 'str8', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str9 = Blockly.JavaScript.valueToCode(block, 'str9', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "esp8266_custom("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+","+value_str8+","+value_str9+")";
-  return [code, Blockly.JavaScript.ORDER_NONE];
+  var code = "esp8266_custom("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+","+value_str8+","+value_str9+");\n";
+  return code;
 };
