272cb3e558b6dbcaf921c246631e71e50db848d7
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 20:47:06 +0800

    Update javascript_ESP8266.js

--- myBlockly/ESP8266/javascript_ESP8266.js
+++ myBlockly/ESP8266/javascript_ESP8266.js
@@ -1,3 +1,12 @@
+Blockly.JavaScript['esp8266_system'] = function(block) {
+  var dropdown_cmd = block.getFieldValue('cmd');
+  var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "esp8266_system('"+dropdown_cmd+"',"+value_str1+","+value_str2+","+value_str3+")";
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
 Blockly.JavaScript['esp8266_car'] = function(block) {
   var value_cmd = Blockly.JavaScript.valueToCode(block, 'cmd', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
@@ -9,15 +18,6 @@ Blockly.JavaScript['esp8266_car'] = function(block) {
   var value_str7 = Blockly.JavaScript.valueToCode(block, 'str7', Blockly.JavaScript.ORDER_ATOMIC);  
   var dropdown_str8 = block.getFieldValue('str8');
   var code = "esp8266_car("+value_cmd+","+value_str1+","+value_str2+","+value_str3+","+value_str4+","+value_str5+","+value_str6+","+value_str7+",'"+dropdown_str8+"')";
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['esp8266_system'] = function(block) {
-  var dropdown_cmd = block.getFieldValue('cmd');
-  var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "esp8266_system('"+dropdown_cmd+"',"+value_str1+","+value_str2+","+value_str3+")";
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
