9d6ae4036ee9b8c0eaa1dadb53fe98b107331012
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 12:36:58 +0800

    Update toolbox_TextToSpeech.js

--- myBlockly/TextToSpeech/toolbox_TextToSpeech.js
+++ myBlockly/TextToSpeech/toolbox_TextToSpeech.js
@@ -1,17 +1,11 @@
-var catSpeechRecognition = '<xml>'+
-'<category name="%{BKY_SPEECHRECOGNITION}" colour="%{BKY_SPEECHRECOGNITION_HUE}">'+
-'  <block type="SpeechRecognition_run">'+  
-'  </block>'+       
-'<variables>'+
-'    <variable type="" id="XET3`{C@a}F7i!;%jtKr">Result</variable>'+
-'</variables>'+
-'<block type="variables_set" id="WgoSc$V{4smbOP8+jr80" x="-13" y="-1">'+
-'  <field name="VAR" id="XET3`{C@a}F7i!;%jtKr" variabletype="">Result</field>'+
-'  <value name="VALUE">'+
-'    <block type="SpeechRecognition_get" id="]VM_67C(_2(3TXND*~)+"></block>'+
-'  </value>'+
-'</block>'+
-'  <block type="SpeechRecognition_listener">'+  
-'  </block>'+      
+var catTextToSpeech = '<xml>'+
+'<category name="%{BKY_TEXTTOSPEECH}" colour="%{BKY_TEXTTOSPEECH_HUE}">'+
+'  <block type="TextToSpeech_run">'+  
+'    <value name="text_">'+
+'      <block type="text">'+
+'        <field name="TEXT"></field>'+
+'      </block>'+    
+'    </value>'+      
+'  </block>'+           
 '</category>'+
 '</xml>';
