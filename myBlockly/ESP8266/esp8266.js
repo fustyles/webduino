9369be13ea5ae8483a1f7cda9ce7d7f104231da0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-25 21:17:21 +0800

    Update esp8266.js

--- myBlockly/ESP8266/esp8266.js
+++ myBlockly/ESP8266/esp8266.js
@@ -1,13 +1,13 @@
 // Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 
-function esp8266_system(cmd,str1,str2,str3) {
+function esp8266_system(ip,cmd,str1,str2,str3) {
   return "?"+cmd+"="+str1+";"+str2+";"+str3;
 }
 
-function esp8266_car(cmd,str1,str2,str3,str4,str5,str6,str7,str8) {
+function esp8266_car(ip,cmd,str1,str2,str3,str4,str5,str6,str7,str8) {
   return "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8;
 }
 
-function esp8266_custom(cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
+function esp8266_custom(ip,cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
   return "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9;
 }
