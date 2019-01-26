02d8a622de24b9729d9d586b1ee8024a1dcec11d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-26 11:49:23 +0800

    Update toolbox_ESP8266.js

--- myBlockly/ESP8266/toolbox_ESP8266.js
+++ myBlockly/ESP8266/toolbox_ESP8266.js
@@ -126,13 +126,26 @@ var catESP8266 = '<xml><category name="%{BKY_ESP8266}" colour="%{BKY_ESP8266_HUE
 '          </block>'+
 '        </value>'+
 '    </block>'+    
-'    <variables><variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable></variables>'+
-'    <block type="variables_set" id="Sh:0%7~`mHXezT$HAE*w" x="97" y="299">'+
-'      <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+
-'      <value name="VALUE">'+
-'        <block type="esp8266_getresponse" id="KE3ci[=9F6s6fOnlqASf"></block>'+
-'      </value>'+
-'    </block>'+
+'    <variables>'+ 
+'      <variable type="" id="+Oog~D)0D,I*Y5(g]2r1">TimerESP8266</variable>'+ 
+'      <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable>'+ 
+'    </variables>'+ 
+'    <block type="document_timer" id="mi/z6I4OGW!r-?w:dktl" x="57" y="57">'+ 
+'      <field name="fuTimer_" id="+Oog~D)0D,I*Y5(g]2r1" variabletype="">TimerESP8266</field>'+ 
+'      <value name="intervals_">'+ 
+'        <block type="math_number" id=";yP~x1l8Ovs6JfUe6u0C">'+ 
+'          <field name="NUM">10</field>'+ 
+'        </block>'+ 
+'      </value>'+ 
+'      <statement name="do_">'+ 
+'        <block type="variables_set" id="Sh:0%7~`mHXezT$HAE*w">'+ 
+'          <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+ 
+'          <value name="VALUE">'+ 
+'            <block type="esp8266_getresponse" id="KE3ci[=9F6s6fOnlqASf"></block>'+ 
+'          </value>'+ 
+'        </block>'+ 
+'      </statement>'+ 
+'    </block>'+     
 '    <block type="esp8266_cmd">'+
 '    </block>'+    
 '</category></xml>';
