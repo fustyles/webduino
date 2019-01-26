e16f82c1338cb8da6cb3de8dea6a8df45bb6a552
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-26 12:47:44 +0800

    Update javascript_ESP8266.js

--- myBlockly/ESP8266/javascript_ESP8266.js
+++ myBlockly/ESP8266/javascript_ESP8266.js
@@ -3,8 +3,7 @@ Blockly.JavaScript['esp8266_system'] = function(block) {
   var value_cmd = block.getFieldValue('cmd');
   var value_str1 = Blockly.JavaScript.valueToCode(block, 'str1', Blockly.JavaScript.ORDER_ATOMIC);
   var value_str2 = Blockly.JavaScript.valueToCode(block, 'str2', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_str3 = Blockly.JavaScript.valueToCode(block, 'str3', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "esp8266_sendCommand("+value_url+",'"+value_cmd+"',"+value_str1+","+value_str2+","+value_str3+",'','','','','','');\n";
+  var code = "esp8266_sendCommand("+value_url+",'"+value_cmd+"',"+value_str1+","+value_str2+",'','','','','','','');\n";
   return code;
 };
 
