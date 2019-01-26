ac08bff8a7b244d40466afde482d8e63862f0a74
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-26 11:24:35 +0800

    Update toolbox_ESP8266.js

--- myBlockly/ESP8266/toolbox_ESP8266.js
+++ myBlockly/ESP8266/toolbox_ESP8266.js
@@ -133,4 +133,6 @@ var catESP8266 = '<xml><category name="%{BKY_ESP8266}" colour="%{BKY_ESP8266_HUE
 '        <block type="esp8266_getresponse" id="KE3ci[=9F6s6fOnlqASf"></block>'+
 '      </value>'+
 '    </block>'+
+'    <block type="esp8266_cmd">'+
+'    </block>'+    
 '</category></xml>';
