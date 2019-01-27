41b6e9e332848e20e1ea12077578945170b8775c
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-28 00:48:56 +0800

    Update toolbox_SpeechRecognition.js

--- myBlockly/SpeechRecognition/toolbox_SpeechRecognition.js
+++ myBlockly/SpeechRecognition/toolbox_SpeechRecognition.js
@@ -2,16 +2,18 @@ var catSpeechRecognition = '<xml>'+
 '<category name="%{BKY_SPEECHRECOGNITION}" colour="%{BKY_SPEECHRECOGNITION_HUE}">'+
 '  <block type="SpeechRecognition_run">'+  
 '  </block>'+       
-'<variables>'+
-'    <variable type="" id="XET3`{C@a}F7i!;%jtKr">Result</variable>'+
-'</variables>'+
-'<block type="variables_set" id="WgoSc$V{4smbOP8+jr80" x="-13" y="-1">'+
-'  <field name="VAR" id="XET3`{C@a}F7i!;%jtKr" variabletype="">Result</field>'+
-'  <value name="VALUE">'+
-'    <block type="SpeechRecognition_get" id="]VM_67C(_2(3TXND*~)+"></block>'+
-'  </value>'+
-'</block>'+
-'  <block type="SpeechRecognition_listener">'+  
-'  </block>'+      
+'  <variables>'+
+'  <variable type="" id="XET3`{C@a}F7i!;%jtKr">Result</variable></variables>'+
+'  <block type="SpeechRecognition_listener" id="5@9068yeP@+Lv8J6dt[;" x="58" y="50">'+
+'    <statement name="do_">'+
+'      <block type="variables_set" id="WgoSc$V{4smbOP8+jr80">'+
+'        <field name="VAR" id="XET3`{C@a}F7i!;%jtKr" variabletype="">Result</field>'+
+'        <value name="VALUE">'+
+'          <block type="SpeechRecognition_get" id="]VM_67C(_2(3TXND*~)+">'+
+'          </block>'+
+'        </value>'+
+'      </block>'+
+'    </statement>'+
+'  </block>'+ 
 '</category>'+
 '</xml>';
