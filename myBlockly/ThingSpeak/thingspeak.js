55b140ff7dbfe34634002ec603f31dc5c0468f74
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-28 18:14:24 +0800

    Update thingspeak.js

--- myBlockly/ThingSpeak/thingspeak.js
+++ myBlockly/ThingSpeak/thingspeak.js
@@ -1,5 +1,8 @@
 // Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 
+var Response=[];
+var getstate = false;
+
 function ThingSpeak_update(key,field1,field2,field3,field4,field5,field6,field7,field8) {
 
   var input_url ="https://api.thingspeak.com/update";
@@ -30,6 +33,42 @@ function ThingSpeak_update(key,field1,field2,field3,field4,field5,field6,field7,
    });
 }
 
-function ThingSpeak_read(key,index,count) {
-  
+function ThingSpeak_read(kind,key,index,count) {
+  if (kind==1)
+    var url ="https://api.thingspeak.com/channels/"+key+"/feeds.json?results="+count;
+  else if (kind==2)
+    var url ="https://api.thingspeak.com/channels/"+key+"/fields/"+count+".json?results="+count;
+  else if (kind==3)
+    var url ="https://api.thingspeak.com/channels/"+key+"/status.json";
+
+  var Response=[];
+  var data = $.ajax({
+      "type": "POST",
+      "dataType": "json",
+      "url": url,
+      success: function(json)
+      {
+        json = eval(json);
+        //console.log(json);
+        getstate = true;
+        for (var i=0;i<json.length;i++) {
+          Response.push(json[i]["data"]+"");
+        }
+        getstate = false;
+      },
+      error: function(jqXHR, textStatus, errorThrown)
+      {
+        //console.log(errorThrown);
+      }
+   });  
+}
+
+function ThingSpeak_getResponse() {
+ if (getstate == false) {
+   var res = Response;
+   Response=[];
+   return res;
+  }
+  else
+    return [];
 }
