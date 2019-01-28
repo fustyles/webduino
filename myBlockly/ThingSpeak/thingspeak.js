114750684ec8f6c2c7b61eddb1f02667dfea5ef0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-28 20:02:26 +0800

    Update thingspeak.js

--- myBlockly/ThingSpeak/thingspeak.js
+++ myBlockly/ThingSpeak/thingspeak.js
@@ -52,7 +52,7 @@ function ThingSpeak_read(kind,key,index,count) {
         console.log(json);
         ThingSpeak_getState = true;
         for (var i=0;i<json.length;i++) {
-          var Feedback= JSON.parse(json[i]);
+          var Feedback= JSON.stringify(json[i]);
           var s=Feedback.indexOf("[");
           Feedback=Feedback.substring(s);
           var e=Feedback.indexOf("]");
