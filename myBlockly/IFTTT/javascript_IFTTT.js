093997502f8bbb3a6d6bc2342108ca1b00455e24
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-30 14:30:21 +0800

    Update javascript_IFTTT.js

--- myBlockly/IFTTT/javascript_IFTTT.js
+++ myBlockly/IFTTT/javascript_IFTTT.js
@@ -2,11 +2,8 @@ Blockly.JavaScript['ifttt_open'] = function (block) {
   var ifttt_event = Blockly.JavaScript.valueToCode(block, 'ifttt_event', Blockly.JavaScript.ORDER_ATOMIC);  
   var ifttt_key = Blockly.JavaScript.valueToCode(block, 'ifttt_key', Blockly.JavaScript.ORDER_ATOMIC);
   var ifttt_value1 = Blockly.JavaScript.valueToCode(block, 'ifttt_value1', Blockly.JavaScript.ORDER_ATOMIC);
-  ifttt_value1 = HextoRgb(ifttt_value1);  //Color Hex to RGB
   var ifttt_value2 = Blockly.JavaScript.valueToCode(block, 'ifttt_value2', Blockly.JavaScript.ORDER_ATOMIC);
-  ifttt_value2 = HextoRgb(ifttt_value2);  //Color Hex to RGB
   var ifttt_value3 = Blockly.JavaScript.valueToCode(block, 'ifttt_value3', Blockly.JavaScript.ORDER_ATOMIC);
-  ifttt_value3 = HextoRgb(ifttt_value3);  //Color Hex to RGB
   
   if (!ifttt_event) ifttt_event='""';
   if (!ifttt_key) ifttt_key='""';
