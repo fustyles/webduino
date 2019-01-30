dd70df7324997d4b3de00d725fa87a1a7d86ceec
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-30 13:45:35 +0800

    Update javascript_SetWindow.js

--- myBlockly/SetWindow/javascript_SetWindow.js
+++ myBlockly/SetWindow/javascript_SetWindow.js
@@ -15,6 +15,7 @@ Blockly.JavaScript['setwindow'] = function (block) {
 Blockly.JavaScript['body_set'] = function (block) {
   var value_property_ = block.getFieldValue('property_');
   var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);
+  value_value_ = HextoRgb(value_value_);  //Color Hex to RGB
   var code = 'body_set("' + value_property_ + '",' + value_value_ + ');\n';
   return code;
 };
@@ -23,3 +24,14 @@ Blockly.JavaScript['window_reload'] = function (block) {
   var code = 'window.location.reload();\n';
   return code;
 };
+
+function HextoRgb(color) {
+  if (!color) return null;
+  if (color.indexOf("'#")==0&&color.length==9) {
+    var color_hex = color.substring(1,color.length-1);
+    var color_rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color_hex);
+    return "'rgb("+parseInt(color_rgb[1], 16)+","+parseInt(color_rgb[2], 16)+","+parseInt(color_rgb[3], 16)+")'";
+  }
+  else
+    return color.replace(/#/g,"%23");
+}
