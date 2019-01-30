04d012801ad75b192d8359158cfaac7517b08f66
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-30 14:19:26 +0800

    Update javascript_IFTTT.js

--- myBlockly/IFTTT/javascript_IFTTT.js
+++ myBlockly/IFTTT/javascript_IFTTT.js
@@ -17,14 +17,3 @@ Blockly.JavaScript['ifttt_open'] = function (block) {
   var code = 'ifttt('+ifttt_event+','+ifttt_key+','+ifttt_value1+','+ifttt_value2+','+ifttt_value3+');\n';
   return code; 
 };
-
-function HextoRgb(color) {
-  if (!color) return null;
-  if (color.indexOf("'#")==0&&color.length==9) {
-    var color_hex = color.substring(1,color.length-1);
-    var color_rgb = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color_hex);
-    return "'rgb("+parseInt(color_rgb[1], 16)+","+parseInt(color_rgb[2], 16)+","+parseInt(color_rgb[3], 16)+")'";
-  }
-  else
-    return color.replace(/#/g,"%23");
-}
