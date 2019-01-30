e154f8e6751bd3d4c3ba098af9a07e829c1f2404
fustyles <34397289+fustyles@users.noreply.github.com>
2019-01-31 01:47:06 +0800

    Update msg_GameElements_zh-hant.js

--- myBlockly/GameElements/msg_GameElements_zh-hant.js
+++ myBlockly/GameElements/msg_GameElements_zh-hant.js
@@ -173,3 +173,4 @@ Blockly.Msg.TEXT_SET_SHOW = "設定";
 Blockly.Msg.TEXT_GET_SHOW = "取得";
 Blockly.Msg.TEXT_DELETE_SHOW = "刪除";
 Blockly.Msg.TEXT_ONCHANGE_SHOW = "改變文字後執行"; 
+Blockly.Msg.BACKCOLOR_SHOW = "背景顏色";
