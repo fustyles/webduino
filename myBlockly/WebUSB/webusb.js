492908fab1c6b80397685220ed5ec11d398e9e67
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-05 02:15:12 +0800

    Update webusb.js

--- myBlockly/WebUSB/webusb.js
+++ myBlockly/WebUSB/webusb.js
@@ -1,33 +1,20 @@
 // Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 
 var webusb_Response=[];
-var webusb_getstate = false;
 
 function webusb_sendCommand(cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
   Response=[];
-  var data = $.ajax({
-      "type": "POST",
-      "dataType": "json",
-      "url": "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9,
-      success: function(json)
-      {
-        json = eval(json);
-        //console.log(json);
-        webusb_getstate = true;
-        for (var i=0;i<json.length;i++) {
-          webusb_Response.push(json[i]["data"]+"");
-        }
-        webusb_getstate = false;
-      },
-      error: function(jqXHR, textStatus, errorThrown)
-      {
-        //console.log(errorThrown);
-      }
-   });
+  document.getElementById("text").value = "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9;
 }
 
 function webusb_getResponse() {
- if (webusb_getstate == false) {
+  if (document.getElementById("text").value != "") {
+    json = eval(document.getElementById("text").value);
+    //console.log(json);
+    for (var i=0;i<json.length;i++) {
+      webusb_Response.push(json[i]["data"]+"");
+    }
+   
    var res = webusb_Response;
    webusb_Response=[];
    return res;
