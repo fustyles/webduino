48f8ac0c92a847c6db0b9bdbb423aabaff541935
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-04 11:28:14 +0800

    Add files via upload

---
+++ myBlockly/WebUSB/demos/arduino/arduino.js
@@ -0,0 +1,66 @@
+(function() {
+  'use strict';
+
+  document.addEventListener('DOMContentLoaded', event => {
+    let connectButton = document.querySelector("#connect");
+    let statusDisplay = document.querySelector('#status');
+    let text = document.querySelector('#text');
+    let send = document.querySelector('#send');
+    let port;
+
+    function connect() {
+      port.connect().then(() => {
+        statusDisplay.textContent = '';
+        connectButton.textContent = 'Disconnect';
+
+        port.onReceive = data => {
+          let textDecoder = new TextDecoder();
+          console.log("receive="+textDecoder.decode(data));
+        }
+        port.onReceiveError = error => {
+          console.error(error);
+        };
+      }, error => {
+        statusDisplay.textContent = error;
+      });
+    }
+
+    function onUpdate() {
+      if (!port) {
+        return;
+      }
+
+      console.log("send="+text.value);
+      const  Input = text.value + '\n';
+      port.send(new TextEncoder("utf-8").encode(Input));
+    };
+
+    send.addEventListener('click', onUpdate);
+
+    connectButton.addEventListener('click', function() {
+      if (port) {
+        port.disconnect();
+        connectButton.textContent = 'Connect';
+        statusDisplay.textContent = '';
+        port = null;
+      } else {
+        serial.requestPort().then(selectedPort => {
+          port = selectedPort;
+          connect();
+        }).catch(error => {
+          statusDisplay.textContent = error;
+        });
+      }
+    });
+
+    serial.getPorts().then(ports => {
+      if (ports.length == 0) {
+        statusDisplay.textContent = 'No device found.';
+      } else {
+        statusDisplay.textContent = 'Connecting...';
+        port = ports[0];
+        connect();
+      }
+    });
+  });
+})();
