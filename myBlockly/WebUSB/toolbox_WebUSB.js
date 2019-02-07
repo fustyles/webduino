1d1813e4e1d8d4688962779b13b2f41a3a6707b1
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-08 00:03:33 +0800

    Update toolbox_WebUSB.js

--- myBlockly/WebUSB/toolbox_WebUSB.js
+++ myBlockly/WebUSB/toolbox_WebUSB.js
@@ -112,61 +112,86 @@ var catWebUSB = '<xml><category name="%{BKY_WEBUSB}" colour="%{BKY_WEBUSB_HUE}">
 '    </block>'+   
 '    <block type="webusb_car_state">'+
 '    </block>'+ 
-'    <variables>'+
-'    <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable></variables>'+
-'    <block type="variables_set" id="Sh:0%7~`mHXezT$HAE*w" x="47" y="75">'+
-'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+
-'    <value name="VALUE">'+
-'    <block type="lists_getIndex" id="YC4ZmrE@j8(9B.R(-EZ^">'+
-'    <mutation statement="false" at="true"></mutation>'+
-'    <field name="MODE">GET</field>'+
-'    <field name="WHERE">FROM_START</field>'+
-'    <value name="VALUE">'+
-'    <block type="webusb_getresponse" id="H@8o|Zo0Ug4PZa}mF1+/"></block>'+
-'    </value>'+
-'    <value name="AT">'+
-'    <block type="math_number" id="E,X52GB~!hog$TAPLq%X">'+
-'    <field name="NUM">1</field></block>'+
-'    </value>'+
-'    </block>'+
-'    </value>'+
-'    <next>'+
-'    <block type="controls_whileUntil" id=";)pwI$s-qv$UjgZ=jc%+">'+
-'    <field name="MODE">WHILE</field>'+
-'    <value name="BOOL">'+
-'    <block type="logic_compare" id="D7f)Ttdk`{FgxL:z0.qd">'+
-'    <field name="OP">EQ</field>'+
-'    <value name="A">'+
-'    <block type="text_indexOf" id="W,B#y$H*|/=mVYD@,d*Y">'+
-'    <field name="END">FIRST</field><value name="VALUE">'+
-'    <block type="variables_get" id="9UYb-];gAMB#tEHD2+Mk">'+
-'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+
-'    </block>'+
-'    </value>'+
-'    <value name="FIND"><block type="text" id="JgcY,U3=qMzoJ(PdraDm"><field name="TEXT">is connected</field>'+
-'    </block>'+
-'    </value>'+
-'    </block>'+
-'    </value>'+
-'    <value name="B">'+
-'    <block type="math_number" id="bkQ[5F;DdLrgbA1Mt?bi">'+
-'    <field name="NUM">0</field>'+
-'    </block>'+
-'    </value>'+
-'    </block>'+
-'    </value>'+
-'    <statement name="DO">'+
-'    <block type="time_delay" id="f[qI4U%I0fat]AKlD%)W">'+
-'    <value name="seconds">'+
-'    <block type="math_number" id="u4NnoJIP%u5eOB~HL/F{">'+
-'    <field name="NUM">0.1</field>'+
-'    </block>'+
-'    </value>'+
-'    </block>'+
-'    </statement>'+
-'    </block>'+
-'    </next>'+
-'    </block>'+
+'    <variables>'+ 
+'    <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable>'+ 
+'    </variables>'+ 
+'    <block type="variables_set" id="Sh:0%7~`mHXezT$HAE*w" x="72" y="326">'+ 
+'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+ 
+'    <value name="VALUE">'+ 
+'    <block type="lists_getIndex" id="YC4ZmrE@j8(9B.R(-EZ^">'+ 
+'    <mutation statement="false" at="true"></mutation>'+ 
+'    <field name="MODE">GET</field>'+ 
+'    <field name="WHERE">FROM_START</field>'+ 
+'    <value name="VALUE">'+ 
+'    <block type="webusb_getresponse" id="H@8o|Zo0Ug4PZa}mF1+/"></block>'+ 
+'    </value>'+ 
+'    <value name="AT">'+ 
+'    <block type="math_number" id="E,X52GB~!hog$TAPLq%X">'+ 
+'    <field name="NUM">1</field>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    <next>'+ 
+'    <block type="controls_whileUntil" id=";)pwI$s-qv$UjgZ=jc%+">'+ 
+'    <field name="MODE">WHILE</field>'+ 
+'    <value name="BOOL">'+ 
+'    <block type="logic_compare" id="D7f)Ttdk`{FgxL:z0.qd">'+ 
+'    <field name="OP">EQ</field>'+ 
+'    <value name="A">'+ 
+'    <block type="text_indexOf" id="W,B#y$H*|/=mVYD@,d*Y">'+ 
+'    <field name="END">FIRST</field>'+ 
+'    <value name="VALUE">'+ 
+'    <block type="variables_get" id="9UYb-];gAMB#tEHD2+Mk">'+ 
+'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    <value name="FIND">'+ 
+'    <block type="text" id="JgcY,U3=qMzoJ(PdraDm">'+ 
+'    <field name="TEXT">is connected</field>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    <value name="B">'+ 
+'    <block type="math_number" id="bkQ[5F;DdLrgbA1Mt?bi">'+ 
+'    <field name="NUM">0</field>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    <statement name="DO">'+ 
+'    <block type="time_delay" id="f[qI4U%I0fat]AKlD%)W">'+ 
+'    <value name="seconds">'+ 
+'    <block type="math_number" id="u4NnoJIP%u5eOB~HL/F{">'+ 
+'    <field name="NUM">0.1</field>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    <next>'+ 
+'    <block type="variables_set" id="P%xFG4!D|8H#j]W:Gf?6">'+ 
+'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+ 
+'    <value name="VALUE">'+ 
+'    <block type="lists_getIndex" id="Fbw4BC|zSKXO*$5O:TNV">'+ 
+'    <mutation statement="false" at="true"></mutation>'+ 
+'    <field name="MODE">GET</field>'+ 
+'    <field name="WHERE">FROM_START</field>'+ 
+'    <value name="VALUE">'+ 
+'    <block type="webusb_getresponse" id=",`ye|0J~]0uRx`TPazU3"></block>'+ 
+'    </value>'+ 
+'    <value name="AT">'+ 
+'    <block type="math_number" id="Vt(E+zy!*=g^U!q,-VgC">'+ 
+'    <field name="NUM">1</field>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    </block>'+ 
+'    </value>'+ 
+'    </block>'+ 
+'    </next>'+ 
+'    </block>'+ 
+'    </statement>'+ 
+'    </block>'+ 
+'    </next>'+ 
+'    </block>'+ 
 '    <variables>'+ 
 '      <variable type="" id="+Oog~D)0D,I*Y5(g]2r1">TimerWEBUSB</variable>'+ 
 '      <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable>'+ 
