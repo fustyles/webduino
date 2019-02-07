59b85f412eac79b84fb29c28cb105adf55b6fcdb
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-07 18:41:09 +0800

    Update en.js

--- myBlockly/en.js
+++ myBlockly/en.js
@@ -19,6 +19,9 @@ Blockly.Msg.height_TEXT = "Height";
 Blockly.Msg.backuptimer_TEXT = "Backup Blocks (every 5 min)";
 Blockly.Msg.example_TEXT = "Example";
 Blockly.Msg.language_TEXT = "Language";
+Blockly.Msg.btndrag_TEXT = "Drag Window";
+Blockly.Msg.btnstopcode_TEXT = "Stop Code";
+Blockly.Msg.btnclosewindow_TEXT = "Close Window";
 
 Blockly.Msg["CATVARIABLES"] = "VARIABLES";
 Blockly.Msg["CATFUNCTIONS"] = "FUNCTIONS";
