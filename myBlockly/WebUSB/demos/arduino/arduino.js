52b10e526508267a13562a0075d4d5808c9a44d8
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-07 15:54:46 +0800

    Update arduino.js

--- myBlockly/WebUSB/demos/arduino/arduino.js
+++ myBlockly/WebUSB/demos/arduino/arduino.js
@@ -10,7 +10,7 @@
 
     function connect() {
       port.connect().then(() => {
-        console.log(port.productName);
+        console.log(port.device_);
         statusDisplay.textContent = 'Device is connected.';
         connectButton.textContent = 'Disconnect to Arduino(USB)';
 
