433505cb134b15b10e0967aa10f0b0f92ff34b88
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-07 12:15:00 +0800

    Update speechrecognition.js

--- myBlockly/SpeechRecognition/speechrecognition.js
+++ myBlockly/SpeechRecognition/speechrecognition.js
@@ -11,6 +11,8 @@ if ('webkitSpeechRecognition' in window) {
   var recognition = new webkitSpeechRecognition();
   recognition.continuous = true;
   recognition.interimResults = true;
+  recognition.start();  // 2019-2-7
+  
   recognition.onstart = function() {
     recognizing = true;
   };
