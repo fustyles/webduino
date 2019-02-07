eb3c1833254e81320bc8fbfa8535587d20f1ccee
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-08 02:02:50 +0800

    Update webusb.js

--- myBlockly/WebUSB/webusb.js
+++ myBlockly/WebUSB/webusb.js
@@ -1,23 +1,22 @@
 // Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 
-var Response="";
 var webusb_Response=[];
 
 function webusb_sendCommand(cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
-  Response = "";
+  document.getElementById("response").innerHTML = "";
   document.getElementById("text").value = "?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9;
 }
 
 function webusb_getResponse() {
   webusb_Response=[];
-  if (Response != "") {
-    if (Response.indexOf("[{")!=-1) {
-      json = eval(Response);
+  if (document.getElementById("response").innerHTML!= "") {
+    if (document.getElementById("response").innerHTML.indexOf("[{")!=-1) {
+      json = eval(document.getElementById("response").innerHTML);
       //console.log(json);
       for (var i=0;i<json.length;i++) {
         webusb_Response.push(json[i]["data"]);
       }
-      Response = "";
+      document.getElementById("response").innerHTML = "";
     } else {
       webusb_Response.push(null);
     }
@@ -31,11 +30,11 @@ function webusb_getResponse() {
 }
 
 function webusb_clearData() {
-  Response = "";
+  document.getElementById("response").innerHTML = "";
   webusb_Response=[];
 }
 
 function webusb_sendCustomCommand(cmd) {
-  Response = "";
+  document.getElementById("response").innerHTML = "";
   document.getElementById("text").value = cmd;
 }
