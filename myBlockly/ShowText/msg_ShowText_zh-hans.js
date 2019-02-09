d1d089f7d16c36d59c798a97191478f3d397c79f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-10 00:33:34 +0800

    Update msg_ShowText_zh-hans.js

--- myBlockly/ShowText/msg_ShowText_zh-hans.js
+++ myBlockly/ShowText/msg_ShowText_zh-hans.js
@@ -5,5 +5,6 @@ Blockly.Msg.SHOWTEXT_SHOW = "显示文字";
 Blockly.Msg.SHOWTEXT_SIZE_SHOW = "大小";
 Blockly.Msg.SHOWTEXT_COLOR_SHOW = "颜色";
 Blockly.Msg.SHOWTEXT_TEXT_SHOW = "文字";
+Blockly.Msg.SHOWTEXT_SET_SHOW = "设定";
 Blockly.Msg.LINEBREAK_SHOW = "换行";
 Blockly.Msg.COMMENT_SHOW = "注解";
