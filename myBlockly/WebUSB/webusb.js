cbcadd65ce0966bd5d96099727022f40204ec956
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-09 22:54:08 +0800

    Update webusb.js

--- myBlockly/WebUSB/webusb.js
+++ myBlockly/WebUSB/webusb.js
@@ -9,7 +9,7 @@ function webusb_sendCommand(cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
 function webusb_getResponse() {
   webusb_Response=[];
   if (document.getElementById("response").innerHTML!= "") {
-    if (document.getElementById("response").innerHTML.indexOf("[{")!=-1) {
+    if ((document.getElementById("response").innerHTML).indexOf("[{")!=-1) {
       json = eval(document.getElementById("response").innerHTML);
       for (var i=0;i<json.length;i++) {
         webusb_Response.push(json[i]["data"]);
