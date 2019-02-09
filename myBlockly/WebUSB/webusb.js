75914b7a981c3e58b5fba6a573d088c2ebc04701
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-09 23:31:13 +0800

    Update webusb.js

--- myBlockly/WebUSB/webusb.js
+++ myBlockly/WebUSB/webusb.js
@@ -8,31 +8,29 @@ function webusb_sendCommand(cmd,P1,P2,P3,P4,P5,P6,P7,P8,P9) {
 
 function webusb_getResponse() {
   webusb_Response=[];
-  if (document.getElementById("response")&&document.getElementById("response")!="undefined") {
-    if (document.getElementById("response").value) {
-      var response = document.getElementById("response").value;
-      document.getElementById("response").value = "";
-    }
-    else {
-      var response = document.getElementById("response").innerHTML;
-      document.getElementById("response").innerHTML = "";
-    }
-    console.log(response);
-    if (response!= "") {
-      if (response.indexOf("[{")!=-1) {
-        json = eval(response);
-        for (var i=0;i<json.length;i++)
-          webusb_Response.push(json[i]["data"]);
-      } 
-      var res = webusb_Response;
-      webusb_Response=[];
-      return res;
-    }
-    else
-      return [];
+  if (!document.getElementById("response")) return [];
+  if (document.getElementById("response").value!="") {
+    var response = document.getElementById("response").value;
+    document.getElementById("response").value = "";
+  }
+  else {
+    var response = document.getElementById("response").innerHTML;
+    document.getElementById("response").innerHTML = "";
+  }
+  if (response=""||response="undefined") return [];
+  console.log(response);
+  if (response!= "") {
+    if (response.indexOf("[{")!=-1) {
+      json = eval(response);
+      for (var i=0;i<json.length;i++)
+        webusb_Response.push(json[i]["data"]);
+    } 
+    var res = webusb_Response;
+    webusb_Response=[];
+    return res;
   }
   else
-    return [];  
+    return [];
 }
 
 function webusb_clearData() {
