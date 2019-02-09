70cd123039516aa4ae6f913901c49a6429fb8994
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-09 23:20:29 +0800

    Update webusb.js

--- myBlockly/WebUSB/webusb.js
+++ myBlockly/WebUSB/webusb.js
@@ -8,25 +8,31 @@ function webusb_sendCommand(cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
 
 function webusb_getResponse() {
   webusb_Response=[];
-  if (document.getElementById("response").value)
-    var response = document.getElementById("response").value;
-  else
-    var response = document.getElementById("response").innerHTML;
-  console.log(response);
-  if (response!= "") {
-    if (response.indexOf("[{")!=-1) {
-      json = eval(response);
-      for (var i=0;i<json.length;i++) {
-        webusb_Response.push(json[i]["data"]);
-      }
+  if (document.getElementById("response")) {
+    if (document.getElementById("response").value) {
+      var response = document.getElementById("response").value;
+      document.getElementById("response").value = "";
+    }
+    else {
+      var response = document.getElementById("response").innerHTML;
       document.getElementById("response").innerHTML = "";
-    } 
-    var res = webusb_Response;
-    webusb_Response=[];
-    return res;
+    }
+    console.log(response);
+    if (response!= "") {
+      if (response.indexOf("[{")!=-1) {
+        json = eval(response);
+        for (var i=0;i<json.length;i++)
+          webusb_Response.push(json[i]["data"]);
+      } 
+      var res = webusb_Response;
+      webusb_Response=[];
+      return res;
+    }
+    else
+      return [];
   }
   else
-    return [];
+    return [];  
 }
 
 function webusb_clearData() {
