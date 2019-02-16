9cecf9815bf2fd0a2ce301d5bef40f8a35e74f83
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-16 17:36:56 +0800

    Update gameelements.js

--- myBlockly/GameElements/gameelements.js
+++ myBlockly/GameElements/gameelements.js
@@ -967,12 +967,14 @@
 	  var height = Number(obj.style.top.replace(/px/ig,"")) + Number(obj.style.height.replace(/px/ig,"")) - Number(canvas.style.top.replace(/px/ig,""));
 	}
 	var context = canvas.getContext("2d");
-        var hexcolor = context.getImageData(left, top, width, height).data;	      
-	for (var i=0;i<hexcolor.length;i+=4) {
-          var r = Number(hexcolor[i]).toString(16).length==1?"0"+Number(hexcolor[i]).toString(16):Number(hexcolor[i]).toString(16);
-          var g = Number(hexcolor[i+1]).toString(16).length==1?"0"+Number(hexcolor[i+1]).toString(16):Number(hexcolor[i+1]).toString(16);
-          var b = Number(hexcolor[i+2]).toString(16).length==1?"0"+Number(hexcolor[i+2]).toString(16):Number(hexcolor[i+2]).toString(16);
-          if (input_color=="#"+r+g+b) return 1;
+	if (width>0&&height>0) {
+          var hexcolor = context.getImageData(left, top, width, height).data;	      
+	  for (var i=0;i<hexcolor.length;i+=4) {
+            var r = Number(hexcolor[i]).toString(16).length==1?"0"+Number(hexcolor[i]).toString(16):Number(hexcolor[i]).toString(16);
+            var g = Number(hexcolor[i+1]).toString(16).length==1?"0"+Number(hexcolor[i+1]).toString(16):Number(hexcolor[i+1]).toString(16);
+            var b = Number(hexcolor[i+2]).toString(16).length==1?"0"+Number(hexcolor[i+2]).toString(16):Number(hexcolor[i+2]).toString(16);
+            if (input_color=="#"+r+g+b) return 1;
+	  }
 	}
         return 0;	      
       }
