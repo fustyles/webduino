2cea32f5bf1ef2b6b0933e76d265ba271bf267c0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-17 23:52:55 +0800

    Update blocks.js

--- GameElements_20190131/blockly/blocks.js
+++ GameElements_20190131/blockly/blocks.js
@@ -1918,7 +1918,7 @@ Blockly.Blocks['select_create'] = {
   this.appendValueInput("color_")
       .setAlign(Blockly.ALIGN_RIGHT)
       .setCheck(null)
-      .appendField(Blockly.Msg.COLOR_SHOW);
+      .appendField(Blockly.Msg.FONTCOLOR_SHOW);
   this.appendValueInput("fontsize_")
       .setAlign(Blockly.ALIGN_RIGHT)
       .setCheck("Number")
