2d4b5e26329a201ad455634a94c9aa9e7b7c28d7
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-18 23:24:33 +0800

    Update linebot.js

--- test_LineBot/linebot.js
+++ test_LineBot/linebot.js
@@ -47,7 +47,7 @@ https://github.com/fustyles/webduino/blob/gs/linenotify_push_message.gs
         "dataType": "jsonp",
         "url": input_url,
         "headers": {
-          "Authorization": 'Bearer ' + notify_token,
+          "Authorization": 'Bearer ' + notify_token
         },
         "data": notify_msg,      
         "payload": notify_msg,
