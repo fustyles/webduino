927eacc33468bf16e9c664e05b929d8f2dd8044c
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-24 20:04:54 +0800

    Update blocks_BitMatrixLed.js

--- myBlockly/BitMatrixLed/blocks_BitMatrixLed.js
+++ myBlockly/BitMatrixLed/blocks_BitMatrixLed.js
@@ -627,3 +627,185 @@ Blockly.Blocks['BitMatrixLed_backcolor'] = {
     this.setColour(300);    
   }
 }; 
+
+Blockly.Blocks['BitMatrixLed_system'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("WebBit System");
+    this.appendDummyInput()
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("cmd")
+        .appendField(new Blockly.FieldDropdown([["inputPullup","inputpullup"], ["pinMode","pinmode"], ["digitalWrite","digitalwrite"], ["digitalRead","digitalread"], ["analogWrite","analogwrite"], ["analogRead","analogread"]]), "cmd");
+    this.appendValueInput("P1")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pin");
+    this.appendValueInput("P2")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("value");
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_custom'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("WebBit Custom");   
+    this.appendValueInput("cmd")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("cmd");
+    this.appendValueInput("P1")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P1");
+    this.appendValueInput("P2")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P2");
+    this.appendValueInput("P3")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P3");
+    this.appendValueInput("P4")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P4");
+    this.appendValueInput("P5")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P5");
+    this.appendValueInput("P6")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P6");
+    this.appendValueInput("P7")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P7");
+    this.appendValueInput("P8")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P8");
+    this.appendValueInput("P9")
+        .setCheck(null)
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("P9");
+    this.setInputsInline(false);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_car'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("WebBit Car");       
+    this.appendValueInput("cmd")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("cmd");    
+    this.appendValueInput("P1")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinL1");
+    this.appendValueInput("P2")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinL2");
+    this.appendValueInput("P3")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinR1");
+    this.appendValueInput("P4")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("pinR2");
+    this.appendValueInput("P5")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("L speed");
+    this.appendValueInput("P6")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("R speed");
+    this.appendValueInput("P7")
+        .setCheck("Number")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("Delay(ms)");
+    this.appendValueInput("P8")
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField("State");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_car_state'] = {
+  init: function () {    
+  this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown([["FORWARD","F"], ["BACKWARD","B"], ["TURNLEFT","L"], ["TURNRIGHT","R"], ["STOP","S"]]), "state");    
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_cmd'] = {
+  init: function () {    
+  this.appendDummyInput()
+      .appendField("cmd")
+      .appendField(new Blockly.FieldDropdown([
+        ["inputPullup","inputpullup"], 
+        ["pinMode(pin)","pinmode"],
+        ["digitalWrite(pin\,value)","digitalwrite"], 
+        ["digitalRead(pin)","digitalread"], 
+        ["analogWrite(pin\,value)","analogwrite"], 
+        ["analogRead(pin)","analogread"],
+        ["car(pinL1\,pinL2\,pinR1\,pinR2\,L_speed\,R_speed\,Delay\,state)","car"],
+        ["i2cLcd(address\,2\,3\,text1\,text2\)","i2cLcd"]
+      ]), "cmd");
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_getresponse'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Get WebBit response data");
+    this.setInputsInline(false);
+    this.setOutput(true, null); 
+    this.setColour(200);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_clearresponse'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Clear WebBit response data");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
+
+Blockly.Blocks['BitMatrixLed_custom_cmd'] = {
+  init: function() {
+    this.appendValueInput("cmd")
+        .setCheck(null)
+        .appendField("WebBit Custom Command");    
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
