1f0041c1920f0c173753d55f840b1c15adfe3f13
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-25 18:40:48 +0800

    Update toolbox_ESP8266.js

--- myBlockly/ESP8266/toolbox_ESP8266.js
+++ myBlockly/ESP8266/toolbox_ESP8266.js
@@ -126,27 +126,46 @@ var catESP8266 = '<xml><category name="%{BKY_ESP8266}" colour="%{BKY_ESP8266_HUE
 '        </value>'+    
 '    </block>'+   
 '    <block type="esp8266_car_state">'+
-'    </block>'+     
-'    <variables>'+ 
-'      <variable type="" id="+Oog~D)0D,I*Y5(g]2r1">TimerESP8266</variable>'+ 
-'      <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable>'+ 
-'    </variables>'+ 
-'    <block type="document_timer" id="mi/z6I4OGW!r-?w:dktl" x="57" y="57">'+ 
-'      <field name="fuTimer_" id="+Oog~D)0D,I*Y5(g]2r1" variabletype="">TimerESP8266</field>'+ 
-'      <value name="intervals_">'+ 
-'        <block type="math_number" id=";yP~x1l8Ovs6JfUe6u0C">'+ 
-'          <field name="NUM">10</field>'+ 
-'        </block>'+ 
-'      </value>'+ 
-'      <statement name="do_">'+ 
-'        <block type="variables_set" id="Sh:0%7~`mHXezT$HAE*w">'+ 
-'          <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+ 
-'          <value name="VALUE">'+ 
-'            <block type="esp8266_getresponse" id="KE3ci[=9F6s6fOnlqASf"></block>'+ 
-'          </value>'+ 
-'        </block>'+ 
-'      </statement>'+ 
-'    </block>'+     
+'    </block>'+   
+'    <variables>'+
+'    <variable type="" id="+Oog~D)0D,I*Y5(g]2r1">Timeresp8266</variable>'+
+'    <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable>'+
+'    </variables>'+
+'    <block type="document_timer" id="mi/z6I4OGW!r-?w:dktl" x="54" y="61">'+
+'    <field name="myTimer_" id="+Oog~D)0D,I*Y5(g]2r1" variabletype="">Timeresp8266</field>'+
+'    <value name="intervals_">'+
+'    <block type="math_number" id=";yP~x1l8Ovs6JfUe6u0C">'+
+'    <field name="NUM">10</field>'+
+'    </block>'+
+'    </value>'+
+'    <statement name="do_">'+
+'    <block type="variables_set" id="3^35KjD6wU1n8qiP7qFf">'+
+'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+
+'    <value name="VALUE">'+
+'    <block type="esp8266_getresponse" id="KE3ci[=9F6s6fOnlqASf"></block>'+
+'    </value>'+
+'    <next>'+
+'    <block type="controls_if" id="w#PldeVhD4}wuNwLI}}}">'+
+'    <value name="IF0">'+
+'    <block type="logic_compare" id="6Ss]O[X@fZ!pd~[B.ral">'+
+'    <field name="OP">NEQ</field>'+
+'    <value name="A">'+
+'    <block type="variables_get" id=":xZEh2=]2it3WG]8r;ym">'+
+'    <field name="VAR" id="ij_ZS;5L7fPB{t3^~CEl" variabletype="">data</field>'+
+'    </block>'+
+'    </value>'+
+'    <value name="B">'+
+'    <block type="text" id="jxnQM7BA/It;K|aWaDE`">'+
+'    <field name="TEXT"></field>'+
+'    </block>'+
+'    </value>'+
+'    </block>'+
+'    </value>'+
+'    </block>'+
+'    </next>'+
+'    </block>'+
+'    </statement>'+
+'    </block>'+    
 '    <block type="esp8266_cmd">'+
 '    </block>'+    
 '    <block type="esp8266_clearresponse">'+
