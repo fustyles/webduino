78bd87574f463e30cb7f25a6823c6d29e912e6e5
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-28 16:56:05 +0800

    Update toolbox_Keyboard.js

--- myBlockly/Keyboard/toolbox_Keyboard.js
+++ myBlockly/Keyboard/toolbox_Keyboard.js
@@ -6,9 +6,11 @@ var catKeyboard = '<xml>'+
 '  </block>'+    
 '  <block type="get_keycode">'+
 '  </block>'+      
-'  <block type="keyboard_keycode">'+
-'  </block>'+    
-'  <block type="hotkey">'+
-'  </block>'+    
+'  <block type="controls_if" id="XgX^Agq}7ua.qa;P`5D0" disabled="true" x="52" y="108">'+
+'  <value name="IF0"><block type="hotkey" id="X$+;Gy]{;C#a08a/M.n1"><field name="keycode">Ctrl</field></block></value>'+
+'  </block>'+
+'  <block type="controls_if" id="M``I^-T|f.+%Fa_#Vdsm" x="192" y="203">'+
+'  <value name="IF0"><block type="keyboard_keycode" id="Yj1F|wtQ:qAeeZ8g`@x*"><field name="keycode">32</field></block></value>'+
+'  </block>'+
 '</category>'+
 '</xml>';
