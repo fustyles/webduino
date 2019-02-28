cb68650f595a4c0aa984970bd8594a8955fa2057
fustyles <34397289+fustyles@users.noreply.github.com>
2019-02-28 16:46:52 +0800

    Update toolbox_WebUSB.js

--- myBlockly/WebUSB/toolbox_WebUSB.js
+++ myBlockly/WebUSB/toolbox_WebUSB.js
@@ -118,7 +118,7 @@ var catWebUSB = '<xml><category name="%{BKY_WEBUSB}" colour="%{BKY_WEBUSB_HUE}">
 '    <variable type="" id="ij_ZS;5L7fPB{t3^~CEl">data</variable>'+
 '    </variables>'+
 '    <block type="document_timer" id="mi/z6I4OGW!r-?w:dktl" x="54" y="61">'+
-'    <field name="fuTimer_" id="+Oog~D)0D,I*Y5(g]2r1" variabletype="">TimerWEBUSB</field>'+
+'    <field name="myTimer_" id="+Oog~D)0D,I*Y5(g]2r1" variabletype="">TimerWEBUSB</field>'+
 '    <value name="intervals_">'+
 '    <block type="math_number" id=";yP~x1l8Ovs6JfUe6u0C">'+
 '    <field name="NUM">10</field>'+
