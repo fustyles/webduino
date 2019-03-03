4f2be12f1ba84957bf57a0069db5efcf3a141c80
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-03 14:54:31 +0800

    Update javascript_Trackface.js

--- myBlockly/tracking/javascript_Trackface.js
+++ myBlockly/tracking/javascript_Trackface.js
@@ -3,12 +3,18 @@ Blockly.JavaScript['trackface_open'] = function (block) {
   var value_left = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_top = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_display = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'openTrackface(' + value_url + ',' + value_left + ',' + value_top + ',' + value_display + ');\n';
+  var code = 'Trackface_open(' + value_url + ',' + value_left + ',' + value_top + ',' + value_display + ');\n';
   return code;
 };
 
 Blockly.JavaScript['trackface_display'] = function(block) {
   var value_display = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'showTrackface(' + value_display + ');\n';
+  var code = 'Trackface_display(' + value_display + ');\n';
   return code;
+};
+
+Blockly.JavaScript['trackface_get'] = function(block) {
+  var value_property = block.getFieldValue('property_');
+  var code = 'Trackface_get("' + value_property + '")';
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
