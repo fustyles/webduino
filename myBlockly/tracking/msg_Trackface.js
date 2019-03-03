9e97838d9b46e103bfb94577a9397d51d2771d7b
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-03 14:55:46 +0800

    Update msg_Trackface.js

--- myBlockly/tracking/msg_Trackface.js
+++ myBlockly/tracking/msg_Trackface.js
@@ -2,7 +2,8 @@ Blockly.Msg["TRACKFACE"] = "Trackface";
 Blockly.Msg["TRACKFACE_HUE"] = "280";
 
 Blockly.Msg.TRACKFACE_OPEN_SHOW = "Open Trackface";
-Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "Show Trackface Window";
+Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "Display Trackface Window";
+Blockly.Msg.TRACKFACE_GET_SHOW = "Get Trackface";
 Blockly.Msg.URL_SHOW = "Url";
 Blockly.Msg.LEFT_SHOW = "Left";
 Blockly.Msg.TOP_SHOW = "Top";
