b0a9e35b76e7e24b559de9b8a2e2d5b07d33d1d7
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-03 14:08:35 +0800

    Update trackface.js

--- myBlockly/tracking/trackface.js
+++ myBlockly/tracking/trackface.js
@@ -1,37 +1,37 @@
   function openURL(input_url,input_left,input_top,input_display) {
    
     if (document.getElementById("trackface")) {
-		var iframe = document.getElementById("trackface");
-		iframe.style.left = input_left + 'px';
-		iframe.style.top = input_top + 'px';
-		if (input_display==1)
-		  iframe.style.display = "block";
-		else
-		  iframe.style.display = "none";
-		iframe.src = input_url;
+	var iframe = document.getElementById("trackface");
+	iframe.style.left = input_left + 'px';
+	iframe.style.top = input_top + 'px';
+	if (input_display==1)
+	  iframe.style.display = "block";
+	else
+	  iframe.style.display = "none";
+	iframe.src = input_url;
     }
     else {
-		var iframe = document.createElement('iframe');
-		iframe.id = "trackface";
-		iframe.style.position = 'absolute';      
-		iframe.style.left = input_left_ + 'px';
-		iframe.style.top = input_top_ + 'px';
-		iframe.style.width = '480px';
-		iframe.style.height = '320px';
-		if (input_display==1)
-		  iframe.style.display = "block";
-		else
-		  iframe.style.display = "none";
-		iframe.setAttribute("src", input_url);
-		document.body.appendChild(iframe);
+	var iframe = document.createElement('iframe');
+	iframe.id = "trackface";
+	iframe.style.position = 'absolute';      
+	iframe.style.left = input_left_ + 'px';
+	iframe.style.top = input_top_ + 'px';
+	iframe.style.width = '480px';
+	iframe.style.height = '320px';
+	if (input_display==1)
+	  iframe.style.display = "block";
+	else
+	  iframe.style.display = "none";
+	iframe.setAttribute("src", input_url);
+	document.body.appendChild(iframe);
     }
   }
   
   function showTrackface(input_display){
     if (document.getElementById("trackface")) {
-		var iframe = document.getElementById("trackface");
-		if (input_display==1)
-		  iframe.style.display = "block";
-		else
-		  iframe.style.display = "none";
+	var iframe = document.getElementById("trackface");
+	if (input_display==1)
+	  iframe.style.display = "block";
+	else
+	  iframe.style.display = "none";
     }
