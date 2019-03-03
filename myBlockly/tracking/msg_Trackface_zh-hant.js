d99a54cf0195ed9b25325486499bf32ca81fd79d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-03 14:10:34 +0800

    Update msg_Trackface_zh-hant.js

--- myBlockly/tracking/msg_Trackface_zh-hant.js
+++ myBlockly/tracking/msg_Trackface_zh-hant.js
@@ -1,8 +1,8 @@
-Blockly.Msg["TRACKFACE"] = "人臉追蹤";
+Blockly.Msg["TRACKFACE"] = "追蹤人臉";
 Blockly.Msg["TRACKFACE_HUE"] = "280";
 
-Blockly.Msg.TRACKFACE_OPEN_SHOW = "開啟人臉追蹤";
-Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "顯示人臉追蹤視窗";
+Blockly.Msg.TRACKFACE_OPEN_SHOW = "開啟追蹤人臉";
+Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "顯示追蹤人臉視窗";
 Blockly.Msg.URL_SHOW = "網址";
 Blockly.Msg.LEFT_SHOW = "靠左距離";
 Blockly.Msg.TOP_SHOW = "靠上距離";
