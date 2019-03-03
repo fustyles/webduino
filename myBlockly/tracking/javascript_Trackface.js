edb81cc4a6f60ffc4a902442fe28233fda7f47e2
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-03 14:07:33 +0800

    Update javascript_Trackface.js

--- myBlockly/tracking/javascript_Trackface.js
+++ myBlockly/tracking/javascript_Trackface.js
@@ -1,12 +1,14 @@
 Blockly.JavaScript['trackface_open'] = function (block) {
-  var value_url_ = Blockly.JavaScript.valueToCode(block, 'url_', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_display_ = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'openTrackface(' + value_url_ + ',' + value_display_ + ');\n';
+  var value_url = Blockly.JavaScript.valueToCode(block, 'url_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_left = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_top = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var value_display = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'openTrackface(' + value_url + ',' + value_left + ',' + value_top + ',' + value_display + ');\n';
   return code;
 };
 
 Blockly.JavaScript['trackface_display'] = function(block) {
-  var value_display_ = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);  
-  var code = 'showTrackface(' + value_display_ + ');\n';
+  var value_display = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'showTrackface(' + value_display + ');\n';
   return code;
 };
