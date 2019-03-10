d42d4c1c345fa3156f83211cd787587d8b057b72
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-11 01:26:45 +0800

    Update blocks_SpeechRecognition.js

--- myBlockly/SpeechRecognition/blocks_SpeechRecognition.js
+++ myBlockly/SpeechRecognition/blocks_SpeechRecognition.js
@@ -138,9 +138,10 @@ Blockly.Blocks['SpeechRecognition_listener'] = {
 
 Blockly.Blocks['SpeechRecognition_keyword'] = {
   init: function () {   
-  this.appendDummyInput() 
-      .appendField(new Blockly.FieldVariable('Result'), 'myVar_')    
-      .appendField(Blockly.Msg.SPEECHRECOGNITION_KEYWORD_SHOW); 
+  this.appendValueInput("myVar_")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.SPEECHRECOGNITION_KEYWORD_SHOW);     
   this.appendValueInput("keyword")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT);     
