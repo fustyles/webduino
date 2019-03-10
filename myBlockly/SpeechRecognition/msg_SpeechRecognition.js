528338202efba0a5d8ac8c13d588e7d3ab8cdc0c
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-10 22:58:23 +0800

    Update msg_SpeechRecognition.js

--- myBlockly/SpeechRecognition/msg_SpeechRecognition.js
+++ myBlockly/SpeechRecognition/msg_SpeechRecognition.js
@@ -3,6 +3,6 @@ Blockly.Msg["SPEECHRECOGNITION_HUE"] = "280";
 
 Blockly.Msg.SPEECHRECOGNITION_SHOW = "Start Speech Recognition";
 Blockly.Msg.SPEECHRECOGNITION_LANGUAGE_SHOW = "Language";
-Blockly.Msg.SPEECHRECOGNITION_GET_SHOW = "Get text of Speech Recognition ";
+Blockly.Msg.SPEECHRECOGNITION_GET_SHOW = "Get Speech Recognition Text";
 Blockly.Msg.SPEECHRECOGNITION_LISTENER_SHOW = "Speech Recognition Listener";
-Blockly.Msg.SPEECHRECOGNITION_KEYWORD_SHOW = "Keyword";
+Blockly.Msg.SPEECHRECOGNITION_KEYWORD_SHOW = "Speech Recognition Text contains Keyword";
