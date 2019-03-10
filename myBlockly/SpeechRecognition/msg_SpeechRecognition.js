14ebbfd4f5a0407f627b9ed2e42aeed38d1dfc43
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-10 22:46:26 +0800

    Update msg_SpeechRecognition.js

--- myBlockly/SpeechRecognition/msg_SpeechRecognition.js
+++ myBlockly/SpeechRecognition/msg_SpeechRecognition.js
@@ -5,3 +5,4 @@ Blockly.Msg.SPEECHRECOGNITION_SHOW = "Start Speech Recognition";
 Blockly.Msg.SPEECHRECOGNITION_LANGUAGE_SHOW = "Language";
 Blockly.Msg.SPEECHRECOGNITION_GET_SHOW = "Get text of Speech Recognition ";
 Blockly.Msg.SPEECHRECOGNITION_LISTENER_SHOW = "Speech Recognition Listener";
+Blockly.Msg.SPEECHRECOGNITION_KEYWORD_SHOW = "Keyword";
