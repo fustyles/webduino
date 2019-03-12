f3925afdd9e90d5a074809454ff01068bdcf4c70
fustyles <34397289+fustyles@users.noreply.github.com>
2019-03-12 19:42:59 +0800

    Update msg_Trackface_zh-hans.js

--- myBlockly/tracking/msg_Trackface_zh-hans.js
+++ myBlockly/tracking/msg_Trackface_zh-hans.js
@@ -7,3 +7,4 @@ Blockly.Msg.TRACKFACE_GET_SHOW = "取得追踪人脸";
 Blockly.Msg.URL_SHOW = "网址";
 Blockly.Msg.LEFT_SHOW = "靠左距离";
 Blockly.Msg.TOP_SHOW = "靠上距离";
+Blockly.Msg.TRACKFACE_DRAWIMAGE_SHOW = "视讯画面描绘至";
