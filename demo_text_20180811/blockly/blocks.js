f7cdc9100ff4cb6a4d6acd83cbdac129c3f895d5
fustyles <34397289+fustyles@users.noreply.github.com>
2019-04-04 20:11:16 +0800

    Update blocks.js

--- demo_text_20180811/blockly/blocks.js
+++ demo_text_20180811/blockly/blocks.js
@@ -8,3 +8,19 @@ Blockly.Blocks['demo_text_add'] = {
     this.setColour(65);
   }
 };
+
+Blockly.Blocks['demo_text_set'] = {
+  init: function() {    
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.DEMO_TEXT_SHOW) 
+      .appendField(Blockly.Msg.DEMO_TEXT_SET_SHOW)     
+      .appendField(new Blockly.FieldDropdown([["left","left"], ["top","top"], ["width","width"], ["height","height"], ["borderstyle","borderstyle"], ["borderwidth","borderwidth"], ["bordercolor","bordercolor"], ["borderradius","borderradius"], ["background","background"], ["color","color"], ["fontsize","fontsize"], ["opacity","opacity"], ["innerHTML","innerHTML"], ["zindex","zindex"], ["display","display"], ["position","position"], ["disabled","disabled"], ["draggable","draggable"]]), "property_");
+  this.appendValueInput("value_")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .setCheck(null);    
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(90);
+  }
+};
