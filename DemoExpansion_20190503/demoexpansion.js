a5f2b2e99b5e0b734cad910b690b4153e4120f3b
fustyles <34397289+fustyles@users.noreply.github.com>
2019-05-04 08:47:17 +0800

    Update demoexpansion.js

--- DemoExpansion_20190503/demoexpansion.js
+++ DemoExpansion_20190503/demoexpansion.js
@@ -8,7 +8,7 @@
 
     var obj = document.getElementById(input_id);
     if (!obj) {
-      var obj = document.getElementsByClassName("demo-area-09-btn " +input_id)[0]; 
+      var obj = document.getElementsByClassName(input_id)[0]; 
     }
     if (!obj) return;
     
