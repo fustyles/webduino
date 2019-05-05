e8f12137ca1b6a65e8f82baf5038ab60990563e9
fustyles <34397289+fustyles@users.noreply.github.com>
2019-05-05 18:09:38 +0800

    Update demoexpansion.js

--- DemoExpansion_20190503/demoexpansion.js
+++ DemoExpansion_20190503/demoexpansion.js
@@ -76,7 +76,7 @@
     if (!document.getElementById("demo-area-01-show")) {
         var div = document.createElement('div');
         div.id = "demo-area-01-show";
-        div.style.zIndex = 999;   
+        div.style.zIndex = 9999;   
         div.style.display = 'block';
         div.innerHTML = "123";
         document.body.insertBefore(div, document.body.firstChild);
