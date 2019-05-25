db99e713c1c4b856164a611ab03d43bcb0d915c4
fustyles <34397289+fustyles@users.noreply.github.com>
2019-05-25 15:37:03 +0800

    Update script.js

--- TensorFlow/DigitRecognition/script.js
+++ TensorFlow/DigitRecognition/script.js
@@ -146,12 +146,15 @@ async function train(model, data) {
 const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
 
 function doPrediction(model, data, testDataSize = 500) {
+  /*
   const IMAGE_WIDTH = 28;
   const IMAGE_HEIGHT = 28;
   const testData = data.nextTestBatch(testDataSize);
   const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
   const labels = testData.labels.argMax([-1]);
   const preds = model.predict(testxs).argMax([-1]);
+  */
+  const preds = model.predict(document.getElementById("digit")).argMax([-1]);
 
   testxs.dispose();
   return [preds, labels];
