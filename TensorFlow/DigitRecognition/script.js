0ada47f2deae132abf52650e6bef0c2c80122aa2
fustyles <34397289+fustyles@users.noreply.github.com>
2019-05-25 15:08:58 +0800

    Update script.js

--- TensorFlow/DigitRecognition/script.js
+++ TensorFlow/DigitRecognition/script.js
@@ -173,3 +173,37 @@ async function showConfusion(model, data) {
 
   labels.dispose();
 }
+
+const classNames = ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+
+function doPrediction(model, data, testDataSize = 500) {
+  const IMAGE_WIDTH = 28;
+  const IMAGE_HEIGHT = 28;
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, IMAGE_WIDTH, IMAGE_HEIGHT, 1]);
+  const labels = testData.labels.argMax([-1]);
+  const preds = model.predict(testxs).argMax([-1]);
+
+  testxs.dispose();
+  return [preds, labels];
+}
+
+
+async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = {name: 'Accuracy', tab: 'Evaluation'};
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames);
+
+  labels.dispose();
+}
+
+async function showConfusion(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  tfvis.render.confusionMatrix(
+      container, {values: confusionMatrix}, classNames);
+
+  labels.dispose();
+}
