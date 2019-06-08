280278204b2862a931d9a9e25612157298c4d26a
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-09 00:06:22 +0800

    Update blocks.js

--- knn-classifier_20190608/blockly/blocks.js
+++ knn-classifier_20190608/blockly/blocks.js
@@ -1,7 +1,7 @@
 Blockly.Blocks['knn-classifier_open'] = {
   init: function() {
   this.appendDummyInput()  
-      .appendField(Blockly.Msg.knn-classifier_OPEN_SHOW);      
+      .appendField(Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW);      
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setColour(65);
@@ -11,7 +11,7 @@ Blockly.Blocks['knn-classifier_open'] = {
 Blockly.Blocks['knn-classifier_proportion'] = {
   init: function() {
   this.appendDummyInput()  
-      .appendField(Blockly.Msg.knn-classifier_PROPORTION_SHOW)
+      .appendField(Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW)
       .appendField(new Blockly.FieldDropdown([["train","train"], ["probability","probability"]]), "property_");    
   this.setInputsInline(true);
   this.setOutput(true, null); 
