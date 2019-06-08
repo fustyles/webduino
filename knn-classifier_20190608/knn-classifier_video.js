b6ef6d827d0cf051fa7c1ce7cb81dd6868b6c9a7
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-09 00:50:10 +0800

    Update knn-classifier_video.js

--- knn-classifier_20190608/knn-classifier_video.js
+++ knn-classifier_20190608/knn-classifier_video.js
@@ -106,7 +106,7 @@ window.onload = function () {
 		//console.log(predict);
 		var msg = "<font color='red'>Label : " + predict.label + "</font><br><br>";;
 		for (i=0;i<Class.length;i++) {
-		  msg += "["+i+"] " + predict.confidences[i.toString()] + "<br>";
+		  if (predict.confidences[i.toString()]) msg += "["+i+"] " + predict.confidences[i.toString()] + "<br>";
 		}
 		result.innerHTML = msg; 
 		train.innerHTML = predict.label;
