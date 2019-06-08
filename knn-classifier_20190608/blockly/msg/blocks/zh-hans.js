12c687f0acb0604d43c791befca49c9365a3868f
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-09 01:20:29 +0800

    Update zh-hans.js

--- knn-classifier_20190608/blockly/msg/blocks/zh-hans.js
+++ knn-classifier_20190608/blockly/msg/blocks/zh-hans.js
@@ -1,2 +1,2 @@
-Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "机械学习KNN 初始化";
-Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "机械学习KNN 最高机率";
+Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "机械学习(KNN) 初始化";
+Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "机械学习(KNN) 最高机率";
