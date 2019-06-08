9a9195d6b07badc806d82a01f86782e910b0c470
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-09 01:12:35 +0800

    Update zh-hant.js

--- knn-classifier_20190608/blockly/msg/blocks/zh-hant.js
+++ knn-classifier_20190608/blockly/msg/blocks/zh-hant.js
@@ -1,2 +1,2 @@
-Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "分類演算法 初始化";
-Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "分類演算法 最高機率";
+Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "KNN分類演算法 初始化";
+Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "KNN分類演算法 最高機率";
