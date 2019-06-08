dd77087ab88fe2830830ee0fe26f780803dc4750
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-09 01:13:03 +0800

    Update en.js

--- knn-classifier_20190608/blockly/msg/blocks/en.js
+++ knn-classifier_20190608/blockly/msg/blocks/en.js
@@ -1,2 +1,2 @@
-Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "knn_classifier Initialize";
-Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "knn_classifier Max Probability";
+Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "KNN Classifier Initialize";
+Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "KNN Classifier Max Probability";
