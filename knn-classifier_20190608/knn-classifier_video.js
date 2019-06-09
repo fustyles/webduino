be53a8291c79cfe78194436845aaa8d31e52e380
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-09 22:22:38 +0800

    Update knn-classifier_video.js

--- knn-classifier_20190608/knn-classifier_video.js
+++ knn-classifier_20190608/knn-classifier_video.js
@@ -104,7 +104,7 @@ window.onload = function () {
     const xlogits = mobilenetModule.infer(Image, 'conv_preds');
     const predict = await classifier.predictClass(xlogits);
     //console.log(predict);
-    var msg = "<font color='red'>Result : train " + predict.label + "</font><br>";
+    var msg = "<font color='red'>Result : class " + predict.label + "</font><br>";
     for (i=0;i<Class.length;i++) {
       if (predict.confidences[i.toString()]>=0) msg += "[train "+i+"] " + predict.confidences[i.toString()] + "<br>";
     }
