3b55e081b7e2592d7358270c5dfa873071c339b0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-15 13:47:16 +0800

    Update javascript_KnnClassifier.js

--- myBlockly/KnnClassifier/javascript_KnnClassifier.js
+++ myBlockly/KnnClassifier/javascript_KnnClassifier.js
@@ -1,9 +1,9 @@
-Blockly.JavaScript['knn_classifier_open'] = function (block) {
+Blockly.JavaScript['knnclassifier_open'] = function (block) {
   var code = 'knn_classifier_open();\n';
   return code;
 };
 
-Blockly.JavaScript['knn_classifier_proportion'] = function(block) { 
+Blockly.JavaScript['knnclassifier_proportion'] = function(block) { 
   var value_property_ = block.getFieldValue('property_');
   var code = 'knn_classifier_proportion("' + value_property_ + '")';
   return [code, Blockly.JavaScript.ORDER_NONE];
