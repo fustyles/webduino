db4ccd5e80260688c92c8efd99c8e022138434ff
fustyles <34397289+fustyles@users.noreply.github.com>
2019-06-15 14:21:12 +0800

    Update msg_KnnClassifier_zh-hant.js

--- myBlockly/KnnClassifier/msg_KnnClassifier_zh-hant.js
+++ myBlockly/KnnClassifier/msg_KnnClassifier_zh-hant.js
@@ -1,4 +1,4 @@
-﻿Blockly.Msg["KNNCLASSIFIER"] = "KNN Classifier";
+﻿Blockly.Msg["KNNCLASSIFIER"] = "機器學習(KNN)";
 Blockly.Msg["KNNCLASSIFIER_HUE"] = "280";
 
 Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "機器學習(KNN) 初始化";
