f17eab79aaccc654ede0694c3abdad4fae0c3904
fustyles <34397289+fustyles@users.noreply.github.com>
2019-07-14 20:00:32 +0800

    Update msg_KnnClassifier_zh-hans.js

--- myBlockly/KnnClassifier/msg_KnnClassifier_zh-hans.js
+++ myBlockly/KnnClassifier/msg_KnnClassifier_zh-hans.js
@@ -1,5 +1,5 @@
-﻿Blockly.Msg["KNNCLASSIFIER"] = "机器学习(KNN)";
+﻿Blockly.Msg["KNNCLASSIFIER"] = "深度学习(KNN)";
 Blockly.Msg["KNNCLASSIFIER_HUE"] = "280";
 
-Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "机器学习(KNN) 初始化";
-Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "机器学习(KNN) 最高";
+Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "深度学习(KNN) 初始化";
+Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "深度学习(KNN) 最高";
