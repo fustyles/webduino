6f3dc020a19533ac30063fe7fd3cbd3bad617a33
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-04 19:11:17 +0800

    Update speechrecognition.js

--- myBlockly/SpeechRecognition/speechrecognition.js
+++ myBlockly/SpeechRecognition/speechrecognition.js
@@ -39,9 +39,10 @@ if ('webkitSpeechRecognition' in window) {
     final_transcript = capitalize(final_transcript);
     Recognition_interim = linebreak(interim_transcript);
     console.log("interim = " + Recognition_interim);
-    if (Recognition_interim.trim()=='')
+    if (Recognition_interim.trim()=='') {
       Recognition_final = linebreak(final_transcript);
       console.log("final   = " + Recognition_interim);
+    }
     else {
       Recognition_final = "";
       if (!document.getElementById("showText")) {
