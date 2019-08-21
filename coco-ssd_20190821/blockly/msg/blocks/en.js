7a93e0cfa4cac52ff78a7773be5db0754682f9e8
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-21 21:48:40 +0800

    Add files via upload

---
+++ coco-ssd_20190821/blockly/msg/blocks/en.js
@@ -0,0 +1,2 @@
+Blockly.Msg.coco_ssd_OPEN_SHOW = "Object Detection (coco-ssd) Initialize";
+Blockly.Msg.coco_ssd_OBJECT_SHOW = "Object Detection (coco-ssd) Object";
---
+++ coco-ssd_20190821/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,2 @@
+Blockly.Msg.coco_ssd_OPEN_SHOW = "物件侦测(coco-ssd) 初始化";
+Blockly.Msg.coco_ssd_OBJECT_SHOW = "物件侦测(coco-ssd) 侦测物件";
