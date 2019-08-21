528359d55f59eeb2f43af8d3b555eddd519836d3
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-21 23:58:21 +0800

    Add files via upload

---
+++ mobilenet_20190821/mobilenet.js
@@ -0,0 +1,22 @@
+// Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
+
++(function (window, document) {
+
+  'use strict';
+
+ function mobilenet_object(input_object){
+    var result = document.getElementById("result").innerHTML.split("<br>");
+    if (result.length>0) {
+      for (var i=0;i<result.length;i++) {
+	var result_detail = result[i].split(",");
+	if (result_detail[0]==input_object) {
+	  return result_detail;
+	}
+      }
+    }
+    return "";
+  }
+
+  window.mobilenet_object = mobilenet_object;
+
+}(window, window.document));
---
+++ mobilenet_20190821/mobilenet_video.js
@@ -0,0 +1,54 @@
+document.write('<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.0.1"></script>');
+document.write('<script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/mobilenet@1.0.0"></script>');
+document.write('<video id="video" width="320" height="240" preload autoplay loop muted></video><canvas id="canvas"></canvas><div id="result" style="width:320px;color:red">Please wait for loading model.</div>');
+
+window.onload = function () {
+    
+  var video = document.getElementById('video');
+  var result = document.getElementById('result');
+  var Model;
+  ObjectDetect(); 
+  
+  function ObjectDetect() {
+    mobilenet.load().then(mobilenet_model => {
+      Model = mobilenet_model;
+      result.innerHTML = "";
+      setTimeout(function(){ startvideo(); }, 2000);
+    }); 
+  }
+  
+  function startvideo() {
+    navigator.mediaDevices
+      .getUserMedia({
+        audio: false,
+        video: {
+          facingMode: "user",
+          width: 320,
+          height: 240
+        }
+      })
+      .then(stream => {
+        video.srcObject = stream
+        video.onloadedmetadata = () => {       
+          video.play();         
+          setTimeout(function(){DetectVideo(); }, 100);
+        }
+      })   
+  }
+                        
+  async function DetectVideo() {
+    await Model.classify(video).then(Predictions => { 
+      result.innerHTML = "";
+      
+      //console.log('Predictions: ', Predictions);
+      if (Predictions.length>0) {
+        for (var i=0;i<Predictions.length;i++) {
+          result.innerHTML+= Predictions[i].className+","+Math.round(Predictions[i].probability,2)+"<br>";
+        }
+      }
+      else
+        result.innerHTML = "Unrecognizable";
+      setTimeout(function(){DetectVideo(); }, 100);
+    });
+  }
+}
