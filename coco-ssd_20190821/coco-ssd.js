82b5cb9bd13d26835f1f6a57a99e9a57cb9800be
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-21 21:53:09 +0800

    Update coco-ssd.js

--- coco-ssd_20190821/coco-ssd.js
+++ coco-ssd_20190821/coco-ssd.js
@@ -6,15 +6,15 @@
 
  function coco_ssd_object(input_object){
     var result = document.getElementById("result").innerHTML.split("<br>");
-	if (result.length>0) {
-      for (i=0;i<result.length;i++) {
-		console.log(result[i]);
-		var result_detail = result[i].split(",");
-		if (result_detail[0].trim()==input_object) {
-	      return result_detail[0];
-		}
-      }
+      if (result.length>0) {
+      for (var i=0;i<result.length;i++) {
+	console.log(result[i]);
+	var result_detail = result[i].split(",");
+	if (result_detail[0].trim()==input_object) {
+	  return result_detail[0];
 	}
+      }
+    }
   }
 
   window.coco_ssd_object = coco_ssd_object;
