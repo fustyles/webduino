f4c1a5c5ff649e1762bcaa2e69e2af8fe75d2073
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-22 17:59:15 +0800

    Add files via upload

---
+++ posenet_20190822/blockly/msg/blocks/en.js
@@ -0,0 +1,2 @@
+Blockly.Msg.MOBILENET_PERSONS_SHOW = "Person Detection (posenet) Number";
+Blockly.Msg.MOBILENET_PART_SHOW = "Part";
---
+++ posenet_20190822/blockly/msg/blocks/zh-hant.js
@@ -0,0 +1,2 @@
+Blockly.Msg.MOBILENET_PERSONS_SHOW = "姿態偵測 (posenet) 人數";
+Blockly.Msg.MOBILENET_PART_SHOW = "部位";
