09f11d18bf3bb6700c988400586fa0a1e9aa149e
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-22 18:04:57 +0800

    Update javascript.js

--- posenet_20190822/blockly/javascript.js
+++ posenet_20190822/blockly/javascript.js
@@ -1,5 +1,5 @@
 Blockly.JavaScript['posenet_person'] = function(block) { 
-  var value_person_ = block.getFieldValue('persons_');
+  var value_person_ = block.getFieldValue('person_');
   var value_part_ = block.getFieldValue('part_');
   var code = 'posenet_person(' + value_person_ + ',"' + value_part_ + '")';
   return [code, Blockly.JavaScript.ORDER_NONE];
