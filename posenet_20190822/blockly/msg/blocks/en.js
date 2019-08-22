94472a5a6e490672e2097040dfff2bb6f6530fe8
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-22 23:26:05 +0800

    Update en.js

--- posenet_20190822/blockly/msg/blocks/en.js
+++ posenet_20190822/blockly/msg/blocks/en.js
@@ -1,2 +1,4 @@
 Blockly.Msg.POSENET_PERSON_SHOW = "Person Detection (posenet) Index";
 Blockly.Msg.POSENET_PART_SHOW = "Part";
+Blockly.Msg.POSENET_WIDTH_SHOW = "Canvas Width";
+Blockly.Msg.POSENET_HEIGHT_SHOW = "Height";
