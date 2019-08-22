300441052d0419b6842a4a62033bec899b72b9ed
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-22 18:40:40 +0800

    Update zh-hant.js

--- posenet_20190822/blockly/msg/blocks/zh-hant.js
+++ posenet_20190822/blockly/msg/blocks/zh-hant.js
@@ -1,2 +1,2 @@
-Blockly.Msg.MOBILENET_PERSONS_SHOW = "姿態偵測 (posenet) 索引";
-Blockly.Msg.MOBILENET_PART_SHOW = "部位";
+Blockly.Msg.POSENET_PERSON_SHOW = "姿態偵測 (posenet) 索引";
+Blockly.Msg.POSENET_PART_SHOW = "部位";
