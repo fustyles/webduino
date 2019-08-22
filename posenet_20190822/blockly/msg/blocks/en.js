00d0506280e7c5ec22041ae23499a7c71900e0eb
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-22 18:09:19 +0800

    Update en.js

--- posenet_20190822/blockly/msg/blocks/en.js
+++ posenet_20190822/blockly/msg/blocks/en.js
@@ -1,2 +1,2 @@
-Blockly.Msg.MOBILENET_PERSONS_SHOW = "Person Detection (posenet) Number";
+Blockly.Msg.MOBILENET_PERSONS_SHOW = "Person Detection (posenet) Index";
 Blockly.Msg.MOBILENET_PART_SHOW = "Part";
