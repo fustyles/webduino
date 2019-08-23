cc33019a5a702bccbf4a9b725e4d859e4e8ab713
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 17:10:35 +0800

    Update zh-hant.js

--- posenet_20190822/blockly/msg/blocks/zh-hant.js
+++ posenet_20190822/blockly/msg/blocks/zh-hant.js
@@ -4,5 +4,6 @@ Blockly.Msg.POSENET_WIDTH_SHOW = "視訊 寬度";
 Blockly.Msg.POSENET_HEIGHT_SHOW = "高度";
 Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "鏡像";
 Blockly.Msg.POSENET_PERSONS_SHOW = "偵測人數";
-Blockly.Msg.POSENET_RESULT_SHOW = "輸出結果";
+Blockly.Msg.POSENET_RESULT_SHOW = "結果";
 Blockly.Msg.POSENET_SCORELIMIT_SHOW = "得分底線";
+Blockly.Msg.POSENET_SKELETON_SHOW = "骨架";
