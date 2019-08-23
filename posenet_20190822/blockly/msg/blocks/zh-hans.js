77510a5ffa9c67d02c3d076c32fc1f3e319d236f
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 11:51:14 +0800

    Update zh-hans.js

--- posenet_20190822/blockly/msg/blocks/zh-hans.js
+++ posenet_20190822/blockly/msg/blocks/zh-hans.js
@@ -3,6 +3,6 @@ Blockly.Msg.POSENET_PART_SHOW = "部位";
 Blockly.Msg.POSENET_WIDTH_SHOW = "视讯 宽度";
 Blockly.Msg.POSENET_HEIGHT_SHOW = "高度";
 Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "镜像";
-Blockly.Msg.POSENET_PERSONS_SHOW = "人数";
+Blockly.Msg.POSENET_PERSONS_SHOW = "侦测人数";
 Blockly.Msg.POSENET_RESULT_SHOW = "输出结果";
 Blockly.Msg.POSENET_SCORELIMIT_SHOW = "得分底线";
