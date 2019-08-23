38903d5913eeeff6fd512dd091917a8e95dae7f8
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 09:24:26 +0800

    Update zh-hant.js

--- posenet_20190822/blockly/msg/blocks/zh-hant.js
+++ posenet_20190822/blockly/msg/blocks/zh-hant.js
@@ -2,3 +2,4 @@ Blockly.Msg.POSENET_PERSON_SHOW = "姿態偵測 (posenet) 索引";
 Blockly.Msg.POSENET_PART_SHOW = "部位";
 Blockly.Msg.POSENET_WIDTH_SHOW = "視訊 寬度";
 Blockly.Msg.POSENET_HEIGHT_SHOW = "高度";
+Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "鏡像";
