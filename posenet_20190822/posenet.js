0dda255ec98edf8b2ec9dbfad167b97b79e6053e
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 17:10:45 +0800

    Update posenet.js

--- posenet_20190822/posenet.js
+++ posenet_20190822/posenet.js
@@ -16,12 +16,13 @@
     return "";
   }
 	
-  function posenet_video(input_width, input_height, input_persons, input_scorelimit, input_mirrorimage, input_result) {
+  function posenet_video(input_width, input_height, input_persons, input_scorelimit, input_mirrorimage, input_result, input_skeleton) {
     document.getElementById('video').width = input_width;
     document.getElementById('video').height = input_height;
     document.getElementById('persons').value = input_persons; 
     document.getElementById('scorelimit').value = input_scorelimit;
     document.getElementById('result').style.display = input_result;
+    document.getElementById('skeleton').value = input_skeleton;
   }
 
   window.posenet_person = posenet_person;
