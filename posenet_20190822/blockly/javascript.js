229b63a879bb66f26f266cc255341bc11a611eae
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 11:31:02 +0800

    Update javascript.js

--- posenet_20190822/blockly/javascript.js
+++ posenet_20190822/blockly/javascript.js
@@ -8,7 +8,9 @@ Blockly.JavaScript['posenet_person'] = function(block) {
 Blockly.JavaScript['posenet_video'] = function(block) { 
   var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_mirrorimage_ = block.getFieldValue('_mirrorimage_');
-  var code = 'posenet_video(' + value_width_ + ',' + value_height_ + ',"' + value_mirrorimage_ + '");';
+  var value_persons_ = block.getFieldValue('persons_');
+  var value_mirrorimage_ = block.getFieldValue('mirrorimage_');
+  var value_result_ = block.getFieldValue('result_');
+  var code = 'posenet_video(' + value_width_ + ',' + value_height_ + ',"' + value_persons_ + '","' + value_mirrorimage_ + '","' + value_result_ + '");';
   return code;
 };
