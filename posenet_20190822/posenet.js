2c7d184b6cb13752349ea9470f223ffddccb6b18
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 08:33:08 +0800

    Update posenet.js

--- posenet_20190822/posenet.js
+++ posenet_20190822/posenet.js
@@ -17,6 +17,7 @@
   }
 	
   function posenet_video(input_width, input_height) {
+    console.log("test");
     document.getElementById('video').width = input_width;
     document.getElementById('video').height = input_height; 
   }
