461c97289380ce617e3a88c27d3926c9c7e7cfff
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 17:10:33 +0800

    Update zh-hans.js

--- posenet_20190822/blockly/msg/blocks/zh-hans.js
+++ posenet_20190822/blockly/msg/blocks/zh-hans.js
@@ -6,3 +6,4 @@ Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "镜像";
 Blockly.Msg.POSENET_PERSONS_SHOW = "侦测人数";
 Blockly.Msg.POSENET_RESULT_SHOW = "输出结果";
 Blockly.Msg.POSENET_SCORELIMIT_SHOW = "得分底线";
+Blockly.Msg.POSENET_SKELETON_SHOW = "骨架";
