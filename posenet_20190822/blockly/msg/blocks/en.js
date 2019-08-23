67142c72b1ff1da1d2d6ae75fbb76bd2f33a7264
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-23 11:51:37 +0800

    Update en.js

--- posenet_20190822/blockly/msg/blocks/en.js
+++ posenet_20190822/blockly/msg/blocks/en.js
@@ -3,6 +3,6 @@ Blockly.Msg.POSENET_PART_SHOW = "Part";
 Blockly.Msg.POSENET_WIDTH_SHOW = "Canvas Width";
 Blockly.Msg.POSENET_HEIGHT_SHOW = "Height";
 Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "Mirror Image";
-Blockly.Msg.POSENET_PERSONS_SHOW = "Persons";
+Blockly.Msg.POSENET_PERSONS_SHOW = "DPersons";
 Blockly.Msg.POSENET_RESULT_SHOW = "Show Result";
 Blockly.Msg.POSENET_SCORELIMIT_SHOW = "Score Limit";
