73450640d025aca2c55eab3891ce41d22a23f354
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 12:00:33 +0800

    Update coco-ssd.js

--- coco-ssd_20190821/coco-ssd.js
+++ coco-ssd_20190821/coco-ssd.js
@@ -16,7 +16,16 @@
     }
     return "";
   }
+	
+  function coco_ssd_video(input_width, input_height, input_result, input_frame, input_mirrorimage) {
+    document.getElementById('video').width = input_width;
+    document.getElementById('video').height = input_height;
+    document.getElementById('result').style.display = input_result;
+    document.getElementById('frame').value = input_frame;
+    document.getElementById('mirrorimage').value = input_mirrorimage;
+  }	
 
   window.coco_ssd_object = coco_ssd_object;
+  window.coco_ssd_video = coco_ssd_video;
 
 }(window, window.document));
