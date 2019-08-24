c3125453a5c4b1f51c37d5d23bb924d940bae6cf
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 11:59:36 +0800

    Update zh-hans.js

--- coco-ssd_20190821/blockly/msg/blocks/zh-hans.js
+++ coco-ssd_20190821/blockly/msg/blocks/zh-hans.js
@@ -1,2 +1,7 @@
-Blockly.Msg.coco_ssd_OPEN_SHOW = "物件侦测(coco-ssd) 初始化";
-Blockly.Msg.coco_ssd_OBJECT_SHOW = "物件侦测(coco-ssd) 侦测物件";
+Blockly.Msg.COCOSSD_OPEN_SHOW = "物件侦测(coco-ssd) 初始化";
+Blockly.Msg.COCOSSD_OBJECT_SHOW = "物件侦测(coco-ssd) 侦测物件";
+Blockly.Msg.COCOSSD_WIDTH_SHOW = "视讯 (coco-ssd) 宽度";
+Blockly.Msg.COCOSSD_HEIGHT_SHOW = "高度";
+Blockly.Msg.COCOSSD_MIRRORIMAGE_SHOW = "镜像";
+Blockly.Msg.COCOSSD_RESULT_SHOW = "结果";
+Blockly.Msg.COCOSSD_FRAME_SHOW = "框架";
