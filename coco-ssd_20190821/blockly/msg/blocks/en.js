feec91f29f9bb68146c5733d1987ee994a2409c7
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 12:00:17 +0800

    Update en.js

--- coco-ssd_20190821/blockly/msg/blocks/en.js
+++ coco-ssd_20190821/blockly/msg/blocks/en.js
@@ -1,2 +1,7 @@
-Blockly.Msg.coco_ssd_OPEN_SHOW = "Object Detection (coco-ssd) Initialize";
-Blockly.Msg.coco_ssd_OBJECT_SHOW = "Object Detection (coco-ssd) Object";
+Blockly.Msg.COCOSSD_OPEN_SHOW = "Object Detection (coco-ssd) Initialize";
+Blockly.Msg.COCOSSD_OBJECT_SHOW = "Object Detection (coco-ssd) Object";
+Blockly.Msg.COCOSSD_WIDTH_SHOW = "Video (coco-ssd) Width";
+Blockly.Msg.COCOSSD_HEIGHT_SHOW = "Height";
+Blockly.Msg.COCOSSD_MIRRORIMAGE_SHOW = "MirrorImage";
+Blockly.Msg.COCOSSD_RESULT_SHOW = "Result";
+Blockly.Msg.COCOSSD_FRAME_SHOW = "Frame";
