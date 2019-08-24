ba87a439a298ecb8a80828f06575640762ddc3e8
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 12:00:53 +0800

    Update javascript.js

--- coco-ssd_20190821/blockly/javascript.js
+++ coco-ssd_20190821/blockly/javascript.js
@@ -3,3 +3,13 @@ Blockly.JavaScript['coco_ssd_object'] = function(block) {
   var code = 'coco_ssd_object("' + value_object_ + '")';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['coco_ssd_video'] = function(block) { 
+  var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_result_ = block.getFieldValue('result_');
+  var value_frame_ = block.getFieldValue('frame_');
+  var value_mirrorimage_ = block.getFieldValue('mirrorimage_');
+  var code = 'posenet_video(' + value_width_ + ',' + value_height_ + ',"' + value_result_ + '","' + value_frame_ + '","' + value_mirrorimage_ + '");';
+  return code;
+};
