0ae5e855a013af2cddb3ed17823b03b401947813
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 13:15:09 +0800

    Update javascript.js

--- knn-classifier_20190608/blockly/javascript.js
+++ knn-classifier_20190608/blockly/javascript.js
@@ -8,3 +8,12 @@ Blockly.JavaScript['knn_classifier_proportion'] = function(block) {
   var code = 'knn_classifier_proportion("' + value_property_ + '")';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
+
+Blockly.JavaScript['knn_classifier_video'] = function(block) { 
+  var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_result_ = block.getFieldValue('result_');
+  var value_mirrorimage_ = block.getFieldValue('mirrorimage_');
+  var code = 'knn_classifier_video(' + value_width_ + ',' + value_height_ + ',"' + value_result_ + '","' + value_mirrorimage_ + '");';
+  return code;
+};
