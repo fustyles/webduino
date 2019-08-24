3dec7cf6bc1e8ba6fd87c67d272b4f1029e2535a
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-24 13:14:00 +0800

    Update en.js

--- knn-classifier_20190608/blockly/msg/blocks/en.js
+++ knn-classifier_20190608/blockly/msg/blocks/en.js
@@ -1,2 +1,6 @@
 Blockly.Msg.KNN_CLASSIFIER_OPEN_SHOW = "Machine Learning(KNN) Initialize";
 Blockly.Msg.KNN_CLASSIFIER_PROPORTION_SHOW = "Machine Learning(KNN) Max";
+Blockly.Msg.KNN_CLASSIFIER_WIDTH_SHOW = "Video (KNN) Width";
+Blockly.Msg.KNN_CLASSIFIER_HEIGHT_SHOW = "Height";
+Blockly.Msg.KNN_CLASSIFIER_MIRRORIMAGE_SHOW = "MirrorImage";
+Blockly.Msg.KNN_CLASSIFIER_RESULT_SHOW = "Result";
