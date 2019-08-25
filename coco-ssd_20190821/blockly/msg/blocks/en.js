148aada8b3b9a4abea7f1fb501acf1d1907521e0
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-25 18:33:00 +0800

    Update en.js

--- coco-ssd_20190821/blockly/msg/blocks/en.js
+++ coco-ssd_20190821/blockly/msg/blocks/en.js
@@ -4,3 +4,4 @@ Blockly.Msg.COCOSSD_HEIGHT_SHOW = "Height";
 Blockly.Msg.COCOSSD_MIRRORIMAGE_SHOW = "MirrorImage";
 Blockly.Msg.COCOSSD_RESULT_SHOW = "Result";
 Blockly.Msg.COCOSSD_FRAME_SHOW = "Frame";
+Blockly.Msg.COCOSSD_OPACITY_SHOW = "Opacity";
