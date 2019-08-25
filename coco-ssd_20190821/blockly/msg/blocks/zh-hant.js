1844f2bac58db27aac80635ec999bde321c0159b
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-25 18:33:12 +0800

    Update zh-hant.js

--- coco-ssd_20190821/blockly/msg/blocks/zh-hant.js
+++ coco-ssd_20190821/blockly/msg/blocks/zh-hant.js
@@ -4,3 +4,4 @@ Blockly.Msg.COCOSSD_HEIGHT_SHOW = "高度";
 Blockly.Msg.COCOSSD_MIRRORIMAGE_SHOW = "鏡像";
 Blockly.Msg.COCOSSD_RESULT_SHOW = "結果";
 Blockly.Msg.COCOSSD_FRAME_SHOW = "框架";
+Blockly.Msg.COCOSSD_OPACITY_SHOW = "透明度";
