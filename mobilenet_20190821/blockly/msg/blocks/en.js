e27fe772260d53edd1d1d9beabb11a16b3930fd7
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-25 18:43:43 +0800

    Update en.js

--- mobilenet_20190821/blockly/msg/blocks/en.js
+++ mobilenet_20190821/blockly/msg/blocks/en.js
@@ -3,3 +3,4 @@ Blockly.Msg.MOBILENET_WIDTH_SHOW = "Video (mobilenet) Width";
 Blockly.Msg.MOBILENET_HEIGHT_SHOW = "Height";
 Blockly.Msg.MOBILENET_MIRRORIMAGE_SHOW = "MirrorImage";
 Blockly.Msg.MOBILENET_RESULT_SHOW = "Result";
+Blockly.Msg.MOBILENET_OPACITY_SHOW = "Opacity";
