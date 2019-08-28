905b5ada4c0e5983c978f9a52e4d87278bffc18c
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-28 09:22:34 +0800

    Add files via upload

---
+++ EDU_addCustomBlocks/addcustomblocks.js
@@ -0,0 +1,13 @@
+// Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
+
++(function (window, document) {
+
+  'use strict';
+
+  function test() {
+    
+  }
+
+  window.test = test;
+
+}(window, window.document));
