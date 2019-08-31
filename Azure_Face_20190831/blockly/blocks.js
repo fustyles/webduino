1477858daaa3445b927f4c5b35e48ef7a8d253f8
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 10:39:48 +0800

    Add files via upload

---
+++ Azure_Face_20190831/blockly/blocks.js
@@ -0,0 +1,81 @@
+Blockly.Blocks['azure_face_settings'] = {
+  init: function() {
+  this.appendDummyInput() 
+      .appendField(Blockly.Msg.AZUREFACE_SETTINGS_SHOW);
+  this.appendValueInput("resourcename_")
+      .setCheck("String")
+      .appendField(Blockly.Msg.AZUREFACE_RESOURCENAME_SHOW);
+  this.appendValueInput("key_")
+      .setCheck("String")
+      .appendField(Blockly.Msg.AZUREFACE_KEY_SHOW);
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['azure_face_part'] = {
+  init: function() { 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.AZUREFACE_PART_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["faceId","faceId"],
+		["faceAttributes","faceAttributes"],
+		["faceRectangle","faceRectangle"],
+		["faceLandmarks","faceLandmarks"],
+		["recognitionModel","recognitionModel"]
+  ]), "part_");  	  
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['azure_face_video'] = {
+  init: function() {
+  this.appendValueInput("width_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.AZUREFACE_WIDTH_SHOW);  
+  this.appendValueInput("height_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.AZUREFACE_HEIGHT_SHOW);  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.AZUREFACE_RESULT_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["show","block"],
+		["hide","none"]	  
+  ]), "result_"); 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.AZUREFACE_FRAME_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["show","1"],
+		["hide","0"]	  
+  ]), "frame_");	  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.AZUREFACE_MIRRORIMAGE_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["yes","1"],
+		["no","0"]	  
+  ]), "mirrorimage_"); 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.AZUREFACE_OPACITY_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["1","1"],
+		["0.9","0.9"],
+		["0.8","0.8"],
+		["0.7","0.7"],
+		["0.6","0.6"],	  
+		["0.5","0.5"],
+		["0.4","0.4"],
+		["0.3","0.3"],
+		["0.2","0.2"],
+		["0.1","0.1"],	
+		["0","0"]	   
+  ]), "opacity_"); 	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
---
+++ Azure_Face_20190831/blockly/javascript.js
@@ -0,0 +1,23 @@
+Blockly.JavaScript['azure_face_settings'] = function(block) { 
+  var value_resourcename_ = Blockly.JavaScript.valueToCode(block, 'resourcename_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_key_ = Blockly.JavaScript.valueToCode(block, 'key_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'azureface_settings(' + value_resourcename_ + ',' + value_key_ + ');';
+  return code;
+};
+
+Blockly.JavaScript['azure_face_part'] = function(block) { 
+  var value_part_ = block.getFieldValue('part_');
+  var code = 'azureface_part("' + value_part_ + '")';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['azure_face_video'] = function(block) { 
+  var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_mirrorimage_ = block.getFieldValue('mirrorimage_');
+  var value_result_ = block.getFieldValue('result_');
+  var value_frame_ = block.getFieldValue('frame_');
+  var value_opacity_ = block.getFieldValue('opacity_');
+  var code = 'azureface_video(' + value_width_ + ',' + value_height_ + ',"' + value_mirrorimage_ + '","' + value_result_ + '","' + value_frame_ + '","' + value_opacity_ + '");';
+  return code;
+};
