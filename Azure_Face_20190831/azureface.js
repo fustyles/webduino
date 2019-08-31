2e7df954effdbdb15b8d84d3793defe95bd41ffb
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 12:13:57 +0800

    Add files via upload

--- Azure_Face_20190831/azureface.js
+++ Azure_Face_20190831/azureface.js
@@ -2,30 +2,30 @@
 
 +(function (window, document) {
 
-  'use strict';
+'use strict';
 
-   function azureface_settings(input_resourceName, input_key){
-     console.log(input_resourceName);
-     console.log(input_key);
-     //document.getElementById('faceApi_ResourceName').innerHTML = "https://" + input_resourceName.trim() + ".cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&recognitionModel=recognition_01&returnRecognitionModel=true&detectionModel=detection_01&returnFaceAttributes=emotion,gender,age";
-     //document.getElementById('faceApi_Key').innerHTML = input_key;
-   }
+function azureface_settings(input_resourceName, input_key){
+console.log(input_resourceName);
+console.log(input_key);
+//document.getElementById('faceApi_ResourceName').innerHTML = "https://" + input_resourceName.trim() + ".cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&recognitionModel=recognition_01&returnRecognitionModel=true&detectionModel=detection_01&returnFaceAttributes=emotion,gender,age";
+//document.getElementById('faceApi_Key').innerHTML = input_key;
+}
 
-   function azureface_part(input_part){
-     return "test";
-   }
+function azureface_part(input_part){
+return "test";
+}
 
-  function azureface_video(input_width, input_height, input_mirrorimage, input_result, input_frame, input_opacity) {
-    document.getElementById('video').width = input_width;
-    document.getElementById('video').height = input_height;
-    document.getElementById('mirrorimage').value = input_mirrorimage;
-    document.getElementById('result').style.display = input_result;
-    document.getElementById('frame').value = input_frame;
-    document.getElementById('opacity').value = input_opacity;
-  }
+function azureface_video(input_width, input_height, input_mirrorimage, input_result, input_frame, input_opacity) {
+document.getElementById('video').width = input_width;
+document.getElementById('video').height = input_height;
+document.getElementById('mirrorimage').value = input_mirrorimage;
+document.getElementById('result').style.display = input_result;
+document.getElementById('frame').value = input_frame;
+document.getElementById('opacity').value = input_opacity;
+}
 
-  window.azureface_settings = azureface_settings;
-  window.azureface_part = azureface_part;
-  window.azureface_video = azureface_video;
+window.azureface_settings = azureface_settings;
+window.azureface_part = azureface_part;
+window.azureface_video = azureface_video;
 
 }(window, window.document));
