b9553da2c63db1d8d36430419a76f668f2d48d58
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 21:14:52 +0800

    Update azureface.js

--- Azure_Face_20190831/azureface.js
+++ Azure_Face_20190831/azureface.js
@@ -25,7 +25,7 @@ function azureface_detect(){
 }
   
 function azureface_get(){
-  return faceApi_result.split(",");
+  return faceApi_result.split(";");
 }
 
 function azureface_video(input_width, input_height, input_result, input_opacity) {
