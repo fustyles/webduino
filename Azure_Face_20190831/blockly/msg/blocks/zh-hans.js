d0830e1ac337d84141f7986b23cd89625c4da810
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 20:19:04 +0800

    Update zh-hans.js

--- Azure_Face_20190831/blockly/msg/blocks/zh-hans.js
+++ Azure_Face_20190831/blockly/msg/blocks/zh-hans.js
@@ -1,8 +1,8 @@
 Blockly.Msg.AZUREFACE_PART_SHOW = "脸部侦测 (Azure) 部位";
 Blockly.Msg.AZUREFACE_WIDTH_SHOW = "视讯 (Azure Face Detection) 宽度";
 Blockly.Msg.AZUREFACE_HEIGHT_SHOW = "高度";
-Blockly.Msg.AZUREFACE_DETECT_SHOW = "脸部侦测";
-Blockly.Msg.AZUREFACE_GET_SHOW = "取得结果";
+Blockly.Msg.AZUREFACE_DETECT_SHOW = "脸部侦测 (Azure)";
+Blockly.Msg.AZUREFACE_GET_SHOW = "取得结果 (Azure)";
 Blockly.Msg.AZUREFACE_RESULT_SHOW = "结果";
 Blockly.Msg.AZUREFACE_OPACITY_SHOW = "透明度";
 Blockly.Msg.AZUREFACE_SETTINGS_SHOW ="脸部侦测 (Azure) 设定";
