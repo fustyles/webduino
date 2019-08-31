42efe189b1d8a47ab068665abe4e3284ba868137
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 11:18:16 +0800

    Update azureface.js

--- Azure_Face_20190831/azureface.js
+++ Azure_Face_20190831/azureface.js
@@ -10,6 +10,7 @@
    function azureface_settings(input_resourceName, input_key){
 	faceApi_ResourceName = "https://" + input_resourceName.trim() + ".cognitiveservices.azure.com/face/v1.0/detect?returnFaceId=true&returnFaceLandmarks=true&recognitionModel=recognition_01&returnRecognitionModel=true&detectionModel=detection_01&returnFaceAttributes=emotion,gender,age";
 	faceApi_Key = input_key;
+	   console.log(input_key);
 	console.log(faceApi_ResourceName);
 	console.log(faceApi_Key);
    }
