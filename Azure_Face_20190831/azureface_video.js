dcc953c20384ee98f237fce90cc3d6bbfe3414e3
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 13:18:46 +0800

    Update azureface_video.js

--- Azure_Face_20190831/azureface_video.js
+++ Azure_Face_20190831/azureface_video.js
@@ -85,6 +85,8 @@ window.onload = function () {
 	      faceApi_result += json[i]["recognitionModel"];
 	      faceApi_result += ";";
 	      */
+		
+	    startvideo()
 	})
 	.fail(function(jqXHR, textStatus, errorThrown) {
 	    // Display error message.
@@ -95,6 +97,7 @@ window.onload = function () {
 		    jQuery.parseJSON(jqXHR.responseText).message :
 			jQuery.parseJSON(jqXHR.responseText).error.message;
 	    alert(errorString);
+	    startvideo()
 	});
 	  
 	  
