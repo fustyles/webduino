12673628fa14ef0d9cdce43c657dfc00142dd71d
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 19:28:22 +0800

    Update azureface.js

--- Azure_Face_20190831/azureface.js
+++ Azure_Face_20190831/azureface.js
@@ -33,6 +33,7 @@ function azureface_video(input_width, input_height, input_result, input_opacity)
 function DetectVideo() {
   if (faceApi_key == ""||faceApi_url == "") return;    
 
+  canvas.style.display = "block";
   const dataURL = canvas.toDataURL('image/png');
   const blobBin = atob(dataURL.split(',')[1]);
   const mime = dataURL.split(',')[0].split(':')[1].split(';')[0];
