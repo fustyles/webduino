aac3528c1cb10d50930fc5239095f90841187e7c
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 13:55:06 +0800

    Update azureface_video.js

--- Azure_Face_20190831/azureface_video.js
+++ Azure_Face_20190831/azureface_video.js
@@ -22,6 +22,7 @@ window.onload = function () {
 	  video.play();
 	  canvas.setAttribute("width", video.width);
 	  canvas.setAttribute("height", video.height); 
+		DetectVideo();
 	}
 	})   
 	                    
