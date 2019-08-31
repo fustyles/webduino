e41dbffda586ce392c774e1b54a4fd47a9a68857
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 10:40:26 +0800

    Add files via upload

---
+++ Azure_Face_20190831/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,10 @@
+Blockly.Msg.AZUREFACE_PART_SHOW = "脸部侦测 (Azure) 部位";
+Blockly.Msg.AZUREFACE_WIDTH_SHOW = "视讯 (Azure Face Detection) 宽度";
+Blockly.Msg.AZUREFACE_HEIGHT_SHOW = "高度";
+Blockly.Msg.AZUREFACE_MIRRORIMAGE_SHOW = "镜像";
+Blockly.Msg.AZUREFACE_RESULT_SHOW = "结果";
+Blockly.Msg.AZUREFACE_FRAME_SHOW = "框架";
+Blockly.Msg.AZUREFACE_OPACITY_SHOW = "透明度";
+Blockly.Msg.AZUREFACE_SETTINGS_SHOW ="脸部侦测 (Azure) 设定";
+Blockly.Msg.AZUREFACE_RESOURCENAME_SHOW ="Resource Name";
+Blockly.Msg.AZUREFACE_KEY_SHOW = "Ocp-Apim-Subscription-Key";
---
+++ Azure_Face_20190831/blockly/msg/blocks/zh-hant.js
@@ -0,0 +1,10 @@
+Blockly.Msg.AZUREFACE_PART_SHOW = "臉部偵測 (Azure) 部位";
+Blockly.Msg.AZUREFACE_WIDTH_SHOW = "視訊 (Azure Face Detection) 寬度";
+Blockly.Msg.AZUREFACE_HEIGHT_SHOW = "高度";
+Blockly.Msg.AZUREFACE_MIRRORIMAGE_SHOW = "鏡像";
+Blockly.Msg.AZUREFACE_RESULT_SHOW = "結果";
+Blockly.Msg.AZUREFACE_FRAME_SHOW = "框架";
+Blockly.Msg.AZUREFACE_OPACITY_SHOW = "透明度";
+Blockly.Msg.AZUREFACE_SETTINGS_SHOW ="臉部偵測 (Azure) 設定";
+Blockly.Msg.AZUREFACE_RESOURCENAME_SHOW ="Resource Name";
+Blockly.Msg.AZUREFACE_KEY_SHOW = "Ocp-Apim-Subscription-Key";
