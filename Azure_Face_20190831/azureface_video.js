7cd271aa61f5a393f62ac5a6fb11e6381aab2434
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 11:43:11 +0800

    Update azureface_video.js

--- Azure_Face_20190831/azureface_video.js
+++ Azure_Face_20190831/azureface_video.js
@@ -1,4 +1,4 @@
-document.write('<div id="region"><video id="video" width="320" height="240" preload autoplay loop muted></video><canvas id="gamecanvas_azureface"></canvas><br>Frame<select id="frame"><option value="1">show</option><option value="0">hide</option></select>MirrorImage<select id="mirrorimage"><option value="1">yes</option><option value="0">no</option></select>Opacity<select id="opacity"><option value="1">1</option><option value="0.9">0.9</option><option value="0.8">0.8</option><option value="0.7">0.7</option><option value="0.6">0.6</option><option value="0.5">0.5</option><option value="0.4">0.4</option><option value="0.3">0.3</option><option value="0.2">0.2</option><option value="0.1">0.1</option><option value="0">0</option></select><br><div id="result" style="width:320px;color:red"></div></div>');
+document.write('<div id="region"><div id="faceApi_ResourceName" style="display:none"><div id="faceApi_Key" style="display:none"></div><video id="video" width="320" height="240" preload autoplay loop muted></video><canvas id="gamecanvas_azureface"></canvas><br>Frame<select id="frame"><option value="1">show</option><option value="0">hide</option></select>MirrorImage<select id="mirrorimage"><option value="1">yes</option><option value="0">no</option></select>Opacity<select id="opacity"><option value="1">1</option><option value="0.9">0.9</option><option value="0.8">0.8</option><option value="0.7">0.7</option><option value="0.6">0.6</option><option value="0.5">0.5</option><option value="0.4">0.4</option><option value="0.3">0.3</option><option value="0.2">0.2</option><option value="0.1">0.1</option><option value="0">0</option></select><br><div id="result" style="width:320px;color:red"></div></div>');
 
 window.onload = function () {
   var video = document.getElementById('video');
@@ -42,7 +42,8 @@ window.onload = function () {
     else
       context.drawImage(video, 0, 0, video.width, video.height);
 
-
+    var faceApi_Key = document.getElementById('faceApi_Key').innerHTML ;
+    var faceApi_ResourceName = document.getElementById('faceApi_ResourceName').innerHTML;
     if (faceApi_Key == ""||faceApi_ResourceName == "") return;
 	  
     console.log("key = " + faceApi_Key);
