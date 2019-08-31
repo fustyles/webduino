d27b1153e971cc9be69ccf469975927e2260962f
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 19:15:09 +0800

    Update en.js

--- Azure_Face_20190831/blockly/msg/blocks/en.js
+++ Azure_Face_20190831/blockly/msg/blocks/en.js
@@ -1,9 +1,7 @@
 Blockly.Msg.AZUREFACE_PART_SHOW = "Face Detection (Azure) Part";
 Blockly.Msg.AZUREFACE_WIDTH_SHOW = "Width";
 Blockly.Msg.AZUREFACE_HEIGHT_SHOW = "Height";
-Blockly.Msg.AZUREFACE_MIRRORIMAGE_SHOW = "Mirror Image";
 Blockly.Msg.AZUREFACE_RESULT_SHOW = "Result";
-Blockly.Msg.AZUREFACE_FRAME_SHOW = "Frame";
 Blockly.Msg.AZUREFACE_OPACITY_SHOW = "Opacity";
 Blockly.Msg.AZUREFACE_SETTINGS_SHOW ="Face Detection (Azure) settings";
 Blockly.Msg.AZUREFACE_RESOURCENAME_SHOW ="Resource Name";
