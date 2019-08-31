d9db7c77430f1b8ec209fc140730deed3a4da554
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-08-31 20:08:40 +0800

    Update zh-hant.js

--- Azure_Face_20190831/blockly/msg/blocks/zh-hant.js
+++ Azure_Face_20190831/blockly/msg/blocks/zh-hant.js
@@ -1,7 +1,8 @@
 Blockly.Msg.AZUREFACE_PART_SHOW = "臉部偵測 (Azure) 部位";
 Blockly.Msg.AZUREFACE_WIDTH_SHOW = "視訊 (Azure Face Detection) 寬度";
 Blockly.Msg.AZUREFACE_HEIGHT_SHOW = "高度";
-Blockly.Msg.AZUREFACE_RESULT_SHOW = "結果";
+Blockly.Msg.AZUREFACE_DETECT_SHOW = "臉部偵測";
+Blockly.Msg.AZUREFACE_GET_SHOW = "取得結果";
 Blockly.Msg.AZUREFACE_OPACITY_SHOW = "透明度";
 Blockly.Msg.AZUREFACE_SETTINGS_SHOW ="臉部偵測 (Azure) 設定";
 Blockly.Msg.AZUREFACE_RESOURCENAME_SHOW ="Resource Name";
