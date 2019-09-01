a71c5cc98813895d43784bc8faf59c9930d45bb2
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-01 20:27:24 +0800

    Update zh-hant.js

--- Azure_ClassifyImage_20190901/blockly/msg/blocks/zh-hant.js
+++ Azure_ClassifyImage_20190901/blockly/msg/blocks/zh-hant.js
@@ -1,8 +1,8 @@
 Blockly.Msg.AZURECLASSIFYIMAGE_PART_SHOW = "圖像分類 (Azure) 部位";
-Blockly.Msg.AZURECLASSIFYIMAGE_WIDTH_SHOW = "視訊 (Azure Object Detection) 寬度";
+Blockly.Msg.AZURECLASSIFYIMAGE_WIDTH_SHOW = "圖像分類 (Azure) 視訊寬度";
 Blockly.Msg.AZURECLASSIFYIMAGE_HEIGHT_SHOW = "高度";
 Blockly.Msg.AZURECLASSIFYIMAGE_DETECT_SHOW = "圖像分類 (Azure)";
-Blockly.Msg.AZURECLASSIFYIMAGE_GET_SHOW = "取得結果 (Azure)";
+Blockly.Msg.AZURECLASSIFYIMAGE_GET_SHOW = "圖像分類取得結果 (Azure)";
 Blockly.Msg.AZURECLASSIFYIMAGE_RESULT_SHOW = "結果";
 Blockly.Msg.AZURECLASSIFYIMAGE_OPACITY_SHOW = "透明度";
 Blockly.Msg.AZURECLASSIFYIMAGE_SETTINGS_SHOW ="圖像分類 (Azure) 設定";
