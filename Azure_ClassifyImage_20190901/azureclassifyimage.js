b5f00e261256d97f8c6bebe2a75b8b8f2c8e2b26
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-01 21:08:54 +0800

    Update azureclassifyimage.js

--- Azure_ClassifyImage_20190901/azureclassifyimage.js
+++ Azure_ClassifyImage_20190901/azureclassifyimage.js
@@ -92,11 +92,10 @@ function azureclassifyimage_detectvideo() {
       }
       Prediction_result = maxName + "," + max + Prediction_result;
       
-		  context.lineWidth = "3";
-		  context.font = "16px Arial";
-		  context.fillStyle = "#99FF99";
-		  context.fillText(maxName+" : "+max , 0,  0);     			
-			      
+      context.lineWidth = "3";
+      context.font = "16px Arial";
+      context.fillStyle = "#99FF99";
+      context.fillText(maxName+" : "+max , 0,  0);     					      
     }
     catch (e) {
       Prediction_result = "Error,0";
