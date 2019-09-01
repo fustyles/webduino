48b0467f7692f52516003c706025adc3dcf40771
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-01 10:01:49 +0800

    Update zh-hans.js

--- Azure_FaceDetection_20190901/blockly/msg/blocks/zh-hans.js
+++ Azure_FaceDetection_20190901/blockly/msg/blocks/zh-hans.js
@@ -1,11 +1,11 @@
 Blockly.Msg.AZUREFACEDETECTION_PART_SHOW = "脸部侦测 (Azure) 部位";
 Blockly.Msg.AZUREFACEDETECTION_WIDTH_SHOW = "视讯 (Azure Face Detection) 宽度";
 Blockly.Msg.AZUREFACEDETECTION_HEIGHT_SHOW = "高度";
-Blockly.Msg.AZUREFACEDETECTION_DETECT_SHOW = "脸部侦测 (Azure)";
-Blockly.Msg.AZUREFACEDETECTION_GET_SHOW = "取得结果 (Azure)";
+Blockly.Msg.AZUREFACEDETECTION_DETECT_SHOW = "脸部执行侦测 (Azure)";
+Blockly.Msg.AZUREFACEDETECTION_GET_SHOW = "取得结果 (Azure Face Detection)";
 Blockly.Msg.AZUREFACEDETECTION_RESULT_SHOW = "结果";
 Blockly.Msg.AZUREFACEDETECTION_OPACITY_SHOW = "透明度";
 Blockly.Msg.AZUREFACEDETECTION_SETTINGS_SHOW ="脸部侦测 (Azure) 设定";
 Blockly.Msg.AZUREFACEDETECTION_RESOURCENAME_SHOW ="Resource Name";
 Blockly.Msg.AZUREFACEDETECTION_KEY_SHOW = "Ocp-Apim-Subscription-Key";
-Blockly.Msg.AZUREFACEDETECTION_SHOWTIME_SHOW = "显示照片(秒)";
+Blockly.Msg.AZUREFACEDETECTION_SHOWTIME_SHOW = " 与显示照片(秒)";
