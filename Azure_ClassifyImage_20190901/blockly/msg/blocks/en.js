9dca748dbc28495f8508f80650ea8f4b462eae78
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-02 22:26:36 +0800

    Update en.js

--- Azure_ClassifyImage_20190901/blockly/msg/blocks/en.js
+++ Azure_ClassifyImage_20190901/blockly/msg/blocks/en.js
@@ -1,11 +1,11 @@
-Blockly.Msg.AZURECLASSIFYIMAGE_PART_SHOW = "Azure Classify Image Part";
+Blockly.Msg.AZURECLASSIFYIMAGE_PART_SHOW = "Classify Image (Azure) Part";
 Blockly.Msg.AZURECLASSIFYIMAGE_WIDTH_SHOW = "Width";
 Blockly.Msg.AZURECLASSIFYIMAGE_HEIGHT_SHOW = "Height";
-Blockly.Msg.AZURECLASSIFYIMAGE_DETECT_SHOW = "Azure Classify Image";
+Blockly.Msg.AZURECLASSIFYIMAGE_DETECT_SHOW = "Classify Image (Azure)";
 Blockly.Msg.AZURECLASSIFYIMAGE_GET_SHOW = "Get Result (Azure Classify Image)";
 Blockly.Msg.AZURECLASSIFYIMAGE_RESULT_SHOW = "Result";
 Blockly.Msg.AZURECLASSIFYIMAGE_OPACITY_SHOW = "Opacity";
-Blockly.Msg.AZURECLASSIFYIMAGE_SETTINGS_SHOW ="Azure Classify Image settings";
+Blockly.Msg.AZURECLASSIFYIMAGE_SETTINGS_SHOW ="Classify Image (Azure) settings";
 Blockly.Msg.AZURECLASSIFYIMAGE_SHOWTIME_SHOW = "Show Image(s)";
 Blockly.Msg.AZURECLASSIFYIMAGE_ENDPOINTHOST_SHOW ="Endpoint Host";
 Blockly.Msg.AZURECLASSIFYIMAGE_PREDICTIONKEY_SHOW = "Prediction Key";
