9d4848c458d18f1d906f046e05afae7055482616
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-08 23:29:29 +0800

    Update setwindow.js

--- myBlockly/SetWindow/setwindow.js
+++ myBlockly/SetWindow/setwindow.js
@@ -6,7 +6,13 @@
     else if (input_property=="overflow") 
       document.body.style.overflow = input_value; 
     else if (input_property=="style")
-      document.body.style = input_value;      
+      document.body.style = input_value;    
+    else if (input_property=="fullscreen") {
+      if (input_value==true)
+        window.parent.document.getElementById("runWindow").webkitRequestFullscreen();
+      else
+        window.parent.document.webkitExitFullscreen();	    
+    }      
     else if (input_property=="droppable") {
       if (input_value==1) {
         document.body.setAttribute("ondrop","javascript:var obj=document.getElementById(event.dataTransfer.getData('text/plain'));obj.style.position='static';event.preventDefault();if(event.target.tagName!='BODY') {return false;} else {event.target.appendChild(obj);obj.style.position='absolute';}");
