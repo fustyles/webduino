cabe35de69e5334e82daac8e98cffe23cd52a1db
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-08 22:55:41 +0800

    Update gameelements.js

--- GameElements_20190131/gameelements.js
+++ GameElements_20190131/gameelements.js
@@ -2064,9 +2064,9 @@ function HextoRgb(color) {
       document.body.style = input_value;
     else if (input_property=="fullscreen") {
       if (input_value==true)
-        window.parent.document.getElementsByTagName("IFRAME")[0].webkitRequestFullscreen();
+        window.parent.document.getElementsByTagName("IFRAME")[1].webkitRequestFullscreen();
       else
-	window.parent.document.getElementsByTagName("IFRAME")[0].webkitExitFullscreen();	    
+	window.parent.document.getElementsByTagName("IFRAME")[1].webkitExitFullscreen();	    
     }
     else if (input_property=="droppable") {
       if (input_value==1) {
