a01262d2c68ab1399707007a6f2260b12dfb4b56
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 12:00:31 +0800

    Update zh-hans.js

--- Tracking_20190917/blockly/msg/blocks/zh-hans.js
+++ Tracking_20190917/blockly/msg/blocks/zh-hans.js
@@ -4,3 +4,4 @@ Blockly.Msg.TRACKFACE_GET_SHOW = "取得追踪人脸";
 Blockly.Msg.LEFT_SHOW = "靠左距离";
 Blockly.Msg.TOP_SHOW = "靠上距离";
 Blockly.Msg.TRACKFACE_DRAWIMAGE_SHOW = "视讯画面描绘至";
+Blockly.Msg.CANVAS_ID_SHOW = "画布代码";
