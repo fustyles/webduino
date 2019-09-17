557b7d65a407c9d4f7d80081974ca8ef109f7ff4
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 10:22:55 +0800

    Update zh-hans.js

--- Tracking_20190917/blockly/msg/blocks/zh-hans.js
+++ Tracking_20190917/blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,3 @@
-Blockly.Msg["TRACKFACE"] = "追踪人脸";
-Blockly.Msg["TRACKFACE_HUE"] = "280";
-
 Blockly.Msg.TRACKFACE_OPEN_SHOW = "开启追踪人脸";
 Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "显示追踪人脸视窗";
 Blockly.Msg.TRACKFACE_GET_SHOW = "取得追踪人脸";
