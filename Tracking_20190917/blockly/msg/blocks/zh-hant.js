eb3942898ac6ff3a8bc83e48abc089098ac0519f
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 10:50:41 +0800

    Update zh-hant.js

--- Tracking_20190917/blockly/msg/blocks/zh-hant.js
+++ Tracking_20190917/blockly/msg/blocks/zh-hant.js
@@ -1,7 +1,5 @@
 Blockly.Msg.TRACKFACE_OPEN_SHOW = "開啟追蹤人臉";
 Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "顯示追蹤人臉視窗";
 Blockly.Msg.TRACKFACE_GET_SHOW = "取得追蹤人臉";
-Blockly.Msg.URL_SHOW = "網址";
 Blockly.Msg.LEFT_SHOW = "靠左距離";
 Blockly.Msg.TOP_SHOW = "靠上距離";
-Blockly.Msg.TRACKFACE_DRAWIMAGE_SHOW = "視訊畫面描繪至";
