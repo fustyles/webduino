e3f6f8920d2ed4ae2aca9771654301d6c52adb07
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 12:02:11 +0800

    Update javascript.js

--- Tracking_20190917/blockly/javascript.js
+++ Tracking_20190917/blockly/javascript.js
@@ -18,25 +18,10 @@ Blockly.JavaScript['trackface_get'] = function(block) {
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
-Blockly.Blocks['trackface_drawimage'] = {
-  init: function() {
-  this.appendDummyInput()  
-      .appendField(Blockly.Msg.TRACKFACE_DRAWIMAGE_SHOW);    
-  this.appendValueInput("id_")
-      .setCheck(null)
-      .setAlign(Blockly.ALIGN_RIGHT)    
-      .appendField(Blockly.Msg.CANVAS_ID_SHOW); 
-  this.appendValueInput("left_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.LEFT_SHOW);    
-  this.appendValueInput("top_")
-      .setCheck("Number")
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.TOP_SHOW); 
-  this.setInputsInline(true);	  
-  this.setPreviousStatement(true);
-  this.setNextStatement(true);
-  this.setColour(60);
-  }
+Blockly.JavaScript['trackface_drawimage'] = function (block) {
+  var value_id = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_left = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_top = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'Trackface_drawimage(' + value_id + ',' + value_left + ',' + value_top + ');\n';
+  return code;
 };
