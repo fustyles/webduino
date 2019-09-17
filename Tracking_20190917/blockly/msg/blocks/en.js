78722ec4dba016e67a77a73e58ad764581ff14e5
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 11:08:06 +0800

    Update en.js

--- Tracking_20190917/blockly/msg/blocks/en.js
+++ Tracking_20190917/blockly/msg/blocks/en.js
@@ -3,3 +3,4 @@ Blockly.Msg.TRACKFACE_DISPLAY_SHOW = "Display TrackFace Window";
 Blockly.Msg.TRACKFACE_GET_SHOW = "TrackFace Get";
 Blockly.Msg.LEFT_SHOW = "Left";
 Blockly.Msg.TOP_SHOW = "Top";
+Blockly.Msg.TRACKFACE_DRAWIMAGE_SHOW = "Capture vedio and draw image on";
