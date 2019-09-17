74224c3cfc7919c75ecec77ebe14c5af50cf085a
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 10:24:55 +0800

    Update javascript.js

--- Tracking_20190917/blockly/javascript.js
+++ Tracking_20190917/blockly/javascript.js
@@ -1,9 +1,8 @@
 Blockly.JavaScript['trackface_open'] = function (block) {
-  var value_url = Blockly.JavaScript.valueToCode(block, 'url_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_left = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
   var value_top = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);  
   var value_display = Blockly.JavaScript.valueToCode(block, 'display_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'Trackface_open(' + value_url + ',' + value_left + ',' + value_top + ',' + value_display + ');\n';
+  var code = 'Trackface_open(' + value_left + ',' + value_top + ',' + value_display + ');\n';
   return code;
 };
 
