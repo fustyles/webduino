6ff0fc8e8cdab5ce0ea3294c4f02e6b40d920ee3
法蘭斯 <34397289+fustyles@users.noreply.github.com>
2019-09-17 11:46:16 +0800

    Update blocks.js

--- Tracking_20190917/blockly/blocks.js
+++ Tracking_20190917/blockly/blocks.js
@@ -110,3 +110,26 @@ Blockly.Blocks['trackface_get'] = {
     this.setColour(200);
   }
 };
+
+Blockly.Blocks['trackface_drawimage'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TRACKFACE_DRAWIMAGE_SHOW);    
+  this.appendValueInput("id_")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)    
+      .appendField(Blockly.Msg.CANVAS_ID_SHOW); 
+  this.appendValueInput("left_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.LEFT_SHOW);    
+  this.appendValueInput("top_")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.TOP_SHOW); 
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
