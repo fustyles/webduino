b8579b8b349ce887591500a32f75e3c4e877cd30
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-22 18:52:18 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -1,6 +1,5 @@
 Blockly.JavaScript['ws2812_expansion_code24'] = function (block) {
   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
-  var ws2812Code = "";
   var code = "";
   for (var i=0;i<=23;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);
@@ -9,28 +8,8 @@ Blockly.JavaScript['ws2812_expansion_code24'] = function (block) {
       if ((value_color.indexOf("(")==0)&&(value_color.lastIndexOf(")")==value_color.length-1))
         value_color = value_color.substring(1,value_color.length-1);
       if (value_color!="#000000"&&value_color.length==7) {
-        console.log(value_color);
-        if (i<16)
-          ws2812Code += "0" + i.toString(16) + value_color.replace("#","");
-        else
-          ws2812Code += i.toString(16) + value_color.replace("#","");
+        code += variable_ws2812+".setColor("+i+",'"+value_color+"');\n";
       }
   }
-   
-  console.log(ws2812Code);
-
-  if (ws2812Code.substring(0, 56)!="") {
-    code += variable_ws2812+".setColor('"+ws2812Code.substring(0, 56)+"');\n";
-  }
-  if (ws2812Code.substring(56, 112)!="") {
-    code += variable_ws2812+".setColor('"+ws2812Code.substring(56, 112)+"');\n";
-  }
-  if (ws2812Code.substring(112, 168)!="") {
-    code += variable_ws2812+".setColor('"+ws2812Code.substring(112, 168)+"');\n";
-  }
-  if (ws2812Code.substring(168, 192)!="") {
-    code += variable_ws2812+".setColor('"+ws2812Code.substring(168, 192)+"');\n";
-  }
-
    return code;
 };
