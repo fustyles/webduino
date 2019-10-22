5e6fdc8e98126e0cae42333bf81dff287d93bbe1
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-22 20:11:47 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -4,7 +4,7 @@ Blockly.JavaScript['ws2812_expansion_1to8'] = function (block) {
   var code = "";
   for (var i=0;i<=7;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
-      if (value_color.indexOf("#000000")==-1&&value_color.length==9) {
+      if (value_color.indexOf("#000000")==-1) {
         if (change==1)
           code += variable_ws2812+".setColor("+i+",'"+"#"+value_color.substr(4,2)+value_color.substr(2,2)+value_color.substr(6,2)+"');\n";
         else
@@ -20,7 +20,7 @@ Blockly.JavaScript['ws2812_expansion_9to16'] = function (block) {
   var code = "";
   for (var i=8;i<=15;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
-      if (value_color.indexOf("#000000")==-1&&value_color.length==9) {
+      if (value_color.indexOf("#000000")==-1) {
         if (change==1)
           code += variable_ws2812+".setColor("+i+",'"+"#"+value_color.substr(4,2)+value_color.substr(2,2)+value_color.substr(6,2)+"');\n";
         else
@@ -36,7 +36,7 @@ Blockly.JavaScript['ws2812_expansion_17to24'] = function (block) {
   var code = "";
   for (var i=16;i<=23;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
-      if (value_color.indexOf("#000000")==-1&&value_color.length==9) {
+      if (value_color.indexOf("#000000")==-1) {
         if (change==1)
           code += variable_ws2812+".setColor("+i+",'"+"#"+value_color.substr(4,2)+value_color.substr(2,2)+value_color.substr(6,2)+"');\n";
         else
