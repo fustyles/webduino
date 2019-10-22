4d5e57451a812652e991afe2099066270d47fc4d
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-22 18:05:05 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -1,30 +1,30 @@
 Blockly.JavaScript['ws2812_expansion_code24'] = function (block) {
-   var ws2812_ = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
-   console.log(ws2812_);
+   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
    var ws2812Code = "";
    var code = "";
    for (var i=0;i<=23;i++) {
-       var color_ = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);
-       console.log(color_);
-       if (color_ != "#000000"&&color_.length=="7") {
+       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);
+       if (value_color!="#000000"&&value_color.length==7) {
          if (i<10)
-           ws2812Code += "0" + i + color_.replace("#","");
+           ws2812Code += "0" + i + value_color.replace("#","");
          else
-           ws2812Code += i + color_.replace("#","");
+           ws2812Code += i + value_color.replace("#","");
        }
    }
+   
+   console.log(ws2812Code);
 
-   if (ws2812Code.substring(0, 55) != "") {
-     code += ws2812_+".setColor('"+ws2812Code.substring(0, 55)+"');\n";
+   if (ws2812Code.substring(0, 56) != "") {
+     code += variable_ws2812+".setColor('"+ws2812Code.substring(0, 56)+"');\n";
    }
-   if (ws2812Code.substring(56, 111) != "") {
-     code += ws2812_+".setColor('"+ws2812Code.substring(56, 111)+"');\n";
+   if (ws2812Code.substring(56, 112) != "") {
+     code += variable_ws2812+".setColor('"+ws2812Code.substring(56, 112)+"');\n";
    }
-   if (ws2812Code.substring(112, 167) != "") {
-     code += ws2812_+".setColor('"+ws2812Code.substring(112, 167)+"');\n";
+   if (ws2812Code.substring(112, 168) != "") {
+     code += variable_ws2812+".setColor('"+ws2812Code.substring(112, 168)+"');\n";
    }
-   if (ws2812Code.substring(168, 191) != "") {
-     code += ws2812_+".setColor('"+ws2812Code.substring(168, 191)+"');\n";
+   if (ws2812Code.substring(168, 192) != "") {
+     code += variable_ws2812+".setColor('"+ws2812Code.substring(168, 192)+"');\n";
    }
 
     return code;
