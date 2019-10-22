a98a72c29033d70e0da881709f6e423130f97fcd
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-22 19:36:25 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -5,7 +5,7 @@ Blockly.JavaScript['ws2812_expansion_code24'] = function (block) {
   for (var i=0;i<=23;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
       console.log(value_color.length);
-      if (value_color.indexOf("#000000")!=-1&&value_color.length==9) {
+      if (value_color.indexOf("#000000")==-1&&value_color.length==9) {
         if (change==1)
           code += variable_ws2812+".setColor("+i+","+value_color.substr(4,2)+value_color.substr(2,2)+value_color.substr(6,2)+");\n";
         else
