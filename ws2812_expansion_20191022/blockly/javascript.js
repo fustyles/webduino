3ca2edfebdab248d2536936ff9814c8e1d857479
fustyles <34397289+fustyles@users.noreply.github.com>
2019-10-22 21:32:28 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -1,3 +1,19 @@
+Blockly.JavaScript['ws2812_expansion_1to24'] = function (block) {
+  var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
+  var change = (block.getFieldValue('change_') == 'TRUE')?"1":"0";
+  var code = "";
+  for (var i=0;i<=23;i++) {
+      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
+      if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
+        if (change==1)
+          code += variable_ws2812+".setColor("+i+",'#'+"+value_color+".substr(4,2)+"+value_color+".substr(2,2)+"+value_color+".substr(6,2));\n";
+        else
+          code += variable_ws2812+".setColor("+i+","+value_color+");\n";
+      }
+  }
+   return code;
+};
+
 Blockly.JavaScript['ws2812_expansion_1to8'] = function (block) {
   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
   var change = (block.getFieldValue('change_') == 'TRUE')?"1":"0";
