518e63fba49ef9d93c4a913168737d0891acb68b
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-14 01:23:11 +0800

    Update en.js

--- knn-classifier_20190608/blockly/msg/blocks/en.js
+++ knn-classifier_20190608/blockly/msg/blocks/en.js
@@ -5,3 +5,5 @@ Blockly.Msg.KNN_CLASSIFIER_HEIGHT_SHOW = "Height";
 Blockly.Msg.KNN_CLASSIFIER_MIRRORIMAGE_SHOW = "MirrorImage";
 Blockly.Msg.KNN_CLASSIFIER_RESULT_SHOW = "Result";
 Blockly.Msg.KNN_CLASSIFIER_OPACITY_SHOW = "Opacity";
+Blockly.Msg.KNN_CLASSIFIER_TRAIN_SHOW = "Train";
+Blockly.Msg.KNN_CLASSIFIER_STARTDETECT_SHOW = "Start Detection";
