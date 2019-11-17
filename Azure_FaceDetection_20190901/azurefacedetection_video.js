a3fad6217d1b610c93fbb6e2e0a8acc5e8406b46
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-17 23:26:00 +0800

    Add files via upload

---
+++ Azure_FaceDetection_20190901/azurefacedetection_video.js
@@ -0,0 +1,22 @@
+document.write('<div id="region"><video id="video" width="320" height="240" preload autoplay loop muted></video><canvas id="gamecanvas_azurefacedetection" style="display:none;"></canvas><br><div id="result" style="width:320px;color:red"></div></div>');
+
+window.onload = function () {
+  var video = document.getElementById('video');
+  var canvas = document.getElementById('gamecanvas_azurefacedetection'); 
+
+  navigator.mediaDevices
+    .getUserMedia({
+      audio: false,
+      video: {
+      facingMode: "user"
+      }
+    })
+   .then(stream => {
+      video.srcObject = stream
+      video.onloadedmetadata = () => {       
+      video.play();     
+      canvas.setAttribute("width", video.width);
+      canvas.setAttribute("height", video.height); 
+    }
+  })   
+}
