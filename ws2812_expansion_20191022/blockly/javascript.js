c904f382d2223f79bf6438c5de5a0604005ae234
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-17 12:31:55 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -2,6 +2,7 @@ Blockly.JavaScript['ws2812_expansion_1to24'] = function (block) {
   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
   var change = (block.getFieldValue('change_') == 'TRUE')?"1":"0";
   var code = "";
+  /*
   for (var i=0;i<=23;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
       if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
@@ -11,9 +12,10 @@ Blockly.JavaScript['ws2812_expansion_1to24'] = function (block) {
           code += variable_ws2812+".setColor("+i+","+value_color+");\n";
       }
   }
+  */
   
-  /*
   var colorcode = "";
+  var count = 0;
   for (var i=0;i<=23;i++) {
     var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC); 
     if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
@@ -29,12 +31,16 @@ Blockly.JavaScript['ws2812_expansion_1to24'] = function (block) {
         else
           colorcode += "'"+i+"'+"+value_color+".substr(1,6)+";
       }
+      count++;
+      if (count==8) {
+        //console.log(colorcode);
+        if (colorcode!="") colorcode = colorcode.substr(0,colorcode.length-1)
+        code += variable_ws2812+".setColor("+colorcode+");\n"; 
+        count=0;
+        colorcode="";
+      }
     }
   }
-  //console.log(colorcode);
-  if (colorcode!="") colorcode = colorcode.substr(0,colorcode.length-1)
-  code = variable_ws2812+".setColor("+colorcode+");\n";
-  */
   
   return code;
 };
