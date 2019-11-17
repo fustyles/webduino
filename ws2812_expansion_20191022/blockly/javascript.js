7fdf2e63735309d0b672cccf6c1d1c1cbd6655c0
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-17 11:47:03 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -30,46 +30,82 @@ Blockly.JavaScript['ws2812_expansion_1to8'] = function (block) {
   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
   var change = (block.getFieldValue('change_') == 'TRUE')?"1":"0";
   var code = "";
+  var colorcode = "";
   for (var i=0;i<=7;i++) {
-      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
+      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC); 
       if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
-        if (change==1)
-          code += variable_ws2812+".setColor("+i+",'#'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2));\n";
-        else
-          code += variable_ws2812+".setColor("+i+","+value_color+");\n";
+        if (change==1) {
+          if (i<10)
+            colorcode += "'0"+i+"'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
+          else 
+            colorcode += i+"+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
+        }
+        else {
+          if (i<10)
+            colorcode += "'0"+i+"'+"+value_color+".substr(1,6)+";
+          else
+            colorcode += i+"+"+value_color+".substr(1,6)+";
+        }
       }
   }
-   return code;
+  //console.log(colorcode);
+  if (colorcode!="") colorcode = colorcode.substr(0,colorcode.length-1)
+  code = variable_ws2812+".setColor("+colorcode+");\n";
+  return code;
 };
 
 Blockly.JavaScript['ws2812_expansion_9to16'] = function (block) {
   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
   var change = (block.getFieldValue('change_') == 'TRUE')?"1":"0";
   var code = "";
+  var colorcode = "";
   for (var i=8;i<=15;i++) {
-      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
+      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC); 
       if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
-        if (change==1)
-          code += variable_ws2812+".setColor("+i+",'#'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2));\n";
-        else
-          code += variable_ws2812+".setColor("+i+","+value_color+");\n";
+        if (change==1) {
+          if (i<10)
+            colorcode += "'0"+i+"'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
+          else 
+            colorcode += i+"+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
+        }
+        else {
+          if (i<10)
+            colorcode += "'0"+i+"'+"+value_color+".substr(1,6)+";
+          else
+            colorcode += i+"+"+value_color+".substr(1,6)+";
+        }
       }
   }
-   return code;
+  //console.log(colorcode);
+  if (colorcode!="") colorcode = colorcode.substr(0,colorcode.length-1)
+  code = variable_ws2812+".setColor("+colorcode+");\n";
+  return code;
 };
 
 Blockly.JavaScript['ws2812_expansion_17to24'] = function (block) {
   var variable_ws2812 = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('ws2812_'), Blockly.Variables.NAME_TYPE);
   var change = (block.getFieldValue('change_') == 'TRUE')?"1":"0";
   var code = "";
+  var colorcode = "";
   for (var i=16;i<=23;i++) {
-      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
+      var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC); 
       if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
-        if (change==1)
-          code += variable_ws2812+".setColor("+i+",'#'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2));\n";
-        else
-          code += variable_ws2812+".setColor("+i+","+value_color+");\n";
+        if (change==1) {
+          if (i<10)
+            colorcode += "'0"+i+"'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
+          else 
+            colorcode += i+"+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
+        }
+        else {
+          if (i<10)
+            colorcode += "'0"+i+"'+"+value_color+".substr(1,6)+";
+          else
+            colorcode += i+"+"+value_color+".substr(1,6)+";
+        }
       }
   }
-   return code;
+  //console.log(colorcode);
+  if (colorcode!="") colorcode = colorcode.substr(0,colorcode.length-1)
+  code = variable_ws2812+".setColor("+colorcode+");\n";
+  return code;
 };
