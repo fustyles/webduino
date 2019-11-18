f5dd1d395b96464f406de97a5c1fc8c8900f5c32
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-18 20:35:38 +0800

    Update zh-hans.js

--- Azure_FaceToFaceVerify_20191118/blockly/msg/blocks/zh-hans.js
+++ Azure_FaceToFaceVerify_20191118/blockly/msg/blocks/zh-hans.js
@@ -1,5 +1,6 @@
 Blockly.Msg.AZUREFACETOFACEVERIFY_DETECT_SHOW = "兩臉核對 (Azure) 偵測";
 Blockly.Msg.AZUREFACETOFACEVERIFY_GET_SHOW = "兩臉核對 (Azure) 取得";
+Blockly.Msg.AZUREFACETOFACEVERIFY_GETDATA_SHOW = "兩臉核對 (Azure) 取得資料";
 Blockly.Msg.AZUREFACETOFACEVERIFY_SETTINGS_SHOW ="兩臉核對 (Azure) 設定";
 Blockly.Msg.AZUREFACETOFACEVERIFY_RESOURCENAME_SHOW ="Resource Host";
 Blockly.Msg.AZUREFACETOFACEVERIFY_KEY_SHOW = "Ocp-Apim-Subscription-Key";
