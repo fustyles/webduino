2c0f60f67f10c2b32b4800d820d412f35b38da59
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-18 20:35:19 +0800

    Update en.js

--- Azure_FaceToFaceVerify_20191118/blockly/msg/blocks/en.js
+++ Azure_FaceToFaceVerify_20191118/blockly/msg/blocks/en.js
@@ -1,5 +1,6 @@
 Blockly.Msg.AZUREFACETOFACEVERIFY_DETECT_SHOW = "Verify Face To Face (Azure) Detect";
 Blockly.Msg.AZUREFACETOFACEVERIFY_GET_SHOW = "Verify Face To Face (Azure) Get";
+Blockly.Msg.AZUREFACETOFACEVERIFY_GETDATA_SHOW = "Verify Face To Face (Azure) Get Data";
 Blockly.Msg.AZUREFACETOFACEVERIFY_SETTINGS_SHOW ="Verify Face To Face (Azure) Settings";
 Blockly.Msg.AZUREFACETOFACEVERIFY_RESOURCENAME_SHOW ="Resource Host";
 Blockly.Msg.AZUREFACETOFACEVERIFY_KEY_SHOW = "Ocp-Apim-Subscription-Key";
