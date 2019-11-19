8aa306ac5c0a1db7122d1e3904b4aba108cca968
fustyles <34397289+fustyles@users.noreply.github.com>
2019-11-19 11:59:53 +0800

    Update javascript.js

--- ws2812_expansion_20191022/blockly/javascript.js
+++ ws2812_expansion_20191022/blockly/javascript.js
@@ -45,7 +45,7 @@ Blockly.JavaScript['ws2812_expansion_1to4'] = function (block) {
   var colorcode = "";
   for (var i=0;i<=3;i++) {
     var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC); 
-    if (value_color!="")||value_color.indexOf("colour_")!=-1) {
+    if (value_color!=""||value_color.indexOf("colour_")!=-1) {
       if (change==1) {
         if (i<10)
           colorcode += "'0"+i+"'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
@@ -74,7 +74,7 @@ Blockly.JavaScript['ws2812_expansion_1to8'] = function (block) {
   var colorcode = "";
   for (var i=0;i<=7;i++) {
     var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC); 
-    if (value_color!="")||value_color.indexOf("colour_")!=-1) {
+    if (value_color!=""||value_color.indexOf("colour_")!=-1) {
       if (change==1) {
         if (i<10)
           colorcode += "'0"+i+"'+"+value_color+".substr(3,2)+"+value_color+".substr(1,2)+"+value_color+".substr(5,2)+";
@@ -101,7 +101,7 @@ Blockly.JavaScript['ws2812_expansion_9to16'] = function (block) {
   var code = "";
   for (var i=8;i<=15;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
-      if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
+      if (value_color!=""||value_color.indexOf("colour_")!=-1) {
         if (change==1)
           code += variable_ws2812+".setColor("+i+",'#'+"+value_color+".substr(4,2)+"+value_color+".substr(2,2)+"+value_color+".substr(6,2));\n";
         else
@@ -117,7 +117,7 @@ Blockly.JavaScript['ws2812_expansion_17to24'] = function (block) {
   var code = "";
   for (var i=16;i<=23;i++) {
       var value_color = Blockly.JavaScript.valueToCode(block, 'color'+i+'_', Blockly.JavaScript.ORDER_ATOMIC);  
-      if ((value_color.indexOf("#000000")==-1&&value_color!="")||value_color.indexOf("colour_")!=-1) {
+      if (value_color!=""||value_color.indexOf("colour_")!=-1) {
         if (change==1)
           code += variable_ws2812+".setColor("+i+",'#'+"+value_color+".substr(4,2)+"+value_color+".substr(2,2)+"+value_color+".substr(6,2));\n";
         else
