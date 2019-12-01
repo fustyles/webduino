0480f5712c8351721d70695358695c918a75ce4c
fustyles <34397289+fustyles@users.noreply.github.com>
2019-12-01 14:50:24 +0800

    Update zh-hans.js

--- posenet_20190822/blockly/msg/blocks/zh-hans.js
+++ posenet_20190822/blockly/msg/blocks/zh-hans.js
@@ -8,3 +8,5 @@ Blockly.Msg.POSENET_RESULT_SHOW = "输出结果";
 Blockly.Msg.POSENET_SCORELIMIT_SHOW = "信心底限";
 Blockly.Msg.POSENET_SKELETON_SHOW = "骨架";
 Blockly.Msg.POSENET_OPACITY_SHOW = "透明度";
+Blockly.Msg.POSENET_DISTANCE_SHOW = "距离";
+Blockly.Msg.POSENET_ANGLE_SHOW = "角度";
