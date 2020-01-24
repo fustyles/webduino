750fa30838406e1fadbc75ff80bbdcba48019eeb
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 01:00:31 +0800

    Update zh-hant.js

--- faceapi_20200124/blockly/msg/blocks/zh-hant.js
+++ faceapi_20200124/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.FACEAPI_INDEX_SHOW = "臉部偵測 索引";
 Blockly.Msg.FACEAPI_PROPERTY_SHOW = "屬性";
-Blockly.Msg.FACEAPI_WIDTH_SHOW = "臉部偵測 寬度";
+Blockly.Msg.FACEAPI_WIDTH_SHOW = "臉部偵測 視訊寬度";
 Blockly.Msg.FACEAPI_HEIGHT_SHOW = "高度";
 Blockly.Msg.FACEAPI_RESULT_SHOW = "結果";
 Blockly.Msg.FACEAPI_OPACITY_SHOW = "透明度";
