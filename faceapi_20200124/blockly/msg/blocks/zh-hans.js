530879736a8601d40498c3c0afa164803579b21a
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 01:00:48 +0800

    Update zh-hans.js

--- faceapi_20200124/blockly/msg/blocks/zh-hans.js
+++ faceapi_20200124/blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,6 @@
 Blockly.Msg.FACEAPI_INDEX_SHOW = "脸部侦测 索引";
 Blockly.Msg.FACEAPI_PROPERTY_SHOW = "属性";
-Blockly.Msg.FACEAPI_WIDTH_SHOW = "脸部侦测 宽度";
+Blockly.Msg.FACEAPI_WIDTH_SHOW = "脸部侦测 视讯宽度";
 Blockly.Msg.FACEAPI_HEIGHT_SHOW = "高度";
 Blockly.Msg.FACEAPI_RESULT_SHOW = "结果";
 Blockly.Msg.FACEAPI_OPACITY_SHOW = "透明度";
