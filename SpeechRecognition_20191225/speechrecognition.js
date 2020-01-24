c26cc5423b4bac4dd8855fdc424fa9ecd5d9f976
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-24 10:43:36 +0800

    Update speechrecognition.js

--- SpeechRecognition_20191225/speechrecognition.js
+++ SpeechRecognition_20191225/speechrecognition.js
@@ -12,65 +12,63 @@
   var Recognition_interim = '';
   var Recognition_final = '';
 
-  function Recognition_initial() {
-    if ('webkitSpeechRecognition' in window) {
-      recognition = new webkitSpeechRecognition();
-      recognition.continuous = true;
-      recognition.interimResults = true;
+  if ('webkitSpeechRecognition' in window) {
+    recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
 
-      recognition.onstart = function() {
-        recognizing = true;
-      };
+    recognition.onstart = function() {
+      recognizing = true;
+    };
 
-      recognition.onerror = function(event) {
-        if (event.error == 'no-speech') ignore_onend = true;
-        if (event.error == 'audio-capture') ignore_onend = true;
-        if (event.error == 'not-allowed') ignore_onend = true;
-        console.log(event.error);
-      };
+    recognition.onerror = function(event) {
+      if (event.error == 'no-speech') ignore_onend = true;
+      if (event.error == 'audio-capture') ignore_onend = true;
+      if (event.error == 'not-allowed') ignore_onend = true;
+      console.log(event.error);
+    };
 
-      recognition.onend = function() {
-        recognizing = false;
-        if (ignore_onend) return;
-        if (!final_transcript) return;
-        recognition.start(); 
-      };
+    recognition.onend = function() {
+      recognizing = false;
+      if (ignore_onend) return;
+      if (!final_transcript) return;
+      recognition.start(); 
+    };
 
-      recognition.onresult = function(event) {
-        var interim_transcript = '';
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-          if (event.results[i].isFinal) {
-            final_transcript = event.results[i][0].transcript;
-          } else {
-            interim_transcript += event.results[i][0].transcript;
-          }
+    recognition.onresult = function(event) {
+      var interim_transcript = '';
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript = event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
         }
-        final_transcript = capitalize(final_transcript);
-        Recognition_interim = linebreak(interim_transcript);
-        if (Recognition_interim=='') {
-          Recognition_final = linebreak(final_transcript);
-          console.log("final   = " + Recognition_final);
+      }
+      final_transcript = capitalize(final_transcript);
+      Recognition_interim = linebreak(interim_transcript);
+      if (Recognition_interim=='') {
+        Recognition_final = linebreak(final_transcript);
+        console.log("final   = " + Recognition_final);
+      }
+      else {
+        Recognition_final = "";
+        if (!document.getElementById("showText")) {
+          var obj = document.createElement('div');
+          obj.id = "showText";
+          obj.style.position = "absolute";
+          obj.style.zIndex = "9999";
+          obj.draggable="true";
+          obj.setAttribute("onclick", "javascript:onclickid_set(this);");
+          obj.setAttribute("ondragstart", "javascript:event.dataTransfer.setData('div/plain',event.target.id);");
+          document.body.appendChild(obj);
         }
-        else {
-          Recognition_final = "";
-          if (!document.getElementById("showText")) {
-            var obj = document.createElement('div');
-            obj.id = "showText";
-            obj.style.position = "absolute";
-            obj.style.zIndex = "9999";
-            obj.draggable="true";
-            obj.setAttribute("onclick", "javascript:onclickid_set(this);");
-            obj.setAttribute("ondragstart", "javascript:event.dataTransfer.setData('div/plain',event.target.id);");
-            document.body.appendChild(obj);
-          }
-          document.getElementById("showText").innerHTML = Recognition_interim;
-          console.log("interim = " + Recognition_interim);
-        }
-      };
-    }
-    else {
-      console.log('webkitSpeechRecognition failed.');
-    }
+        document.getElementById("showText").innerHTML = Recognition_interim;
+        console.log("interim = " + Recognition_interim);
+      }
+    };
+  }
+  else {
+    console.log('webkitSpeechRecognition failed.');
   }
 
   function linebreak(s) {
@@ -96,7 +94,6 @@
     return result;
   }
 
-  window.Recognition_initial = Recognition_initial;
   window.linebreak = linebreak;
   window.capitalize = capitalize;
   window.startButton = startButton;
