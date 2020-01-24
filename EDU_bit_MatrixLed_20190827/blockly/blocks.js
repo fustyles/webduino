6fea54e3ad51acbab37d1cb730038e3f64106c56
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 00:07:32 +0800

    Update blocks.js

--- EDU_bit_MatrixLed_20190827/blockly/blocks.js
+++ EDU_bit_MatrixLed_20190827/blockly/blocks.js
@@ -1,39 +1,3 @@
-Blockly.Blocks['addmyblocks'] = {
-  init: function() {
-  this.appendDummyInput()
-      .appendField(Blockly.Msg.ADDMYBLOCKS_SHOW);
-  this.appendValueInput("url")
-      .setCheck("String"); 
-  this.appendDummyInput()
-      .appendField(new Blockly.FieldDropdown([
-        ["",""],
-        ["add","add"], 
-        ["remove","remove"]
-      ]), "func");
-  this.setTooltip('');
-  this.setColour(20);
-  },
-  onchange: function (event) {
-    if (event.element=="field") {
-      if (this.id==event.blockId) {
-	  
-          if (event.name=='func') {
-            var url = this.getInputTargetBlock("url").getFieldValue("TEXT");
-            if (this.getFieldValue('func')=='add') {
-                  Code.customTab.$__add__(url);
-                  this.getInputTargetBlock("url").setFieldValue("", "TEXT");
-            }
-            else if (this.getFieldValue('func')=='remove') {
-                  Code.customTab.$__remove__(url);
-                  this.getInputTargetBlock("url").setFieldValue("", "TEXT");
-            }
-            this.getField('func').setValue("");
-          }
-      }
-    }
-  }
-};    
-
 Blockly.Blocks['matrix_led_showstate'] = {
   init: function() {
     this.appendDummyInput()
