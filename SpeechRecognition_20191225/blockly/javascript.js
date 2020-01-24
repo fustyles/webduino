9f6f9ee4fa946a0ea4fa49b5e2240e4836e8b74c
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-24 10:47:54 +0800

    Update javascript.js

--- SpeechRecognition_20191225/blockly/javascript.js
+++ SpeechRecognition_20191225/blockly/javascript.js
@@ -1,6 +1,6 @@
 Blockly.JavaScript['SpeechRecognition_run'] = function (block) {
   var value_language_ = block.getFieldValue('language_');
-  var code = 'recognition.lang="'+value_language_+'";\nif (!recognizing) startButton(event);\n';
+  var code = 'Recognition_initial();\nrecognition.lang="'+value_language_+'";\nif (!recognizing) startButton(event);\n';
   return code;
 };
 
