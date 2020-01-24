7649d7e0911028996c391342eb2ee5343ca2ec32
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-24 13:17:10 +0800

    Update en.js

--- SpeechRecognition_20191225/blockly/msg/blocks/en.js
+++ SpeechRecognition_20191225/blockly/msg/blocks/en.js
@@ -1,6 +1,7 @@
 Blockly.Msg.SPEECHRECOGNITION_SHOW = "Start Speech Recognition";
 Blockly.Msg.SPEECHRECOGNITION_LANGUAGE_SHOW = "Language";
 Blockly.Msg.SPEECHRECOGNITION_FINAL_GET_SHOW = "Get Speech Recognition Final Text";
+Blockly.Msg.SPEECHRECOGNITION_FINAL_CLEAR_SHOW = "Clear Speech Recognition Final Text";
 Blockly.Msg.SPEECHRECOGNITION_INTERIM_GET_SHOW = "Get Speech Recognition Interim Text";
 Blockly.Msg.SPEECHRECOGNITION_LISTENER_SHOW = "Speech Recognition Listener";
 Blockly.Msg.SPEECHRECOGNITION_KEYWORD_SHOW = "contains Keyword";
