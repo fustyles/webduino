afa2ea466a73a2e9229e5838081e7fcdc262e6a9
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 01:00:17 +0800

    Update en.js

--- faceapi_20200124/blockly/msg/blocks/en.js
+++ faceapi_20200124/blockly/msg/blocks/en.js
@@ -1,6 +1,6 @@
 Blockly.Msg.FACEAPI_INDEX_SHOW = "Face Detection Index";
 Blockly.Msg.FACEAPI_PROPERTY_SHOW = "Property";
-Blockly.Msg.FACEAPI_WIDTH_SHOW = "Face Detection Width";
+Blockly.Msg.FACEAPI_WIDTH_SHOW = "Face Detection Video Width";
 Blockly.Msg.FACEAPI_HEIGHT_SHOW = "Height";
 Blockly.Msg.FACEAPI_RESULT_SHOW = "Result";
 Blockly.Msg.FACEAPI_OPACITY_SHOW = "Opacity";
