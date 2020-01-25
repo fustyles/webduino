97448db67fb0264ee07d96f3dc7113f28a9d7a46
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 20:18:44 +0800

    Update zh-hant.js

--- posenet_20190822/blockly/msg/blocks/zh-hant.js
+++ posenet_20190822/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.POSENET_PERSON_SHOW = "姿態偵測 (posenet) 索引";
 Blockly.Msg.POSENET_PART_SHOW = "部位";
-Blockly.Msg.POSENET_WIDTH_SHOW = "視訊 (posenet) 寬度";
+Blockly.Msg.POSENET_WIDTH_SHOW = "姿態偵測 (posenet) 視訊寬度";
 Blockly.Msg.POSENET_HEIGHT_SHOW = "高度";
 Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "鏡像";
 Blockly.Msg.POSENET_PERSONS_SHOW = "偵測人數";
