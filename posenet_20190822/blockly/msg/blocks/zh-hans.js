38a10c61c75c0c19fbcc4d3ef9132de45408dad5
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 20:18:59 +0800

    Update zh-hans.js

--- posenet_20190822/blockly/msg/blocks/zh-hans.js
+++ posenet_20190822/blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,6 @@
 Blockly.Msg.POSENET_PERSON_SHOW = "姿态侦测 (posenet) 索引";
 Blockly.Msg.POSENET_PART_SHOW = "部位";
-Blockly.Msg.POSENET_WIDTH_SHOW = "视讯 (posenet) 宽度";
+Blockly.Msg.POSENET_WIDTH_SHOW = "姿态侦测 (posenet) 视讯宽度";
 Blockly.Msg.POSENET_HEIGHT_SHOW = "高度";
 Blockly.Msg.POSENET_MIRRORIMAGE_SHOW = "镜像";
 Blockly.Msg.POSENET_PERSONS_SHOW = "侦测人数";
