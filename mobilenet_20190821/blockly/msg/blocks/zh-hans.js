12a977f503ee0d28935f2a247d4d2c1647b23d74
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 22:41:26 +0800

    Update zh-hans.js

--- mobilenet_20190821/blockly/msg/blocks/zh-hans.js
+++ mobilenet_20190821/blockly/msg/blocks/zh-hans.js
@@ -1,5 +1,5 @@
-Blockly.Msg.MOBILENET_OBJECT_SHOW = "物件侦测(mobilenet) 侦测物件";
-Blockly.Msg.MOBILENET_WIDTH_SHOW = "视讯 (mobilenet) 宽度";
+Blockly.Msg.MOBILENET_OBJECT_SHOW = "物件侦测 (mobilenet) 侦测物件";
+Blockly.Msg.MOBILENET_WIDTH_SHOW = "物件侦测 (mobilenet) 视讯宽度";
 Blockly.Msg.MOBILENET_HEIGHT_SHOW = "高度";
 Blockly.Msg.MOBILENET_MIRRORIMAGE_SHOW = "镜像";
 Blockly.Msg.MOBILENET_RESULT_SHOW = "结果";
