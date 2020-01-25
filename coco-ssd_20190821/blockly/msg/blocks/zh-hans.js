b71430070011c2450915dc07fdc4f48e4af7e992
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 22:40:06 +0800

    Update zh-hans.js

--- coco-ssd_20190821/blockly/msg/blocks/zh-hans.js
+++ coco-ssd_20190821/blockly/msg/blocks/zh-hans.js
@@ -1,5 +1,5 @@
-Blockly.Msg.COCOSSD_OBJECT_SHOW = "物件侦测(coco-ssd) 侦测物件";
-Blockly.Msg.COCOSSD_WIDTH_SHOW = "视讯 (coco-ssd) 宽度";
+Blockly.Msg.COCOSSD_OBJECT_SHOW = "物件侦测 (coco-ssd) 侦测物件";
+Blockly.Msg.COCOSSD_WIDTH_SHOW = "物件侦测 (coco-ssd) 视讯宽度";
 Blockly.Msg.COCOSSD_HEIGHT_SHOW = "高度";
 Blockly.Msg.COCOSSD_MIRRORIMAGE_SHOW = "镜像";
 Blockly.Msg.COCOSSD_RESULT_SHOW = "结果";
