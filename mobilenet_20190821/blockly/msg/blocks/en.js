8b6e87db3d7bd8deb45d1c4e899d8b387a33da9e
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 22:41:10 +0800

    Update en.js

--- mobilenet_20190821/blockly/msg/blocks/en.js
+++ mobilenet_20190821/blockly/msg/blocks/en.js
@@ -1,5 +1,5 @@
 Blockly.Msg.MOBILENET_OBJECT_SHOW = "Object Detection (mobilenet) Object";
-Blockly.Msg.MOBILENET_WIDTH_SHOW = "Video (mobilenet) Width";
+Blockly.Msg.MOBILENET_WIDTH_SHOW = "Object Detection (mobilenet) Video Width";
 Blockly.Msg.MOBILENET_HEIGHT_SHOW = "Height";
 Blockly.Msg.MOBILENET_MIRRORIMAGE_SHOW = "MirrorImage";
 Blockly.Msg.MOBILENET_RESULT_SHOW = "Result";
