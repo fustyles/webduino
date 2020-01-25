ce2024f2116fb84ef5c2b40135d61dd39f513b27
fustyles <34397289+fustyles@users.noreply.github.com>
2020-01-25 22:41:39 +0800

    Update zh-hant.js

--- mobilenet_20190821/blockly/msg/blocks/zh-hant.js
+++ mobilenet_20190821/blockly/msg/blocks/zh-hant.js
@@ -1,5 +1,5 @@
-Blockly.Msg.MOBILENET_OBJECT_SHOW = "物件偵測(mobilenet) 偵測物件";
-Blockly.Msg.MOBILENET_WIDTH_SHOW = "視訊 (mobilenet) 寬度";
+Blockly.Msg.MOBILENET_OBJECT_SHOW = "物件偵測 (mobilenet) 偵測物件";
+Blockly.Msg.MOBILENET_WIDTH_SHOW = "物件偵測 (mobilenet) 視訊寬度";
 Blockly.Msg.MOBILENET_HEIGHT_SHOW = "高度";
 Blockly.Msg.MOBILENET_MIRRORIMAGE_SHOW = "鏡像";
 Blockly.Msg.MOBILENET_RESULT_SHOW = "結果";
