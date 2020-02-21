18338a111677246cce7c95d1bf8ed9f1ab2c32de
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-21 17:39:33 +0800

    Update blocks.js

--- ThingSpeak_20180401/blockly/blocks.js
+++ ThingSpeak_20180401/blockly/blocks.js
@@ -44,3 +44,83 @@ Blockly.Blocks['thingspeak_update'] = {
   this.setHelpUrl(mainUrl + 'basic/blockly/blockly-information.html' + utmUrl);
   }
 };
+
+
+Blockly.Blocks['thingspeak_read1'] = {
+  init: function() {  
+  this.appendDummyInput()
+      .appendField("Get a Channel Feed");
+  this.appendValueInput("key")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("https://api.thingspeak.com/channels/");
+  this.appendValueInput("count")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("/feeds.json?results=");
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['thingspeak_read2'] = {
+  init: function() {  
+  this.appendDummyInput()
+      .appendField("Get a Channel Feed");
+  this.appendValueInput("key")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("https://api.thingspeak.com/channels/");
+  this.appendValueInput("field")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("/fields/");    
+  this.appendValueInput("count")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(".json?results=");
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['thingspeak_read3'] = {
+  init: function() {  
+  this.appendDummyInput()
+      .appendField("Get Channel Status Updates");
+  this.appendValueInput("key")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField("https://api.thingspeak.com/channels/");
+  this.appendDummyInput()
+      .appendField("/status.json");  
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['thingspeak_getresponse'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("ThingSpeak get response data");
+    this.setInputsInline(false);
+    this.setOutput(true, null); 
+    this.setColour(200);
+  }
+};
+
+Blockly.Blocks['thingspeak_clearresponse'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("ThingSpeak clear response data");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
