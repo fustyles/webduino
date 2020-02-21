6131f765174b3df9ebae96fc5d4bd301d2f05c3a
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-21 17:37:22 +0800

    Update thingspeak.js

--- ThingSpeak_20180401/thingspeak.js
+++ ThingSpeak_20180401/thingspeak.js
@@ -3,6 +3,9 @@
 +(function (window, document) {
 
   'use strict';
+  
+  var ThingSpeak_response=[];
+  var ThingSpeak_getState = false;  
 
   function ThingSpeak_update(key,field1,field2,field3,field4,field5,field6,field7,field8) {
     
@@ -33,7 +36,65 @@
         }
      });
   }
+  
+  function ThingSpeak_read(kind,key,index,count) {
+    if (kind==1)
+      var url ="https://api.thingspeak.com/channels/"+key+"/feeds.json?results="+count;
+    else if (kind==2)
+      var url ="https://api.thingspeak.com/channels/"+key+"/fields/"+index+".json?results="+count;
+    else if (kind==3)
+      var url ="https://api.thingspeak.com/channels/"+key+"/status.json";
+
+    ThingSpeak_response=[];
+    var data = $.ajax({
+        "type": "POST",
+        "dataType": "jsonp",
+        "url": url,
+        success: function(json)
+        {
+          json = eval(json.feeds);
+          ThingSpeak_getState = true;
+          for (var i=0;i<json.length;i++) {
+            var Feedback= JSON.stringify(json[i]);
+            Feedback= Feedback.replace(/},{/g,";");
+            Feedback= Feedback.replace(/\":\"/g,",");
+            Feedback= Feedback.replace(/\":/g,",");
+            Feedback= Feedback.replace(/\,\"/g,","); 
+            Feedback= Feedback.replace(/\"/g,"");
+            Feedback= Feedback.replace(/\{/g,"");
+            Feedback= Feedback.replace(/\}/g,"");
+            Feedback= Feedback.replace(/\[/g,"");
+            Feedback= Feedback.replace(/\]/g,"");
+            Feedback= Feedback.replace(/,\"/g,",");
+            Feedback= Feedback.replace(/\":/g,",");
+            ThingSpeak_response.push(Feedback);
+          }
+          ThingSpeak_getState = false;
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+          //console.log(errorThrown);
+        }
+     });  
+  }
+
+  function ThingSpeak_getResponse() {
+   if (ThingSpeak_getState == false) {
+     var res = ThingSpeak_response;
+     ThingSpeak_response=[];
+     return res;
+    }
+    else
+      return [];
+  }
+
+  function ThingSpeak_clearResponse() {
+   ThingSpeak_response=[];
+  }  
 
   window.ThingSpeak_update = ThingSpeak_update;
+  window.ThingSpeak_read = ThingSpeak_read;
+  window.ThingSpeak_getResponse = ThingSpeak_getResponse;
+  window.ThingSpeak_clearResponse = ThingSpeak_clearResponse;
 
 }(window, window.document));
