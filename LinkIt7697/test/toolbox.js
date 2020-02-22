e2e21fb2d764149a7a3c0ec9941b54dd088c2f30
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:48:13 +0800

    Update toolbox.js

--- LinkIt7697/test/toolbox.js
+++ LinkIt7697/test/toolbox.js
@@ -1,5 +1,5 @@
 var catOpenURL = '<xml>'+
-'<category id="category_test" name="測試">'+
+'<category id="category_test" name="%{Blockly.Msg.TEST_SHOW}">'+
 '  <block type="iframe_open">'+
 '    <value name="url_">'+
 '      <block type="text">'+
