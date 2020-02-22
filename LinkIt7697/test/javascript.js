752a7a7b9921ffcf90e0b7424d5b5adb59960e35
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 18:22:04 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -1,10 +1,10 @@
-Blockly.Arduino['iframe_open'] = function (block) { 
+Blockly.Arduino['test_open'] = function (block) { 
   var value_url_ = Blockly.Arduino.valueToCode(block, 'url_', Blockly.Arduino.ORDER_ATOMIC);
   var code = 'Hello(' + value_url_ + ');\n';
   return code;
 };
 
-Blockly.Arduino['iframe_close'] = function(block) { 
+Blockly.Arduino['test_close'] = function(block) { 
   var code = 'World();\n';
   return code;
 };
