d19a2c5902a579662df197c472789822a84978ea
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:30:26 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -1,17 +1,10 @@
-Blockly.Arduino['iframe_open'] = function (block) {
-  var value_openid_ = Blockly.Arduino.valueToCode(block, 'openid_', Blockly.Arduino.ORDER_ATOMIC);  
+Blockly.Arduino['iframe_open'] = function (block) { 
   var value_url_ = Blockly.Arduino.valueToCode(block, 'url_', Blockly.Arduino.ORDER_ATOMIC);
-  var value_width_ = Blockly.Arduino.valueToCode(block, 'width_', Blockly.Arduino.ORDER_ATOMIC);
-  var value_height_ = Blockly.Arduino.valueToCode(block, 'height_', Blockly.Arduino.ORDER_ATOMIC);
-  var value_left_ = Blockly.Arduino.valueToCode(block, 'left_', Blockly.Arduino.ORDER_ATOMIC);
-  var value_top_ = Blockly.Arduino.valueToCode(block, 'top_', Blockly.Arduino.ORDER_ATOMIC);
-  var value_zindex_ = Blockly.Arduino.valueToCode(block, 'zindex_', Blockly.Arduino.ORDER_ATOMIC);
-  var code = 'Hello(' + value_openid_ + ',' + value_url_ + ','+ value_width_ + ',' + value_height_ + ',' + value_left_ + ',' + value_top_ + ',' + value_zindex_ + ');\n';
+  var code = 'Hello(' + value_url_ + ');\n';
   return code;
 };
 
-Blockly.Arduino['iframe_close'] = function(block) {
-  var value_closeid_ = Blockly.Arduino.valueToCode(block, 'closeid_', Blockly.Arduino.ORDER_ATOMIC);  
-  var code = 'World(' + value_closeid_ + ');\n';
+Blockly.Arduino['iframe_close'] = function(block) { 
+  var code = 'World();\n';
   return code;
 };
