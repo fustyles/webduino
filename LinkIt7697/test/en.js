6ddd995f9f83c6da39100081e853c5358158e4ed
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:46:46 +0800

    Update en.js

--- LinkIt7697/test/en.js
+++ LinkIt7697/test/en.js
@@ -4,5 +4,5 @@ goog.provide('Blockly.Msg.en');
 goog.require('Blockly.Msg');
 
 Blockly.Msg.TEST_SHOW = "TEST";
-Blockly.Msg.URL_SHOW = "Open";
+Blockly.Msg.OPEN_SHOW = "Open";
 Blockly.Msg.CLOSE_SHOW = "Close";
