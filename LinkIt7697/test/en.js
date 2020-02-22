fabf7b6dd2b548509fae28a32e5acf54b0b23177
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:31:44 +0800

    Update en.js

--- LinkIt7697/test/en.js
+++ LinkIt7697/test/en.js
@@ -4,12 +4,4 @@ goog.provide('Blockly.Msg.en');
 goog.require('Blockly.Msg');
 
 Blockly.Msg.TEST_SHOW = "TEST";
-Blockly.Msg.OPEN_SHOW = "Open Link";
-Blockly.Msg.CLOSE_SHOW = "Close Link";
-Blockly.Msg.OPENID_SHOW = "ID";
-Blockly.Msg.CLOSEID_SHOW = "ID";
 Blockly.Msg.URL_SHOW = "Url";
-Blockly.Msg.WIDTH_SHOW = "Width";
-Blockly.Msg.LEFT_SHOW = "Left";
-Blockly.Msg.TOP_SHOW = "Top";
-Blockly.Msg.ZINDEX_SHOW = "Z-index";
