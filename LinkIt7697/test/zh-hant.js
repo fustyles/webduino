fac45dade3e748c5443c83693bbde6fe8a96075b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:44:37 +0800

    Update zh-hant.js

--- LinkIt7697/test/zh-hant.js
+++ LinkIt7697/test/zh-hant.js
@@ -4,5 +4,5 @@ goog.provide('Blockly.Msg.zh.hant');
 goog.require('Blockly.Msg');
 
 Blockly.Msg.TEST_SHOW = "測試";
-Blockly.Msg.URL_SHOW = "開啟連線";
-Blockly.Msg.CLOSE_SHOW = "關閉連線";
+Blockly.Msg.URL_SHOW = "開啟";
+Blockly.Msg.CLOSE_SHOW = "關閉";
