978addc328b2ca4da843c2e16078b84940d67b1f
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:54:17 +0800

    Update zh-hant.js

--- LinkIt7697/test/zh-hant.js
+++ LinkIt7697/test/zh-hant.js
@@ -1,8 +1,5 @@
-'use strict';
-
-goog.provide('Blockly.Msg.zh.hant');
-goog.require('Blockly.Msg');
-
+Blockly.Msg["TEST"] = "測試";
+Blockly.Msg["TEST_HUE"] = "280";
 Blockly.Msg.TEST_SHOW = "測試";
 Blockly.Msg.OPEN_SHOW = "開啟";
 Blockly.Msg.CLOSE_SHOW = "關閉";
