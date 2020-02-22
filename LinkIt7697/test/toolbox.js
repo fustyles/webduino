0b9ee1f8f4db1a24ab7fab34a1971e0342bd59d2
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-22 15:31:00 +0800

    Update toolbox.js

--- LinkIt7697/test/toolbox.js
+++ LinkIt7697/test/toolbox.js
@@ -1,48 +1,13 @@
 var catOpenURL = '<xml>'+
 '<category id="category_test" name="測試">'+
 '  <block type="iframe_open">'+
-'    <value name="openid_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+
 '    <value name="url_">'+
 '      <block type="text">'+
 '        <field name="TEXT">about:blank</field>'+
 '      </block>'+
 '    </value>'+
-'    <value name="width_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">500</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="height_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">400</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="left_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="top_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">100</field>'+
-'      </block>'+
-'    </value>  '+
-'    <value name="zindex_">'+
-'      <block type="math_number">'+
-'        <field name="NUM">0</field>'+
-'      </block>'+
-'    </value>'+
 '  </block>'+
 '  <block type="iframe_close">'+
-'    <value name="closeid_">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+
 '  </block>'+
 '</category>'+
 '</xml>';
