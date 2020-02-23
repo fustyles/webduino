0553df2f82c7a77efbad884740075dfad5b77243
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 21:02:37 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -1,4 +1,4 @@
-Blockly.Arduino['test_open'] = function (block) { 
+Blockly.Arduino['test_code'] = function (block) { 
   var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
   console.log(text);
   if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
@@ -9,7 +9,7 @@ Blockly.Arduino['test_open'] = function (block) {
   return code;
 };
 
-Blockly.Arduino['test_close'] = function(block) { 
+Blockly.Arduino['test_comment'] = function(block) { 
   var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
   if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
     text = text.substring(1,text.length-1);
