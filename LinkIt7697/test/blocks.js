0e50d865bd16938519edb9dde0cf61909973217d
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 22:39:09 +0800

    Update blocks.js

--- LinkIt7697/test/blocks.js
+++ LinkIt7697/test/blocks.js
@@ -23,6 +23,17 @@ Blockly.Blocks['test_code'] = {
   }
 };
 
+Blockly.Blocks['test_code_get'] = {
+  init: function() {
+    this.appendValueInput("text")
+        .setCheck("String")
+        .setAlign(Blockly.ALIGN_RIGHT);   
+    this.setInputsInline(true);
+    this.setOutput(true, null); 
+    this.setColour(200);
+  }
+};
+
 Blockly.Blocks['test_comment'] = {
   init: function() {
     this.appendValueInput("text")
