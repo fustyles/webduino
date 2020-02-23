bc663bae85519894c072f98029629ba424f0cc60
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 21:49:00 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -16,7 +16,7 @@ Blockly.Arduino['test_code'] = function (block) {
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
     text = text.substring(1,text.length-1);
-  var code = text+'\n';
+  var code = text.replace(/<br>/mg,"\n") + '\n';
   return code;
 };
 
@@ -26,6 +26,6 @@ Blockly.Arduino['test_comment'] = function(block) {
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
     text = text.substring(1,text.length-1);
-  var code = '//' + text+'\n';
+  var code = '//' + text.replace(/<br>/mg,"\n") +'\n';
   return code;
 };
