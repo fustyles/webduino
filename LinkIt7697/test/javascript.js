5fcf797afc6a742de6d86ea6b8fe28a236b0d1f3
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 22:39:10 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -11,13 +11,22 @@ Blockly.Arduino['test_head'] = function(block) {
 
 Blockly.Arduino['test_code'] = function (block) { 
   var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
-  console.log(text);
   if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
     text = text.substring(1,text.length-1);
   var code = text.replace(/<br>/mg,"\n") + '\n';
   return code;
+};
+
+Blockly.Arduino['test_code_get'] = function (block) { 
+  var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
+  if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
+    text = text.substring(1,text.length-1);
+  if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
+    text = text.substring(1,text.length-1);
+  var code = text.replace(/<br>/mg,"\n");
+  return [code, Blockly.Arduino.ORDER_NONE];
 };
 
 Blockly.Arduino['test_comment'] = function(block) { 
