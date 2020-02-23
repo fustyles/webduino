439bc1809d26ec100bea807fcf389e4b099fbfca
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 11:52:20 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -1,5 +1,6 @@
 Blockly.Arduino['test_open'] = function (block) { 
   var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
+  console.log(text);
   if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
