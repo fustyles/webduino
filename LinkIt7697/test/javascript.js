8c2665b408d06dcb51f83b68c846a2a31c8eb9e8
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 22:53:49 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -25,7 +25,7 @@ Blockly.Arduino['test_code_get'] = function (block) {
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
     text = text.substring(1,text.length-1);
-  var code = text+";";
+  var code = text;
   return [code, Blockly.Arduino.ORDER_NONE];
 };
 
