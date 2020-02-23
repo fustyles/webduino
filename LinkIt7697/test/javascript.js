1d56e17400524309fbf72514b958ee3eb8754213
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 11:45:40 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -1,10 +1,19 @@
 Blockly.Arduino['test_open'] = function (block) { 
-  var value_url_ = Blockly.Arduino.valueToCode(block, 'url_', Blockly.Arduino.ORDER_ATOMIC);
-  var code = 'Hello(' + value_url_ + ');\n';
+  var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
+  if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
+    text = text.substring(1,text.length-1);
+  if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
+    text = text.substring(1,text.length-1);
+  var code = text+'\n';
   return code;
 };
 
 Blockly.Arduino['test_close'] = function(block) { 
-  var code = 'World();\n';
+  var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC);
+  if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
+    text = text.substring(1,text.length-1);
+  if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
+    text = text.substring(1,text.length-1);
+  var code = '//' + text+'\n';
   return code;
 };
