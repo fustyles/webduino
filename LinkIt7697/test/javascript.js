cd6e8e3bad8ca80b514cfdc25633b08eebc1449b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-23 11:51:24 +0800

    Update javascript.js

--- LinkIt7697/test/javascript.js
+++ LinkIt7697/test/javascript.js
@@ -1,6 +1,6 @@
 Blockly.Arduino['test_open'] = function (block) { 
   var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
-  if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
+  if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
     text = text.substring(1,text.length-1);
@@ -10,7 +10,7 @@ Blockly.Arduino['test_open'] = function (block) {
 
 Blockly.Arduino['test_close'] = function(block) { 
   var text = Blockly.Arduino.valueToCode(block, 'text', Blockly.Arduino.ORDER_ATOMIC);
-  if ((text.indexOf("'")==0)&&(text.lastIndexOf("'")==text.length-1))
+  if ((text.indexOf('"')==0)&&(text.lastIndexOf('"')==text.length-1))
     text = text.substring(1,text.length-1);
   if ((text.indexOf("(")==0)&&(text.lastIndexOf(")")==text.length-1))
     text = text.substring(1,text.length-1);
