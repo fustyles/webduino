a11e3c35a1b37ae9e0db29e90b4160474da7946f
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-24 22:41:49 +0800

    Update zh-hant.js

--- LinkIt7697/test/zh-hant.js
+++ LinkIt7697/test/zh-hant.js
@@ -1,3 +1,9 @@
+'use strict';
+
+goog.provide('Blockly.Msg.zh.hant');
+
+goog.require('Blockly.Msg');
+
 Blockly.Msg.TEST_SHOW = "測試";
 Blockly.Msg.CODE_SHOW = "自訂程式碼";
 Blockly.Msg.COMMENT_SHOW = "註解";
