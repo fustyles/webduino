dde0bc00c50c166f8aafba367e5b704d4afb6b9b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-02-24 22:41:23 +0800

    Update en.js

--- LinkIt7697/test/en.js
+++ LinkIt7697/test/en.js
@@ -1,3 +1,9 @@
+'use strict';
+
+goog.provide('Blockly.Msg.en');
+
+goog.require('Blockly.Msg');
+
 Blockly.Msg.TEST_SHOW = "TEST";
 Blockly.Msg.CODE_SHOW = "Custom Code";
 Blockly.Msg.COMMENT_SHOW = "Comment";
