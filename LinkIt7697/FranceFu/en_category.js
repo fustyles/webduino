d04d152dc42260783a22c90da75de3c1ec02fead
fustyles <34397289+fustyles@users.noreply.github.com>
2020-03-07 10:11:39 +0800

    Update en_category.js

--- LinkIt7697/FranceFu/en_category.js
+++ LinkIt7697/FranceFu/en_category.js
@@ -1,2 +1,2 @@
 var category = ["FRANCEFU", "customcode", "tcp", "line", "esp32", "others"];
-var category_replace = ["FranceFu", "Custom Code", "TCP", "Line", "ESP32", "Others"];
+var category_replace = ["FranceFu", "Custom Code", "TCP", "Line Notify", "ESP32", "Others"];
