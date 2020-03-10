29a36426cf277da47d9c727aacd6be5d3813c407
fustyles <34397289+fustyles@users.noreply.github.com>
2020-03-10 23:07:43 +0800

    Delete en_category.js

--- LinkIt7697/FranceFu/en_category.js
+++
@@ -1,2 +0,0 @@
-var category = ["FRANCEFU", "customcode", "tcp", "line", "esp32", "thingspeak", "others"];
-var category_replace = ["FranceFu", "Custom Code", "TCP", "Line Notify", "ESP32", "Thingspeak", "Others"];
