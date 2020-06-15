d8c1a21927c718dea2827632bebb68abb848e1b3
fustyles <34397289+fustyles@users.noreply.github.com>
2020-06-15 18:00:21 +0800

    Update zh-hans.js

--- tesseract.js_20200615/blockly/msg/blocks/zh-hans.js
+++ tesseract.js_20200615/blockly/msg/blocks/zh-hans.js
@@ -1,6 +1,6 @@
 Blockly.Msg.TESSERACT_WIDTH_SHOW = "文字辨识 视讯宽度";
 Blockly.Msg.TESSERACT_HEIGHT_SHOW = "高度";
-Blockly.Msg.TESSERACT_RECOGNITION_SHOW = "文字辨识 辨识";
+Blockly.Msg.TESSERACT_RECOGNITION_SHOW = "文字辨识 ";
 Blockly.Msg.TESSERACT_GET_SHOW = "文字辨识 取得结果";
 Blockly.Msg.TESSERACT_RESULT_SHOW = "结果";
 Blockly.Msg.TESSERACT_OPACITY_SHOW = "透明度";
