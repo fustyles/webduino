3600a2092c18195cace54b8f53419e5cf08152ce
fustyles <34397289+fustyles@users.noreply.github.com>
2020-06-15 18:00:25 +0800

    Update zh-hant.js

--- tesseract.js_20200615/blockly/msg/blocks/zh-hant.js
+++ tesseract.js_20200615/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.TESSERACT_WIDTH_SHOW = "文字辨識 視訊寬度";
 Blockly.Msg.TESSERACT_HEIGHT_SHOW = "高度";
-Blockly.Msg.TESSERACT_RECOGNITION_SHOW = "文字辨識 辨識";
+Blockly.Msg.TESSERACT_RECOGNITION_SHOW = "文字辨識 ";
 Blockly.Msg.TESSERACT_GET_SHOW = "文字辨識 取得結果";
 Blockly.Msg.TESSERACT_RESULT_SHOW = "結果";
 Blockly.Msg.TESSERACT_OPACITY_SHOW = "透明度";
