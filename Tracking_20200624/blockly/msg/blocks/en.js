f37541ae94f019422e7cbd35ad9ef661d4bef55f
fustyles <34397289+fustyles@users.noreply.github.com>
2020-06-25 14:04:50 +0800

    Add files via upload

---
+++ Tracking_20200624/blockly/msg/blocks/en.js
@@ -0,0 +1,13 @@
+Blockly.Msg.TRACKCOLOR_SET_SHOW = "TrackColor Window Set";
+Blockly.Msg.TRACKCOLOR_WIDTH_SHOW = "Width";   
+Blockly.Msg.TRACKCOLOR_HEIGHT_SHOW = "Height";	  
+Blockly.Msg.TRACKCOLOR_DISPLAY_SHOW = "Display";
+Blockly.Msg.TRACKCOLOR_PIXEL_SHOW = "Pixel map";
+Blockly.Msg.TRACKCOLOR_CUSTOM_SHOW = "Custom Color Range(0~255)";	  
+Blockly.Msg.TRACKCOLOR_RECT_SHOW = "Rect Color";
+Blockly.Msg.TRACKCOLOR_RED_SHOW = "R Range";	
+Blockly.Msg.TRACKCOLOR_TO_SHOW = "~"; 
+Blockly.Msg.TRACKCOLOR_GREEN_SHOW = "G Range";	
+Blockly.Msg.TRACKCOLOR_BLUE_SHOW = "B Range";	
+Blockly.Msg.TRACKCOLOR_GET_SHOW = "TrackColor Get "; 	  
+Blockly.Msg.TRACKCOLOR_GET_NUMBER_SHOW = "TrackColor Get Number";
