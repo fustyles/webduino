507b62921f2b924a35a9290a7e587083755fb74a
fustyles <34397289+fustyles@users.noreply.github.com>
2020-06-25 14:54:10 +0800

    Update zh-hans.js

--- Tracking_20200625/blockly/msg/blocks/zh-hans.js
+++ Tracking_20200625/blockly/msg/blocks/zh-hans.js
@@ -10,4 +10,4 @@ Blockly.Msg.TRACKCOLOR_TO_SHOW = "~";
 Blockly.Msg.TRACKCOLOR_GREEN_SHOW = "G范围";
 Blockly.Msg.TRACKCOLOR_BLUE_SHOW = "B范围";
 Blockly.Msg.TRACKCOLOR_GET_SHOW = "取得侦测结果";
-Blockly.Msg.TRACKCOLOR_GET_NUMBER_SHOW = "取得侦测结果总数";
+Blockly.Msg.TRACKCOLOR_GET_NUMBER_SHOW = "取得侦测结果区块数";
