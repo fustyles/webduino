5604c560ba9887e6717ca6236e9883c28dc9526b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-07-30 07:23:42 +0800

    Update en.js

--- teachablemachine_20200729/blockly/msg/blocks/en.js
+++ teachablemachine_20200729/blockly/msg/blocks/en.js
@@ -4,4 +4,4 @@ Blockly.Msg.TEACHABLEMACHINE_RESULT_SHOW = "Result";
 Blockly.Msg.TEACHABLEMACHINE_MIRRORIMAGE_SHOW = "MirrorImage";
 Blockly.Msg.TEACHABLEMACHINE_OPACITY_SHOW = "Opacity";
 Blockly.Msg.TEACHABLEMACHINE_MODEL_SHOW = "Model Path";
-Blockly.Msg.TEACHABLEMACHINE_PROPORTION_SHOW = "Teachable Machine (custom model) Result";
+Blockly.Msg.TEACHABLEMACHINE_PROPORTION_SHOW = "Teachable Machine (custom model) ";
