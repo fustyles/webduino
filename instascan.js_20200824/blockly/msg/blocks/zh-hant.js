8d56a8e34b0b26d27983d780444bdafb7e14e0b8
fustyles <34397289+fustyles@users.noreply.github.com>
2020-08-24 21:19:37 +0800

    Update zh-hant.js

--- instascan.js_20200824/blockly/msg/blocks/zh-hant.js
+++ instascan.js_20200824/blockly/msg/blocks/zh-hant.js
@@ -3,4 +3,4 @@ Blockly.Msg.INSTASCAN_HEIGHT_SHOW = "高度";
 Blockly.Msg.INSTASCAN_GET_SHOW = "QRcode辨識 取得結果";
 Blockly.Msg.INSTASCAN_RESULT_SHOW = "結果";
 Blockly.Msg.INSTASCAN_OPACITY_SHOW = "透明度";
-Blockly.Msg.INSTASCAN_CLEAR_SHOW = "清除QRcode辨識結果";
+Blockly.Msg.INSTASCAN_CLEAR_SHOW = "QRcode辨識 清除結果";
