ddc5f742432ee7483c4f4ddf7a1e2c2d5cccf29c
fustyles <34397289+fustyles@users.noreply.github.com>
2020-08-24 21:19:48 +0800

    Update zh-hans.js

--- instascan.js_20200824/blockly/msg/blocks/zh-hans.js
+++ instascan.js_20200824/blockly/msg/blocks/zh-hans.js
@@ -3,4 +3,4 @@ Blockly.Msg.INSTASCAN_HEIGHT_SHOW = "高度";
 Blockly.Msg.INSTASCAN_GET_SHOW = "QRcode辨识 取得结果";
 Blockly.Msg.INSTASCAN_RESULT_SHOW = "结果";
 Blockly.Msg.INSTASCAN_OPACITY_SHOW = "透明度";
-Blockly.Msg.INSTASCAN_CLEAR_SHOW = "清除QRcode辨识结果";
+Blockly.Msg.INSTASCAN_CLEAR_SHOW = "QRcode辨识 清除结果";
