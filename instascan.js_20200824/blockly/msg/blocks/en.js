beae5aa122e64d18a38ee62b53ec40c9e5c7c49b
fustyles <34397289+fustyles@users.noreply.github.com>
2020-08-24 21:20:34 +0800

    Update en.js

--- instascan.js_20200824/blockly/msg/blocks/en.js
+++ instascan.js_20200824/blockly/msg/blocks/en.js
@@ -3,4 +3,4 @@ Blockly.Msg.INSTASCAN_HEIGHT_SHOW = "Height";
 Blockly.Msg.INSTASCAN_GET_SHOW = "Get Result (QRcode recognition)";
 Blockly.Msg.INSTASCAN_RESULT_SHOW = "Result";
 Blockly.Msg.INSTASCAN_OPACITY_SHOW = "Opacity";
-Blockly.Msg.INSTASCAN_CLEAR_SHOW = "Clear QRcode Result";
+Blockly.Msg.INSTASCAN_CLEAR_SHOW = "Clear Result (QRcode recognition)";
