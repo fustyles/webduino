ee136c2b7f266e34f4b7e7281662a4b2ab032b83
fustyles <34397289+fustyles@users.noreply.github.com>
2020-09-08 09:43:54 +0800

    Update javascript.js

--- GameElements_20190131/blockly/javascript.js
+++ GameElements_20190131/blockly/javascript.js
@@ -1194,7 +1194,7 @@ Blockly.JavaScript['ajax_get'] = function (block) {
 
   Blockly.JavaScript.definitions_['ajaxData_'+value_id_.replace(/'/g,"")] = 'var ajaxData_'+value_id_.replace(/'/g,"")+';';
 
-  var code = '$.ajax({ type: "'+value_type_+'" , url: '+value_url_+' , dataType: "'+value_datatype_+'", async: '+value_async_+', success: function(data, textStatus) {ajaxData_'+value_id_.replace(/'/g,"")+'=data;} });\n';
+  var code = '$.ajax({ \ntype: "'+value_type_+'" , \nurl: '+value_url_+' , \ndataType: "'+value_datatype_+'", \nasync: '+value_async_+', \nsuccess: function(data, textStatus) {\najaxData_'+value_id_.replace(/'/g,"")+'=data;\n} \n});\n';
   return code;
 };
 
