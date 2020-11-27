638885b35751b23853da4b0a50959259a669fe2c
fustyles <34397289+fustyles@users.noreply.github.com>
2020-11-27 15:03:46 +0800

    Update zh-hant.js

--- GameElements_20190131/blockly/msg/blocks/zh-hant.js
+++ GameElements_20190131/blockly/msg/blocks/zh-hant.js
@@ -433,3 +433,4 @@ Blockly.Msg.PROPERTY_CAM = "網路攝影機";
 Blockly.Msg.VIDEO_BASE64 = "截圖轉換Base64字串";
 Blockly.Msg.CANVAS_CAPTUREVIDEO = "畫布 捕捉視訊畫面";
 Blockly.Msg.STREAMURL = "串流網址";
+Blockly.Msg.CUSTOM_COMMENT_SHOW = "註解";
