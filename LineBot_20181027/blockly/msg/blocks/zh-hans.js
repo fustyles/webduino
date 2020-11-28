815c49ed05a5960b00419b5dcc7354f9e6815f7f
fustyles <34397289+fustyles@users.noreply.github.com>
2020-11-28 23:54:17 +0800

    Update zh-hans.js

--- LineBot_20181027/blockly/msg/blocks/zh-hans.js
+++ LineBot_20181027/blockly/msg/blocks/zh-hans.js
@@ -1,3 +1,4 @@
 Blockly.Msg.LINENOTIFY_IMAGE_SHOW = "LineNotify 传送影像";
 Blockly.Msg.LINENOTIFY_GETID_SHOW = "取得影像元素 ID";
 Blockly.Msg.LINENOTIFY_ID = "代码";
+Blockly.Msg.LINENOTIFY_SCRIPTURL = "Script网址";
