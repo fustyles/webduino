e14a7941c6b5a1cca391d9c0d661de44f3f8eccc
fustyles <34397289+fustyles@users.noreply.github.com>
2020-11-28 23:54:15 +0800

    Update en.js

--- LineBot_20181027/blockly/msg/blocks/en.js
+++ LineBot_20181027/blockly/msg/blocks/en.js
@@ -1,3 +1,4 @@
 Blockly.Msg.LINENOTIFY_IMAGE_SHOW = "LineNotify Image";
 Blockly.Msg.LINENOTIFY_GETID_SHOW = "Get Element ID";
 Blockly.Msg.LINENOTIFY_ID = "ID";
+Blockly.Msg.LINENOTIFY_SCRIPTURL = "Script URL";
