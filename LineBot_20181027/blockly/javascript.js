7aabdf6637c6b80c976b4f9ed90d2de06cb7689e
fustyles <34397289+fustyles@users.noreply.github.com>
2020-11-28 22:43:25 +0800

    Update javascript.js

--- LineBot_20181027/blockly/javascript.js
+++ LineBot_20181027/blockly/javascript.js
@@ -71,7 +71,6 @@ Blockly.JavaScript['linenotify_image'] = function (block) {
   else if (notify_source=="image")
 	source_id = 'gameimage_'+notify_videoid;
 	
-  
   var code = 'linenotify_push_image('+notify_script+','+notify_token+','+source_id+');\n';
   return code; 
 };
