a888f40ed8f07a67da93b700678dbfcafe77ed3f
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-04 20:55:09 +0800

    Delete zh-hans.js

--- temp/blockly/msg/blocks/zh-hans.js
+++
@@ -1,4 +0,0 @@
-Blockly.Msg.LINENOTIFY_IMAGE_SHOW = "LineNotify 传送影像";
-Blockly.Msg.LINENOTIFY_GETID_SHOW = "取得影像元素 ID";
-Blockly.Msg.LINENOTIFY_ID = "代码";
-Blockly.Msg.LINENOTIFY_SCRIPTURL = "Script网址";
