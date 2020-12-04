734987c1122a3cda205c4e796b80f3444e230329
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-04 20:55:13 +0800

    Delete en.js

--- temp/blockly/msg/blocks/en.js
+++
@@ -1,4 +0,0 @@
-Blockly.Msg.LINENOTIFY_IMAGE_SHOW = "LineNotify Image";
-Blockly.Msg.LINENOTIFY_GETID_SHOW = "Get Element ID";
-Blockly.Msg.LINENOTIFY_ID = "ID";
-Blockly.Msg.LINENOTIFY_SCRIPTURL = "Script URL";
