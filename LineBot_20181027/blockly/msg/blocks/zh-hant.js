646143b93a98d20384bb3a276a0f196e6ff740f3
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-04 20:48:25 +0800

    Update zh-hant.js

--- LineBot_20181027/blockly/msg/blocks/zh-hant.js
+++ LineBot_20181027/blockly/msg/blocks/zh-hant.js
@@ -2,3 +2,6 @@ Blockly.Msg.LINENOTIFY_IMAGE_SHOW = "LineNotify 傳送影像";
 Blockly.Msg.LINENOTIFY_GETID_SHOW = "取得影像元素 ID";
 Blockly.Msg.LINENOTIFY_ID = "代碼";
 Blockly.Msg.LINENOTIFY_SCRIPTURL = "Script網址";
+Blockly.Msg.LINENOTIFY_VIDEO = "視訊媒體";
+Blockly.Msg.LINENOTIFY_CANVAS = "畫布";
+Blockly.Msg.LINENOTIFY_IMAGE = "圖片";
