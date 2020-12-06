f0316247050cb56252735982e5f635d3dfc570b3
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-06 23:16:15 +0800

    Update esp32cam.js

--- ESP32-CAM_20191201/esp32cam.js
+++ ESP32-CAM_20191201/esp32cam.js
@@ -6,7 +6,7 @@ var esp32cam_Response=[];
 var esp32cam_getstate = false;
 
 function esp32cam_sendCommand(url,cmd,str1,str2,str3,str4,str5,str6,str7,str8,str9) {
-  //console.log(url+"?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9);
+  console.log(url+"?"+cmd+"="+str1+";"+str2+";"+str3+";"+str4+";"+str5+";"+str6+";"+str7+";"+str8+";"+str9);
   esp32cam_Response=[];
   var data = $.ajax({    
       "type": "POST",
