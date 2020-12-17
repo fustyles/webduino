68690520e45e56c260aa7a2a39e9b20b6f6644c1
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-17 08:16:03 +0800

    Update blocks.js

--- EDU_addCustomBlocks/blockly/blocks.js
+++ EDU_addCustomBlocks/blockly/blocks.js
@@ -152,6 +152,8 @@ Blockly.Blocks['addMyBlocks'] = {
                   Code.customTab.$__add__('https://fustyles.github.io/webduino/coco-ssd_20201012/blockly.json');
                   Code.customTab.$__add__('https://fustyles.github.io/webduino/mobilenet_20201012/blockly.json');
                   Code.customTab.$__add__('https://fustyles.github.io/webduino/teachablemachine_20201012/blockly.json');
+		    
+		  Code.customTab.$__add__('https://fustyles.github.io/webduino/GameElements_20190131/blockly.json');
 				  
                   this.getInputTargetBlock("url").setFieldValue("", "TEXT");
           }
