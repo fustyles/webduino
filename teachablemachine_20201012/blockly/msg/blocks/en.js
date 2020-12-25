c9c38f95b6207d71e10426621b99a4b09849d119
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-25 18:29:49 +0800

    Update en.js

--- teachablemachine_20201012/blockly/msg/blocks/en.js
+++ teachablemachine_20201012/blockly/msg/blocks/en.js
@@ -14,3 +14,5 @@ Blockly.Msg.TEACHABLEMACHINE_FACING_FRONT_SHOW = "Front";
 Blockly.Msg.TEACHABLEMACHINE_FACING_BACK_SHOW = "Back";
 Blockly.Msg.TEACHABLEMACHINE_SRC_SHOW = "Stream URL";
 Blockly.Msg.TEACHABLEMACHINE_PROJECT_SHOW = "Teachable Machine (custom model) Project";
+Blockly.Msg.TEACHABLEMACHINE_MAXCLASS_SHOW = "Max ClassName";
+Blockly.Msg.TEACHABLEMACHINE_MAXPROBABILITY_SHOW = "Max Probability";
