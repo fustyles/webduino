e09e53a118ae74cc4fd963d3a247ec72c34c5c72
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-28 23:53:41 +0800

    Update zh-hant.js

--- Telegram_20200809/blockly/msg/blocks/zh-hant.js
+++ Telegram_20200809/blockly/msg/blocks/zh-hant.js
@@ -1,3 +1,6 @@
 Blockly.Msg.TELEGRAM_MESSAGE_SHOW = "Telegram 傳送訊息";
 Blockly.Msg.TELEGRAM_IMAGE_SHOW = "Telegram 傳送影像";
 Blockly.Msg.TELEGRAM_GETID_SHOW = "取得影像元素 ID";
+Blockly.Msg.TELEGRAM_VIDEO = "視訊媒體";
+Blockly.Msg.TELEGRAM_CANVAS = "畫布";
+Blockly.Msg.TELEGRAM_IMAGE = "圖片";
