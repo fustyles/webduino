341fbafbb580a613a8c56f448b482ff36901e931
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-28 23:54:56 +0800

    Update javascript.js

--- Telegram_20200809/blockly/javascript.js
+++ Telegram_20200809/blockly/javascript.js
@@ -14,9 +14,29 @@ Blockly.JavaScript['telegram_image'] = function (block) {
   var telegram_script = Blockly.JavaScript.valueToCode(block, 'telegram_script', Blockly.JavaScript.ORDER_ATOMIC);
   var telegram_token = Blockly.JavaScript.valueToCode(block, 'telegram_token', Blockly.JavaScript.ORDER_ATOMIC); 
   var telegram_chatid = Blockly.JavaScript.valueToCode(block, 'telegram_chatid', Blockly.JavaScript.ORDER_ATOMIC);
+  var telegram_source = block.getFieldValue('telegram_source');	
   var telegram_videoid = Blockly.JavaScript.valueToCode(block, 'telegram_videoid', Blockly.JavaScript.ORDER_ATOMIC);
 
-  var code = 'telegram_push_image('+telegram_script+','+telegram_token+','+telegram_chatid+','+telegram_videoid+');\n';
+  if (!telegram_script) telegram_script='""';
+  if (!telegram_token) telegram_token='""';
+  if (!telegram_chatid) telegram_chatid='""';
+
+  if ((telegram_videoid.indexOf("(")==0)&&(telegram_videoid.lastIndexOf(")")==telegram_videoid.length-1))
+    telegram_videoid = telegram_videoid.substring(1,telegram_videoid.length-1);	
+  if ((telegram_videoid.indexOf("'")==0)&&(telegram_videoid.lastIndexOf("'")==telegram_videoid.length-1))
+    telegram_videoid = telegram_videoid.substring(1,telegram_videoid.length-1);
+  if ((telegram_videoid.indexOf('"')==0)&&(telegram_videoid.lastIndexOf('"')==telegram_videoid.length-1))
+    telegram_videoid = telegram_videoid.substring(1,telegram_videoid.length-1);	
+
+  var source_id;
+  if (telegram_source=="video")
+	source_id = 'gamevideo_'+telegram_videoid;
+  else if (telegram_source=="canvas")
+	source_id = 'gamecanvas_'+telegram_videoid;
+  else if (telegram_source=="image")
+	source_id = 'gameimage_'+telegram_videoid;
+	
+  var code = 'telegram_push_image('+telegram_script+','+telegram_token+','+telegram_chatid+','+source_id+');\n';
   return code; 
 };
 
