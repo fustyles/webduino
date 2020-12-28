4d3f5be8198b91c4222c9c0dfc816c45cb611734
fustyles <34397289+fustyles@users.noreply.github.com>
2020-12-29 00:01:04 +0800

    Update en.js

--- Telegram_20200809/blockly/msg/blocks/en.js
+++ Telegram_20200809/blockly/msg/blocks/en.js
@@ -1,6 +1,7 @@
 Blockly.Msg.TELEGRAM_MESSAGE_SHOW = "Telegram Message";
 Blockly.Msg.TELEGRAM_IMAGE_SHOW = "Telegram Image";
 Blockly.Msg.TELEGRAM_GETID_SHOW = "Get Element ID";
+Blockly.Msg.TELEGRAM_ID = "ID";
 Blockly.Msg.TELEGRAM_VIDEO = "Video";
 Blockly.Msg.TELEGRAM_CANVAS = "Canvas";
 Blockly.Msg.TELEGRAM_IMAGE = "Image";
