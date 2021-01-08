cd7bad29e43e5f7d83ee6fb5fe20f45cf9086a80
fustyles <34397289+fustyles@users.noreply.github.com>
2021-01-08 22:52:18 +0800

    Update zh-hans.js

--- Telegram_20200809/blockly/msg/blocks/zh-hans.js
+++ Telegram_20200809/blockly/msg/blocks/zh-hans.js
@@ -4,5 +4,5 @@ Blockly.Msg.TELEGRAM_GETID_SHOW = "取得影像元素 ID";
 Blockly.Msg.TELEGRAM_ID_SHOW = "代码";
 Blockly.Msg.TELEGRAM_VIDEO_SHOW = "视讯媒体";
 Blockly.Msg.TELEGRAM_CANVAS_SHOW = "画布";
-Blockly.Msg.TELEGRAM_GETUPDATES_SHOW = "Telegram 取得讯息";
+Blockly.Msg.TELEGRAM_GETUPDATES_SHOW = "Telegram 取得最新讯息";
 Blockly.Msg.TELEGRAM_GETMESSAGE_SHOW = " = Telegram 讯息";
