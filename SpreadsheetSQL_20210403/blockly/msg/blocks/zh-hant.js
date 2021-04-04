0377cacbb9dd96e4241941f561afd818e5f244e9
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-04 10:34:33 +0800

    Update zh-hant.js

--- SpreadsheetSQL_20210403/blockly/msg/blocks/zh-hant.js
+++ SpreadsheetSQL_20210403/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.SPREADSHEETSQL_SHOW = "Google試算表SQL查詢 設定";
 Blockly.Msg.SPREADSHEETSQL_EXECUTESQL_SHOW = "Google試算表SQL查詢";
-Blockly.Msg.SPREADSHEETSQL_ID_SHOW = "代碼";
+Blockly.Msg.SPREADSHEETSQL_ID_SHOW = "試算表ID";
 Blockly.Msg.SPREADSHEETSQL_NAME_SHOW = "工作表名稱";
 Blockly.Msg.SPREADSHEETSQL_SQL_SHOW = "SQL語法";
 Blockly.Msg.SPREADSHEETSQL_GETDATA_SHOW = "Google試算表 取得SQL查詢結果";
