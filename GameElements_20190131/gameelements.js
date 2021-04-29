5a7415ac0b739543b85eb84b7a18fec23859d640
fustyles <34397289+fustyles@users.noreply.github.com>
2021-04-29 15:22:46 +0800

    Update gameelements.js

--- GameElements_20190131/gameelements.js
+++ GameElements_20190131/gameelements.js
@@ -3660,7 +3660,7 @@ function HextoRgb(color) {
 
 			$.ajax({
 				"type": "POST",
-				"dataType": "json",
+				"dataType": "jsonp",
 				"headers": { 
 				  "Content-Type": "application/x-www-form-urlencoded"
 				},				
