24ce36d9ecebf6f525882634ba285c2963c22647
fustyles <34397289+fustyles@users.noreply.github.com>
2021-05-06 15:50:47 +0800

    Update zh-hant.js

--- Telegram_20200809/blockly/msg/blocks/zh-hant.js
+++ Telegram_20200809/blockly/msg/blocks/zh-hant.js
@@ -1,10 +1,11 @@
 Blockly.Msg.TELEGRAM_MESSAGE_SHOW = "Telegram機器人 傳送訊息";
 Blockly.Msg.TELEGRAM_PHOTO_SHOW = "Telegram機器人 傳送圖片";
-Blockly.Msg.TELEGRAM_IMAGE_SHOW = "Telegram機器人 傳送影像";
+Blockly.Msg.TELEGRAM_VIDEO_SHOW = "Telegram機器人 傳送影像";
 Blockly.Msg.TELEGRAM_GETID_SHOW = "取得影像元素 ID";
 Blockly.Msg.TELEGRAM_ID_SHOW = "代碼";
 Blockly.Msg.TELEGRAM_VIDEO_SHOW = "視訊媒體";
 Blockly.Msg.TELEGRAM_CANVAS_SHOW = "畫布";
+Blockly.Msg.TELEGRAM_IMAGE_SHOW = "圖片";
 Blockly.Msg.TELEGRAM_GETUPDATES_SHOW = "Telegram機器人 取得最新訊息";
 Blockly.Msg.TELEGRAM_GETMESSAGE_SHOW = " 取得 Telegram機器人 最新訊息";
 Blockly.Msg.TELEGRAM_BR_SHOW = "Telegram機器人 訊息換行";
