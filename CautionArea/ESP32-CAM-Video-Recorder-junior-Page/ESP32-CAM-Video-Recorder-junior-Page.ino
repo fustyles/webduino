b95bfb327b9b21c6c1b7301cec6210e415487b7a
fustyles <34397289+fustyles@users.noreply.github.com>
2021-06-29 23:26:28 +0800

    Update ESP32-CAM-Video-Recorder-junior-Page.ino

--- CautionArea/ESP32-CAM-Video-Recorder-junior-Page/ESP32-CAM-Video-Recorder-junior-Page.ino
+++ CautionArea/ESP32-CAM-Video-Recorder-junior-Page/ESP32-CAM-Video-Recorder-junior-Page.ino
@@ -1491,7 +1491,11 @@ static esp_err_t cmd_handler(httpd_req_t *req){
             break;
           }
         }
-      }                 
+      }
+      else if (cmd=="clearwifi") {  //清除閃存中Wi-Fi資料  
+        Preferences_write("wifi", "ssid", "");
+        Preferences_write("wifi", "password", "");
+      }      
       else if (cmd=="record") {
           Serial.println("");
           Serial.println("Start recording");
