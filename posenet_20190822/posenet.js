5494a84bb00286fb9e0dba5199a1165912f0fca0
fustyles <34397289+fustyles@users.noreply.github.com>
2021-07-08 23:13:21 +0800

    Update posenet.js

--- posenet_20190822/posenet.js
+++ posenet_20190822/posenet.js
@@ -68,8 +68,11 @@
   } 	
 	
   function posenet_angle(input_x0,input_y0,input_x1,input_y1) {
-      return (Math.atan((input_y1-input_y0)/(input_x1-input_x0)) / Math.PI) * 180;
-  } 
+      var angle = (Math.atan((input_y1-input_y0)/(input_x1-input_x0)) / Math.PI) * 180;
+      if (angle<0) angle = 180 + angle;
+      if (input_y0<input_y1) angle = 180 + angle;
+      return angle;
+  }
   
   function posenet_state(input_state){
     document.getElementById('posenetState').innerHTML = input_state;
