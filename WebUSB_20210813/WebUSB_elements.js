3c149212c5f3afb0e2ca3491ccb8efedf0c50201
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-14 00:11:24 +0800

    Update WebUSB_elements.js

--- WebUSB_20210813/WebUSB_elements.js
+++ WebUSB_20210813/WebUSB_elements.js
@@ -2,4 +2,4 @@ document.write('<input type="button" id="connect" value="Connect to device" styl
 document.write('<span id="status" style="display:none">Select your device</span>');
 document.write('<input type="text" id="command" value="" style="display:none">');
 document.write('<input type="button" id="send" value="Send command" style="display:none">');
-document.write('<span id="response" style="display:none"></span>');
+document.write('<span id="response"></span>');
