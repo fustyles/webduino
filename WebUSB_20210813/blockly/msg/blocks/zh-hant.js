d96bd126b7f888d88af3be28aa98b723fb103cc9
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-14 00:42:29 +0800

    Update zh-hant.js

--- WebUSB_20210813/blockly/msg/blocks/zh-hant.js
+++ WebUSB_20210813/blockly/msg/blocks/zh-hant.js
@@ -1,4 +1,4 @@
-Blockly.Msg.WEBUSB_BUTTON_SHOW = "WebUSB 開發板按鈕顯示";
+Blockly.Msg.WEBUSB_BUTTON_SHOW = "WebUSB 開發板連結按鈕顯示";
 Blockly.Msg.WEBUSB_STATE_SHOW = "WebUSB 開發板連線狀態";
 Blockly.Msg.WEBUSB_SEND_SHOW = "WebUSB 傳送指令";
 Blockly.Msg.WEBUSB_GET_SHOW = "WebUSB 取得回傳資料";
