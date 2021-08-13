dcc584c0a6b1780b8ed46746979da691c83ac3c5
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-14 00:37:27 +0800

    Update en.js

--- WebUSB_20210813/blockly/msg/blocks/en.js
+++ WebUSB_20210813/blockly/msg/blocks/en.js
@@ -1,5 +1,5 @@
 Blockly.Msg.WEBUSB_BUTTON_SHOW = "WebUSB connect button";
-Blockly.Msg.WEBUSB_WAIT_SHOW = "WebUSB connect state";
+Blockly.Msg.WEBUSB_STATE_SHOW = "WebUSB connect state";
 Blockly.Msg.WEBUSB_SEND_SHOW = "WebUSB send command";
 Blockly.Msg.WEBUSB_GET_SHOW = "WebUSB get data";
 Blockly.Msg.WEBUSB_WAITTIME_SHOW = "Wait(ms)";
