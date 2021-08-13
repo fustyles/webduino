7248c225441f4ad3c6165365c021fde32721df31
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-13 23:13:38 +0800

    Update javascript.js

--- WebUSB_20210813/blockly/javascript.js
+++ WebUSB_20210813/blockly/javascript.js
@@ -1,3 +1,8 @@
+Blockly.JavaScript['webusb_wait'] = function(block) {
+  var code = 'weusb_wait()';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
 Blockly.JavaScript['webusb_send'] = function(block) {
   var value_cmd = Blockly.JavaScript.valueToCode(block, 'cmd_', Blockly.JavaScript.ORDER_ATOMIC); 
   var code = 'webusb_send('+value_cmd+');\n';
