2c02d4947f0939a5bb1454254e36267cf60ec64c
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-13 23:06:15 +0800

    Update zh-hant.js

--- WebUSB_20210813/blockly/msg/blocks/zh-hant.js
+++ WebUSB_20210813/blockly/msg/blocks/zh-hant.js
@@ -1,4 +1,3 @@
-Blockly.Msg.WEBUSB_LINK_SHOW = "WebUSB 連結開發版";
 Blockly.Msg.WEBUSB_SEND_SHOW = "WebUSB 傳送指令";
 Blockly.Msg.WEBUSB_GET_SHOW = "WebUSB 取得回傳資料";
 Blockly.Msg.WEBUSB_CLEAR_SHOW = "WebUSB 清除回傳資料";
