372e666f79fc4db4507af38c094033d7784179ab
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-14 18:21:54 +0800

    Update WebUSB.js

--- WebUSB_20210813/WebUSB.js
+++ WebUSB_20210813/WebUSB.js
@@ -14,6 +14,7 @@
 	}
 	
 	function webusb_button_position(input_id, input_left, input_top) {
+		document.getElementById(input_id).style.position = "absolute";
 		document.getElementById(input_id).style.left = input_left;
 		document.getElementById(input_id).style.top = input_top;		
 	}
