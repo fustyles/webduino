d1b5f051901fdc81bd6f5fedb8e1899499dd9e02
fustyles <34397289+fustyles@users.noreply.github.com>
2021-08-14 18:08:41 +0800

    Update zh-hans.js

--- WebUSB_20210813/blockly/msg/blocks/zh-hans.js
+++ WebUSB_20210813/blockly/msg/blocks/zh-hans.js
@@ -7,3 +7,5 @@ Blockly.Msg.WEBUSB_WAITTIME_SHOW = "逾时(毫秒)";
 Blockly.Msg.WEBUSB_CLEAR_SHOW = "WebUSB 清除回传资料";
 Blockly.Msg.WEBUSB_CONNECT_SHOW = "开发板连结";
 Blockly.Msg.WEBUSB_STATUS_SHOW = "状态";
+Blockly.Msg.WEBUSB_BUTTON_LEFT_SHOW = "靠左距离"; 
+Blockly.Msg.WEBUSB_BUTTON_TOP_SHOW = "靠上距离";
