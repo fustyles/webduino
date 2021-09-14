956d4f7a37d435f365d1e4f4d75e7815693e1b09
fustyles <34397289+fustyles@users.noreply.github.com>
2021-09-14 09:54:19 +0800

    Update mybackpack.js

--- LinkIt7697/MyBackpack/mybackpack.js
+++ LinkIt7697/MyBackpack/mybackpack.js
@@ -33,13 +33,13 @@ const myBackpack = new MyBackpack(workspace, "category_functions" , true);
 
 'use strict';
 
-Blockly.Msg["MYBACKPACK_ADD"] = "Copy to my backpack";
+Blockly.Msg["MYBACKPACK_ADD"] = "Copy block to my backpack";
 Blockly.Msg["MYBACKPACK_REMOVE"] = "Remove from my backpack";
 Blockly.Msg["MYBACKPACK_REMOVE_ALL"] = "Remove all blocks from my backpack";
 Blockly.Msg["MYBACKPACK_REMOVE_ALL_TITLE"] = "Are you sure that remove all my backpack?";
-Blockly.Msg["MYBACKPACK_IMPORT_FILE"] = "Copy to my backpack from backpack file";
-Blockly.Msg["MYBACKPACK_IMPORT_WORKSPACE"] = "Copy to my backpack from workspace";
-Blockly.Msg["MYBACKPACK_IMPORT_WORKSPACE_TITLE"] = "Are you sure that import blocks to my backpack from workspace?";
+Blockly.Msg["MYBACKPACK_IMPORT_FILE"] = "Copy blocks to my backpack from backpack file";
+Blockly.Msg["MYBACKPACK_IMPORT_WORKSPACE"] = "Copy blocks to my backpack from workspace";
+Blockly.Msg["MYBACKPACK_IMPORT_WORKSPACE_TITLE"] = "Are you sure that copy blocks to my backpack from workspace?";
 Blockly.Msg["MYBACKPACK_WORKSPACE_EXPORT_FILE"] = "Export blocks to backpack file from workspace";
 Blockly.Msg["MYBACKPACK_MYBACKPACK_EXPORT_FILE"] = "Export all blocks to backpack file from my backpack";
 Blockly.Msg["MYBACKPACK_TITLE"] = "My backpack";
