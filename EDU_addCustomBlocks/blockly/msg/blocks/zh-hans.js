ebd148010c9f8df0e1f44a90b04608a7f612805b
fustyles <34397289+fustyles@users.noreply.github.com>
2021-09-15 08:43:06 +0800

    Update zh-hans.js

--- EDU_addCustomBlocks/blockly/msg/blocks/zh-hans.js
+++ EDU_addCustomBlocks/blockly/msg/blocks/zh-hans.js
@@ -24,3 +24,5 @@ Blockly.Msg.ADDCUSTOMBLOCKS_TELEGRAM_SHOW = "[取得连结] Telegram";
 Blockly.Msg.ADDCUSTOMBLOCKS_LINEBOT_SHOW = "[取得连结] LineBot";
 Blockly.Msg.ADDCUSTOMBLOCKS_TELLO_SHOW = "[取得连结] Tello";
 Blockly.Msg.ADDCUSTOMBLOCKS_SPREADSHEETSQL_SHOW = "[取得连结] Google试算表SQL查询";
+Blockly.Msg.ADDCUSTOMBLOCKS_WEBUSB_SHOW = "[取得连结] WEBUSB";
+Blockly.Msg.ADDCUSTOMBLOCKS_MYBACKPACK_SHOW = "[取得连结] 暂存积木";
