c0ab9b86aaea8a294e31d2e77a1fdc2f392dfd9b
fustyles <34397289+fustyles@users.noreply.github.com>
2021-09-22 11:45:28 +0800

    Update index.js

--- SPduino_tool/js/index.js
+++ SPduino_tool/js/index.js
@@ -114,7 +114,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('copycode').onclick = function () {
 		var text = document.getElementById('showcode').innerText;
 		navigator.clipboard.writeText(text).then(function() {
-			//console.log('Copying to clipboard was successful!');
+			alert('Copying to clipboard was successful!');
 		}, function(err) {
 			console.error(err);
 		});
