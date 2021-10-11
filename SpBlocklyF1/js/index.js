07d95403b4b3843350619a9763a4082e1d33e320
fustyles <34397289+fustyles@users.noreply.github.com>
2021-10-11 10:24:29 +0800

    Update index.js

--- SpBlocklyF1/js/index.js
+++ SpBlocklyF1/js/index.js
@@ -374,7 +374,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	document.getElementById('button_copycode').onclick = function () {
 		var text = document.getElementById('arduino_code').innerText;
 		navigator.clipboard.writeText(text).then(function() {
-			console.log('Async: Copying to clipboard was successful!');
+			alert('Copying to clipboard was successful!');
 		}, function(err) {
 			console.error('Async: Could not copy text: ', err);
 		});
