36c2ec5c7e8fd9fc776e94526cecfab99c8d0bc1
fustyles <34397289+fustyles@users.noreply.github.com>
2021-10-11 19:23:41 +0800

    Add files via upload

--- SpBlocklyF1/customBlocks/customblocks.js
+++ SpBlocklyF1/customBlocks/customblocks.js
@@ -1,7 +1,5 @@
 var customBlocks = [
 ["https://circuspi.github.io/ICSHOP/","category_sep_custom"]
 ,["https://lioujj.github.io/LinkIt7697/extensions_0922/","category_sep_custom"]
-,["https://fustyles.github.io/webduino/LinkIt7697/FranceFu_Webbit/","category_sep_custom"]
-,["https://fustyles.github.io/webduino/LinkIt7697/FranceFu_Elements/","category_sep_custom"]
-,["https://fustyles.github.io/webduino/LinkIt7697/FranceFu/","category_sep_custom"]
+,["https://fustyles.github.io/webduino/LinkIt7697/France/","category_sep_custom"]
 ];
