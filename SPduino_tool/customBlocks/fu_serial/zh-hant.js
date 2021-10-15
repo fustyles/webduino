e8e4ee89f69dc13802f333d447e151c097dd22c0
fustyles <34397289+fustyles@users.noreply.github.com>
2021-10-15 13:16:08 +0800

    Add files via upload

---
+++ SPduino_tool/customBlocks/fu_serial/zh-hant.js
@@ -0,0 +1,15 @@
+//SERIAL
+Blockly.Msg["FU_SERIAL_HUE"] = "60";
+Blockly.Msg["FU_SERIAL"] = "序列埠";
+Blockly.Msg["FU_SERIAL_BAUDRATE"] = "通訊起始鮑率";
+Blockly.Msg["FU_SERIAL_CONFIG"] = "格式";
+Blockly.Msg["FU_SERIAL_READY"] = "等待連線";
+Blockly.Msg["FU_SERIAL_END"] = "通訊停止";
+Blockly.Msg["FU_SERIAL_PRINT"] = "印出資料";
+Blockly.Msg["FU_SERIAL_WRITE"] = "印出二進制資料";
+Blockly.Msg["FU_SERIAL_PRINTLN"] = "印出資料(換行)";
+Blockly.Msg["FU_SERIAL_FORMAT"] = "格式";
+Blockly.Msg["FU_SERIAL_PRINT_BIN"] = "二進制"; 
+Blockly.Msg["FU_SERIAL_PRINT_OCT"] = "十進制";
+Blockly.Msg["FU_SERIAL_PRINT_DEC"] = "十二進制"; 
+Blockly.Msg["FU_SERIAL_PRINT_HEX"] = "十六進制";
