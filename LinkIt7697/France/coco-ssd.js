4f507df2342beac910e35594e09efefeda2a5aa9
fustyles <34397289+fustyles@users.noreply.github.com>
2021-10-30 02:51:43 +0800

    Update coco-ssd.js

--- LinkIt7697/France/coco-ssd.js
+++ LinkIt7697/France/coco-ssd.js
@@ -1,6 +1,6 @@
 addScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js',1);
 addScript('https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js',1);
-addScript('https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd@2.1.0',0);
+addScript('https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd@2.1.0',1);
 
 	function addScript(url, pos) {
 		var s=document.createElement('script');
