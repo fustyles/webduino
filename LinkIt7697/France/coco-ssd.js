7290e1ef9274137b87ea5586360efec479a9262d
fustyles <34397289+fustyles@users.noreply.github.com>
2021-10-30 00:42:28 +0800

    Update coco-ssd.js

--- LinkIt7697/France/coco-ssd.js
+++ LinkIt7697/France/coco-ssd.js
@@ -1,7 +1,17 @@
-document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>');
-document.write('<script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js"></script>');
-document.write('<script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd@2.1.0"></script>');
+addScript('https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js',1);
+addScript('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js',1);
+addScript('https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd@2.1.0',1);
 document.write('<img id="ShowImage" style="display:none" crossorigin="anonymous"><canvas id="canvas"></canvas><span id="object" style="display:none"></span><span id="score" style="display:none"></span>');
+
+function addScript(url, pos) {
+	var s=document.createElement('script');
+	s.type='text/javascript';
+	s.src= url;
+	if (pos==1)
+		document.getElementsByTagName('head')[0].append(s);
+	else
+		document.body.append(s);		
+}
 
 window.onload = function () {
 	
