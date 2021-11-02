7e25ce52241bbf013ea2e69ea892b82ffcd16e5c
fustyles <34397289+fustyles@users.noreply.github.com>
2021-11-03 00:42:57 +0800

    Update javascript.js

--- LinkIt7697/FranceFu/javascript.js
+++ LinkIt7697/FranceFu/javascript.js
@@ -1,11 +1,12 @@
 Blockly.Arduino['fu_oled_initial'] = function(block) {
+  var dropdown_format = block.getFieldValue('format');
   var dropdown_display = block.getFieldValue('display');
   var dropdown_utf8 = (block.getFieldValue('utf8')=='Y')?"  u8g2.enableUTF8Print();":"  u8g2.disableUTF8Print();";
   var text_font = block.getFieldValue('font');
   
   Blockly.Arduino.definitions_['u8g2_definition'] = '#include <U8g2lib.h>\n'+
 												  '#include <Wire.h>\n'+
-												  'U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2('+dropdown_display+', /* reset=*/ U8X8_PIN_NONE);';												  
+												  dropdown_format.replace('U8G2_R0',dropdown_display);												  
   Blockly.Arduino.setups_['u8g2_setup'] = 'u8g2.begin();\n  '+
 										  'u8g2.setFont('+text_font+');\n'+
 										  dropdown_utf8;
