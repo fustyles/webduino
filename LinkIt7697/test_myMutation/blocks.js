f3cfc3eb27dede98916d135242db5791defcaf3d
fustyles <34397289+fustyles@users.noreply.github.com>
2021-11-24 20:43:25 +0800

    Update blocks.js

--- LinkIt7697/test_myMutation/blocks.js
+++ LinkIt7697/test_myMutation/blocks.js
@@ -1,11 +1,11 @@
 Blockly.Blocks["mutation_test"] = {
   init: function() {
     this.appendDummyInput()
-        .appendField("Hello World");	  
+        .appendField("Hello World");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldLabelSerializable(""), "xml");	  
     this.appendDummyInput()
         .appendField(new Blockly.FieldLabelSerializable(""), "code");
-    this.appendDummyInput()
-        .appendField(new Blockly.FieldLabelSerializable(""), "xml");
     this.setInputsInline(true);		
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
@@ -13,12 +13,12 @@ Blockly.Blocks["mutation_test"] = {
 	
     this.setMutator(new Blockly.myMutator(["controls_if","logic_compare","math_number"]));
 	
-    this.getField("code").setVisible(false);
-    this.setFieldValue('if (0 > 0) {\n}\n',"code");
-	
     this.getField("xml").setVisible(false);
     var initBlocks = '<xml xmlns="https://developers.google.com/blockly/xml"><block type="controls_if"><value name="IF0"><block type="logic_compare"><field name="OP">GT</field><value name="B"><block type="math_number"><field name="NUM">0</field></block></value></block></value></block></xml>';
     this.setFieldValue(initBlocks,"xml");
+	  
+    this.getField("code").setVisible(false);
+    this.setFieldValue('if (0 > 0) {\n}\n',"code");
   },
   myWorkspaceInitial: function(myWorkspace) {
 	if (this.getFieldValue("xml")) {
