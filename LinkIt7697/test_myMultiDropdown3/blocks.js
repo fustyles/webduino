5fd8e168b5bc2a94aade628d3e9890ca4a3fb239
fustyles <34397289+fustyles@users.noreply.github.com>
2021-11-28 13:13:24 +0800

    Add files via upload

---
+++ LinkIt7697/test_myMultiDropdown3/blocks.js
@@ -0,0 +1,87 @@
+var list = [
+	["a","a1","a11"],
+	["a","a1","a12"],	
+	["a","a2","a21"],
+	["a","a2","a22"],	
+	["b","b1","b11"],
+	["b","b1","b12"],	
+	["b","b2","b21"],
+	["b","b2","b22"]	
+];
+
+Blockly.Blocks["multidropdown_test"] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Hello World");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown(this.getParentOptions, this.validate1),"S1");
+    this.appendDummyInput("second")
+        .appendField(new Blockly.FieldDropdown([["",""]], this.validate2),"S2");
+    this.appendDummyInput("third")
+        .appendField(new Blockly.FieldDropdown([["",""]]),"S3");			
+    this.setInputsInline(true);		
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(100); 
+  },
+  getParentOptions: function() {
+     var opt = [];
+	 for (var i=0;i<list.length;i++) {
+		 for (var j=0;j<opt.length;j++) {
+			if (opt[j][0]==list[i][0]) {
+				opt.splice(j, 1);
+				break;
+			}
+		 }
+		 opt.push([list[i][0],list[i][0]]);
+	 }
+	 return opt;
+  },
+  validate1: function(newValue) {
+	 const sourceBlock = this.getSourceBlock();
+     var opt = [];
+	 for (var i=0;i<list.length;i++) {
+		if (list[i][0]==newValue) {
+			for (var j=0;j<opt.length;j++) {
+				if (opt[j][1]==list[i][1]) {
+					opt.splice(j, 1);
+					break;
+				}
+			}				
+			opt.push([list[i][1],list[i][1]]);
+		}
+	 }
+	 if (opt.length==0)
+		 opt.push(["",""]);
+	 sourceBlock.getInput("second").removeField("S2");
+	 sourceBlock.getInput("second").appendField(new Blockly.FieldDropdown(opt, sourceBlock.validate2),"S2");
+	 sourceBlock.validate2(newValue+";"+sourceBlock.getFieldValue("S2"));
+  },
+  validate2: function(newValue) {
+	 if (!this.type) {
+		var sourceBlock = this.getSourceBlock();
+		var firstValue = sourceBlock.getFieldValue("S1");
+	 }
+	 else {
+	    var sourceBlock = this;
+		var firstValue = newValue.split(";")[0];
+		newValue = newValue.split(";")[1];
+	 }
+     var opt = [];
+	 for (var i=0;i<list.length;i++) {
+		 if (list[i][0]==firstValue&&list[i][1]==newValue) {
+			 for (var j=0;j<opt.length;j++) {
+				if (opt[j][1]==list[i][2]) {
+					opt.splice(j, 1);
+					break;
+				}
+			 }			 
+			 opt.push([list[i][2],list[i][2]]);
+		 }
+	 }
+	 if (opt.length==0)
+		 opt.push(["",""]);	 
+	 sourceBlock.getInput("third").removeField("S3");
+	 sourceBlock.getInput("third").appendField(new Blockly.FieldDropdown(opt),"S3");
+  }   
+};
