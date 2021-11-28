75325bd1eb7085354ed5183be6dcef772b12e6f0
fustyles <34397289+fustyles@users.noreply.github.com>
2021-11-28 13:19:25 +0800

    Update javascript.js

--- LinkIt7697/test_myMultiDropdown3/javascript.js
+++ LinkIt7697/test_myMultiDropdown3/javascript.js
@@ -1,16 +1,3 @@
-/**
- * @license
- * Copyright 2021 Taiwan (ChungYi Fu)
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/**
- * @fileoverview my Multi Dropdown.
- * @author https://www.facebook.com/francefu/
- * @Update 11/24/2021 20:30 (Taiwan Standard Time)
- */
-
-
 
 Blockly.Arduino['multidropdown_test'] = function(block) {
   return '';
