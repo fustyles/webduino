523e190e4c07c8c8a25b40fed1422352648a0a46
fustyles <34397289+fustyles@users.noreply.github.com>
2021-12-13 01:05:12 +0800

    Update blocks.js

--- LinkIt7697/test_myFieldFilter/blocks.js
+++ LinkIt7697/test_myFieldFilter/blocks.js
@@ -14,3 +14,43 @@ Blockly.Blocks["test_fieldDropdownFilter"] = {
 Blockly.Arduino['test_fieldDropdownFilter'] = function(block) {
   return '';
 };
+
+Blockly.Blocks["test_fieldDropdownFilter1"] = {
+  init: function() {
+	this.options = [
+		['',''],	
+		['ap','apple'],
+		['ba','banana'],
+		['ch','cherry']
+	];
+	var options = [];
+	this.options.forEach(
+		element => options.push(element[0])
+	);
+	
+	this.field = new CustomFields.FieldFilter('', options, this.validate);
+    this.appendDummyInput()
+        .appendField('Filter')
+        .appendField(this.field, 'FILTER');
+		
+    this.appendDummyInput()
+        .appendField('', 'VALUE');
+    this.setStyle('loop_blocks');
+    this.setInputsInline(true);		
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);	
+  },
+  validate: function(newValue) {
+	const block = this.sourceBlock_;
+	block.options.forEach(function(element) {
+		if (element[0]==CustomFields.FieldFilter.WORDS[Number(newValue)]) {
+			console.log(element[1]);
+			block.setFieldValue(element[1], 'VALUE');
+		}
+	})
+  }
+};
+
+Blockly.Arduino['test_fieldDropdownFilter1'] = function(block) {
+  return '';
+};
