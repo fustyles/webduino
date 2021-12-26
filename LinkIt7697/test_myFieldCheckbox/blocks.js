060bc6b277116b87c84455b6cec4b1c0b12829d6
fustyles <34397289+fustyles@users.noreply.github.com>
2021-12-26 11:57:31 +0800

    Update blocks.js

--- LinkIt7697/test_myFieldCheckbox/blocks.js
+++ LinkIt7697/test_myFieldCheckbox/blocks.js
@@ -1,22 +1,23 @@
 Blockly.Blocks["test_fieldCheckbox"] = {
   init: function() {
-	this.options1 = [
-		['aaa','a'],
-		['bbb','b'],
-		['ccc','c']
-	];
+    this.options1 = [
+        ['aaa','a'],
+        ['bbb','b'],
+        ['ccc','c']
+    ];
     this.appendDummyInput()
         .appendField('Checkbox1')	
         .appendField(new CustomFields.FieldCheckbox('', this.options1, this.id+"_1"), 'myCheckbox1');
 		
-	this.options2 = [
-		['ddd','d'],
-		['eee','e'],
-		['fff','f']
-	];
+    this.options2 = [
+        ['ddd','d'],
+        ['eee','e'],
+        ['fff','f']
+    ];
     this.appendDummyInput()
         .appendField('Checkbox2')		
-        .appendField(new CustomFields.FieldCheckbox('', this.options2, this.id+"_2"), 'myCheckbox2');		
+        .appendField(new CustomFields.FieldCheckbox('', this.options2, this.id+"_2"), 'myCheckbox2');
+	  
     this.setStyle('loop_blocks');
     this.setInputsInline(true);		
     this.setPreviousStatement(true, null);
