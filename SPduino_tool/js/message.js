bbc4854da5c80d2692a27fd6ad6ff0a1861c7f23
fustyles <34397289+fustyles@users.noreply.github.com>
2021-12-31 11:48:09 +0800

    Update message.js

--- SPduino_tool/js/message.js
+++ SPduino_tool/js/message.js
@@ -14,7 +14,6 @@ var msg = [
 ["button_addExtensionBlocks", "title", Blockly.Msg["BUTTON_ADDEXTENSIONBLOCKS"]],
 ["button_tool", "title", Blockly.Msg["BUTTON_TOOL"]],
 ["button_webSerial", "title", Blockly.Msg["BUTTON_WEBSERIAL"]],
-["button_uploadCode", "title", Blockly.Msg["BUTTON_UPLOADCODE"]],
 ["button_copycode", "title", Blockly.Msg["BUTTON_COPYCODE"]],
 ["lang-selector", "title", Blockly.Msg["LANG-SELECTOR"]],
 ["title_code", "innerHTML", Blockly.Msg["MSG_CODE"]],
