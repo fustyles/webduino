d351ac8f3d38be3eeadfcfda15760ec88047f10d
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-01 00:20:18 +0800

    Delete zh-hant.js

--- SPduino_tool/customBlocks/fu_time/zh-hant.js
+++
@@ -1,7 +0,0 @@
-Blockly.Msg["FU_TIME_HUE"] = "40";
-Blockly.Msg["FU_TIME"] = "時間";
-Blockly.Msg["FU_TIME_DELAY"] = "時間延遲";
-Blockly.Msg["FU_TIME_MILLISSECONDS"] = "毫秒";
-Blockly.Msg["FU_TIME_MICROSECONDS"] = "微秒"
-Blockly.Msg["FU_TIME_DELAYMICROS"] = "運行至今的微秒數"
-Blockly.Msg["FU_TIME_DELAYMILLIS"] = "運行至今的毫秒數"
