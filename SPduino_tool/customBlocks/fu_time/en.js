03924d045028bc35bb8bf2ef3208168494e9c5e1
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-01 00:20:12 +0800

    Delete en.js

--- SPduino_tool/customBlocks/fu_time/en.js
+++
@@ -1,7 +0,0 @@
-Blockly.Msg["FU_TIME_HUE"] = "40";
-Blockly.Msg["FU_TIME"] = "TIME";
-Blockly.Msg["FU_TIME_DELAY"] = "delay";
-Blockly.Msg["FU_TIME_MILLISSECONDS"] = "(ms)";
-Blockly.Msg["FU_TIME_MICROSECONDS"] = "(us)"
-Blockly.Msg["FU_TIME_DELAYMICROS"] = " the number of microseconds since the board began"
-Blockly.Msg["FU_TIME_DELAYMILLIS"] = " the number of milliseconds passed since the board began"
