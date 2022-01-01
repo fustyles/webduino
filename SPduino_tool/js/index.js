4e981a9d979c2e356ac062ee3bd62693c786d6a5
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-01 11:27:47 +0800

    Add files via upload

--- SPduino_tool/js/index.js
+++ SPduino_tool/js/index.js
@@ -1,7 +1,11 @@
 /*
- * Copyright 2021 ChungYi Fu, Taiwan
- * @fileoverview SpBlockly tool
- * @author https://www.facebook.com/francefu/
+@license
+Copyright 2022 Taiwan (ChungYi Fu)
+SPDX-License-Identifier: Apache-2.0
+
+@fileoverview SpBlockly V1 (Developer tool)
+@author https://www.facebook.com/francefu/
+@Update 1/1/2022 00:00 (Taiwan Standard Time)
 */
 
 var lang = "en";
