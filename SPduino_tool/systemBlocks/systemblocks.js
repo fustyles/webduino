a6023515a9159de73a2279aa7902ebb02a8504e2
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-01 10:17:24 +0800

    Update systemblocks.js

--- SPduino_tool/systemBlocks/systemblocks.js
+++ SPduino_tool/systemBlocks/systemblocks.js
@@ -1,5 +1,5 @@
 var systemBlocks = [
-["systemBlocks/myBackPack/","category_sep_main"],
+["systemBlocks/myBackPack/","category_procedures"],
 ["systemBlocks/fu_serial/","category_sep_main"],
 ["systemBlocks/fu_time/","category_sep_main"],
 ["systemBlocks/fu_io/","category_sep_main"]
