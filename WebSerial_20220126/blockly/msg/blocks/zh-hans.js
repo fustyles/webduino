2579b24c2c6d0ec87f113813f92a9094f4c86995
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-26 20:47:31 +0800

    Update zh-hans.js

--- WebSerial_20220126/blockly/msg/blocks/zh-hans.js
+++ WebSerial_20220126/blockly/msg/blocks/zh-hans.js
@@ -12,3 +12,4 @@ Blockly.Msg.WEBSERIAL_BUTTON_TOP_SHOW = "靠上距离";
 Blockly.Msg.WEBSERIAL_BAUDRATE_SHOW = "Web Serial 序列埠鲍率";
 Blockly.Msg.WEBSERIAL_SENDTEXT_SHOW = "Web Serial 传送文字";
 Blockly.Msg.WEBSERIAL_SENDUINT8_SHOW = "Web Serial 传送unit8Array";
+Blockly.Msg.WEBSERIAL_GETID_SHOW = "Web Serial 取得按钮ID";
