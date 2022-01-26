2e6b79f600107792869ee50c6298270ebc841bf3
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-26 20:08:34 +0800

    Update zh-hans.js

--- WebSerial_20220126/blockly/msg/blocks/zh-hans.js
+++ WebSerial_20220126/blockly/msg/blocks/zh-hans.js
@@ -2,6 +2,7 @@ Blockly.Msg.WEBSERIAL_ELEMENT_SHOW = "Web Serial 元素";
 Blockly.Msg.WEBSERIAL_SHOW_SHOW = "显示";
 Blockly.Msg.WEBSERIAL_STATE_SHOW = "Web Serial 开发板连线状态";
 Blockly.Msg.WEBSERIAL_GET_SHOW = "Web Serial 取得资料";
+Blockly.Msg.WEBSERIAL_GETSTATE_SHOW = "Web Serial 取得连线状态";
 Blockly.Msg.WEBSERIAL_CLEAR_SHOW = "Web Serial 清除回传资料";
 Blockly.Msg.WEBSERIAL_CONNECT_SHOW = "选择序列埠";
 Blockly.Msg.WEBSERIAL_CLOSE_SHOW = "关闭序列埠";
