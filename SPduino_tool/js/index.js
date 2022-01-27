b3a9534ce8b2a8863c876bf18581657480da6d53
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-27 08:51:38 +0800

    Update index.js

--- SPduino_tool/js/index.js
+++ SPduino_tool/js/index.js
@@ -686,7 +686,7 @@ document.addEventListener('DOMContentLoaded', function() {
 						serial_timer = setTimeout(function() {
 							if (serial_readSting != "") serial_message(serial_readSting,"green");
 							serial_readSting = "";
-						}, 10);
+						}, 50);
 					}
 				}
 			  }
