f225a942245ed66e658b61fa8a4776e385b96e76
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-28 09:14:11 +0800

    Update javascript.js

--- RobotFly_20220127/blockly/javascript.js
+++ RobotFly_20220127/blockly/javascript.js
@@ -14,19 +14,19 @@ Blockly.JavaScript['robotfly_command1'] = function(block) {
   var yaw_calibration = Blockly.JavaScript.valueToCode(block, 'yaw_', Blockly.JavaScript.ORDER_ATOMIC);
   var throttle_calibration = Blockly.JavaScript.valueToCode(block, 'throttle_', Blockly.JavaScript.ORDER_ATOMIC); 
 
-  var code = "robotfly_command1("+roll_calibration+","+pitch_calibration+","+yaw_calibration+","+throttle_calibration+");\n";
+  var code = "robotfly_calibration("+roll_calibration+","+pitch_calibration+","+yaw_calibration+","+throttle_calibration+");\n";
   return code;
 };
 
 Blockly.JavaScript['robotfly_command2'] = function(block) {
   var value_func = block.getFieldValue('func_');
-  var code = "robotfly_command2('"+value_func+"')";
+  var code = "robotfly_basic('"+value_func+"')";
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['robotfly_command3'] = function(block) {
   var value_func = block.getFieldValue('func_');
-  var code = "robotfly_command3('"+value_func+"')";
+  var code = "robotfly_move('"+value_func+"')";
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
@@ -39,6 +39,6 @@ Blockly.JavaScript['robotfly_command4'] = function(block) {
 Blockly.JavaScript['robotfly_command5'] = function(block) {
   var value_func = block.getFieldValue('func_');
   var value_val = Blockly.JavaScript.valueToCode(block, 'val_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = "robotfly_command5('"+value_func+"',"+value_val+");\n";
+  var code = "robotfly_set('"+value_func+"',"+value_val+");\n";
   return code;
 };
