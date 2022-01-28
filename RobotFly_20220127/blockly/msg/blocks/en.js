05e787b8483e56a72e70f40b9ae1daa4e707b942
fustyles <34397289+fustyles@users.noreply.github.com>
2022-01-28 09:07:42 +0800

    Update en.js

--- RobotFly_20220127/blockly/msg/blocks/en.js
+++ RobotFly_20220127/blockly/msg/blocks/en.js
@@ -20,3 +20,4 @@ Blockly.Msg.ROBOTFLY_STOP_SHOW = "Stop";
 Blockly.Msg.ROBOTFLY_TURNRIGHT_SHOW = "Turn right";
 Blockly.Msg.ROBOTFLY_TURNLEFT_SHOW = "Turn left";
 Blockly.Msg.ROBOTFLY_SET_SHOW = "Set";
+Blockly.Msg.ROBOTFLY_DEGREE_SHOW = "Degree";
