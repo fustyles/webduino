1f69e2c09bad93ff5b898a9668893bebe05d3556
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-04 12:14:39 +0800

    Update zh-hant.js

--- WebSerial_20220126/blockly/msg/blocks/zh-hant.js
+++ WebSerial_20220126/blockly/msg/blocks/zh-hant.js
@@ -13,3 +13,4 @@ Blockly.Msg.WEBSERIAL_BAUDRATE_SHOW = "Web Serial 序列埠鮑率";
 Blockly.Msg.WEBSERIAL_SENDTEXT_SHOW = "Web Serial 傳送文字";
 Blockly.Msg.WEBSERIAL_SENDUINT8_SHOW = "Web Serial 傳送unit8Array";
 Blockly.Msg.WEBSERIAL_GETID_SHOW = "Web Serial 取得按鈕ID";
+Blockly.Msg.WEBSERIAL_ELEMENT_ONCLICK_SHOW = "Web Serial 設定序列埠按鈕ID";
