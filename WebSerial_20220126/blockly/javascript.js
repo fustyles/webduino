b5059006595965a300b80228dfe6d7c69e0524c1
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-04 14:58:20 +0800

    Update javascript.js

--- WebSerial_20220126/blockly/javascript.js
+++ WebSerial_20220126/blockly/javascript.js
@@ -16,7 +16,7 @@ Blockly.JavaScript['webserial_button_position'] = function(block) {
 Blockly.JavaScript['webserial_button_onclick'] = function(block) {
   var value_button = block.getFieldValue('button_');   
   var value_id = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'webserial_button_onclick("'+value_button+"','+value_id+');\n';
+  var code = 'webserial_button_onclick("'+value_button+'",'+value_id+');\n';
   return code;
 };
 
