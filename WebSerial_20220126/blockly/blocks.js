23c35a3b9b4ca88093f204978bed2a8e0aa1980a
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-09 18:54:12 +0800

    Update blocks.js

--- WebSerial_20220126/blockly/blocks.js
+++ WebSerial_20220126/blockly/blocks.js
@@ -142,11 +142,11 @@ Blockly.Blocks['webserial_baudrate'] = {
         .appendField(Blockly.Msg.WEBSERIAL_BAUDRATE_SHOW);  
     this.appendDummyInput()	  
 		.appendField(new Blockly.FieldDropdown([
-			["9600","9600"],
 			["300","300"],
 			["1200","1200"],
 			["2400","2400"],
 			["4800","4800"],
+			["9600","9600"],	    
 			["19200","19200"],
 			["38400","38400"],
 			["57600","57600"],
