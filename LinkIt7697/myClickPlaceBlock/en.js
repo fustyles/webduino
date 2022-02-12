c90499a98856f299c89677260f091d3cdeadf7b8
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-13 01:25:58 +0800

    Delete en.js

--- LinkIt7697/myClickPlaceBlock/en.js
+++
@@ -1,7 +0,0 @@
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PICK = "Get source block";
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PLACE_NEXT = "Place source block(Next)";
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PLACE_PREVIOUS = "Place source block(Previous)";
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PLACE_OUTPUT0 = "Place source block(Input 0)";
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PLACE_OUTPUT1 = "Place source block(Input 1)";
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PLACE_OUTPUT2 = "Place source block(Input 2)";
-Blockly.Msg.MYCLICKPLACEBLOCK_CLICK_TO_PLACE_OUTPUT3 = "Place source block(Input 3)";
