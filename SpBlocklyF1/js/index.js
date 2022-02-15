4435479f226f04df07e625d771191d5958b153fd
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-15 23:02:33 +0800

    Update index.js

--- SpBlocklyF1/js/index.js
+++ SpBlocklyF1/js/index.js
@@ -14,6 +14,7 @@ var customCategory = [];
 var customCategoryInsertAfter = "category_sep_main";
 var languageList = "msg/language.js";
 var xmlValue = "";
+var topChecked = true;
 
 document.addEventListener('DOMContentLoaded', function() {
 
