bcb824e4e296489d53e14b18ab2185a29fe306b2
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-15 23:13:08 +0800

    Update blocks_compressed.js

--- SpBlocklyF1/js/blocks_compressed.js
+++ SpBlocklyF1/js/blocks_compressed.js
@@ -96,7 +96,7 @@ Blockly.Blocks.initializes_loop={
 					p = p.getParent()||p.getPreviousBlock()?p.getParent()||p.getPreviousBlock():"";
 				}
 				if (!p) {
-					if (topChecked) blocks[i].setEnabled(false);
+					if (topCheck) blocks[i].setEnabled(false);
 				}
 				if (blocks[i].getParent()&&blocks[i].getPreviousBlock()) {
 					if (variableBlockList.includes(p.type)&&variableBlockList.includes(blocks[i].getParent().type)) {
