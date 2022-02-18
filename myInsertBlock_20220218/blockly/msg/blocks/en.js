101e0359fe6f366c08c2621258d4388eca30f91b
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-18 20:57:33 +0800

    Add files via upload

---
+++ myInsertBlock_20220218/blockly/msg/blocks/en.js
@@ -0,0 +1,2 @@
+Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_EXPORT"] = "Export selected block to local file";
+Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_INSERT"] = "Insert a block into workspace from local file";
---
+++ myInsertBlock_20220218/blockly/msg/blocks/zh-hans.js
@@ -0,0 +1,2 @@
+Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_EXPORT"] = "汇出单一积木至档案";
+Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_INSERT"] = "开启单一积木档案插入至工作区";
