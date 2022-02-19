24c7e76e69855887db9e3d75123eaf9d838ca8ec
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-19 13:07:43 +0800

    Delete en.js

--- myInsertBlock_20220218/blockly/msg/blocks/en.js
+++
@@ -1,2 +0,0 @@
-Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_EXPORT"] = "Export selected block to local file";
-Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_INSERT"] = "Insert a block into workspace from local file";
