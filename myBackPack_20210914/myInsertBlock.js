290fca66924dc514970c85094c6a90e83cb8a7f1
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-19 13:09:19 +0800

    Add files via upload

---
+++ myBackPack_20210914/myInsertBlock.js
@@ -0,0 +1,104 @@
+/*
+Last Update Time : 18/2/2022 00:00 (Taiwan Standard Time)
+
+Author: ChungYi Fu, Taiwan
+https://github.com/fustyles
+https://www.facebook.com/francefu/
+
+
+*/
+
+Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_EXPORT"] = "Export selected block to local file";
+Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_INSERT"] = "Insert a block into workspace from local file";
+
+function registerWorkspaceBlockExportFile() {
+  if (Blockly.ContextMenuRegistry.registry.getItem('workspace_block_export_file')) {
+	return;
+  }
+  const workspaceExportFile = {
+	displayText: function(){
+		return Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_EXPORT"];
+	},
+	preconditionFn: function(a) {
+		return 'enabled';
+	},
+	callback: function(a) {
+		var dom = Blockly.Xml.blockToDom(a.block,true);
+		var xml = Blockly.Xml.domToText(dom).replace(/(?:\r\n|\r|\n|\t)/g, "");
+		console.log(xml);
+		
+		var link = document.createElement('a');
+		link.download="sample.xmlone";
+		link.href="data:application/octet-stream;utf-8," + encodeURIComponent(xml);
+		document.body.appendChild(link);
+		link.click();
+		link.remove();		
+	},
+	scopeType: Blockly.ContextMenuRegistry.ScopeType.BLOCK,
+	id: 'workspace_block_export_file',
+	weight: 209,
+  };
+  Blockly.ContextMenuRegistry.registry.register(workspaceExportFile);
+}
+  
+registerWorkspaceBlockExportFile();
+
+		
+function registerWorkspaceInsertBlockFromFile() {
+  if (Blockly.ContextMenuRegistry.registry.getItem('workspace_insert_block')) {
+    return;
+  }
+  const fileInsertBlocks = {
+    displayText: function(){
+		return Blockly.Msg["INSERTBLOCKS_WORKSPACE_BLOCK_INSERT"];
+	},
+    preconditionFn: function(a) {
+		return 'enabled';
+    },
+    callback: function(a) {
+		var e = document.getElementById("workspace_insert_block");
+		if (e) {
+			e.click();
+			return;
+		}
+		
+		var input=document.createElement('input');
+		input.type="file";
+		input.id="fileInsertBlocks";
+		input.style.display = "none";
+		input.accept=".xmlone";
+		input.onchange = function(element) {
+			try {	
+				var file = this.files[0];
+				if (file) {
+					var fr = new FileReader();           
+					fr.onload = function (event) {
+						var dom = Blockly.Xml.textToDom(event.target.result);
+						console.log(dom);
+						block = Blockly.Xml.textToDom('<xml>' + Blockly.Xml.domToText(dom).replace(/(?:\r\n|\r|\n|\t)/g, "") + '</xml>');
+						var id = Blockly.Xml.appendDomToWorkspace(block, Blockly.getMainWorkspace());
+						block = Blockly.getMainWorkspace().getBlockById(id);
+						block.initSvg();
+						a.workspace.render();
+						block.moveBy(100,20);
+					};
+					fr.readAsText(file);
+				}
+			} catch (e) {
+				alert(e);
+			}	  
+		}
+
+		document.body.appendChild(input);
+		setTimeout(function(){
+			input.click();
+		},500);
+    },
+    scopeType: Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+    id: 'workspace_insert_block',
+    weight: 210,
+  };
+  Blockly.ContextMenuRegistry.registry.register(fileInsertBlocks);
+}
+  
+registerWorkspaceInsertBlockFromFile();
