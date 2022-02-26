9cccdec7239750ce060726c73956b5d1976a06e0
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-26 13:34:26 +0800

    Update customblocks.js

--- SPduino_tool/customBlocks/customblocks.js
+++ SPduino_tool/customBlocks/customblocks.js
@@ -5,7 +5,6 @@ var customBlocks = [
 /*
 var customBlocks = [
 ["https://circuspi.github.io/ICSHOP/","category_sep_custom"]
-,["https://lioujj.github.io/LinkIt7697/extensions_0922/","category_sep_custom"]
 ,["https://fustyles.github.io/webduino/LinkIt7697/France/","category_sep_custom"]
 ];
 */
