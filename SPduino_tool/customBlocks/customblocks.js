7e5f021a726a90507e27175308fa8101520f3664
fustyles <34397289+fustyles@users.noreply.github.com>
2022-02-27 11:47:27 +0800

    Update customblocks.js

--- SPduino_tool/customBlocks/customblocks.js
+++ SPduino_tool/customBlocks/customblocks.js
@@ -1,10 +1,3 @@
 var customBlocks = [
 ["https://fustyles.github.io/webduino/LinkIt7697/France/","category_sep_custom"]
 ];
-
-/*
-var customBlocks = [
-["https://circuspi.github.io/ICSHOP/","category_sep_custom"]
-,["https://fustyles.github.io/webduino/LinkIt7697/France/","category_sep_custom"]
-];
-*/
