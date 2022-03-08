501af3c30c6cc6558b1eb2ea0a517fb10ece8e67
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-08 20:38:09 +0800

    Update javascript.js

--- demo_text_20180811/blockly/javascript.js
+++ demo_text_20180811/blockly/javascript.js
@@ -1,10 +1,10 @@
-Blockly.JavaScript['demo_text_add'] = function(block) {
+Blockly.JavaScript['demo_add'] = function(block) {
   var value_id = Blockly.JavaScript.valueToCode(block, 'id_', Blockly.JavaScript.ORDER_ATOMIC);	
   var code = 'createDemoText('+value_id+');\n';
   return code;
 };
 
-Blockly.JavaScript['demo_text_set'] = function (block) {
+Blockly.JavaScript['demo_set'] = function (block) {
   var value_property_ = block.getFieldValue('property_');
   var value_value_ = Blockly.JavaScript.valueToCode(block, 'value_', Blockly.JavaScript.ORDER_ATOMIC);
   var code = 'DemoText_set("' + value_property_ + '",' + value_value_ + ');\n';
