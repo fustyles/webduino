a6422f7d29a3f89e7050d54d3395daa8cfbd48d6
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-08 20:11:26 +0800

    Update demotext.js

--- demo_text_20180811/demotext.js
+++ demo_text_20180811/demotext.js
@@ -14,7 +14,7 @@
 	    if (!document.getElementById("demo-area-01-show")) {
 		var div = document.createElement('div');
 		div.id = "demo-area-01-show";
-		div.style.zIndex = 999;   
+		div.style.zIndex = 99999;   
 		div.style.display = 'block';
 		div.innerHTML = "123";
 		document.body.insertBefore(div, document.body.firstChild);
