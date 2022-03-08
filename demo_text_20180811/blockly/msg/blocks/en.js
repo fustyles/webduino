cc4d5333e3a263f4b87fa471301f1573e8e272ae
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-08 19:27:02 +0800

    Update en.js

--- demo_text_20180811/blockly/msg/blocks/en.js
+++ demo_text_20180811/blockly/msg/blocks/en.js
@@ -1,4 +1,4 @@
-Blockly.Msg.DEMO_TEXT_ADD_SHOW = "Demo Text initialize";
+Blockly.Msg.DEMO_TEXT_ADD_SHOW = "Demo Interactions initialize";
 Blockly.Msg.DEMO_TEXT_SHOW = "Demo Text"; 
 Blockly.Msg.DEMO_TEXT_SET_SHOW = "Set";
 Blockly.Msg.DEMO_LEFT = "LEFT"; 
