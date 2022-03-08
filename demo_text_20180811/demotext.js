6aa8badaaea9714f9ff6004243d879ffc9bc5c83
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-08 20:02:37 +0800

    Update demotext.js

--- demo_text_20180811/demotext.js
+++ demo_text_20180811/demotext.js
@@ -4,12 +4,12 @@
 
   'use strict';
 
-  var obj = "";
+  var obj_demo = "";
   function createDemoText(input_id) {
 	if (document.getElementsByClassName(input_id))
-		obj = document.getElementsByClassName(input_id)[0];
+		obj_demo = document.getElementsByClassName(input_id)[0];
 	else if (document.getElementById(input_id)) 
-		obj = document.getElementById(input_id);
+		obj_demo = document.getElementById(input_id);
 	else {
 	    if (!document.getElementById("demo-area-01-show")) {
 		var div = document.createElement('div');
@@ -19,59 +19,60 @@
 		div.innerHTML = "123";
 		document.body.insertBefore(div, document.body.firstChild);
 	    }
-	    obj = document.getElementById("demo-area-01-show");
+	    obj_demo = document.getElementById("demo-area-01-show");
 	}
+	console.log(obj_demo);
   }
   
   function DemoText_set(input_property,input_value) {
-    if (!obj) return;
+    if (!obj_demo) return;
 	  
     if (input_property=="left")
-      obj.style.left = input_value + "px";
+      obj_demo.style.left = input_value + "px";
     else if (input_property=="top")
-      obj.style.top = input_value + "px";
+      obj_demo.style.top = input_value + "px";
     else if (input_property=="width")
-      obj.style.width = input_value + "px";
+      obj_demo.style.width = input_value + "px";
     else if (input_property=="height")
-      obj.style.height = input_value + "px";
+      obj_demo.style.height = input_value + "px";
     else if (input_property=="borderstyle")
-      obj.style.borderStyle = input_value;
+      obj_demo.style.borderStyle = input_value;
     else if (input_property=="borderwidth")
-      obj.style.borderWidth = input_value + "px";
+      obj_demo.style.borderWidth = input_value + "px";
     else if (input_property=="bordercolor")
-      obj.style.borderColor = input_value; 
+      obj_demo.style.borderColor = input_value; 
     else if (input_property=="borderradius") {
       if (input_value.toString().toLowerCase().indexOf("px")==-1)
-        obj.style.borderRadius = input_value + "px";
+        obj_demo.style.borderRadius = input_value + "px";
       else
-        obj.style.borderRadius = input_value;
+        obj_demo.style.borderRadius = input_value;
     }
     else if (input_property=="background")
-      obj.style.background = input_value;
+      obj_demo.style.background = input_value;
     else if (input_property=="color")
-      obj.style.color = input_value;
+      obj_demo.style.color = input_value;
     else if (input_property=="fontsize")
-      obj.style.fontSize = input_value + "px";	    
+      obj_demo.style.fontSize = input_value + "px";	    
     else if (input_property=="opacity")
-      obj.style.opacity = input_value;    
+      obj_demo.style.opacity = input_value;    
     else if (input_property=="innerHTML")
-      obj.innerHTML = input_value; 
+      obj_demo.innerHTML = input_value; 
     else if (input_property=="innerTEXT")
-      obj.innerTEXT = input_value;   
+      obj_demo.innerTEXT = input_value;   
     else if (input_property=="zindex")
-      obj.style.zIndex = input_value;
+      obj_demo.style.zIndex = input_value;
     else if (input_property=="display"){ 
       if (input_value==1)
-        obj.style.display = "block";    
+        obj_demo.style.display = "block";    
       else if (input_value==0)
-        obj.style.display = "none";
+        obj_demo.style.display = "none";
     }
     else if (input_property=="position")
-      obj.style.position = input_value;		    
+      obj_demo.style.position = input_value;		    
     else if (input_property=="disabled")
-      obj.disabled = input_value;
+      obj_demo.disabled = input_value;
     else if (input_property=="draggable")
-      obj.draggable = input_value;		
+      obj_demo.draggable = input_value;		
   }  
  
   window.createDemoText = createDemoText;
