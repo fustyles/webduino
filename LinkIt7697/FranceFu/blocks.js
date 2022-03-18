1939bea32c86c16ded9d47eafd6116c8b88f292b
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-18 20:03:13 +0800

    Update blocks.js

--- LinkIt7697/FranceFu/blocks.js
+++ LinkIt7697/FranceFu/blocks.js
@@ -4955,7 +4955,7 @@ Blockly.Blocks['linenotify_all'] = {
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
         .appendField(Blockly.Msg.ESP32_LINE_PREVIEWIMAGEURL_SHOW,"I2");		
-	this.setInputsInline(false);	  
+	this.setInputsInline(true);	  
 	this.setPreviousStatement(!0);
 	this.setNextStatement(!0);
 	this.setColour(200);
@@ -4979,7 +4979,8 @@ Blockly.Blocks['linenotify_all'] = {
 		 block.getInput("originalContentUrl_").setVisible(false);
 		 block.getField("I1").setVisible(false);
 		 block.getInput("previewImageUrl_").setVisible(false);
-		 block.getField("I2").setVisible(false);		 
+		 block.getField("I2").setVisible(false);
+		 block.setInputsInline(true);
 	 }
 	 else if (newValue=="sticker") {
 		 block.getInput("packageId_").setVisible(true);
@@ -4990,6 +4991,7 @@ Blockly.Blocks['linenotify_all'] = {
 		 block.getField("I1").setVisible(false);
 		 block.getInput("previewImageUrl_").setVisible(false);
 		 block.getField("I2").setVisible(false); 
+		 block.setInputsInline(false);		 
 	 }
 	 else if (newValue=="image") {
 		 block.getInput("packageId_").setVisible(false);
@@ -5000,6 +5002,7 @@ Blockly.Blocks['linenotify_all'] = {
 		 block.getField("I1").setVisible(true);
 		 block.getInput("previewImageUrl_").setVisible(true);
 		 block.getField("I2").setVisible(true);
+		 block.setInputsInline(false);		 
 	 }	 
   } 
 };
