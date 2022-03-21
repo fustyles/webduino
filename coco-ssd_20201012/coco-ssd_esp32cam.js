057a5d90f6224d5b11e7ea682a965443bd19be1e
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-21 18:40:17 +0800

    Update coco-ssd_esp32cam.js

--- coco-ssd_20201012/coco-ssd_esp32cam.js
+++ coco-ssd_20201012/coco-ssd_esp32cam.js
@@ -87,7 +87,7 @@ window.onload = function () {
 					context.font = Math.round(s/30) + "px Arial";
 					context.fillText(Predictions[i].class, x, y);
 				}
-				res+= predictions[i].class+","+Math.round(predictions[i].score,2)+","+Math.round(x)+","+Math.round(y)+","+Math.round(width)+","+Math.round(height)+"<br>";
+				res+= Predictions[i].class+","+Math.round(Predictions[i].score,2)+","+Math.round(x)+","+Math.round(y)+","+Math.round(width)+","+Math.round(height)+"<br>";
 		   }
 			if (res!="")
 				result.innerHTML = res.substr(0,result.innerHTML.length-4);		   
