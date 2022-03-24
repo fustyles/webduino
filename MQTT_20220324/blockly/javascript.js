a8a32021ef40f7714f65d8d09e446ac3d3c8562e
fustyles <34397289+fustyles@users.noreply.github.com>
2022-03-24 20:20:10 +0800

    Add files via upload

---
+++ MQTT_20220324/blockly/javascript.js
@@ -0,0 +1,56 @@
+Blockly.JavaScript['fu_mqtt_setup_js'] = function(block) {
+  var server = Blockly.JavaScript.valueToCode(block, 'server', Blockly.JavaScript.ORDER_ATOMIC);
+  var user = Blockly.JavaScript.valueToCode(block, 'user', Blockly.JavaScript.ORDER_ATOMIC);
+  var password = Blockly.JavaScript.valueToCode(block, 'password', Blockly.JavaScript.ORDER_ATOMIC);
+  var topic_subscribe = Blockly.JavaScript.statementToCode(block, 'topic_subscribe');
+  var topic_getdata = Blockly.JavaScript.statementToCode(block, 'topic_getdata');
+  
+
+  Blockly.JavaScript.definitions_.define_mqtt_js ='const clientId = "mqtt_" + Math.random().toString(16).substr(2, 8);\n'+			
+											'const options = {\n'+
+											'	username: '+user+',\n'+
+											'	password: '+password+',\n'+
+											'	keepalive: 60,\n'+
+											'	clientId: clientId,\n'+
+											'	protocolId: "MQTT",\n'+
+											'	protocolVersion: 4,\n'+
+											'	clean: true,\n'+
+											'	reconnectPeriod: 1000,\n'+
+											'	connectTimeout: 30 * 1000\n'+
+											'}';										
+  Blockly.JavaScript.definitions_.define_mqtt_connect_js = 'var mqtt_client = mqtt.connect('+server+',options);\n'+				
+															'mqtt_client.on("connect", ()=>{\n'+
+															'	console.log("connected");\n'+topic_subscribe+
+															'	mqtt_client.on("message", function (topic, payload) {\n'+topic_getdata+
+															'   })\n'+
+															'  }\n'+															
+															')';	
+												
+  code = '';
+  return code;
+};
+
+Blockly.JavaScript['fu_mqtt_subscribe_js'] = function(block) {
+  var topic = Blockly.JavaScript.valueToCode(block, 'topic', Blockly.JavaScript.ORDER_ATOMIC);
+  code = 'mqtt_client.subscribe('+topic+');\n';
+  return code;
+};
+
+Blockly.JavaScript['fu_mqtt_gettopic_js'] = function(block) {
+  var topic = Blockly.JavaScript.valueToCode(block, 'topic', Blockly.JavaScript.ORDER_ATOMIC);
+  var topic_getdata = Blockly.JavaScript.statementToCode(block, 'topic_getdata'); 
+  code = 'if (topic=='+topic+') {\n'+topic_getdata+'}\n';
+  return code;
+};
+
+Blockly.JavaScript['fu_mqtt_senddata_js'] = function(block) {
+  var topic = Blockly.JavaScript.valueToCode(block, 'topic', Blockly.JavaScript.ORDER_ATOMIC);
+  var text = Blockly.JavaScript.valueToCode(block, 'text', Blockly.JavaScript.ORDER_ATOMIC); 
+  code = 'mqtt_client.publish('+topic+', '+text+');\n';
+  return code;
+};
+
+Blockly.JavaScript['fu_mqtt_getdata_js'] = function(block) {
+  code = 'payload';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
---
+++ MQTT_20220324/blockly/msg/zh-hant.js
@@ -0,0 +1,1 @@
+MSG.catmqtt = "MQTT";
