6a64381d9fe54d921b50ff2954aee635f2d42c31
fustyles <34397289+fustyles@users.noreply.github.com>
2022-07-08 18:24:29 +0800

    Update customblocks.js

--- SpBlocklyF1/customBlocks/customblocks.js
+++ SpBlocklyF1/customBlocks/customblocks.js
@@ -1,5 +1,5 @@
 var customBlocks = [
 ["https://circuspi.github.io/ICSHOP/","category_sep_custom"]
-,["https://lioujj.github.io/LinkIt7697/extensions_0922/","category_sep_custom"]
+,["https://lioujj.github.io/LinkIt7697/extensions/","category_sep_custom"]
 ,["https://fustyles.github.io/webduino/LinkIt7697/France/","category_sep_custom"]
 ];
