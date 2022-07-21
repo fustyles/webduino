526926f29fdaa6b0f9f3937ec12497b88b8b8dbf
fustyles <34397289+fustyles@users.noreply.github.com>
2022-07-22 00:04:05 +0800

    Update en.js

--- SpreadsheetSQL_20210403/blockly/msg/blocks/en.js
+++ SpreadsheetSQL_20210403/blockly/msg/blocks/en.js
@@ -8,3 +8,4 @@ Blockly.Msg.SPREADSHEETSQL_GETDATACOUNT_SHOW = "Google Spreadsheet Get SQL Respo
 Blockly.Msg.SPREADSHEETSQL_CLEARDATA_SHOW = "Google Spreadsheet Clear SQL Response";
 Blockly.Msg.SPREADSHEETSQL_GETDATACOUNTCOLS_SHOW = "Columns";
 Blockly.Msg.SPREADSHEETSQL_GETDATACOUNTROWS_SHOW = "Rows";
+Blockly.Msg.SPREADSHEETSQL_GETDATAFINISH_SHOW = "Google Spreadsheet SQL Query execute after getting data";
