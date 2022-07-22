0042423d6d12890c9222eaf401845d3971235de4
fustyles <34397289+fustyles@users.noreply.github.com>
2022-07-22 11:24:14 +0800

    Update spreadsheetsql.js

--- SpBlocklyJS/SpreadsheetSQL_20210403/spreadsheetsql.js
+++ SpBlocklyJS/SpreadsheetSQL_20210403/spreadsheetsql.js
@@ -73,21 +73,24 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
   }   
   
   function spreadsheet_insert(func, myData, myRow, myCol, myText, mySpreadsheeturl, mySpreadsheetname, myScript) {
+	  var postData = {
+      		"func":func,
+      		"data":encodeURIComponent(myData),
+      		"spreadsheeturl":mySpreadsheeturl,
+      		"spreadsheetname":mySpreadsheetname,
+      		"row":myRow,
+      		"col":myCol,
+      		"text":encodeURIComponent(myText)	 
+	  }
+	  console.log(JSON.stringify(postData));
+
       $.ajax({
       	headers: {'Access-Control-Allow-Origin': '*'},			
       	type: "POST",
       	url: myScript,
       	dataType: "json",
       	contentType: "application/json; charset=utf-8",
-      	data: JSON.stringify({
-      		"func":func,
-      		"data":myData,
-      		"spreadsheeturl":mySpreadsheeturl,
-      		"spreadsheetname":mySpreadsheetname,
-      		"row":myRow,
-      		"col":myCol,
-      		"text":myText				
-      	}),
+      	data: JSON.stringify(postData),
       	success: function (response) {
       		//console.log(response.data);			
       	},
@@ -95,7 +98,7 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
       		console.log(thrownError);
       	}
       });
-  }	  
+  }	 	  
   
   window.spreadsheetsql_settings = spreadsheetsql_settings;
   window.spreadsheetsql_executeSql = spreadsheetsql_executeSql;
