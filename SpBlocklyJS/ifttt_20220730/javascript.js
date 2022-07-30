17aed26d232d8c429d173d222ae14c6244fbb174
fustyles <34397289+fustyles@users.noreply.github.com>
2022-07-30 13:16:02 +0800

    Update javascript.js

--- SpBlocklyJS/ifttt_20220730/javascript.js
+++ SpBlocklyJS/ifttt_20220730/javascript.js
@@ -4,11 +4,7 @@ Blockly.JavaScript['ifttt'] = function(block) {
   var value1 = Blockly.JavaScript.valueToCode(block, 'value1', Blockly.JavaScript.ORDER_ATOMIC);
   var value2 = Blockly.JavaScript.valueToCode(block, 'value2', Blockly.JavaScript.ORDER_ATOMIC);
   var value3 = Blockly.JavaScript.valueToCode(block, 'value3', Blockly.JavaScript.ORDER_ATOMIC);  
-  
-  value1 = (value1+"").replace(/\"/g,"%22");
-  value2 = (value2+"").replace(/\"/g,"%22");
-  value3 = (value3+"").replace(/\"/g,"%22");
-	
+
   var data = "{'value1':"+value1+",'value2':"+value2+",'value3':"+value3+"}";
   var code = 'ifttt_push('+eventname+','+key+','+data+');\n';
   return code;
