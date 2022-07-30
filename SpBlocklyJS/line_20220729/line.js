7304945548019b223234eeef3d0755cfa52eaf60
fustyles <34397289+fustyles@users.noreply.github.com>
2022-07-30 20:53:07 +0800

    Add files via upload

--- SpBlocklyJS/line_20220729/line.js
+++ SpBlocklyJS/line_20220729/line.js
@@ -51,8 +51,88 @@ function linenotify_push_message(notify_token, notify_msg) {
      });
 }  
 
-function linenotify_push_image(token, id, message) {
-	var img = document.getElementById(id);
+function linenotify_push_image(notify_token, notify_id, notify_msg) {
+	var img = document.getElementById(notify_id);
+
+	if (!document.getElementById("myCanvas")) {
+		var myCanvas = document.createElement('canvas');
+		myCanvas.id = "myCanvas";
+		myCanvas.style.display = "none";
+		document.body.appendChild(myCanvas);
+	}
+	else {
+		var myCanvas = document.getElementById("myCanvas");
+	}
+
+	if (!document.getElementById("myIframe")) {
+		var myIframe = document.createElement('iframe');
+		myIframe.id = "myIframe";
+		myIframe.name = "myIframe";
+		myIframe.style.display = "none";
+		document.body.appendChild(myIframe);
+	}
+	else {
+		var myIframe = document.getElementById("myIframe");
+	}
+
+	var myContext = myCanvas.getContext('2d');
+	myCanvas.setAttribute("width", img.width);
+    myCanvas.setAttribute("height", img.height);
+	myContext.drawImage(img, 0, 0, img.width, img.height);
+
+	if (!document.getElementById("myForm")) {
+		var myForm = document.createElement("form");
+		myForm.id = "myForm";
+		myForm.name = "myForm";
+		myForm.method = "POST";
+		myForm.target = "myIframe";
+		myForm.action = "https://script.google.com/macros/s/AKfycbyp1xvWg-UCSrLsL8zt-ba_0n96uNTpAFyRry9ifCnRbtK-vgg/exec";
+		document.body.appendChild(myForm);
+	} 
+	else {
+		var myForm = document.getElementById("myForm");
+	}
+
+	if (!document.getElementById("myToken")) {
+		var myToken = document.createElement("input");
+		myToken.type = "hidden";
+		myToken.id = "myToken";
+		myToken.name = "myToken";
+		myForm.appendChild(myToken);
+	}
+	else {
+		var myToken = document.getElementById("myToken");
+	}
+	myToken.value = notify_token;
+
+	if (!document.getElementById("myFilename")) {
+		var myFilename = document.createElement("input");
+		myFilename.type = "hidden";
+		myFilename.id = "myFilename";
+		myFilename.name = "myFilename";
+		myForm.appendChild(myFilename);
+	}
+	else {
+		var myFilename = document.getElementById("myFilename");
+	}
+	var date = new Date();
+	myFilename.value = date.getFullYear()+"/"+(date.getMonth()+1)+"/"+date.getDate()+" "+("0"+date.getHours()).substr(-2,2)+":"+("0"+date.getMinutes()).substr(-2,2)+":"+("0"+date.getSeconds()).substr(-2,2);	
+
+	if (!document.getElementById("myFile")) {
+		var myFile = document.createElement("textarea");
+		myFile.id = "myFile";
+		myFile.name = "myFile";
+		myFile.style.display = "none";
+		myForm.appendChild(myFile);
+	}
+	else {
+		var myFile = document.getElementById("myFile");
+	}
+	myFile.value = myCanvas.toDataURL();
+
+    myForm.submit();	
+	/*
+	var img = document.getElementById(notify_id);
 
 	if (!document.getElementById("myCanvas")) {
 		var myCanvas = document.createElement('canvas');
@@ -68,11 +148,10 @@ function linenotify_push_image(token, id, message) {
 	myCanvas.setAttribute("height", img.height);
 	var myContext = myCanvas.getContext("2d");
 	myContext.drawImage(img, 0, 0, img.width, img.height);
-	var imageData = myCanvas.toDataURL('image/jpeg');
 	
 	var boundary = "------------------------------";
 	var imageData_s = "--" + boundary + "\r\n"
-	  + "Content-Disposition: form-data; name=\"message\"; \r\n\r\n" + message + "\r\n"
+	  + "Content-Disposition: form-data; name=\"message\"; \r\n\r\n" + notify_msg + "\r\n"
 	  + "--" + boundary + "\r\n"
 	  + "Content-Disposition: form-data; name=\"imageFile\"; filename=\"" + "SpBlockly.jpg" + "\"\r\n"
 	  + "Content-Type: " + 'image/jpeg' +"\r\n\r\n";
@@ -88,21 +167,44 @@ function linenotify_push_image(token, id, message) {
 		}
 		return bytes.buffer;
 	}
-	const data = _base64ToArrayBuffer(imageData_s+imageData+imageData_e);
-
-    $.ajax({
-        "url": "https://notify-api.line.me/api/notify",
-		"method" : "post",
-		"contentType" : "multipart/form-data; boundary=" + boundary,
-		"payload" : data,    
-		"headers" : {"Authorization" : "Bearer " + token},
-        success: function(jsonp)
-        {
-          console.log(jsonp);
-        },
-        error: function(jqXHR, textStatus, errorThrown)
-        {
-          //console.log(errorThrown);
-        }
-     });
+	imageData_s = _base64ToArrayBuffer(imageData_s);
+	imageData_e = _base64ToArrayBuffer(imageData_e);
+	
+	var imageData = "";
+	var state = false;
+	
+    const fileReader = new FileReader()
+    fileReader.addEventListener('loadend', function () {
+      if (this.error) {
+        reject(this.error);
+      } else {
+        imageData = this.result;
+		
+		if (state == false) {
+			state = true;
+			var test = new Blob([imageData_s, imageData, imageData_e]);
+			fileReader.readAsArrayBuffer(test);
+		} 
+		else {
+			$.ajax({
+				"url": "https://notify-api.line.me/api/notify",
+				"method" : "post",
+				"dataType": "jsonp",
+				"contentType" : "multipart/form-data; boundary=" + boundary,
+				"payload" : imageData,    
+				"headers" : {"Authorization" : "Bearer " + notify_token},
+				success: function(jsonp)
+				{
+				  console.log(jsonp);
+				},
+				error: function(jqXHR, textStatus, errorThrown)
+				{
+				  //console.log(errorThrown);
+				}
+			 });
+		}
+      }
+    })
+    myCanvas.toBlob(blob => fileReader.readAsArrayBuffer(blob), 'image/jpeg', 0.1)
+	*/
 } 
