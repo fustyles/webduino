61553dd1b905ed8ebc44db7e20534da215544351
fustyles <34397289+fustyles@users.noreply.github.com>
2022-07-31 23:48:53 +0800

    Update toolbox.js

--- SpBlocklyJS/WebSerial_20220126/toolbox.js
+++ SpBlocklyJS/WebSerial_20220126/toolbox.js
@@ -1,5 +1,19 @@
 var catWebSerial ='<xml>'+
 '<category name="%{BKY_CATSERIAL}" id="catwebserial" colour="250">'+
+'<block type="webserial_button">'+ 
+'</block>'+	
+'<block type="webserial_button_position">'+
+'<value name="left_">'+
+'<block type="math_number">'+
+'<field name="NUM">0</field>'+
+'</block>'+
+'</value>'+ 
+'<value name="top_">'+
+'<block type="math_number">'+
+'<field name="NUM">0</field>'+
+'</block>'+
+'</value>'+ 
+'</block>'+
 '<block type="webserial_baudrate">'+ 
 '<field name="baudrate_">9600</field>'+
 '</block>'+ 
@@ -23,20 +37,6 @@ var catWebSerial ='<xml>'+
 '</block>'+
 '<block type="webserial_getstate">'+ 
 '</block>'+ 
-'<block type="webserial_button">'+ 
-'</block>'+	
-'<block type="webserial_button_position">'+
-'<value name="left_">'+
-'<block type="math_number">'+
-'<field name="NUM">0</field>'+
-'</block>'+
-'</value>'+ 
-'<value name="top_">'+
-'<block type="math_number">'+
-'<field name="NUM">0</field>'+
-'</block>'+
-'</value>'+ 
-'</block>'+
 '<block type="webserial_button_onclick">'+
 '<value name="id_">'+
 '<block type="text">'+
