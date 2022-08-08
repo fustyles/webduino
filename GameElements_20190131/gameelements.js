8fc45c32e7a793947b088d84a101663a1aa4c4c8
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-08 20:23:48 +0800

    Update gameelements.js

--- GameElements_20190131/gameelements.js
+++ GameElements_20190131/gameelements.js
@@ -1044,16 +1044,25 @@
   function elements_collision(element1,input_id1,element2,input_id2,input_position) {
     if ((document.getElementById(element1+input_id1))&&(document.getElementById(element2+input_id2)))
     {
-      var obj1 = document.getElementById(element1+input_id1).style;
-      var obj2 = document.getElementById(element2+input_id2).style;
-      var x1 = Number(obj1.left.replace(/px/ig,""));
-      var x1_w = Number(obj1.left.replace(/px/ig,"")) + Number(obj1.width.replace(/px/ig,""));
-      var y1 = Number(obj1.top.replace(/px/ig,""));
-      var y1_h = Number(obj1.top.replace(/px/ig,"")) + Number(obj1.height.replace(/px/ig,""));
-      var x2 = Number(obj2.left.replace(/px/ig,""));
-      var x2_w = Number(obj2.left.replace(/px/ig,"")) + Number(obj2.width.replace(/px/ig,""));
-      var y2 = Number(obj2.top.replace(/px/ig,""));
-      var y2_h = Number(obj2.top.replace(/px/ig,"")) + Number(obj2.height.replace(/px/ig,""));
+      var obj1 = document.getElementById(element1+input_id1);
+      var x1 = Number(obj1.style.left.replace(/px/ig,""));
+      var y1 = Number(obj1.style.top.replace(/px/ig,""));
+      if (obj1.parentElement.id.indexOf("region_")!=-1) {
+	x1 += obj1.parentElement.style.left.replace(/px/ig,"");
+	y1 += obj1.parentElement.style.top.replace(/px/ig,"");
+      } 
+      var x1_w = Number(obj1.style.left.replace(/px/ig,"")) + Number(obj1.style.width.replace(/px/ig,""));
+      var y1_h = Number(obj1.style.top.replace(/px/ig,"")) + Number(obj1.style.height.replace(/px/ig,""));
+	  
+      var obj2 = document.getElementById(element2+input_id2);
+      var x2 = Number(obj2.style.left.replace(/px/ig,""));
+      var y2 = Number(obj2.style.top.replace(/px/ig,""));
+      if (obj2.parentElement.id.indexOf("region_")!=-1) {
+	x2 += obj2.parentElement.style.left.replace(/px/ig,"");
+	y2 += obj2.parentElement.style.top.replace(/px/ig,"");
+      } 
+      var x2_w = Number(obj2.style.left.replace(/px/ig,"")) + Number(obj2.style.width.replace(/px/ig,""));
+      var y2_h = Number(obj2.style.top.replace(/px/ig,"")) + Number(obj2.style.height.replace(/px/ig,""));
     
       var state=0;
       if ((((x2>=x1)&&(x2<=x1_w))&&((y2>=y1)&&(y2<=y1_h)))||(((x2>=x1)&&(x2<=x1_w))&&((y2_h>=y1)&&(y2_h<=y1_h)))||(((x2_w>=x1)&&(x2_w<=x1_w))&&((y2>=y1)&&(y2<=y1_h)))||(((x2_w>=x1)&&(x2_w<=x1_w))&&((y2_h>=y1)&&(y2_h<=y1_h)))||((x1>=x2&&x1_w<=x2_w)&&(y1<=y2&&y1_h>=y2_h))||((x1<=x2&&x1_w>=x2_w)&&(y1>=y2&&y1_h<=y2_h))||((x1>=x2&&x1_w<=x2_w)&&(y1>=y2&&y1_h<=y2_h)))
