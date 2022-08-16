3b752f2600eac5624ef3fa531fd7d42fe02000ee
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-16 17:46:31 +0800

    Update zh-hans.js

--- WebSerial_20220126/blockly/msg/blocks/zh-hans.js
+++ WebSerial_20220126/blockly/msg/blocks/zh-hans.js
@@ -15,3 +15,4 @@ Blockly.Msg.WEBSERIAL_SENDUINT8_SHOW = "Web Serial 传送unit8Array";
 Blockly.Msg.WEBSERIAL_GETID_SHOW = "Web Serial 取得按钮ID";
 Blockly.Msg.WEBSERIAL_ELEMENT_ONCLICK_SHOW = "Web Serial 设定";
 Blockly.Msg.WEBSERIAL_ELEMENT_ID_SHOW = "为网页元素代码";
+Blockly.Msg.WEBSERIAL_WAIT_SHOW = "网页序列埠 等待连线后向下执行";
