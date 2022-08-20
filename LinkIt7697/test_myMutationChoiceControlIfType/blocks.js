a8ea3b17280b71037ba4a3d8b8f86d8f496aa07c
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-20 14:24:02 +0800

    Update blocks.js

--- LinkIt7697/test_myMutationChoiceControlIfType/blocks.js
+++ LinkIt7697/test_myMutationChoiceControlIfType/blocks.js
@@ -39,6 +39,10 @@ Blockly.Blocks['test'] = {
 		xml = Blockly.Xml.textToDom('<xml xmlns="https://developers.google.com/blockly/xml">'+xml+'</xml>');
 		myWorkspace.clear();
 		Blockly.Xml.domToWorkspace(xml, myWorkspace);
+	  
+		var blocks = myWorkspace.getAllBlocks();
+		for (var i =0;i<blocks.length;i++)
+			blocks[i].setMovable(false);	  
 	
 		function onMyMutatorBlocksChange(event) {
 			if (event.type=="click") {
