8b48b59464d143cba03a6d49e1a25d4fd18bd225
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-20 21:47:41 +0800

    Update toolbox.js

--- SpBlocklyJS/test/toolbox.js
+++ SpBlocklyJS/test/toolbox.js
@@ -1,6 +1,8 @@
 var catTEST ='<xml>'+
 '  <category name="TEST" id="catTEST" colour="170">'+
-'	  <block type="test">'+
+'	  <block type="test_blocksFlydown1">'+
+'	  </block>'+
+'	  <block type="test_blocksFlydown2">'+
 '	  </block>'+
 '  </category>'+
 '</xml>';
