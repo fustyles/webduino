bdfad25aad8e3910174516362203625ed168b5eb
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-22 12:12:55 +0800

    Add files via upload

--- LinkIt7697/France/javascript.js
+++ LinkIt7697/France/javascript.js
@@ -1565,6 +1565,11 @@ Blockly.Arduino['fu_servo'] = function(block) {
 Blockly.Arduino['esp32_pixelbit_stream_myfirmata'] = function(block) {
 	
   var mainpage = Blockly.Arduino.valueToCode(block, 'mainpage', Blockly.Arduino.ORDER_ATOMIC);
+  if ((mainpage.indexOf('(')==0)&&(mainpage.lastIndexOf(')')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);  
+  if ((mainpage.indexOf('"')==0)&&(mainpage.lastIndexOf('"')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);
+
   var ssid = Blockly.Arduino.valueToCode(block, 'ssid', Blockly.Arduino.ORDER_ATOMIC);
   var pass = Blockly.Arduino.valueToCode(block, 'password', Blockly.Arduino.ORDER_ATOMIC);
   var ssid_ap = Blockly.Arduino.valueToCode(block, 'ssid_ap', Blockly.Arduino.ORDER_ATOMIC);
@@ -2322,6 +2327,11 @@ Blockly.Arduino['esp32_pixelbit_stream_myfirmata'] = function(block) {
 Blockly.Arduino['esp32_pixelbit_myfirmata'] = function(block) {
 	
   var mainpage = Blockly.Arduino.valueToCode(block, 'mainpage', Blockly.Arduino.ORDER_ATOMIC);
+  if ((mainpage.indexOf('(')==0)&&(mainpage.lastIndexOf(')')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);  
+  if ((mainpage.indexOf('"')==0)&&(mainpage.lastIndexOf('"')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);
+
   var ssid = Blockly.Arduino.valueToCode(block, 'ssid', Blockly.Arduino.ORDER_ATOMIC);
   var pass = Blockly.Arduino.valueToCode(block, 'password', Blockly.Arduino.ORDER_ATOMIC);
   var ssid_ap = Blockly.Arduino.valueToCode(block, 'ssid_ap', Blockly.Arduino.ORDER_ATOMIC);
@@ -6323,6 +6333,11 @@ Blockly.Arduino['esp32_digitalwrite'] = function(block) {
 
 Blockly.Arduino['esp32_myfirmata'] = function(block) {
   var mainpage = Blockly.Arduino.valueToCode(block, 'mainpage', Blockly.Arduino.ORDER_ATOMIC);
+  if ((mainpage.indexOf('(')==0)&&(mainpage.lastIndexOf(')')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);  
+  if ((mainpage.indexOf('"')==0)&&(mainpage.lastIndexOf('"')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);
+
   var ssid = Blockly.Arduino.valueToCode(block, 'ssid', Blockly.Arduino.ORDER_ATOMIC);
   var pass = Blockly.Arduino.valueToCode(block, 'password', Blockly.Arduino.ORDER_ATOMIC);
   var ssid_ap = Blockly.Arduino.valueToCode(block, 'ssid_ap', Blockly.Arduino.ORDER_ATOMIC);
@@ -7049,6 +7064,11 @@ Blockly.Arduino['esp32_bluetooth_getdata'] = function(block) {
 
 Blockly.Arduino['linkit7697_myfirmata'] = function(block) {
   var mainpage = Blockly.Arduino.valueToCode(block, 'mainpage', Blockly.Arduino.ORDER_ATOMIC);
+  if ((mainpage.indexOf('(')==0)&&(mainpage.lastIndexOf(')')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);  
+  if ((mainpage.indexOf('"')==0)&&(mainpage.lastIndexOf('"')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);
+
   var ssid = Blockly.Arduino.valueToCode(block, 'ssid', Blockly.Arduino.ORDER_ATOMIC);
   var pass = Blockly.Arduino.valueToCode(block, 'password', Blockly.Arduino.ORDER_ATOMIC);
   var baudrate = block.getFieldValue('baudrate');
@@ -11889,6 +11909,11 @@ Blockly.Arduino['fu_mqtt_sendimage'] = function(block) {
 Blockly.Arduino['esp32_cam_myfirmata'] = function(block) {
 	
   var mainpage = Blockly.Arduino.valueToCode(block, 'mainpage', Blockly.Arduino.ORDER_ATOMIC);
+  if ((mainpage.indexOf('(')==0)&&(mainpage.lastIndexOf(')')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);  
+  if ((mainpage.indexOf('"')==0)&&(mainpage.lastIndexOf('"')==mainpage.length-1))
+    mainpage = mainpage.substring(1,mainpage.length-1);
+
   var ssid = Blockly.Arduino.valueToCode(block, 'ssid', Blockly.Arduino.ORDER_ATOMIC);
   var pass = Blockly.Arduino.valueToCode(block, 'password', Blockly.Arduino.ORDER_ATOMIC);
   var ssid_ap = Blockly.Arduino.valueToCode(block, 'ssid_ap', Blockly.Arduino.ORDER_ATOMIC);
