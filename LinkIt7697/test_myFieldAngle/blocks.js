e3e90c6f64d05e39264a27089f44661f8659b220
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-28 21:04:33 +0800

    Update blocks.js

--- LinkIt7697/test_myFieldAngle/blocks.js
+++ LinkIt7697/test_myFieldAngle/blocks.js
@@ -23,12 +23,11 @@
 /*
 Blockly.Blocks["test"] = {
 	init:  function() {
-
 		var field = new myFieldAngle(30);
 		field.options = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360];
-
 		this.appendDummyInput()
 			.appendField(field, "test");
+			
 		this.setInputsInline(true);		
 		this.setPreviousStatement(true, null);
 		this.setNextStatement(true, null);
