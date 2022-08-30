fbeca4b292bd1bd35c10094ad07292c3e9723706
fustyles <34397289+fustyles@users.noreply.github.com>
2022-08-30 10:15:52 +0800

    Update toolbox.js

--- LinkIt7697/test_myBlocksFlydownImage_202208/toolbox.js
+++ LinkIt7697/test_myBlocksFlydownImage_202208/toolbox.js
@@ -1,8 +1,6 @@
 var catTEST ='<xml>'+
 '  <category name="TEST" id="catTEST" colour="170">'+
-'	  <block type="test_blocksFlydown1">'+
-'	  </block>'+
-'	  <block type="test_blocksFlydown2">'+
+'	  <block type="test">'+
 '	  </block>'+
 '  </category>'+
 '</xml>';
