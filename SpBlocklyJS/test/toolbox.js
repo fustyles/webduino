9a1a94a7c18407038c30566106e9af1625f19e2d
fustyles <34397289+fustyles@users.noreply.github.com>
2022-09-19 11:41:12 +0800

    Delete toolbox.js

--- SpBlocklyJS/test/toolbox.js
+++
@@ -1,8 +0,0 @@
-var catTEST ='<xml>'+
-'  <category name="TEST" id="catTEST" colour="170">'+
-'	  <block type="test_blocksFlydown1">'+
-'	  </block>'+
-'	  <block type="test_blocksFlydown2">'+
-'	  </block>'+
-'  </category>'+
-'</xml>';
