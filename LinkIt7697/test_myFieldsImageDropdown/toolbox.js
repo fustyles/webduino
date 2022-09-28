e18c54af91d95d3a125b91681bd885c136313b14
fustyles <34397289+fustyles@users.noreply.github.com>
2022-09-28 12:33:25 +0800

    Update toolbox.js

--- LinkIt7697/test_myFieldsImageDropdown/toolbox.js
+++ LinkIt7697/test_myFieldsImageDropdown/toolbox.js
@@ -1,6 +1,10 @@
 var catTEST ='<xml>'+
 '  <category name="TEST" id="catTEST" colour="170">'+
-'	  <block type="test">'+
+'	  <block type="test1">'+
+'	  </block>'+
+'	  <block type="test2">'+
+'	  </block>'+
+'	  <block type="test3">'+
 '	  </block>'+
 '  </category>'+
 '</xml>';
