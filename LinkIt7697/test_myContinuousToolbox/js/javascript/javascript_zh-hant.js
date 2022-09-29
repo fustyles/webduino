cd03d29feb11990830d3f937b760a0d131d21d61
fustyles <34397289+fustyles@users.noreply.github.com>
2022-09-29 14:12:47 +0800

    Update javascript_zh-hant.js

--- LinkIt7697/test_myContinuousToolbox/js/javascript/javascript_zh-hant.js
+++ LinkIt7697/test_myContinuousToolbox/js/javascript/javascript_zh-hant.js
@@ -7,7 +7,7 @@ var Blockly = Blockly || { Msg: Object.create(null) };
 
 //*************** Custom Message ******************
 
-Blockly.Msg["CUSTOM_NAME"] = "自訂積木";
+Blockly.Msg["CUSTOM_NAME"] = "自訂";
 Blockly.Msg["HELLOWORLD_MSG"] = "對話視窗顯示";
 Blockly.Msg["HELLOWORLD_GET"] = "取得資料";
 Blockly.Msg["HELLOWORLD_HUE"] = "100";
