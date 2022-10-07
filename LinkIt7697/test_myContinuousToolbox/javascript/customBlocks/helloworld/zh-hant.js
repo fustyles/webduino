61558dfe59b11eaf2e13323efe3ff821365b44f6
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-07 09:29:23 +0800

    Delete zh-hant.js

--- LinkIt7697/test_myContinuousToolbox/javascript/customBlocks/helloworld/zh-hant.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Blockly.Msg["HELLOWORLD_NAME"] = "自訂積木";
-Blockly.Msg["HELLOWORLD_MSG"] = "視窗顯示";
-Blockly.Msg["HELLOWORLD_GET"] = "取得資料";
-Blockly.Msg["HELLOWORLD_DELAY"] = "等待(秒)";
-Blockly.Msg["HELLOWORLD_HUE"] = "100";
