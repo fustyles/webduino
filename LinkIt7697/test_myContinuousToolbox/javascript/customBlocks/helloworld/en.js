cab6396156bc14e0285117ead55ee069584ad5f8
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-07 09:29:33 +0800

    Delete en.js

--- LinkIt7697/test_myContinuousToolbox/javascript/customBlocks/helloworld/en.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Blockly.Msg["HELLOWORLD_NAME"] = "CUSTOM";
-Blockly.Msg["HELLOWORLD_MSG"] = "Print";
-Blockly.Msg["HELLOWORLD_GET"] = "get message";
-Blockly.Msg["HELLOWORLD_DELAY"] = "delay(s)";
-Blockly.Msg["HELLOWORLD_HUE"] = "100";
