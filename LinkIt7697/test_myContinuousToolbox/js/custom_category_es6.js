e3bdcca5c1ca28cada02f7905af99853c20852b2
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-16 01:02:40 +0800

    Update custom_category_es6.js

--- LinkIt7697/test_myContinuousToolbox/js/custom_category_es6.js
+++ LinkIt7697/test_myContinuousToolbox/js/custom_category_es6.js
@@ -61,8 +61,7 @@ class CustomCategory extends Blockly.ToolboxCategory {
    */
   createIconDom_() {
     const iconImg = document.createElement('img');
-    iconImg.src = 'svg/'+this.toolboxItemDef_.name+'.svg';      //iconImg.src = 'svg/logo_only.svg';
-    console.log(iconImg.src);
+    iconImg.src = 'svg/'+this.toolboxItemDef_.categorystyle+'.svg';      //iconImg.src = 'svg/logo_only.svg';
     iconImg.alt = '';
     iconImg.width = '25';
     iconImg.height = '25';
