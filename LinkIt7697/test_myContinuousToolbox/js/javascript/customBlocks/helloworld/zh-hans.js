5b55dcc49fee75bb5d3a02b9e853b510b69a482e
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-16 20:25:51 +0800

    Update zh-hans.js

--- LinkIt7697/test_myContinuousToolbox/js/javascript/customBlocks/helloworld/zh-hans.js
+++ LinkIt7697/test_myContinuousToolbox/js/javascript/customBlocks/helloworld/zh-hans.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Blockly.Msg["HELLOWORLD_NAME"] = "自订积木";
+Blockly.Msg["HELLOWORLD_NAME"] = "自订";
 Blockly.Msg["HELLOWORLD_MSG"] = "视窗显示";
 Blockly.Msg["HELLOWORLD_GET"] = "取得资料";
 Blockly.Msg["HELLOWORLD_DELAY"] = "等待(秒)";
