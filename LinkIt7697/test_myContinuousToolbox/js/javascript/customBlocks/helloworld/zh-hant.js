0f0ddbc172c0d51627e0399d67f13259ab1ab80f
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-16 20:26:01 +0800

    Update zh-hant.js

--- LinkIt7697/test_myContinuousToolbox/js/javascript/customBlocks/helloworld/zh-hant.js
+++ LinkIt7697/test_myContinuousToolbox/js/javascript/customBlocks/helloworld/zh-hant.js
@@ -1,6 +1,6 @@
 'use strict';
 
-Blockly.Msg["HELLOWORLD_NAME"] = "自訂積木";
+Blockly.Msg["HELLOWORLD_NAME"] = "自訂";
 Blockly.Msg["HELLOWORLD_MSG"] = "視窗顯示";
 Blockly.Msg["HELLOWORLD_GET"] = "取得資料";
 Blockly.Msg["HELLOWORLD_DELAY"] = "等待(秒)";
