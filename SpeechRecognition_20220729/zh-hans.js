6dbc69795d40f32ecbd62984d1a42df02e534ddb
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-29 19:30:06 +0800

    Add files via upload

---
+++ SpeechRecognition_20220729/zh-hans.js
@@ -0,0 +1,15 @@
+Blockly.Msg["CATSPEECHRECOGNITION"] = "语音辨识";
+Blockly.Msg["SPEECHRECOGNITION_SHOW"] = "语音辨识";
+Blockly.Msg["SPEECHRECOGNITION_LANGUAGE_SHOW"] = "设定语言";
+Blockly.Msg["SPEECHRECOGNITION_GET_SHOW"] = "取得辨识文字";
+Blockly.Msg["SPEECHRECOGNITION_LISTENER_SHOW"] = "语音辨识完成辨识后执行";
+Blockly.Msg["SPEECHRECOGNITION_KEYWORD_SHOW"] = "包含关键字";
+Blockly.Msg["SPEECHRECOGNITION_INTERIM"] = "语音辨识 显示过程文字";
+Blockly.Msg["SPEECHRECOGNITION_LEFT"] = "靠左距离";   
+Blockly.Msg["SPEECHRECOGNITION_TOP"] = "靠上距离";	  
+Blockly.Msg["SPEECHRECOGNITION_FONTSIZE"] = "字型大小";
+Blockly.Msg["SPEECHRECOGNITION_ZINDEX"] = "层叠索引";
+Blockly.Msg["SPEECHRECOGNITION_DISPLAY"] = "显示";
+Blockly.Msg["SPEECHRECOGNITION_STATE"] = "语音辨识启动状态";
+Blockly.Msg["SPEECHRECOGNITION_START"] = "启动";
+Blockly.Msg["SPEECHRECOGNITION_STOP"] = "停止";
---
+++ SpeechRecognition_20220729/zh-hant.js
@@ -0,0 +1,15 @@
+Blockly.Msg["CATSPEECHRECOGNITION"] = "語音辨識";
+Blockly.Msg["SPEECHRECOGNITION_SHOW"] = "語音辨識";
+Blockly.Msg["SPEECHRECOGNITION_LANGUAGE_SHOW"] = "設定語言";
+Blockly.Msg["SPEECHRECOGNITION_GET_SHOW"] = "取得辨識文字";
+Blockly.Msg["SPEECHRECOGNITION_LISTENER_SHOW"] = "語音辨識完成辨識後執行";
+Blockly.Msg["SPEECHRECOGNITION_KEYWORD_SHOW"] = "包含關鍵字";
+Blockly.Msg["SPEECHRECOGNITION_INTERIM"] = "語音辨識 顯示過程文字";
+Blockly.Msg["SPEECHRECOGNITION_LEFT"] = "靠左距離";   
+Blockly.Msg["SPEECHRECOGNITION_TOP"] = "靠上距離";	  
+Blockly.Msg["SPEECHRECOGNITION_FONTSIZE"] = "字型大小";
+Blockly.Msg["SPEECHRECOGNITION_ZINDEX"] = "層疊索引";
+Blockly.Msg["SPEECHRECOGNITION_DISPLAY"] = "顯示";
+Blockly.Msg["SPEECHRECOGNITION_STATE"] = "語音辨識啟動狀態";
+Blockly.Msg["SPEECHRECOGNITION_START"] = "啟動";
+Blockly.Msg["SPEECHRECOGNITION_STOP"] = "停止";
