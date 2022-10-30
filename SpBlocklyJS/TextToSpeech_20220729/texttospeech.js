1d42d995ffb0a97b54821b9b454f10ecd1fda56e
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-30 20:30:13 +0800

    Update texttospeech.js

--- SpBlocklyJS/TextToSpeech_20220729/texttospeech.js
+++ SpBlocklyJS/TextToSpeech_20220729/texttospeech.js
@@ -28,11 +28,9 @@ function ttsSetProperty(volume, rate, pitch, lang) {
 
 function ttsSpeak(voice, text) {
 	if (ttsSpeakState) {
-		if (typeof Recognition_switch === 'function') Recognition_switch(0);
 		ttsSetVoice(voice);
 		tts.text = text;
 		synth.speak(tts);
-		if (typeof Recognition_switch === 'function') Recognition_switch(1);
 	}
 }
 
