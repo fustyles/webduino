67bc2d40a783804c05d65e79e3e877842702fbea
fustyles <34397289+fustyles@users.noreply.github.com>
2022-10-31 13:58:04 +0800

    Update blocks.js

--- LinkIt7697/France/blocks.js
+++ LinkIt7697/France/blocks.js
@@ -14035,7 +14035,7 @@ Blockly.Blocks['async_function'] = {
   this.appendDummyInput()  
       .appendField("(");    
   this.appendValueInput("parameter_")
-      .setCheck("String");    
+      .setCheck(null);    
   this.appendDummyInput()  
       .appendField(")");    
   this.appendStatementInput("do_");
@@ -14054,7 +14054,7 @@ Blockly.Blocks['call_async_function'] = {
   this.appendDummyInput()  
       .appendField("(");
   this.appendValueInput("parameter_")
-      .setCheck("String");   
+      .setCheck(null);   
   this.appendDummyInput()  
       .appendField(")");    
   this.setInputsInline(true);
