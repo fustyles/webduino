8778fe375c08ce8c4b08a03b4a643d5217bc065f
fustyles <34397289+fustyles@users.noreply.github.com>
2022-12-14 09:41:23 +0800

    Update zh-hans.js

--- SpBlocklyJS/openAI_20221212/zh-hans.js
+++ SpBlocklyJS/openAI_20221212/zh-hans.js
@@ -1,7 +1,7 @@
 Blockly.Msg["CATOPENAITEXT"] = "openAI 聊天";
 Blockly.Msg.OPENAI_TEXT_SHOW = "openAI 聊天";
 Blockly.Msg.OPENAI_TEXT_INITIAL_SHOW = "初始化";
-Blockly.Msg.OPENAI_TEXT_TOKEN_SHOW = "权杖";
+Blockly.Msg.OPENAI_TEXT_TOKEN_SHOW = "API权杖";
 Blockly.Msg.OPENAI_TEXT_TOKENS_SHOW = "最大字数";
 Blockly.Msg.OPENAI_TEXT_REQUEST_SHOW = "对话内容";
 Blockly.Msg.OPENAI_TEXT_RESPONSE_SHOW = "取得回应内容后执行";
