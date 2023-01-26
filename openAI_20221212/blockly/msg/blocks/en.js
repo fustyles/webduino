5b9c2ce7991cab876add31cad9a181f8c72b6249
fustyles <34397289+fustyles@users.noreply.github.com>
2023-01-26 13:48:11 +0800

    Update en.js

--- openAI_20221212/blockly/msg/blocks/en.js
+++ openAI_20221212/blockly/msg/blocks/en.js
@@ -1,6 +1,6 @@
 Blockly.Msg.OPENAI_TEXT_SHOW = "openAI";
 Blockly.Msg.OPENAI_TEXT_INITIAL_SHOW = "Initial";
-Blockly.Msg.OPENAI_TEXT_TOKEN_SHOW = "API Token";
+Blockly.Msg.OPENAI_TEXT_TOKEN_SHOW = "API Key";
 Blockly.Msg.OPENAI_TEXT_TOKENS_SHOW = "Max tokens";
 Blockly.Msg.OPENAI_TEXT_REQUEST_SHOW = "Request content";
 Blockly.Msg.OPENAI_TEXT_RESPONSE_SHOW = "After getting response";
