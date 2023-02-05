9a7ad34bacc50b15f0722695e9d743af4d1c4f5e
fustyles <34397289+fustyles@users.noreply.github.com>
2023-02-06 00:43:44 +0800

    Update WebUSB_elements_microbit.js

--- SpBlocklyJS/WebUSB_20210813/WebUSB_elements_microbit.js
+++ SpBlocklyJS/WebUSB_20210813/WebUSB_elements_microbit.js
@@ -2,7 +2,7 @@ document.write('<input type="button" id="gamebutton_webusb_open" value="Connect 
 document.write('<input type="button" id="gamebutton_webusb_close" value="Disconnect device" style="display:none;z-index:999">');
 document.write('<input type="button" id="gamebutton_send" value="Send command" style="display:none;z-index:999">');
 document.write('<input type="text" id="command" value="" style="display:none;z-index:999">');
-document.write('<span id="gamespan_status" style="display:none;z-index:999">Select your device</span>');
+document.write('<span id="gamespan_status" style="display:none;z-index:999">disconnected</span>');
 document.write('<span id="gamespan_response" style="display:none;z-index:999"></span>');
 
 // Append a line to the console frame
