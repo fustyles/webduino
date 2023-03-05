99e72f7e66a44a11a80f27fe594695e6de7d0475
fustyles <34397289+fustyles@users.noreply.github.com>
2023-03-05 12:55:55 +0800

    Update blocks.js

--- openAI_20221212/blockly/blocks.js
+++ openAI_20221212/blockly/blocks.js
@@ -161,9 +161,9 @@ Blockly.Blocks['openai_chat_initial'] = {
   this.appendValueInput("role")
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.OPENAI_ROLE_SHOW);	  
+      .appendField(Blockly.Msg.OPENAI_CHAT_ROLE_SHOW);	  
   this.appendDummyInput()  
-      .appendField(Blockly.Msg.OPENAI_MODEL_SHOW)
+      .appendField(Blockly.Msg.OPENAI_CHAT_MODEL_SHOW)
       .appendField(new Blockly.FieldDropdown([
 			["gpt-3.5-turbo","gpt-3.5-turbo"],
 			["gpt-3.5-turbo-0301","gpt-3.5-turbo-0301"]	  
