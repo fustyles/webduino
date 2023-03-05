244feb7605441612fee9e95ca9ac03f544704542
fustyles <34397289+fustyles@users.noreply.github.com>
2023-03-05 12:54:35 +0800

    Update en.js

--- openAI_20221212/blockly/msg/blocks/en.js
+++ openAI_20221212/blockly/msg/blocks/en.js
@@ -17,3 +17,6 @@ Blockly.Msg.OPENAI_IMAGE_RESPONSE_CLEAR_SHOW = "Clear image url";
 Blockly.Msg.OPENAI_IMAGE_SIZE_SHOW = "Size";
 Blockly.Msg.OPENAI_CHAT_SHOW = "openAI chatGPT";
 Blockly.Msg.OPENAI_CHAT_REQUEST_SHOW = "Send chat content";
+Blockly.Msg.OPENAI_CHAT_CONTENT_CLEAR_SHOW = "Clear content history";
+Blockly.Msg.OPENAI_ROLE_SHOW = "assistant behavior";
+Blockly.Msg.OPENAI_MODEL_SHOW = "model";
