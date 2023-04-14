2ccb7bc87d6f6b8b7210cc91a3b21b2296d51244
fustyles <34397289+fustyles@users.noreply.github.com>
2023-04-14 12:52:11 +0800

    Add files via upload

--- LinkIt7697/test_myToolboxPopup/index.js
+++ LinkIt7697/test_myToolboxPopup/index.js
@@ -90,13 +90,13 @@ function init() {
 	document.getElementById('logic').onclick = function () {
 		var xmlDoc = '<xml id="toolbox"><block type="controls_if"></block><block type="logic_compare"></block><block type="logic_operation"></block><block type="logic_negate"></block><block type="logic_boolean"></block></xml>';
 		
-		showFlyout('logic', xmlDoc);
+		showFlyout(this.id, xmlDoc);
 	} 
 	
 	document.getElementById('loop').onclick = function () {
 		var xmlDoc = '<xml id="toolbox"><block type="controls_repeat_ext"><value name="TIMES"><shadow type="math_number"><field name="NUM">10</field></shadow></value></block><block type="controls_flow_statements"></block></xml>';
 		
-		showFlyout('loop', xmlDoc);
+		showFlyout(this.id, xmlDoc);
 	}	
 	
 	function showFlyout(item, xmlDoc) {
