1095a1937e86465e95c2e5d84ef56328b8e44686
fustyles <34397289+fustyles@users.noreply.github.com>
2023-05-29 12:48:38 +0800

    Update python_function.js

--- LinkIt7697/test_myContinuousToolbox/js/python/python_function.js
+++ LinkIt7697/test_myContinuousToolbox/js/python/python_function.js
@@ -122,7 +122,8 @@ function start() {
 		else if (event.type=="toolbox_item_select"&&(!event.newItem)&&continuousFlyout.isVisible_==true) {
 			workspace.toolbox_.clearSelection();
 			setTimeout(function(){
-				continuousFlyout.setVisible(false);
+				if (continuousFlyout.isVisible_ == true)
+					continuousFlyout.setVisible(false);
 			}, 10);
 		}
 	}
