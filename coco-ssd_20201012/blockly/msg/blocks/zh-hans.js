76ddc0284d054b79f34030bc8033be5ae79d1435
fustyles <34397289+fustyles@users.noreply.github.com>
2023-06-01 22:14:38 +0800

    Update zh-hans.js

--- coco-ssd_20201012/blockly/msg/blocks/zh-hans.js
+++ coco-ssd_20201012/blockly/msg/blocks/zh-hans.js
@@ -29,4 +29,4 @@ Blockly.Msg.COCOSSD_DATA_BOTTOMY_SHOW = "下方Y";
 Blockly.Msg.COCOSSD_CLEAR_SHOW = "物件侦测(coco-ssd) 清除结果";
 Blockly.Msg.COCOSSD_PAUSE_SHOW = "物件侦测(coco-ssd) 暂停侦测";
 Blockly.Msg.COCOSSD_RECOGNITIED_SHOW = "物件侦测 侦测到物件后执行";
-Blockly.Msg.COCOSSD_RECOGNITIED_SHOW = "物件侦测 侦测不到物件后执行";
+Blockly.Msg.COCOSSD_UNRECOGNITIED_SHOW = "物件侦测 侦测不到物件后执行";
