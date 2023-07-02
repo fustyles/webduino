69160005193bc7050a6a83d5d4e1f960285ecbda
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-02 20:54:52 +0800

    Update javascript.js

--- GameElements_20190131/blockly/javascript.js
+++ GameElements_20190131/blockly/javascript.js
@@ -1,6 +1,10 @@
 Blockly.JavaScript['code_text'] = function (block) {
-  var code = Blockly.JavaScript.valueToCode(block, 'code', Blockly.JavaScript.ORDER_ATOMIC)||""; 
-  return code.replace(/'/g,"").replace(/"/g,"");
+  var code = Blockly.JavaScript.valueToCode(block, 'code', Blockly.JavaScript.ORDER_ATOMIC)||"";
+  if ((code.indexOf("'")==0)&&(code.lastIndexOf("'")==code.length-1))
+    code = code.substring(1,code.length-1);
+  if ((code.indexOf('"')==0)&&(code.lastIndexOf('"')==code.length-1))
+    code = code.substring(1,code.length-1);	
+  return code;
 };
 
 Blockly.JavaScript['window_messagebox'] = function (block) {
