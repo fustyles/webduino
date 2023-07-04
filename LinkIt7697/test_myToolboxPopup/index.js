95b8aaae205de13170c12cfa1dd99a28ddbbb509
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-04 18:33:19 +0800

    Add files via upload

--- LinkIt7697/test_myToolboxPopup/index.js
+++ LinkIt7697/test_myToolboxPopup/index.js
@@ -103,7 +103,7 @@ function init() {
                     }, 1000);
                 }
                 block = Blockly.Xml.blockToDom(block, true);
-                block = Blockly.Xml.textToDom('<xml xmlns="https://developers.google.com/blockly/xml">' + Blockly.Xml.domToText(block) + '</xml>');
+                block = Blockly.utils.xml.textToDom('<xml xmlns="https://developers.google.com/blockly/xml">' + Blockly.Xml.domToText(block) + '</xml>');
 
                 var id = Blockly.Xml.appendDomToWorkspace(block, secondaryWorkspace);
                 newBlock = secondaryWorkspace.getBlockById(id[0]);
