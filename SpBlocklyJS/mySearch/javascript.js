beea8ff20e94d925b535ec1e37fac190a3d24350
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-18 20:09:39 +0800

    Update javascript.js

--- SpBlocklyJS/mySearch/javascript.js
+++ SpBlocklyJS/mySearch/javascript.js
@@ -12,6 +12,11 @@
 
 /*
 
+Blockly.Msg["MYSEARCH"] = "SEARCH";
+Blockly.Msg["MYSEARCH_QUERY"] = "Search blocks";
+Blockly.Msg["MYSEARCH_PROMPT"] = "Please input the keyword of blocks content.";
+Blockly.Msg["MYSEARCH_HUE"] = "210";
+
 var categoryBlocks = [];
 function updateCategoryBlocks(newCategory) {
 	categoryBlocks = [];
