15084b99dbcb8b2d2192af9baba5fba349b21eb0
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-18 13:18:02 +0800

    Update zh-hans.js

--- SpBlocklyJS/mySearch/zh-hans.js
+++ SpBlocklyJS/mySearch/zh-hans.js
@@ -1,6 +1,4 @@
 Blockly.Msg["MYSEARCH"] = "搜寻";
 Blockly.Msg["MYSEARCH_QUERY"] = "搜寻积木";
 Blockly.Msg["MYSEARCH_PROMPT"] = "请输入积木文字关键字";
-Blockly.Msg["MYSEARCH_TITLE"] = "搜寻";
 Blockly.Msg["MYSEARCH_HUE"] = "210";
-Blockly.Msg.MYSEARCH_TITLE = "搜寻";
