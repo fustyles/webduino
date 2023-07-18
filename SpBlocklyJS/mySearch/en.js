bc0f50232810641c823d072b659f1371a33a842d
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-18 13:16:38 +0800

    Update en.js

--- SpBlocklyJS/mySearch/en.js
+++ SpBlocklyJS/mySearch/en.js
@@ -3,4 +3,3 @@ Blockly.Msg["MYSEARCH_QUERY"] = "Search blocks";
 Blockly.Msg["MYSEARCH_PROMPT"] = "Please input the keyword of blocks content.";
 Blockly.Msg["MYSEARCH_TITLE"] = "SEARCH";
 Blockly.Msg["MYSEARCH_HUE"] = "210";
-Blockly.Msg.MYSEARCH_TITLE = "SEARCH";
