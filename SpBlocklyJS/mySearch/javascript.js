19dc020d796b7066e056d19b74cc2afb8e8e2103
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-19 02:37:27 +0800

    Update javascript.js

--- SpBlocklyJS/mySearch/javascript.js
+++ SpBlocklyJS/mySearch/javascript.js
@@ -13,7 +13,7 @@
 /*
 
 Blockly.Msg["MYSEARCH"] = "SEARCH";
-Blockly.Msg["MYSEARCH_QUERY"] = "Search blocks";
+Blockly.Msg["MYSEARCH_QUERY"] = "Search for blocks";
 Blockly.Msg["MYSEARCH_PROMPT"] = "Please input the keyword of blocks content.";
 Blockly.Msg["MYSEARCH_HUE"] = "210";
 
