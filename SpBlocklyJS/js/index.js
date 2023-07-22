bd5c8847d6902e446c859db34be3f3055590e250
fustyles <34397289+fustyles@users.noreply.github.com>
2023-07-22 18:39:53 +0800

    Update index.js

--- SpBlocklyJS/js/index.js
+++ SpBlocklyJS/js/index.js
@@ -1331,8 +1331,8 @@ function searchBlocks() {
 		modal: false,
 		//show: "blind",
 		//hide: "blind",			
-		width: 240,
-		height: 170,
+		width: 260,
+		height: 190,
 		buttons: [	
 			{
 				text: Blockly.Msg.BUTTON_CLOSE,
