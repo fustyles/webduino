9f0b9f242bbb965e54f595f513e07d723536c5b5
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-07 23:07:01 +0800

    Update trackingcolor_esp32cam.js

--- trackingcolor_20201012/trackingcolor_esp32cam.js
+++ trackingcolor_20201012/trackingcolor_esp32cam.js
@@ -41,19 +41,12 @@ window.onload = function() {
     }
 
     ShowImage.onload = function(event) {
-        if (!ShowImage.width) {
-            console.log("ok");
-            ShowImage.setAttribute("width", ShowImage.naturalWidth);
-            ShowImage.setAttribute("height", ShowImage.naturalHeight);
-            canvas.setAttribute("width", ShowImage.width);
-            canvas.setAttribute("height", ShowImage.height);
-            canvas.style.width = ShowImage.width + "px";
-            canvas.style.height = ShowImage.height + "px";
-        }
+        ShowImage.setAttribute("width", ShowImage.naturalWidth);
+        ShowImage.setAttribute("height", ShowImage.naturalHeight);
+        canvas.setAttribute("width", ShowImage.width);
+        canvas.setAttribute("height", ShowImage.height);
         canvas_custom.setAttribute("width", canvas.width);
-        canvas_custom.setAttribute("height", canvas.height);
-        canvas_custom.style.width = canvas.width + "px";
-        canvas_custom.style.height = canvas.height + "px";        
+        canvas_custom.setAttribute("height", canvas.height);     
         context.drawImage(ShowImage, 0, 0, ShowImage.width, ShowImage.height);
 
         clearInterval(myTimer);
