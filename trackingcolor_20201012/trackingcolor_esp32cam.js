a1bb87b13473582186a2b46abb5fd807f52114b5
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-07 17:22:03 +0800

    Update trackingcolor_esp32cam.js

--- trackingcolor_20201012/trackingcolor_esp32cam.js
+++ trackingcolor_20201012/trackingcolor_esp32cam.js
@@ -7,11 +7,11 @@ document.write('<div id="trackingcolorState" style="position:absolute;display:no
 document.write('<div id="sourceId_trackingcolor" style="position:absolute;display:none;"></div>');
 
 window.onload = function () {
-	var ShowImage = document.getElementById('gameimage_trackingcolor');	
+    var ShowImage = document.getElementById('gameimage_trackingcolor');	
     var canvas = document.getElementById('gamecanvas_trackingcolor');
     var context = canvas.getContext('2d');
     var canvas_custom = document.getElementById('gamecanvas_custom');
-	var trackingcolorState = document.getElementById('trackingcolorState');
+    var trackingcolorState = document.getElementById('trackingcolorState');
     var context_custom = canvas_custom.getContext('2d');
     var result = document.getElementById('gamediv_trackingcolor');
     var mirrorimage = document.getElementById("mirrorimage_trackingcolor");
@@ -55,83 +55,87 @@ window.onload = function () {
 
 	tracker.on('track', function(event) {
 		
-		if (mirrorimage==1) {
-		  context.translate((canvas.width + ShowImage.width) / 2, 0);
-		  context.scale(-1, 1);
-		  context.drawImage(ShowImage, 0, 0, ShowImage.width, ShowImage.height);
-		  context.setTransform(1, 0, 0, 1, 0, 0);
-		}
-		else
-		  context.drawImage(ShowImage, 0, 0, ShowImage.width, ShowImage.height);
-	  
-		if (trackingcolorState.innerHTML=="0") {
-			//result.innerHTML = "";
-			return;
-		}				  
-
-		myColor_r_min1 = document.getElementById('myColor_r_min1').value;
-		myColor_r_max1 = document.getElementById('myColor_r_max1').value;
-		myColor_g_min1 = document.getElementById('myColor_g_min1').value;
-		myColor_g_max1 = document.getElementById('myColor_g_max1').value;
-		myColor_b_min1 = document.getElementById('myColor_b_min1').value;
-		myColor_b_max1 = document.getElementById('myColor_b_max1').value;
-
-		myColor_r_min2 = document.getElementById('myColor_r_min2').value;
-		myColor_r_max2 = document.getElementById('myColor_r_max2').value;
-		myColor_g_min2 = document.getElementById('myColor_g_min2').value;
-		myColor_g_max2 = document.getElementById('myColor_g_max2').value;
-		myColor_b_min2 = document.getElementById('myColor_b_min2').value;
-		myColor_b_max2 = document.getElementById('myColor_b_max2').value;
-
-		myColor_r_min3 = document.getElementById('myColor_r_min3').value;
-		myColor_r_max3 = document.getElementById('myColor_r_max3').value;
-		myColor_g_min3 = document.getElementById('myColor_g_min3').value;
-		myColor_g_max3 = document.getElementById('myColor_g_max3').value;
-		myColor_b_min3 = document.getElementById('myColor_b_min3').value;
-		myColor_b_max3 = document.getElementById('myColor_b_max3').value;
-
-		result.innerHTML = "";
-
-		var imgData=context.getImageData(0,0,canvas.width,canvas.height);
-
-		for (var i=0;i<imgData.data.length;i+=4) {
-			var r=0;
-			var g=0;
-			var b=0;
-			if ((imgData.data[i]>=myColor_r_min1&&imgData.data[i]<=myColor_r_max1)&&(imgData.data[i+1]>=myColor_g_min1&&imgData.data[i+1]<=myColor_g_max1)&&(imgData.data[i+2]>=myColor_b_min1&&imgData.data[i+2]<=myColor_b_max1)) {
-			  r=255;
+		if (ShowImage.width>0) {
+		
+			if (mirrorimage==1) {
+			  context.translate((canvas.width + ShowImage.width) / 2, 0);
+			  context.scale(-1, 1);
+			  context.drawImage(ShowImage, 0, 0, ShowImage.width, ShowImage.height);
+			  context.setTransform(1, 0, 0, 1, 0, 0);
 			}
-			if ((imgData.data[i]>=myColor_r_min2&&imgData.data[i]<=myColor_r_max2)&&(imgData.data[i+1]>=myColor_g_min2&&imgData.data[i+1]<=myColor_g_max2)&&(imgData.data[i+2]>=myColor_b_min2&&imgData.data[i+2]<=myColor_b_max2)) {
-			  g=255;
-			}
-			if ((imgData.data[i]>=myColor_r_min3&&imgData.data[i]<=myColor_r_max3)&&(imgData.data[i+1]>=myColor_g_min3&&imgData.data[i+1]<=myColor_g_max3)&&(imgData.data[i+2]>=myColor_b_min3&&imgData.data[i+2]<=myColor_b_max3)) {
-			  b=255;
+			else
+			  context.drawImage(ShowImage, 0, 0, ShowImage.width, ShowImage.height);
+		  
+			if (trackingcolorState.innerHTML=="0") {
+				//result.innerHTML = "";
+				return;
+			}				  
+
+			myColor_r_min1 = document.getElementById('myColor_r_min1').value;
+			myColor_r_max1 = document.getElementById('myColor_r_max1').value;
+			myColor_g_min1 = document.getElementById('myColor_g_min1').value;
+			myColor_g_max1 = document.getElementById('myColor_g_max1').value;
+			myColor_b_min1 = document.getElementById('myColor_b_min1').value;
+			myColor_b_max1 = document.getElementById('myColor_b_max1').value;
+
+			myColor_r_min2 = document.getElementById('myColor_r_min2').value;
+			myColor_r_max2 = document.getElementById('myColor_r_max2').value;
+			myColor_g_min2 = document.getElementById('myColor_g_min2').value;
+			myColor_g_max2 = document.getElementById('myColor_g_max2').value;
+			myColor_b_min2 = document.getElementById('myColor_b_min2').value;
+			myColor_b_max2 = document.getElementById('myColor_b_max2').value;
+
+			myColor_r_min3 = document.getElementById('myColor_r_min3').value;
+			myColor_r_max3 = document.getElementById('myColor_r_max3').value;
+			myColor_g_min3 = document.getElementById('myColor_g_min3').value;
+			myColor_g_max3 = document.getElementById('myColor_g_max3').value;
+			myColor_b_min3 = document.getElementById('myColor_b_min3').value;
+			myColor_b_max3 = document.getElementById('myColor_b_max3').value;
+
+			result.innerHTML = "";
+
+			var imgData=context.getImageData(0,0,canvas.width,canvas.height);
+
+			for (var i=0;i<imgData.data.length;i+=4) {
+				var r=0;
+				var g=0;
+				var b=0;
+				if ((imgData.data[i]>=myColor_r_min1&&imgData.data[i]<=myColor_r_max1)&&(imgData.data[i+1]>=myColor_g_min1&&imgData.data[i+1]<=myColor_g_max1)&&(imgData.data[i+2]>=myColor_b_min1&&imgData.data[i+2]<=myColor_b_max1)) {
+				  r=255;
+				}
+				if ((imgData.data[i]>=myColor_r_min2&&imgData.data[i]<=myColor_r_max2)&&(imgData.data[i+1]>=myColor_g_min2&&imgData.data[i+1]<=myColor_g_max2)&&(imgData.data[i+2]>=myColor_b_min2&&imgData.data[i+2]<=myColor_b_max2)) {
+				  g=255;
+				}
+				if ((imgData.data[i]>=myColor_r_min3&&imgData.data[i]<=myColor_r_max3)&&(imgData.data[i+1]>=myColor_g_min3&&imgData.data[i+1]<=myColor_g_max3)&&(imgData.data[i+2]>=myColor_b_min3&&imgData.data[i+2]<=myColor_b_max3)) {
+				  b=255;
+				}
+
+				imgData.data[i]=r;
+				imgData.data[i+1]=g;
+				imgData.data[i+2]=b;
+				imgData.data[i+3]=255;
 			}
 
-			imgData.data[i]=r;
-			imgData.data[i+1]=g;
-			imgData.data[i+2]=b;
-			imgData.data[i+3]=255;
-		}
+			context_custom.putImageData(imgData,0,0);
 
-		context_custom.putImageData(imgData,0,0);
+			event.data.forEach(function(rect) {
+				if (mirrorimage.value==1) {
+					context.strokeStyle = rect.color;
+					context.strokeRect(obj.width-rect.x-rect.width, rect.y, rect.width, rect.height);
+					result.innerHTML+= rect.color+","+(obj.width-rect.x-rect.width)+","+rect.y+","+rect.width+","+rect.height+"<br>";
+				}
+				else {
+					context.strokeStyle = rect.color;
+					context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+					result.innerHTML+= rect.color+","+rect.x+","+rect.y+","+rect.width+","+rect.height+"<br>";
+				}
+			});
 
-		event.data.forEach(function(rect) {
-			if (mirrorimage.value==1) {
-				context.strokeStyle = rect.color;
-				context.strokeRect(obj.width-rect.x-rect.width, rect.y, rect.width, rect.height);
-				result.innerHTML+= rect.color+","+(obj.width-rect.x-rect.width)+","+rect.y+","+rect.width+","+rect.height+"<br>";
+			if (result.innerHTML!="") {
+				result.innerHTML = result.innerHTML.substr(0,result.innerHTML.length-4);
+				if (typeof trackingcolor_recognitionFinish === 'function') trackingcolor_recognitionFinish();
 			}
-			else {
-				context.strokeStyle = rect.color;
-				context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-				result.innerHTML+= rect.color+","+rect.x+","+rect.y+","+rect.width+","+rect.height+"<br>";
-			}
-		});
-
-		if (result.innerHTML!="") {
-			result.innerHTML = result.innerHTML.substr(0,result.innerHTML.length-4);
-			if (typeof trackingcolor_recognitionFinish === 'function') trackingcolor_recognitionFinish();
+		
 		}
 	});
 	
