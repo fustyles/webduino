31544f86a98cee6261f7fe50b84f8bff672baeeb
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-10 21:00:58 +0800

    Update en.js

--- SpBlocklyJS/teachablemachine_20201012/en.js
+++ SpBlocklyJS/teachablemachine_20201012/en.js
@@ -1,5 +1,5 @@
 Blockly.Msg["CATTEACHABLEMACHINE"] = "Teachable Machine";
-Blockly.Msg.TEACHABLEMACHINE_SHOW = "Teachable Machine (custom model)";
+Blockly.Msg.TEACHABLEMACHINE_SHOW = "Teachable Machine (google)";
 Blockly.Msg.TEACHABLEMACHINE_WIDTH_SHOW = "Width";
 Blockly.Msg.TEACHABLEMACHINE_HEIGHT_SHOW = "Height";
 Blockly.Msg.TEACHABLEMACHINE_RESULT_SHOW = "Result";
