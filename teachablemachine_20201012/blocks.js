e0ced44d3522616e2ca4aaf094ca116c6eb929a2
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-10 20:13:51 +0800

    Add files via upload

---
+++ teachablemachine_20201012/blocks.js
@@ -0,0 +1,190 @@
+Blockly.Blocks['teachablemachine_video'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_RESULT_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["Y","block"],
+		["N","none"]	  
+  ]), "result_"); 	  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_MIRRORIMAGE_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["Y","1"],
+		["N","0"]	  
+  ]), "mirrorimage_"); 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_OPACITY_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["1","1"],
+		["0.9","0.9"],
+		["0.8","0.8"],
+		["0.7","0.7"],
+		["0.6","0.6"],	  
+		["0.5","0.5"],
+		["0.4","0.4"],
+		["0.3","0.3"],
+		["0.2","0.2"],
+		["0.1","0.1"],	
+		["0","0"]	   
+  ]), "opacity_"); 	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_model'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);	  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_PROJECT_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["image","image"],
+		["pose","pose"]	  
+  ]), "project_"); 	  
+  this.appendValueInput("model_")
+      .setCheck("String")
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_MODEL_SHOW);  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_result'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);		  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_PROPORTION_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+	  [Blockly.Msg.TEACHABLEMACHINE_MAXCLASS_SHOW,"maxClass"], 
+	  [Blockly.Msg.TEACHABLEMACHINE_MAXPROBABILITY_SHOW,"maxProbability"],
+	  [Blockly.Msg.TEACHABLEMACHINE_DATA_ARRAY_SHOW,""]
+	  ]), "result_");
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(65);
+  }
+};
+
+Blockly.Blocks['teachablemachine_state'] = {
+  init: function() { 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_STATE_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+        ["Y","1"],
+        ["N","0"]
+  ]), "state_");   
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_video_position'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);	  
+  this.appendValueInput("left_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_VIDEO_LEFT_SHOW);  
+  this.appendValueInput("top_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_VIDEO_TOP_SHOW);   	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_startvideo_media'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW); 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_FACING_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+        [Blockly.Msg.TEACHABLEMACHINE_FACING_FRONT_SHOW,"front"],
+        [Blockly.Msg.TEACHABLEMACHINE_FACING_BACK_SHOW,"back"]
+  ]), "facing_");  
+  this.appendValueInput("index_")
+      .setCheck("Number");  
+  this.appendValueInput("width_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_WIDTH_SHOW);  
+  this.appendValueInput("height_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_HEIGHT_SHOW);	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_startvideo_stream'] = {
+  init: function() { 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);    
+  this.appendValueInput("src_")
+      .setCheck("String")
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SRC_SHOW);	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_pause'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);	  
+  this.appendDummyInput()  
+	  .appendField(Blockly.Msg.TEACHABLEMACHINE_PAUSE_SHOW);
+  this.appendValueInput("time_")
+	  .setCheck("Number")    
+	  .appendField("(ms)"); 	  
+    this.setInputsInline(true);    
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_clear'] = {
+  init: function() { 
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_CLEAR_SHOW); 
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
+
+Blockly.Blocks['teachablemachine_recognitied'] = {
+  init: function () {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_SHOW);	  
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.TEACHABLEMACHINE_RECOGNITIED_SHOW);
+  this.appendStatementInput("do_");
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(60);
+  }
+};
---
+++ teachablemachine_20201012/javascript.js
@@ -0,0 +1,65 @@
+Blockly.JavaScript['teachablemachine_video'] = function(block) { 
+  var value_result_ = block.getFieldValue('result_');
+  var value_mirrorimage_ = block.getFieldValue('mirrorimage_');
+  var value_opacity_ = block.getFieldValue('opacity_');
+  var code = 'teachablemachine_video("' + value_result_ + '","' + value_mirrorimage_ + '","' + value_opacity_ + '");\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_model'] = function(block) {
+  var value_project_ = block.getFieldValue('project_');
+  var value_model_ = Blockly.JavaScript.valueToCode(block, 'model_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'teachablemachine_model("' + value_project_ + '",' + value_model_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_result'] = function(block) { 
+  var value_result_ = block.getFieldValue('result_');
+  var code = 'teachablemachine_result("' + value_result_ + '")';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['teachablemachine_state'] = function(block) {
+  var value_state_ = block.getFieldValue('state_');
+  var code = 'teachablemachine_state(' + value_state_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_video_position'] = function(block) { 
+  var value_left_ = Blockly.JavaScript.valueToCode(block, 'left_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_top_ = Blockly.JavaScript.valueToCode(block, 'top_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'teachablemachine_video_position(' + value_left_ + ',' + value_top_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_startvideo_media'] = function(block) { 
+  var value_width_ = Blockly.JavaScript.valueToCode(block, 'width_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_height_ = Blockly.JavaScript.valueToCode(block, 'height_', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_facing_ = block.getFieldValue('facing_');
+  var value_index_ = Blockly.JavaScript.valueToCode(block, 'index_', Blockly.JavaScript.ORDER_ATOMIC);  
+  var code = 'teachablemachine_startvideo_media(' + value_width_ + ',' + value_height_ + ',"' + value_facing_ + '",' + value_index_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_startvideo_stream'] = function(block) { 
+  var value_src_ = Blockly.JavaScript.valueToCode(block, 'src_', Blockly.JavaScript.ORDER_ATOMIC); 
+  var code = 'teachablemachine_startvideo_stream(' + value_src_ + ');\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_pause'] = function(block) { 
+  var value_time = Blockly.JavaScript.valueToCode(block, 'time_', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = 'teachablemachine_state(0);setTimeout(function(){teachablemachine_state(1);}, '+value_time+');\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_clear'] = function(block) { 
+  var code = 'teachablemachine_clear();\n';
+  return code;
+};
+
+Blockly.JavaScript['teachablemachine_recognitied'] = function(block) { 
+  var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
+  var code = 'teachablemachine_recognitionFinish = async function() {\nteachablemachine_state(0);\n' + statements_do + '\nteachablemachine_state(1);};\n';
+  return code;
+};
