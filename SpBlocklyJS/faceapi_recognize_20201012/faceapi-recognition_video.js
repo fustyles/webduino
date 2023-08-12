3079667927d8fcb44668576cd7d00940ec312349
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-12 18:57:23 +0800

    Add files via upload

--- SpBlocklyJS/faceapi_recognize_20201012/faceapi-recognition_video.js
+++ SpBlocklyJS/faceapi_recognize_20201012/faceapi-recognition_video.js
@@ -1,4 +1,4 @@
-document.write('<div id="region_faceapirecognize" style="z-index:999;position:absolute"><video id="gamevideo_faceapirecognize" style="position:absolute;z-index:998;" preload autoplay loop muted></video><img id="gameimage_faceapirecognize" style="position:absolute;z-index:998;" crossorigin="anonymous"><canvas id="gamecanvas_faceapirecognize" style="z-index:999;"></canvas><br><br><div id="gamediv_faceapirecognize" style="color:red;position:absolute; style="z-index:997;"></div></div>');
+document.write('<div id="region_faceapirecognize" style="z-index:999;position:absolute"><video id="gamevideo_faceapirecognize" style="position:absolute;z-index:998;" preload autoplay loop muted></video><img id="gameimage_faceapirecognize" style="position:absolute;z-index:998;" crossorigin="anonymous"><canvas id="gamecanvas_faceapirecognize" style="z-index:999;position:absolute;"></canvas><br><br><div id="gamediv_faceapirecognize" style="color:red;position:absolute; style="z-index:997;"></div></div>');
 document.write('<div id="faceapirecognizeState" style="position:absolute;display:none;">1</div>');
 document.write('<div id="sourceId_faceapirecognize" style="position:absolute;display:none;"></div>');
 document.write('<div id="size_faceapirecognize" style="position:absolute;display:none;"></div>');
@@ -71,7 +71,10 @@ async function DetectVideo(obj) {
 	const resizedDetections = faceapi.resizeResults(detections, JSON.parse(size.innerHTML));
 
 	const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
+
 	message.innerHTML = "";
+	setTimeout(function(){canvas.style.display = "none";}, myTimer*1000);
+	
 	if (results.length>0) {
 		var res = "";
 		for (var i=0;i<results.length;i++) {
@@ -80,21 +83,21 @@ async function DetectVideo(obj) {
 		message.innerHTML = res;
 		if (message.innerHTML!="")
 			message.innerHTML = message.innerHTML.substr(0,message.innerHTML.length-4);
+		
+		results.forEach((result, i) => {
+			const box = resizedDetections[i].detection.box
+			var drawBox;
+			if (result.distance<=distanceLimit)
+				drawBox = new faceapi.draw.DrawBox(box, { label: result.toString()})
+			else
+				drawBox = new faceapi.draw.DrawBox(box, { label: (Math.round(result.distance*100)/100).toString()})
+			drawBox.draw(canvas);
+		})	
+		
 		if (typeof faceapirecognize_recognitionFinish === 'function') faceapirecognize_recognitionFinish();
 	}
 	else
 		if (typeof faceapirecognize_unrecognitionFinish === 'function') faceapirecognize_unrecognitionFinish();
-
-	results.forEach((result, i) => {
-		const box = resizedDetections[i].detection.box
-		var drawBox;
-		if (result.distance<=distanceLimit)
-			drawBox = new faceapi.draw.DrawBox(box, { label: result.toString()})
-		else
-			drawBox = new faceapi.draw.DrawBox(box, { label: (Math.round(result.distance*100)/100).toString()})
-		drawBox.draw(canvas);
-	})
-	setTimeout('canvas.style.display = "none";', myTimer*1000);
 }
 
 function loadLabeledImages() {
