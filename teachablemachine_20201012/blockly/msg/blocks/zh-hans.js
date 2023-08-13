10dc084014a7183ec6afc48fc8ecb1a47e0a0e47
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-13 15:12:22 +0800

    Update zh-hans.js

--- teachablemachine_20201012/blockly/msg/blocks/zh-hans.js
+++ teachablemachine_20201012/blockly/msg/blocks/zh-hans.js
@@ -1,4 +1,4 @@
-Blockly.Msg.TEACHABLEMACHINE_SHOW = "机器学习(Teachable Machine)";
+Blockly.Msg.TEACHABLEMACHINE_SHOW = "机器学习(google)";
 Blockly.Msg.TEACHABLEMACHINE_WIDTH_SHOW = "宽度";
 Blockly.Msg.TEACHABLEMACHINE_HEIGHT_SHOW = "高度";
 Blockly.Msg.TEACHABLEMACHINE_RESULT_SHOW = "结果";
