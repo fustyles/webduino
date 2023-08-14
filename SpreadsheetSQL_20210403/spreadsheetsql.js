b54cd61233ae3c902bfb335fe0432a0cd2a04427
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-14 23:28:43 +0800

    Update spreadsheetsql.js

--- SpreadsheetSQL_20210403/spreadsheetsql.js
+++ SpreadsheetSQL_20210403/spreadsheetsql.js
@@ -7,22 +7,22 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
   'use strict';
   
   var spreadsheetsql_response = [];
-  var spreadsheet_id = "";
-  var spreadsheet_name = "";
+  var spreadsheetsql_head_response = [];
+  var spreadsheetsql_id = "";
+  var spreadsheetsql_name = "";
   
   function spreadsheetsql_settings(spreadsheetid, spreadsheetname) {
-    spreadsheet_id = spreadsheetid;
-    spreadsheet_name = spreadsheetname;
+    spreadsheetsql_id = spreadsheetid;
+    spreadsheetsql_name = spreadsheetname;
   }
   
   function spreadsheetsql_executeSql(spreadsheet_sql) {
-	spreadsheetsql_response = [];
 	//google.load("visualization", "1", {packages:["corechart"]});
-    	//var input_url="https://docs.google.com/spreadsheets/d/"+spreadsheet_id+"/gviz/tq?tqx=out:json&sheet="+spreadsheet_name+"&tq="+spreadsheet_sql;
-    	google.load('visualization', '1', {
+    //var input_url="https://docs.google.com/spreadsheets/d/"+spreadsheetsql_id+"/gviz/tq?tqx=out:json&sheet="+spreadsheetsql_name+"&tq="+spreadsheet_sql;
+    google.load('visualization', '1', {
 	  packages: ['corechart'],
 	  callback: function () {
-		var query = new google.visualization.Query("https://docs.google.com/spreadsheets/d/"+spreadsheet_id+"/gviz/tq?tqx=out:json&sheet="+spreadsheet_name);
+		var query = new google.visualization.Query("https://docs.google.com/spreadsheets/d/"+spreadsheetsql_id+"/gviz/tq?tqx=out:json&sheet="+spreadsheetsql_name);
 		//console.log(query);
 		query.setQuery(spreadsheet_sql);
 		query.send(spreadsheetsql_QueryResponse);
@@ -33,7 +33,10 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
   function spreadsheetsql_QueryResponse(res) {
 	  //console.log(res.qb.fg);
 	  //console.log(res.qb.fg.length);
-	  var arr = [];  
+	  //console.log(res);
+	  spreadsheetsql_response = [];
+	  spreadsheetsql_head_response = [];
+	  var arr = []; 
 	  if (res.aY=="error") {
 		  if (typeof spreadsheetsql_getDataFinish === 'function') spreadsheetsql_getDataFinish();
 		  return;
@@ -52,8 +55,11 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 		  for (var i=0;i<res.qb.fg.length;i++) {
 			  for (var j=0;j<res.qb.fg[i].c.length;j++) {
 				if (res.qb.fg[i].c[j]) {
-					if (res.qb.fg[i].c[j].v)
-						arr.push((res.qb.fg[i].c[j].f)?res.qb.fg[i].c[j].f:res.qb.fg[i].c[j].v);
+					if (res.qb.fg[i].c[j].v!==undefined) {
+						arr.push((res.qb.fg[i].c[j].f!==undefined)?res.qb.fg[i].c[j].f:res.qb.fg[i].c[j].v);
+					}
+					else
+						arr.push("");
 				}
 				else
 					arr.push("");
@@ -70,9 +76,60 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 	  return spreadsheetsql_response;
   }
   
-  function spreadsheetsql_getDataCount() {
-	  return spreadsheetsql_response.length;
-  }
+  function spreadsheetsql_getCell(col, row) {
+	  if (row<=spreadsheetsql_response.length) {
+		  if (col<=spreadsheetsql_response[0].length) {
+			return spreadsheetsql_response[row-1][col-1];
+		  }
+	  }
+	  return "";
+  }  
+  
+  function spreadsheetsql_getDataTable(input_id, input_width, input_fontsize, input_left, input_top, input_column) {
+	if (document.getElementById("gametable_"+input_id))
+		document.getElementById("gametable_"+input_id).parentNode.removeChild(document.getElementById("gametable_"+input_id));
+		
+	if (spreadsheetsql_response.length>0) {
+		var obj = document.createElement('table');
+		obj.id = "gametable_"+input_id;
+		obj.style.position = "absolute";
+		obj.style.width = input_width + 'px';
+		obj.style.left = input_left + 'px';
+		obj.style.top = input_top + 'px';
+		obj.style.fontSize = input_fontsize + "px";
+		obj.style.border = "1px solid black";
+		obj.style.borderCollapse = "collapse";
+		obj.style.zIndex = 9999;
+		
+		var innerData = "";
+	  
+		var k = 0;
+		if (input_column) {
+			for (var i=0;i<spreadsheetsql_head_response.length;i++) {
+				k++;
+				innerData += "<tr style='border:1px solid black;border-collapse: collapse;'>";
+				for (var j=0;j<spreadsheetsql_head_response[i].length;j++) {
+					innerData += "<td id='gametable_td_"+input_id+"_"+i+"_"+j+"' style='border:1px solid black;border-collapse: collapse;' onclick='javascript:onclickid_set(this);'>"+spreadsheetsql_head_response[i][j]+"</td>";
+				}
+				innerData += "</tr>";
+			}
+		}
+		for (var i=0;i<spreadsheetsql_response.length;i++) {
+			innerData += "<tr style='border:1px solid black;border-collapse: collapse;'>";
+			for (var j=0;j<spreadsheetsql_response[i].length;j++) {
+				innerData += "<td id='gametable_td_"+input_id+"_"+(i+k)+"_"+j+"' style='border:1px solid black;border-collapse: collapse;' onclick='javascript:onclickid_set(this);'>"+spreadsheetsql_response[i][j]+"</td>";
+			}
+			innerData += "</tr>";
+		}
+		obj.innerHTML = innerData;
+		document.body.appendChild(obj);  
+	}
+  }  
+  
+  function spreadsheetsql_clearDataTable(input_id) {
+	if (document.getElementById("gametable_"+input_id))
+		document.getElementById("gametable_"+input_id).parentNode.removeChild(document.getElementById("gametable_"+input_id));
+  }    
 
   function spreadsheetsql_getDataCount(count) {
 	if (count=="ROWS")
@@ -90,11 +147,34 @@ Author: Chung-Yi Fu (Kaohsiung, Taiwan)   https://www.facebook.com/francefu
 	  spreadsheetsql_response = [];
   }   
   
+  function spreadsheet_insert(func, myData, myRow, myCol, myText, mySpreadsheeturl, mySpreadsheetname, myScript) {
+	  myScript += "?func="+func+"&data="+myData+"&spreadsheeturl="+mySpreadsheeturl+"&spreadsheetname="+mySpreadsheetname+"&row="+myRow+"&col="+myCol+"&text="+myText;
+	  console.log(myScript);
+	  
+      $.ajax({			
+      	type: "GET",
+      	url: myScript,
+      	dataType: "text",
+		crossOrigin: true,
+      	contentType: "application/x-www-form-urlencoded; charset=utf-8",
+      	success: function (response) {
+      		//console.log(response.data);			
+      	},
+      	error: function (thrownError) {
+      		console.log(thrownError);
+      	}
+      });
+  }	  
+  
   window.spreadsheetsql_settings = spreadsheetsql_settings;
   window.spreadsheetsql_executeSql = spreadsheetsql_executeSql;
   window.spreadsheetsql_QueryResponse = spreadsheetsql_QueryResponse;
   window.spreadsheetsql_getData = spreadsheetsql_getData;
+  window.spreadsheetsql_getCell = spreadsheetsql_getCell;
+  window.spreadsheetsql_getDataTable = spreadsheetsql_getDataTable;
+  window.spreadsheetsql_clearDataTable = spreadsheetsql_clearDataTable;  
   window.spreadsheetsql_getDataCount = spreadsheetsql_getDataCount;
   window.spreadsheetsql_clearData = spreadsheetsql_clearData;
+  window.spreadsheet_insert = spreadsheet_insert;
   
 }(window, window.document));
