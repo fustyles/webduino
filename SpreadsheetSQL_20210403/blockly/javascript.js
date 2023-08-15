9733d160aa8215bd4bc1fc519088323c3ff2e3d9
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-15 18:04:05 +0800

    Update javascript.js

--- SpreadsheetSQL_20210403/blockly/javascript.js
+++ SpreadsheetSQL_20210403/blockly/javascript.js
@@ -82,3 +82,26 @@ Blockly.JavaScript['spreadsheetsql_getdatafinish'] = function(block) {
   var code = 'spreadsheetsql_getDataFinish = function() {\n' + statements_do + '\n};\n';
   return code;
 };
+
+Blockly.JavaScript['controls_spreadsheet'] = function(block){
+	var spreadsheeturl = Blockly.JavaScript.valueToCode(block,"spreadsheeturl",Blockly.JavaScript.ORDER_NONE)||"";
+	var spreadsheetname = Blockly.JavaScript.valueToCode(block,"spreadsheetname",Blockly.JavaScript.ORDER_NONE)||"";	
+	var func = block.getFieldValue('func');
+	var data = Blockly.JavaScript.valueToCode(block,"VALUE",Blockly.JavaScript.ORDER_NONE)||"";
+	data = 'String('+data+')+"|"';
+	for (var i=0;i<26;i++) {
+		var text = Blockly.JavaScript.valueToCode(block,String.fromCharCode(i+65),Blockly.JavaScript.ORDER_NONE);
+		if (block.getInput(String.fromCharCode(i+65)))
+			data += '+String('+text+')+"|"';
+	}
+	data = data.substring(0, data.length-4);
+			
+	var code = 'spreadsheet_insert("' + func + '", ' + data + ', 0, 0, "", ' + spreadsheeturl + ', ' + spreadsheetname + ', ' +  '"https://script.google.com/macros/s/AKfycbxA3hhTlntwVTOcqngOC_iJL_zLmRwzcDbMYDs7FD8iinNsY9XZsMkD7AcXTIUbEc33EA/exec");\n';
+	return code;
+};
+
+Blockly.JavaScript['controls_spreadsheet_datetime'] = function(block){
+	var datetime = block.getFieldValue('datetime');	
+	var code = '"'+datetime+'"';
+	return [code,Blockly.JavaScript.ORDER_ATOMIC];
+};
