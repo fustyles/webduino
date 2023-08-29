bb340f266229c705a573d3c2bb4eafe826b9745d
fustyles <34397289+fustyles@users.noreply.github.com>
2023-08-29 11:40:49 +0800

    Update javascript.js

--- GameElements_20190131/blockly/javascript.js
+++ GameElements_20190131/blockly/javascript.js
@@ -2203,6 +2203,15 @@ Blockly.JavaScript['code_text'] = function (block) {
   return code;
 };
 
+Blockly.JavaScript['code_text_input'] = function (block) {
+  var code = Blockly.JavaScript.valueToCode(block, 'code', Blockly.JavaScript.ORDER_ATOMIC)||"";
+  if ((code.indexOf("'")==0)&&(code.lastIndexOf("'")==code.length-1))
+    code = code.substring(1,code.length-1);
+  if ((code.indexOf('"')==0)&&(code.lastIndexOf('"')==code.length-1))
+    code = code.substring(1,code.length-1);	
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
 Blockly.JavaScript['window_messagebox'] = function (block) {
   var message = Blockly.JavaScript.valueToCode(block, 'message', Blockly.JavaScript.ORDER_ATOMIC); 
   var code = 'alert('+message+');\n';  
