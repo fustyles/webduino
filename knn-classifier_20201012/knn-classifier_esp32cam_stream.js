6aaf70f4cbcacfc7018f1f6ec56aafaafcc7ed8f
fustyles <34397289+fustyles@users.noreply.github.com>
2023-09-26 22:06:49 +0800

    Update knn-classifier_esp32cam_stream.js

--- knn-classifier_20201012/knn-classifier_esp32cam_stream.js
+++ knn-classifier_20201012/knn-classifier_esp32cam_stream.js
@@ -28,6 +28,7 @@ window.onload = function () {
 	var mobilenetModule;
 
 	ShowImage.src = document.location.origin+':81/?stream';
+	ShowImage.src = ShowImage.src.replace(":81:81",":81");
 	ShowImage.style.visibility = "visible";	
 
 	classifier = knnClassifier.create();
