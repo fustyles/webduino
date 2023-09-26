2ec58acec2a9481ed778552b66da7a4af2ea1f24
fustyles <34397289+fustyles@users.noreply.github.com>
2023-09-26 22:07:25 +0800

    Update coco-ssd_esp32cam_stream.js

--- coco-ssd_20201012/coco-ssd_esp32cam_stream.js
+++ coco-ssd_20201012/coco-ssd_esp32cam_stream.js
@@ -12,6 +12,7 @@ window.onload = function () {
 	var Model;
 
 	ShowImage.src = document.location.origin+':81/?stream';
+	ShowImage.src = ShowImage.src.replace(":81:81",":81");
 	ShowImage.style.visibility = "visible";
 	
 	cocoSsd.load().then(cocoSsd_Model => {
