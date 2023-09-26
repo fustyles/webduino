2865e77fba9c296408a4b504b988a31b1002f830
fustyles <34397289+fustyles@users.noreply.github.com>
2023-09-26 22:07:12 +0800

    Update tesseract_esp32cam_stream.js

--- tesseract.js_20201012/tesseract_esp32cam_stream.js
+++ tesseract.js_20201012/tesseract_esp32cam_stream.js
@@ -10,6 +10,7 @@ var mode = document.getElementById('mode_tesseract');
 var reference = document.getElementById('reference_tesseract');
 
 ShowImage.src = document.location.origin+':81/?stream';
+ShowImage.src = ShowImage.src.replace(":81:81",":81");
 
 function DetectVideo() {
 	if (canvas.style.visibility == 'visible') return;
