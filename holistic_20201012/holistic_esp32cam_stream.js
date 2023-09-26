570acd95df8861433ea455f158cc197adf4a8499
fustyles <34397289+fustyles@users.noreply.github.com>
2023-09-26 22:06:40 +0800

    Update holistic_esp32cam_stream.js

--- holistic_20201012/holistic_esp32cam_stream.js
+++ holistic_20201012/holistic_esp32cam_stream.js
@@ -20,6 +20,7 @@ window.onload = function () {
 	var result_righthand = document.getElementById("gamediv_righthand_holistic");
 	
 	ShowImage.src = document.location.origin+':81/?stream';
+	ShowImage.src = ShowImage.src.replace(":81:81",":81");
 	ShowImage.style.visibility = "visible";
 	setTimeout(function(){ShowImage.style.visibility = "hidden";start();},5000);
 	
