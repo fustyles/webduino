b99cb1672fe13a4e975990044152bd60c02d5a08
fustyles <34397289+fustyles@users.noreply.github.com>
2023-10-09 13:21:20 +0800

    Update speechrecognition.js

--- SpBlocklyJS/SpeechRecognition_20220729/speechrecognition.js
+++ SpBlocklyJS/SpeechRecognition_20220729/speechrecognition.js
@@ -54,10 +54,11 @@ if ('webkitSpeechRecognition' in window) {
 	  Recognition_final = linebreak(final_transcript);
 	  document.getElementById("gamespan_recognition").innerHTML = Recognition_final;
 	  if (typeof SpeechRecognition_interim === 'object' && SpeechRecognition_interim !== null) {
-		if (SpeechRecognition_interim.value)
-		  SpeechRecognition_interim.value = Recognition_final;
-		else
+		  console.log(SpeechRecognition_interim);
+		if (SpeechRecognition_interim.innerHTML)
 		  SpeechRecognition_interim.innerHTML = Recognition_final;
+		else
+		  SpeechRecognition_interim.value = Recognition_final;
 	  }
 	  if (typeof Recognition_recognitionFinish === 'function') Recognition_recognitionFinish();
     }
@@ -65,10 +66,11 @@ if ('webkitSpeechRecognition' in window) {
       	  Recognition_final = "";
 	  document.getElementById("gamespan_recognition").innerHTML = Recognition_interim;
 	  if (typeof SpeechRecognition_interim === 'object' && SpeechRecognition_interim !== null) {
-		if (SpeechRecognition_interim.value)
-		  SpeechRecognition_interim.value = Recognition_interim;
-		else
+		  console.log(SpeechRecognition_interim);
+		if (SpeechRecognition_interim.innerHTML)
 		  SpeechRecognition_interim.innerHTML = Recognition_interim;
+		else
+		  SpeechRecognition_interim.value = Recognition_interim;
 	  } 
     }
   };	
