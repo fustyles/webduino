d8d9a5d34970b36cc542923e753b23b0678a463b
fustyles <34397289+fustyles@users.noreply.github.com>
2023-10-09 15:40:50 +0800

    Update speechrecognition.js

--- SpBlocklyJS/SpeechRecognition_20220729/speechrecognition.js
+++ SpBlocklyJS/SpeechRecognition_20220729/speechrecognition.js
@@ -54,30 +54,28 @@ if ('webkitSpeechRecognition' in window) {
 	  Recognition_final = linebreak(final_transcript);
 	  document.getElementById("gamespan_recognition").innerHTML = Recognition_final;
 	  if (typeof SpeechRecognition_interim === 'object' && SpeechRecognition_interim !== null) {
-		if (SpeechRecognition_interim.textContent)
+		if (SpeechRecognition_interim.hasOwnProperty('value'))
+		  SpeechRecognition_interim.value = Recognition_final;		  
+		else if (SpeechRecognition_interim.hasOwnProperty('textContent'))
 		  SpeechRecognition_interim.textContent = Recognition_final;
-		else if (SpeechRecognition_interim.innerText)
+		else if (SpeechRecognition_interim.hasOwnProperty('innerText'))
 		  SpeechRecognition_interim.innerText = Recognition_final;			
-		else if (SpeechRecognition_interim.innerHTML)
+		else if (SpeechRecognition_interim.hasOwnProperty('innerHTML'))
 		  SpeechRecognition_interim.innerHTML = Recognition_final;
-		else
-		  SpeechRecognition_interim.value = Recognition_final;
 	  }
 	  if (typeof Recognition_recognitionFinish === 'function') Recognition_recognitionFinish();
     }
     else {
       	  Recognition_final = "";
 	  document.getElementById("gamespan_recognition").innerHTML = Recognition_interim;
-	  if (typeof SpeechRecognition_interim === 'object' && SpeechRecognition_interim !== null) {
-		if (SpeechRecognition_interim.textContent)
+		if (SpeechRecognition_interim.hasOwnProperty('value'))
+		  SpeechRecognition_interim.value = Recognition_interim;		  
+		else if (SpeechRecognition_interim.hasOwnProperty('textContent'))
 		  SpeechRecognition_interim.textContent = Recognition_interim;
-		else if (SpeechRecognition_interim.innerText)
+		else if (SpeechRecognition_interim.hasOwnProperty('innerText'))
 		  SpeechRecognition_interim.innerText = Recognition_interim;			
-		else if (SpeechRecognition_interim.innerHTML)
+		else if (SpeechRecognition_interim.hasOwnProperty('innerHTML'))
 		  SpeechRecognition_interim.innerHTML = Recognition_interim;
-		else
-		  SpeechRecognition_interim.value = Recognition_interim;
-	  } 
     }
   };	
 }
