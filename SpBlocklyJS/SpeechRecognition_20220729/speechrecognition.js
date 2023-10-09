022fa161a28a11b33a3468c5857a4afe801326de
fustyles <34397289+fustyles@users.noreply.github.com>
2023-10-09 12:48:17 +0800

    Update speechrecognition.js

--- SpBlocklyJS/SpeechRecognition_20220729/speechrecognition.js
+++ SpBlocklyJS/SpeechRecognition_20220729/speechrecognition.js
@@ -54,14 +54,14 @@ if ('webkitSpeechRecognition' in window) {
 	  Recognition_final = linebreak(final_transcript);
 	  document.getElementById("gamespan_recognition").innerHTML = Recognition_final;
 	  if (typeof SpeechRecognition_interim === 'object' && SpeechRecognition_interim !== null)
-		  SpeechRecognition_interim.innerHTML = Recognition_final;	    
+		  SpeechRecognition_interim.value = Recognition_final;	    
 	  if (typeof Recognition_recognitionFinish === 'function') Recognition_recognitionFinish();
     }
     else {
       	  Recognition_final = "";
 	  document.getElementById("gamespan_recognition").innerHTML = Recognition_interim;
 	  if (typeof SpeechRecognition_interim === 'object' && SpeechRecognition_interim !== null)
-		  SpeechRecognition_interim.innerHTML = Recognition_interim;	    
+		  SpeechRecognition_interim.value = Recognition_interim;	    
     }
   };	
 }
