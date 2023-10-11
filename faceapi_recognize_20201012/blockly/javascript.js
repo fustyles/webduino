85fcf9609948d2a924d87bed1dcc5319bfe4d003
fustyles <34397289+fustyles@users.noreply.github.com>
2023-10-11 20:39:43 +0800

    Update javascript.js

--- faceapi_recognize_20201012/blockly/javascript.js
+++ faceapi_recognize_20201012/blockly/javascript.js
@@ -34,6 +34,17 @@ Blockly.JavaScript['faceapirecognize_video_capture'] = function(block) {
   return code;
 };
 
+Blockly.JavaScript['faceapirecognize_video_base64'] = function(block) {
+  var value_facelabel = Blockly.JavaScript.valueToCode(block, 'facelabel', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_result = block.getFieldValue('result_');
+  var value_opacity = block.getFieldValue('opacity_');  
+  var value_timer = Blockly.JavaScript.valueToCode(block, 'timer', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_distancelimit = Blockly.JavaScript.valueToCode(block, 'distancelimit', Blockly.JavaScript.ORDER_ATOMIC);
+
+  var code = 'faceapirecognize_video("' + value_result + '","' + value_opacity + '",' + value_timer + ',"" ,'+value_facelabel+' ,"",' + value_distancelimit + ');\n';
+  return code;
+};
+
 Blockly.JavaScript['faceapirecognize_detect'] = function(block) { 
   var code = 'faceapirecognize_detect();\n';
   return code;
