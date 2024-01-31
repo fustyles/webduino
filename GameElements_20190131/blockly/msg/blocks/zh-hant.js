b219adcfbb9b08a97f7b1967c5c3a4e9b89dc8b8
fustyles <34397289+fustyles@users.noreply.github.com>
2024-01-31 21:44:58 +0800

    Update zh-hant.js

--- GameElements_20190131/blockly/msg/blocks/zh-hant.js
+++ GameElements_20190131/blockly/msg/blocks/zh-hant.js
@@ -616,3 +616,7 @@ Blockly.Msg["DECODEURI"] = "網址解碼";
 Blockly.Msg["GET_KEYCODE_SHOW"] = "取得鍵碼值";
 Blockly.Msg["KEYCODE_SHOW"] = "取得鍵碼值為";
 Blockly.Msg["HOTKEY_SHOW"] = "取得組合鍵碼值為";
+
+Blockly.Msg["GOOGLEMAP_EMBED"] = "Google地圖嵌入程式碼";
+Blockly.Msg["GOOGLEMAP_EMBED_KEY"] = "金鑰";
+Blockly.Msg["GOOGLEMAP_EMBED_QUERY"] = "查詢條件";
