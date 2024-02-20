e023a3a3ee639061406459cca23fd28cb2e6714c
fustyles <34397289+fustyles@users.noreply.github.com>
2024-02-20 22:08:36 +0800

    Update gemini.js

--- SpBlocklyJS/gemini_2024029/gemini.js
+++ SpBlocklyJS/gemini_2024029/gemini.js
@@ -18,7 +18,7 @@ function gemini_text_initial(input_key, input_model) {
 		var gemini_mod = document.createElement("script");
 		gemini_mod.type = "module";
 		gemini_mod.textContent = 'import { GoogleGenerativeAI } from "@google/generative-ai";\nconst genAI = new GoogleGenerativeAI("'+input_key+'");\nasync function gemini_run(prompt) {\nawait genAI.getGenerativeModel({ model: "'+input_model+'"}).generateContent(prompt).then(function(result) {\nconst response = result.response;\nconst text = response.text();\nif (typeof gemini_text_respsonse === "function") gemini_text_respsonse(text);\n});\n}\nwindow.gemini_run = gemini_run;\n';
-		document.getElementsByTagName('head')[0].append(gemini_map);
+		document.body.appendChild(gemini_mod);
 } 
 
 function gemini_text_respsonse_br(data, newline) {
