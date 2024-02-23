4f20d9cf821baf1be81b127085b58ed9e2fc8a98
fustyles <34397289+fustyles@users.noreply.github.com>
2024-02-23 23:40:23 +0800

    Update faceapi-recognition_esp32cam.js

--- faceapi_recognize_20201012/faceapi-recognition_esp32cam.js
+++ faceapi_recognize_20201012/faceapi-recognition_esp32cam.js
@@ -106,7 +106,7 @@ window.onload = function () {
 
 		const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor));
 		message.innerHTML = "";
-		setTimeout(function(){canvas.style.display = "none";}, showtime*1000);
+		
 		if (results.length>0) {
 			var res = "";
 			for (var i=0;i<results.length;i++) {
@@ -131,7 +131,7 @@ window.onload = function () {
 		if (typeof recognitionFinish === 'function') recognitionFinish();
 		sourceId.innerHTML = "image";
 		
-		setTimeout(function(){start();}, 150);
+		setTimeout(function(){canvas.style.display = "none";start();}, showtime*1000);
 	}  
 	
 	function loadLabeledImages() {
