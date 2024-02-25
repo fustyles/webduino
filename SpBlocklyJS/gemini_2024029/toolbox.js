f4ec52a86da7075fa77d73f4f65f83f8ba35620a
fustyles <34397289+fustyles@users.noreply.github.com>
2024-02-25 12:16:44 +0800

    Update toolbox.js

--- SpBlocklyJS/gemini_2024029/toolbox.js
+++ SpBlocklyJS/gemini_2024029/toolbox.js
@@ -13,7 +13,7 @@ var catGEMINI = '<xml>'+
 '    </value>'+
 '    <value name="role">'+
 '      <block type="text">'+
-'        <field name="TEXT">妳是繁體中文的助理，請簡短回覆我的詢問。</field>'+
+'        <field name="TEXT">你是繁體中文的助理，請簡短回覆我的問題。</field>'+
 '      </block>'+
 '    </value>'+  
 '  </block>'+
