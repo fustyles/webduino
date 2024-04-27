5aba0992ba026c7e6662d10578df57d93d852a9e
fustyles <34397289+fustyles@users.noreply.github.com>
2024-04-27 16:35:54 +0800

    Update zh-hant.js

--- SpBlocklyJS/GameElements_20190131/zh-hant.js
+++ SpBlocklyJS/GameElements_20190131/zh-hant.js
@@ -639,7 +639,6 @@ Blockly.Msg["DECODEURI"] = "網址解碼";
 Blockly.Msg["CATPAGEELEMENTS_JSON"] = "Json";
 Blockly.Msg["JSON_VARIABLE_SET"] = "設定為";
 Blockly.Msg["JSON_TYPE_STRING"] = "轉成JSON格式";
-Blockly.Msg["JSON_TYPE_JSON"] = "原為JSON格式";
 Blockly.Msg["JSON_VARIABLE_GET"] = "取得JSON資料";
 Blockly.Msg["JSON_COLUMN"] = "參數名稱";
 Blockly.Msg["JSON_INDEX"] = "索引";
