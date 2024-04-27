6c6a6f70ce313042d57d6da96a2543b451cdbf62
fustyles <34397289+fustyles@users.noreply.github.com>
2024-04-27 16:19:55 +0800

    Update zh-hans.js

--- SpBlocklyJS/GameElements_20190131/zh-hans.js
+++ SpBlocklyJS/GameElements_20190131/zh-hans.js
@@ -641,6 +641,6 @@ Blockly.Msg["JSON_VARIABLE_SET"] = "设定为";
 Blockly.Msg["JSON_TYPE_STRING"] = "转成JSON格式";
 Blockly.Msg["JSON_TYPE_JSON"] = "原为JSON格式";
 Blockly.Msg["JSON_VARIABLE_GET"] = "取得";
-Blockly.Msg["JSON_COLUMN"] = "栏位名称";
+Blockly.Msg["JSON_COLUMN"] = "参数名称";
 Blockly.Msg["JSON_INDEX"] = "索引";
 Blockly.Msg["JSON_VARIABLE_STRING"] = "JSON格式转文字";
