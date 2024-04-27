62814b96d6ca153c12f8065eb3ab8f8cf810d849
fustyles <34397289+fustyles@users.noreply.github.com>
2024-04-27 16:42:44 +0800

    Update zh-hans.js

--- GameElements_20190131/blockly/msg/blocks/zh-hans.js
+++ GameElements_20190131/blockly/msg/blocks/zh-hans.js
@@ -623,8 +623,7 @@ Blockly.Msg["GOOGLEMAP_EMBED_QUERY"] = "查询条件";
 
 Blockly.Msg["CATPAGEELEMENTS_JSON"] = "Json";
 Blockly.Msg["JSON_VARIABLE_SET"] = "设定为";
-Blockly.Msg["JSON_TYPE_STRING"] = "转成JSON格式";
-Blockly.Msg["JSON_TYPE_JSON"] = "原为JSON格式";
+Blockly.Msg["JSON_TYPE_STRING"] = "文字格式转成JSON格式";
 Blockly.Msg["JSON_VARIABLE_GET"] = "获取JSON数据";
 Blockly.Msg["JSON_COLUMN"] = "参数名称";
 Blockly.Msg["JSON_INDEX"] = "索引";
