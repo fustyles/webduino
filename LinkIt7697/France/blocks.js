58600021f65537be32869fa5839f679cb5566e99
fustyles <34397289+fustyles@users.noreply.github.com>
2024-05-02 23:40:48 +0800

    Add files via upload

--- LinkIt7697/France/blocks.js
+++ LinkIt7697/France/blocks.js
@@ -295,14 +295,18 @@ Blockly.Blocks['amb82_mini_objectdetection_rtsp'] = {
 			.appendField(new Blockly.FieldDropdown([
  				["YOLOv4","DEFAULT_YOLOV4TINY"],
  				["YOLOv7","DEFAULT_YOLOV7TINY"],
- 				["YOLOv3","DEFAULT_YOLOV3TINY"],				
- 				["SCRFD","DEFAULT_SCRFD"],
- 				["MobileFaceNet","DEFAULT_MOBILEFACENET"],
- 				["YOLOv4-SD", "CUSTOMIZED_YOLOV4TINY"],
- 				["YOLOv7-SD", "CUSTOMIZED_YOLOV7TINY"],
- 				["YOLOv3-SD", "CUSTOMIZED_YOLOV3TINY"],				
- 				["SCRFD-SD", "CUSTOMIZED_SCRFD"],
- 				["MobileFaceNet-SD", "CUSTOMIZED_MOBILEFACENET"]
+ 				["YOLOv3(X)","DEFAULT_YOLOV3TINY"],				
+ 				["SCRFD(X)","DEFAULT_SCRFD"],
+ 				["MobileFaceNet(X)","DEFAULT_MOBILEFACENET"],
+ 				["YAMNET(X)","DEFAULT_YAMNET"],
+ 				["IMGCLASS(X)","DEFAULT_IMGCLASS"],
+ 				["YOLOv4_SD", "CUSTOMIZED_YOLOV4TINY"],
+ 				["YOLOv7_SD", "CUSTOMIZED_YOLOV7TINY"],
+ 				["YOLOv3_SD", "CUSTOMIZED_YOLOV3TINY"],				
+ 				["SCRFD_SD", "CUSTOMIZED_SCRFD"],
+ 				["MobileFaceNet_SD", "CUSTOMIZED_MOBILEFACENET"],
+ 				["YAMNET_SD","CUSTOMIZED_YAMNET"],
+ 				["IMGCLASS_SD","CUSTOMIZED_IMGCLASS"]
 		]), "model");
     this.appendDummyInput()	 
 		.setAlign(Blockly.ALIGN_RIGHT)
