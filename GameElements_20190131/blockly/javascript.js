21d4427990482037eab64bf62003bfa55aa5a8ea
fustyles <34397289+fustyles@users.noreply.github.com>
2024-08-18 00:17:32 +0800

    Update javascript.js

--- GameElements_20190131/blockly/javascript.js
+++ GameElements_20190131/blockly/javascript.js
@@ -1,14 +1,12 @@
 Blockly.JavaScript['javascript_function_general'] = function(block) {
 	var p1 = Blockly.JavaScript.valueToCode(block, 'p1', Blockly.JavaScript.ORDER_ATOMIC)||"";
-	var p2 = Blockly.JavaScript.valueToCode(block, 'p2', Blockly.JavaScript.ORDER_ATOMIC)||"";
-	var code = block.getFieldValue('function').replace("%1", p1).replace("%2", p2)+";\n";	
+	var code = block.getFieldValue('function').replace("%1", p1)+";\n";	
 	return code;
 };
 
 Blockly.JavaScript['javascript_function_general_get'] = function(block) {
 	var p1 = Blockly.JavaScript.valueToCode(block, 'p1', Blockly.JavaScript.ORDER_ATOMIC)||"";
-	var p2 = Blockly.JavaScript.valueToCode(block, 'p2', Blockly.JavaScript.ORDER_ATOMIC)||"";
-	var code = block.getFieldValue('function').replace("%1", p1).replace("%2", p2);	
+	var code = block.getFieldValue('function').replace("%1", p1);	
 	return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
