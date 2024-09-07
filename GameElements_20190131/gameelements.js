643170e08e71274cc49cdf8bbe17e9dc5cda84ee
fustyles <34397289+fustyles@users.noreply.github.com>
2024-09-07 20:14:39 +0800

    Update gameelements.js

--- GameElements_20190131/gameelements.js
+++ GameElements_20190131/gameelements.js
@@ -4359,9 +4359,9 @@ function HextoRgb(color) {
 	}
   }
   
-  function video_base64_spreadsheet(input_id, mySpreadsheet, mySpreadsheetName, myCellCol, myCellRow, myCellColHeight, myFileFormat, myScriptUrl) {
-    if (document.getElementById(input_id)) {	
-		var obj = document.getElementById(input_id);
+  function video_base64_spreadsheet(source_id, value_spreadsheetUrl, value_spreadsheetName, value_datetime, value_position, value_column, value_row, value_format, value_script) {
+    if (document.getElementById(source_id)) {	
+		var obj = document.getElementById(source_id);
 		var canvas = document.createElement('canvas');
 		canvas.id = 'tmp';
 		canvas.style.position = "absolute";
@@ -4372,11 +4372,7 @@ function HextoRgb(color) {
 		var context = canvas.getContext("2d");
 		try {
 			context.drawImage(obj,0,0,obj.width,obj.height);
-			var myFile = canvas.toDataURL('image/jpeg');
-			var head = myFile.substring(0, myFile.indexOf(",")+1);
-			var data = myFile.substring(myFile.indexOf(",")+1);
-			data = encodeURIComponent(data);
-			myFile = head+data;
+			var myFile = canvas.toDataURL();
 
 			$.ajax({
 				"type": "POST",
@@ -4384,15 +4380,20 @@ function HextoRgb(color) {
 				"headers": { 
 				  "Content-Type": "application/x-www-form-urlencoded"
 				},				
-				"url": myScriptUrl,
+				"url": value_script,
 				"data": {
-							"mySpreadsheet":mySpreadsheet,
-							"mySpreadsheetName":mySpreadsheetName,
-							"myCellCol":myCellCol,
-							"myCellRow":myCellRow,
-							"myCellColHeight":myCellColHeight,
-							"myFile":myFile,
-							"myFileFormat":myFileFormat					
+							"foldername": "",
+							"datetime": value_datetime,
+							"format": value_format,
+							"cellwidth": "",
+							"cellheight": "",
+							"position": value_position,
+							"row": value_row,
+							"column": value_column,
+							"spreadsheeturl": value_spreadsheetUrl,
+							"spreadsheetname": encodeURIComponent(value_spreadsheetName),
+							"file": myFile,
+							"linetoken": ""			
 						},
 				success: function(jsonp)
 				{
