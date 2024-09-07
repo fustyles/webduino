eb08e47bebd6d258c95df13babef719ef505a3c9
fustyles <34397289+fustyles@users.noreply.github.com>
2024-09-07 22:33:38 +0800

    Update gameelements.js

--- GameElements_20190131/gameelements.js
+++ GameElements_20190131/gameelements.js
@@ -4374,6 +4374,10 @@ function HextoRgb(color) {
 		try {
 			context.drawImage(obj,0,0,obj.width,obj.height);
 			var myFile = canvas.toDataURL('image/jpeg');			
+			var head = myFile.substring(0, myFile.indexOf(",")+1);
+			var data = myFile.substring(myFile.indexOf(",")+1);
+			data = encodeURIComponent(data);
+			myFile = head+data;
 			
 			$.ajax({
 				"type": "POST",
@@ -4427,7 +4431,11 @@ function HextoRgb(color) {
 		try {
 			context.drawImage(obj,0,0,obj.width,obj.height);
 			var myFile = canvas.toDataURL('image/jpeg');
-
+			var head = myFile.substring(0, myFile.indexOf(",")+1);
+			var data = myFile.substring(myFile.indexOf(",")+1);
+			data = encodeURIComponent(data);
+			myFile = head+data;
+			
 			$.ajax({
 				"type": "POST",
 				"dataType": "json",
@@ -4502,7 +4510,11 @@ function HextoRgb(color) {
 		try {
 			context.drawImage(obj,0,0,obj.width,obj.height);
 			var myFile = canvas.toDataURL('image/jpeg');	
-
+			var head = myFile.substring(0, myFile.indexOf(",")+1);
+			var data = myFile.substring(myFile.indexOf(",")+1);
+			data = encodeURIComponent(data);
+			myFile = head+data;
+			
 			$.ajax({
 				"type": "POST",
 				"dataType": "json",
