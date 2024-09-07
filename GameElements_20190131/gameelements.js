68188479348a7cfe3eded0fe9f2ab8878ba200e6
fustyles <34397289+fustyles@users.noreply.github.com>
2024-09-07 21:48:25 +0800

    Update gameelements.js

--- GameElements_20190131/gameelements.js
+++ GameElements_20190131/gameelements.js
@@ -4373,11 +4373,7 @@ function HextoRgb(color) {
 	    
 		try {
 			context.drawImage(obj,0,0,obj.width,obj.height);
-			var myFile = canvas.toDataURL('image/jpeg');
-			var head = myFile.substring(0, myFile.indexOf(",")+1);
-			var data = myFile.substring(myFile.indexOf(",")+1);
-			data = encodeURIComponent(data);
-			myFile = head+data;			
+			var myFile = canvas.toDataURL('image/jpeg');			
 			
 			$.ajax({
 				"type": "POST",
@@ -4505,11 +4501,7 @@ function HextoRgb(color) {
 		var context = canvas.getContext("2d");
 		try {
 			context.drawImage(obj,0,0,obj.width,obj.height);
-			var myFile = canvas.toDataURL('image/jpeg');
-			var head = myFile.substring(0, myFile.indexOf(",")+1);
-			var data = myFile.substring(myFile.indexOf(",")+1);
-			data = encodeURIComponent(data);
-			myFile = head+data;		
+			var myFile = canvas.toDataURL('image/jpeg');	
 
 			$.ajax({
 				"type": "POST",
