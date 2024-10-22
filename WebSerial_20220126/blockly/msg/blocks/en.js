4744898760283406c4a6cd5027c8c65e1fd18695
fustyles <34397289+fustyles@users.noreply.github.com>
2024-10-22 16:17:38 +0800

    Update en.js

--- WebSerial_20220126/blockly/msg/blocks/en.js
+++ WebSerial_20220126/blockly/msg/blocks/en.js
@@ -14,6 +14,6 @@ Blockly.Msg.WEBSERIAL_SENDTEXT_SHOW = "Web Serial send text";
 Blockly.Msg.WEBSERIAL_SENDUINT8_SHOW = "Web Serial send unit8Array";
 Blockly.Msg.WEBSERIAL_GETID_SHOW = "Web Serial get button ID";
 Blockly.Msg.WEBSERIAL_ELEMENT_ONCLICK_SHOW = "Web Serial set";
-Blockly.Msg.WEBSERIAL_ELEMENT_ID_SHOW = "Element ID";
+Blockly.Msg.WEBSERIAL_ELEMENT_ID_SHOW = "Element original ID";
 Blockly.Msg.WEBSERIAL_WAIT_SHOW = "Web Serial wait until connected";
 Blockly.Msg.WEBSERIAL_GETDATA_SHOW = "Web Serial after getting data";
