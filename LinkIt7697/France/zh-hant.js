b69a1e2e3a77c45100897c17f312b0ecf8b01d7d
fustyles <34397289+fustyles@users.noreply.github.com>
2024-10-31 15:17:44 +0800

    Add files via upload

--- LinkIt7697/France/zh-hant.js
+++ LinkIt7697/France/zh-hant.js
@@ -73,9 +73,9 @@ Blockly.Msg["AMB82_MINI_MODE"] = "格式";
 Blockly.Msg["AMB82_MINI_RTSP"] = "RTSP串流(預設)";
 Blockly.Msg["AMB82_MINI_TCP"] = "TCP串流";
 Blockly.Msg["AMB82_MINI_TCPSTREAM"] = "TCP串流 啟用";
-Blockly.Msg["AMB82_MINI_RTSP_STILL"] = "RTSP串流與允許截圖";
-Blockly.Msg["AMB82_MINI_STILL"] = "允許截圖";
-Blockly.Msg["AMB82_MINI_TCP_STILL"] = "TCP串流與允許截圖";
+Blockly.Msg["AMB82_MINI_RTSP_STILL"] = "RTSP串流與可截取影像";
+Blockly.Msg["AMB82_MINI_STILL"] = "可截取影像";
+Blockly.Msg["AMB82_MINI_TCP_STILL"] = "TCP串流與可截取影像";
 Blockly.Msg["AMB82_MINI_STILL_NEW"] = "即時";
 Blockly.Msg["AMB82_MINI_STILL_OLD"] = "暫存";
 
