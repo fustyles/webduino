11c46780e7bbb18c25bc56ae70012d50c52e2ef5
fustyles <34397289+fustyles@users.noreply.github.com>
2024-11-17 19:31:12 +0800

    Update zh-hans.js

--- SpBlocklyJS/dvcbot_20241116/zh-hans.js
+++ SpBlocklyJS/dvcbot_20241116/zh-hans.js
@@ -1,9 +1,9 @@
-Blockly.Msg["CATDVCBOT"] = "达哥助手";
-Blockly.Msg.DVCBOT_SHOW = "达哥助手";
+Blockly.Msg["CATDVCBOT"] = "达哥助理";
+Blockly.Msg.DVCBOT_SHOW = "达哥助理";
 Blockly.Msg.DVCBOT_INITIAL_SHOW = "初始化";
 Blockly.Msg.DVCBOT_APIKEY_SHOW = "API密钥";
-Blockly.Msg.DVCBOT_ASSISTANTID_SHOW = "助手代码";
-Blockly.Msg.DVCBOT_RESPONSE_SHOW = "获取程序返回值";
+Blockly.Msg.DVCBOT_ASSISTANTID_SHOW = "助理代码";
+Blockly.Msg.DVCBOT_RESPONSE_SHOW = "获取Plugin返回值";
 Blockly.Msg.DVCBOT_RESULT_SHOW = "获取回应";
 Blockly.Msg.DVCBOT_USERMESSAGE_SHOW = "对话消息";
 Blockly.Msg.DVCBOT_RESULT_GET_BR_SHOW = "自动换行";
