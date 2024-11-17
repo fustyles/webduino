a706248e6f1031990670c006a0f40195796e2549
fustyles <34397289+fustyles@users.noreply.github.com>
2024-11-17 19:30:15 +0800

    Update zh-hant.js

--- SpBlocklyJS/dvcbot_20241116/zh-hant.js
+++ SpBlocklyJS/dvcbot_20241116/zh-hant.js
@@ -1,9 +1,9 @@
-Blockly.Msg["CATDVCBOT"] = "達哥助手";
-Blockly.Msg.DVCBOT_SHOW = "達哥助手";
+Blockly.Msg["CATDVCBOT"] = "達哥助理";
+Blockly.Msg.DVCBOT_SHOW = "達哥助理";
 Blockly.Msg.DVCBOT_INITIAL_SHOW = "初始化";
 Blockly.Msg.DVCBOT_APIKEY_SHOW = "API金鑰";
-Blockly.Msg.DVCBOT_ASSISTANTID_SHOW = "助手代碼";
-Blockly.Msg.DVCBOT_RESPONSE_SHOW = "取得程式回傳值";
+Blockly.Msg.DVCBOT_ASSISTANTID_SHOW = "助理代碼";
+Blockly.Msg.DVCBOT_RESPONSE_SHOW = "取得Plugin回傳值";
 Blockly.Msg.DVCBOT_RESULT_SHOW = "取得回應";
 Blockly.Msg.DVCBOT_USERMESSAGE_SHOW = "對話訊息";
 Blockly.Msg.DVCBOT_RESULT_GET_BR_SHOW = "自動換行";
