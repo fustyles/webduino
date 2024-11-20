6e6f4c27e02407de642c62ccba0ae1af7388a0be
fustyles <34397289+fustyles@users.noreply.github.com>
2024-11-21 00:54:27 +0800

    Update dvcbot.js

--- SpBlocklyJS/dvcbot_20241116/dvcbot.js
+++ SpBlocklyJS/dvcbot_20241116/dvcbot.js
@@ -147,13 +147,12 @@ async function getRunResult(threadId, runUrl, runId) {
 		const output = toolXhr.responseText.substring(0, 8000).replace(/"/g, '\\"');
 		const callId = toolCalls[i].id;
 
-		const pluginResponse = JSON.parse(toolXhr.responseText);
-		if (pluginResponse.text) {
-		    try {
+		try {
+		    const pluginResponse = JSON.parse(toolXhr.responseText);
+		    if (pluginResponse.text)
 		        dvcbot_plugin_response.push(JSON.stringify(JSON.parse(pluginResponse.text)));
-		    } catch (error) {
-		        dvcbot_plugin_response.push(pluginResponse.text);
-		    }
+		} catch (error) {
+		    dvcbot_plugin_response.push(output);
 		}
 	
 		toolOutputs.push({
