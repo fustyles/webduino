d5e7053e28dc77be5dc5c47440ec2d6ab377f282
fustyles <34397289+fustyles@users.noreply.github.com>
2024-11-22 15:04:19 +0800

    Add files via upload

--- LinkIt7697/France/javascript.js
+++ LinkIt7697/France/javascript.js
@@ -20754,13 +20754,17 @@ Blockly.Arduino['fu_mqtt_setup'] = function(block) {
 														'      delay(5000);\n'+
 														'    }\n'+
 														'  }\n'+
-														'}\n';											
-				
-	Blockly.Arduino.setups_.setup_mqtt= '  randomSeed(micros());\n  '+
-										'mqtt_client.setServer(mqtt_server,mqtt_port);\n  '+
-										'mqtt_client.setCallback(callback);\n  '+
-										'//mqtt_client.setBufferSize(1024);\n  '+
-										'reconnect();\n  ';										
+														'}\n';	
+
+	Blockly.Arduino.setups_.setup_mqtt = '';													
+	if (selectBoardType()=="AMB82-MINI"||selectBoardType()=="HUB-8735_ultra"||selectBoardType()=="HUB-8735")
+		Blockly.Arduino.setups_.setup_mqtt += 'espClient.setNonBlockingMode();\n  ';
+	
+	Blockly.Arduino.setups_.setup_mqtt += 'randomSeed(micros());\n  '+
+										  'mqtt_client.setServer(mqtt_server,mqtt_port);\n  '+
+										  'mqtt_client.setCallback(callback);\n  '+
+										  '//mqtt_client.setBufferSize(1024);\n  '+
+										  'reconnect();\n  ';										
 										
   Blockly.Arduino.functions_.define_mqtt_callback = 'void callback(char* topic, byte* payload, unsigned int length) {\n'+
 														'  mqtt_data = "";\n'+
