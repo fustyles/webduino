1750d5796f7d6e480533b8250ff12ef6e355b5b4
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-09 22:47:22 +0800

    Update blocks.js

--- SpreadsheetSQL_20210403/blockly/blocks.js
+++ SpreadsheetSQL_20210403/blockly/blocks.js
@@ -169,15 +169,15 @@ Blockly.Blocks['spreadsheetsql_executeSql1'] = {
 	  .setAlign(Blockly.ALIGN_RIGHT)
 	  .appendField(Blockly.Msg["SPREADSHEETSQL_TASK_SHOW"])
       .appendField(new Blockly.FieldTextInput(""), "task_id");
-/*
-  this.getInput("rows").setVisible(false);
-  this.getInput("cols").setVisible(false);
-  this.getInput("rowe").setVisible(false);
-  this.getInput("cole").setVisible(false);
-  this.getInput("comparision").setVisible(false);
-  this.getInput("op").setVisible(false);
-  this.getInput("like").setVisible(false);
-*/
+  window.setTimeout(function() {
+	  this.getInput("rows").setVisible(false);
+	  this.getInput("cols").setVisible(false);
+	  this.getInput("rowe").setVisible(false);
+	  this.getInput("cole").setVisible(false);
+	  this.getInput("comparision").setVisible(false);
+	  this.getInput("op").setVisible(false);
+	  this.getInput("like").setVisible(false);
+  }, 1000);
   this.setInputsInline(false);	  
   this.setPreviousStatement(true);
   this.setNextStatement(true);
