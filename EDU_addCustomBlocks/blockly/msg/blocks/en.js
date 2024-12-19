ca2a7d8b2cbf03cdaa9bfa2edee8f6130dcc60e1
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-20 07:56:20 +0800

    Update en.js

--- EDU_addCustomBlocks/blockly/msg/blocks/en.js
+++ EDU_addCustomBlocks/blockly/msg/blocks/en.js
@@ -37,3 +37,4 @@ Blockly.Msg.ADDCUSTOMBLOCKS_GEMINI_TEXT_SHOW = "[link] Gemini";
 Blockly.Msg.ADDCUSTOMBLOCKS_SPEECHRECOGNITION_SHOW = "[link] Speech recognition";
 Blockly.Msg.ADDCUSTOMBLOCKS_TEXTTOSPEECH_SHOW = "[link] Speech synthesis";
 Blockly.Msg.ADDCUSTOMBLOCKS_DVCBOT_SHOW = "[link] DaVinci Assistant";
+Blockly.Msg.ADDCUSTOMBLOCKS_LLM_TEXT_SHOW = "[link] LLM";
