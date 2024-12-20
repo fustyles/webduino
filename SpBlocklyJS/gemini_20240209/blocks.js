ac7f1d3de2e8fdc9c3afd66abcd6039d8bc1b0c8
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-20 21:51:43 +0800

    Update blocks.js

--- SpBlocklyJS/gemini_20240209/blocks.js
+++ SpBlocklyJS/gemini_20240209/blocks.js
@@ -56,7 +56,7 @@ Blockly.Blocks['gemini_chat_image_request'] = {
       .setCheck("String")
       .setAlign(Blockly.ALIGN_RIGHT)
       .appendField(Blockly.Msg.GEMINI_CHAT_IMAGE_URL_SHOW);	  
-  this.setInputsInline(true);	  
+  this.setInputsInline(false);	  
   this.setPreviousStatement(true);
   this.setNextStatement(true);
   this.setColour(40);
