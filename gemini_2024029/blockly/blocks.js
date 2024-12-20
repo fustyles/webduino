b087f70830ded5074cd81ddd5a93141267920267
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-21 00:08:18 +0800

    Update blocks.js

--- gemini_2024029/blockly/blocks.js
+++ gemini_2024029/blockly/blocks.js
@@ -204,5 +204,6 @@ Blockly.Blocks['gemini_chat_content_file_remote_read'] = {
   this.setPreviousStatement(true);
   this.setNextStatement(true); 
   this.setColour(40);
+  this.setHelpUrl("https://ai.google.dev/gemini-api/docs/document-processing?hl=zh-tw");	  
   }
 };
