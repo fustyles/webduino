d9edf933bd1739f84f9753a1adb50cdfc7246946
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-23 14:07:06 +0800

    Update gemini.js

--- gemini_2024029/gemini.js
+++ gemini_2024029/gemini.js
@@ -19,6 +19,7 @@ function gemini_chat_initial(input_key, input_model, input_tokens) {
 		'import { GoogleGenerativeAI } from "@google/generative-ai";\n'+
 		'const genAI = new GoogleGenerativeAI("'+input_key+'");\n'+
 		'var chatHistory = {history: [],generationConfig: {maxOutputTokens: '+input_tokens+',},};\n'+
+		'window.chatHistory = chatHistory;\n'+			
 		'async function gemini_chat_run(prompt) {\n'+
 		'	const model = await genAI.getGenerativeModel({ model: "'+input_model+'"});\n'+
 		'	const chat = model.startChat(chatHistory);\n'+
@@ -200,7 +201,15 @@ async function gemini_chat_image_request(message, imageURL) {
         let result = json.candidates[0].content.parts[0].text;
         if (result === "null") {
             result = json.error.message;
-        }
+        } else {
+            var char_request = {};
+            char_request.role = "model";
+            char_request.parts = [];
+            var char_request_text = {};
+            char_request_text.text = result;
+            char_request.parts.push(char_request_text);
+            chatHistory["history"].push(char_request);
+	}
 	if (typeof gemini_chat_respsonse === "function") gemini_chat_respsonse(result);
     } catch (error) {
 	if (typeof gemini_chat_respsonse === "function") gemini_chat_respsonse(JSON.stringify(error));
@@ -234,7 +243,15 @@ async function gemini_chat_file_request(fileType, fileURL, message) {
         let result = json.candidates[0].content.parts[0].text;
         if (result === "null") {
             result = json.error.message;
-        }
+        } else {
+            var char_request = {};
+            char_request.role = "model";
+            char_request.parts = [];
+            var char_request_text = {};
+            char_request_text.text = result;
+            char_request.parts.push(char_request_text);
+            chatHistory["history"].push(char_request);
+	}
 	if (typeof gemini_chat_respsonse === "function") gemini_chat_respsonse(result);
     } catch (error) {
 	if (typeof gemini_chat_respsonse === "function") gemini_chat_respsonse(JSON.stringify(error));
