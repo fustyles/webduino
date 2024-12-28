454a49d53d4cab43f47e57d6c2e69ea050c085c5
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 19:22:43 +0800

    Update googlemap.js

--- googlemap_20241226/googlemap.js
+++ googlemap_20241226/googlemap.js
@@ -2,7 +2,7 @@
 
 let mapMarkers = [];
 
-function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
+async function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
 	let markerData = {
 	  position: { lat: pLat, lng: pLng },
 	  title: pTitle,
