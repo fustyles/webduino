c4a0c534704ae498eafd279f791bdeaf6ddc1533
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 19:02:45 +0800

    Update javascript.js

--- googlemap_20241226/blockly/javascript.js
+++ googlemap_20241226/blockly/javascript.js
@@ -14,17 +14,13 @@ Blockly.JavaScript['googlemap_initial'] = function (block) {
 	     '			}\n'+
 	     '		}\n'+
 	     '}\n'+
-	     'var url1 = "https://fustyles.github.io/webduino/googlemap_20241226/googlemap.js";\n'+
-             'var s1 = document.createElement("script");\n'+
-             's1.src = url1;\n'+
-             'document.getElementsByTagName("head")[0].append(s1);\n'+
              'await delay(3);\n'+	  
-	     'var url2 = "https://maps.googleapis.com/maps/api/js?key='+key+'&callback=initMap&v=weekly&libraries=marker";\n'+
-             'var s2 = document.createElement("script");\n'+
-             's2.async = true;\n'+
-             's2.defer = true;\n'+
-             's2.src = url2;\n'+
-             'document.getElementsByTagName("head")[0].append(s2);\n'+
+	     'var url = "https://maps.googleapis.com/maps/api/js?key='+key+'&callback=initMap&v=weekly&libraries=marker";\n'+
+             'var s = document.createElement("script");\n'+
+             's.async = true;\n'+
+             's.defer = true;\n'+
+             's.src = url;\n'+
+             'document.getElementsByTagName("head")[0].append(s);\n'+
              'function initMap() {\n'+statement+'\n}\n'+
              'await delay(3);\n';	  
   return code;
