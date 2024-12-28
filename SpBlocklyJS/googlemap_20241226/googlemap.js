b76c3f1fffa7b19317184c2f0ced82387d1596e9
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 16:36:43 +0800

    Update googlemap.js

--- SpBlocklyJS/googlemap_20241226/googlemap.js
+++ SpBlocklyJS/googlemap_20241226/googlemap.js
@@ -1,101 +1,90 @@
-+(function (window, document) {
-	
-	'use strict';
+'use strict';
 
-	let mapMarkers = [];
+let mapMarkers = [];
 
-	function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
-		let markerData = {
-		  position: { lat: pLat, lng: pLng },
-		  title: pTitle,
-		};
-		let marker = new google.maps.marker.AdvancedMarkerElement({
-		  position: markerData.position,
-		  map: pMapId,
-		  title: markerData.title,
-		});
-		let infoWindow = new google.maps.InfoWindow({
-		  content: pContent,
-		});
+function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
+	let markerData = {
+	  position: { lat: pLat, lng: pLng },
+	  title: pTitle,
+	};
+	let marker = new google.maps.marker.AdvancedMarkerElement({
+	  position: markerData.position,
+	  map: pMapId,
+	  title: markerData.title,
+	});
+	let infoWindow = new google.maps.InfoWindow({
+	  content: pContent,
+	});
 
-		marker.addListener('click', () => {
-		  infoWindow.open(pMapId, marker);
-		});
+	marker.addListener('click', () => {
+	  infoWindow.open(pMapId, marker);
+	});
 
-		mapMarkers.push(["point_"+pID, pMapId, marker, infoWindow, markerData.position]);
-	}
+	mapMarkers.push(["point_"+pID, pMapId, marker, infoWindow, markerData.position]);
+}
 
-	function clearMarker(pID) {
-		for (var i=0;i<mapMarkers.length;i++) {
-			if ("point_"+pID==mapMarkers[i][0]) {
-				mapMarkers[i][2].setMap(null);
-				mapMarkers.splice(i, 1);
-				break;
-			}
+function clearMarker(pID) {
+	for (var i=0;i<mapMarkers.length;i++) {
+		if ("point_"+pID==mapMarkers[i][0]) {
+			mapMarkers[i][2].setMap(null);
+			mapMarkers.splice(i, 1);
+			break;
 		}
 	}
+}
 
-	function updateMarkerContent(pID, newContent, type) {
-		for (var i=0;i<mapMarkers.length;i++) {
-			if ("point_"+pID==mapMarkers[i][0]) {
-				if (type=="content") {
-					mapMarkers[i][3].setContent(newContent);
-				} 
-				else if (type === "latitude") {
-					mapMarkers[i][4].lat = newContent;
-					mapMarkers[i][2].position = mapMarkers[i][4];					
-				} 
-				else if (type === "longitude") {
-					mapMarkers[i][4].lng = newContent;
-					mapMarkers[i][2].position = mapMarkers[i][4];					
-				}
-				else if (type === "position") {
-					newContent = newContent.split(",");
-					mapMarkers[i][4].lat = Number(newContent[0]);
-					mapMarkers[i][4].lng = Number(newContent[1]);
-					mapMarkers[i][2].position = mapMarkers[i][4];					
-				}				
-				else if (type=="title") {
-					mapMarkers[i][2].title = newContent;
-				}  			
-				break;
+function updateMarkerContent(pID, newContent, type) {
+	for (var i=0;i<mapMarkers.length;i++) {
+		if ("point_"+pID==mapMarkers[i][0]) {
+			if (type=="content") {
+				mapMarkers[i][3].setContent(newContent);
+			} 
+			else if (type === "latitude") {
+				mapMarkers[i][4].lat = newContent;
+				mapMarkers[i][2].position = mapMarkers[i][4];					
+			} 
+			else if (type === "longitude") {
+				mapMarkers[i][4].lng = newContent;
+				mapMarkers[i][2].position = mapMarkers[i][4];					
 			}
+			else if (type === "position") {
+				newContent = newContent.split(",");
+				mapMarkers[i][4].lat = Number(newContent[0]);
+				mapMarkers[i][4].lng = Number(newContent[1]);
+				mapMarkers[i][2].position = mapMarkers[i][4];					
+			}				
+			else if (type=="title") {
+				mapMarkers[i][2].title = newContent;
+			}  			
+			break;
 		}
 	}
+}
 
-	function openMarkerContent(pID) {
-		for (var i=0;i<mapMarkers.length;i++) {
-			if ("point_"+pID==mapMarkers[i][0]) {
-				mapMarkers[i][3].open(mapMarkers[i][1], mapMarkers[i][2]);
-				break;
-			}
+function openMarkerContent(pID) {
+	for (var i=0;i<mapMarkers.length;i++) {
+		if ("point_"+pID==mapMarkers[i][0]) {
+			mapMarkers[i][3].open(mapMarkers[i][1], mapMarkers[i][2]);
+			break;
 		}
 	}
+}
 
-	function closeMarkerContent(pID) {
-		for (var i=0;i<mapMarkers.length;i++) {
-			if ("point_"+pID==mapMarkers[i][0]) {
-				mapMarkers[i][3].close();
-				break;
-			}
+function closeMarkerContent(pID) {
+	for (var i=0;i<mapMarkers.length;i++) {
+		if ("point_"+pID==mapMarkers[i][0]) {
+			mapMarkers[i][3].close();
+			break;
 		}
 	}
+}
 
-	function clearMap(pMapId) {
-		for (var i=0;i<mapMarkers.length;i++) {
-			if (pMapId==mapMarkers[i][1]) {
-				mapMarkers[i][2].setMap(null);
-				mapMarkers.splice(i, 1);
-				i--;
-			}
+function clearMap(pMapId) {
+	for (var i=0;i<mapMarkers.length;i++) {
+		if (pMapId==mapMarkers[i][1]) {
+			mapMarkers[i][2].setMap(null);
+			mapMarkers.splice(i, 1);
+			i--;
 		}
 	}
-	
-	window.addMapPoint = addMapPoint;
-	window.clearMarker = clearMarker;
-	window.updateMarkerContent = updateMarkerContent;  
-	window.openMarkerContent = openMarkerContent;
-	window.closeMarkerContent = closeMarkerContent;  
-	window.clearMap = clearMap; 	
-	
-}(window, window.document));
+}
