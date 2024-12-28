aacce3790984702c38cf730267bec16909a9b2c9
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 19:26:11 +0800

    Update googlemap.js

--- googlemap_20241226/googlemap.js
+++ googlemap_20241226/googlemap.js
@@ -7,8 +7,7 @@ async function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
 	  position: { lat: pLat, lng: pLng },
 	  title: pTitle,
 	};
-	const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
-	let marker = new AdvancedMarkerElement({
+	let marker = new google.maps.marker.AdvancedMarkerElement({
 	  position: markerData.position,
 	  map: pMapId,
 	  title: markerData.title,
