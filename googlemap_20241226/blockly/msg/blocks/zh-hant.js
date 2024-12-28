29b26c59ae1260cbdcdd7331d3da5d3c66a7ba35
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 23:06:44 +0800

    Update zh-hant.js

--- googlemap_20241226/blockly/msg/blocks/zh-hant.js
+++ googlemap_20241226/blockly/msg/blocks/zh-hant.js
@@ -22,3 +22,4 @@ Blockly.Msg["GOOGLEMAP_POINT_UPDATE_TITLE"] = "更新名稱";
 Blockly.Msg["GOOGLEMAP_POINT_OPEN_CONTENT"] = "開啟內容";
 Blockly.Msg["GOOGLEMAP_POINT_CLOSE_CONTENT"] = "關閉內容";
 Blockly.Msg["GOOGLEMAP_POINT_CLEAR"] = "清除所有位置標記";
+Blockly.Msg["GOOGLEMAP_CENTERMAP"] = "設定地圖中心位置";
