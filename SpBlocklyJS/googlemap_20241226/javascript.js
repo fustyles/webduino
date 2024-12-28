5abf068c4429c142de52b9d88fb3a81099bc4337
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 21:28:47 +0800

    Update javascript.js

--- SpBlocklyJS/googlemap_20241226/javascript.js
+++ SpBlocklyJS/googlemap_20241226/javascript.js
@@ -9,9 +9,15 @@ Blockly.JavaScript['googlemap_initial'] = function (block) {
              's.defer = true;\n'+
              's.src = url;\n'+
              'document.body.append(s);\n'+
-			 'function initMap() {\n'+statement+'\n}\n'+
-			 'window.initMap = initMap;\n'+
-			 'await delay(3);\n';
+			 'function initMap() {\n'+statement+'\nloadedMap();\n}\n'+
+			 'window.initMap = initMap;\n';
+  return code;
+};
+
+Blockly.JavaScript['googlemap_loaded'] = function (block) {
+  var statement = Blockly.JavaScript.statementToCode(block, 'statement');
+  var code = 'function loadedMap() {\n'+statement+'\n}\n'+
+	     'window.loadedMap = loadedMap;\n';
   return code;
 };
 
