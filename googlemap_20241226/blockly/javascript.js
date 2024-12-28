b0f2bb729600f7ee0776ad7ef0e5640f8b056e9f
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-28 18:41:02 +0800

    Update javascript.js

--- googlemap_20241226/blockly/javascript.js
+++ googlemap_20241226/blockly/javascript.js
@@ -13,16 +13,22 @@ Blockly.JavaScript['googlemap_initial'] = function (block) {
 	     '				rule.style.overflow = "";\n'+
 	     '			}\n'+
 	     '		}\n'+
-	     '}\n'+
+	     '}\n'+	  
 	     'var url = "https://maps.googleapis.com/maps/api/js?key='+key+'&callback=initMap&v=weekly&libraries=marker";\n'+
              'var s = document.createElement("script");\n'+
              's.async = true;\n'+
              's.defer = true;\n'+
              's.src = url;\n'+
              'document.body.append(s);\n'+
-			 'function initMap() {\n'+statement+'\n}\n'+
-			 'window.initMap = initMap;\n'+
-			 'await delay(3);\n';
+             'function initMap() {\n'+statement+'\n}\n'+
+             'window.initMap = initMap;\n'+
+             'await delay(3);\n'+
+	     'url = "https://fustyles.github.io/webduino/googlemap_20241226/googlemap.js";\n'+
+             'var s = document.createElement("script");\n'+
+             's.async = true;\n'+
+             's.defer = true;\n'+
+             's.src = url;\n'+
+             'document.body.append(s);\n';	  
   return code;
 };
 
