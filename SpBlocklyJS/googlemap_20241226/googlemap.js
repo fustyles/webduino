6c6b885cf5ad90487c31af166f4849b065c0fe53
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-29 10:58:37 +0800

    Update googlemap.js

--- SpBlocklyJS/googlemap_20241226/googlemap.js
+++ SpBlocklyJS/googlemap_20241226/googlemap.js
@@ -57,7 +57,7 @@ function updateMarkerContent(pID, newContent, type) {
 				mapMarkers[i][2].title = newContent;
 			}
 			else if (type=="icon") {
-				mapMarkers[i][2].customIcon= newContent;
+				mapMarkers[i][2].icon= newContent;
 			} 			
 			break;
 		}
