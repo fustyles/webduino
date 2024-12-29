13d7b0934d42ce1eaca18e32dd3accb2dde43109
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-29 10:26:55 +0800

    Update blocks.js

--- SpBlocklyJS/googlemap_20241226/blocks.js
+++ SpBlocklyJS/googlemap_20241226/blocks.js
@@ -111,7 +111,8 @@ Blockly.Blocks['googlemap_point_function'] = {
 			[Blockly.Msg["GOOGLEMAP_POINT_UPDATE_LATITUDE"],"update_latitude"],
 			[Blockly.Msg["GOOGLEMAP_POINT_UPDATE_LONGITUDE"],"update_longitude"],
 			[Blockly.Msg["GOOGLEMAP_POINT_UPDATE_POSITION"],"update_position"],
-			[Blockly.Msg["GOOGLEMAP_POINT_UPDATE_TITLE"],"update_title"],			
+			[Blockly.Msg["GOOGLEMAP_POINT_UPDATE_TITLE"],"update_title"],
+			[Blockly.Msg["GOOGLEMAP_POINT_UPDATE_ICON"],"update_icon"],			
 			[Blockly.Msg["GOOGLEMAP_POINT_OPEN_CONTENT"],"open_content"],
 			[Blockly.Msg["GOOGLEMAP_POINT_CLOSE_CONTENT"],"close_content"],			
 			[Blockly.Msg["GOOGLEMAP_POINT_CLEAR"],"clear"]	
@@ -128,7 +129,7 @@ Blockly.Blocks['googlemap_point_function'] = {
   validate: function(newValue) {
 	 const block = this.sourceBlock_;
 	 if (!block) return;
-	 if (newValue=="update_content"||newValue=="update_latitude"||newValue=="update_longitude"||newValue=="update_position"||newValue=="update_title")
+	 if (newValue=="update_content"||newValue=="update_latitude"||newValue=="update_longitude"||newValue=="update_position"||newValue=="update_title"||newValue=="update_icon")
 		 block.getInput("content").setVisible(true);
 	 else
 		 block.getInput("content").setVisible(false);
