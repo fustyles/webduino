d46a22fe7613667f1f97d3dffa7a253932329855
fustyles <34397289+fustyles@users.noreply.github.com>
2024-12-30 11:20:35 +0800

    Update zh-hant.js

--- SpBlocklyJS/googlemap_20241226/zh-hant.js
+++ SpBlocklyJS/googlemap_20241226/zh-hant.js
@@ -19,6 +19,7 @@ Blockly.Msg["GOOGLEMAP_POINT_UPDATE_LATITUDE"] = "更新緯度";
 Blockly.Msg["GOOGLEMAP_POINT_UPDATE_LONGITUDE"] = "更新經度";
 Blockly.Msg["GOOGLEMAP_POINT_UPDATE_POSITION"] = "更新緯度,經度";
 Blockly.Msg["GOOGLEMAP_POINT_UPDATE_TITLE"] = "更新名稱";
+Blockly.Msg["GOOGLEMAP_POINT_UPDATE_ZINDEX"] = "更新層疊索引";
 Blockly.Msg["GOOGLEMAP_POINT_UPDATE_ICON"] = "更新圖示";
 Blockly.Msg["GOOGLEMAP_MAP_UPDATE_ZOOM"] = "更新範圍縮放值";
 Blockly.Msg["GOOGLEMAP_POINT_OPEN_CONTENT"] = "開啟內容";
