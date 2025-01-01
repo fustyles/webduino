880ca2f3288e2224e458c9f1dded725dc1fea683
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-01 23:54:26 +0800

    Update googlemap.js

--- googlemap_20241226/googlemap.js
+++ googlemap_20241226/googlemap.js
@@ -2,15 +2,15 @@
 
 let mapMarkers = [];
 
-function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
+function addMapPoint(pID, pMapId, pLat, pLng, pAlt, pTitle, pContent) {
 	let markerData = {
-	  position: { lat: pLat, lng: pLng },
+	  position: { lat: pLat, lng: pLng, altitude: pAlt},
 	  title: pTitle,
 	};
 	let marker = new google.maps.marker.AdvancedMarkerElement({
 	  position: markerData.position,
 	  map: pMapId,
-	  title: markerData.title,
+	  title: markerData.title,	
 	});
 	let infoWindow = new google.maps.InfoWindow({
 	  content: pContent,
@@ -23,71 +23,114 @@ function addMapPoint(pID, pMapId, pLat, pLng, pTitle, pContent) {
 	mapMarkers.push(["point_"+pID, pMapId, marker, infoWindow, markerData.position]);
 }
 
-function clearMarker(pID) {
+function getMarker(pID) {
 	for (var i=0;i<mapMarkers.length;i++) {
 		if ("point_"+pID==mapMarkers[i][0]) {
-			mapMarkers[i][2].setMap(null);
-			mapMarkers.splice(i, 1);
-			break;
+			return mapMarkers[i][2];
 		}
 	}
+	return null;
 }
 
 function updateMarkerContent(pID, newContent, type) {
 	for (var i=0;i<mapMarkers.length;i++) {
 		if ("point_"+pID==mapMarkers[i][0]) {
-			if (type=="content") {
+			if (type=="update_content") {
 				mapMarkers[i][3].setContent(newContent);
-			}
-			else if (type=="headercontent") {
+			} else if (type=="update_headercontent") {
 				mapMarkers[i][3].setHeaderContent(newContent);
-			}				
-			else if (type === "latitude") {
+			} else if (type === "update_latitude") {
 				mapMarkers[i][4].lat = Number(newContent);
+				mapMarkers[i][4].lng = mapMarkers[i][2].position.lng;
+				mapMarkers[i][4].altitude = mapMarkers[i][2].position.altitude;
 				mapMarkers[i][2].position = mapMarkers[i][4];					
-			} 
-			else if (type === "longitude") {
+			} else if (type === "update_longitude") {
+				mapMarkers[i][4].lat = mapMarkers[i][2].position.lat;
 				mapMarkers[i][4].lng = Number(newContent);
+				mapMarkers[i][4].altitude = mapMarkers[i][2].position.altitude;
+				mapMarkers[i][2].position = mapMarkers[i][4];
+			} else if (type === "update_altitude") {
+				mapMarkers[i][4].lat = mapMarkers[i][2].position.lat;
+				mapMarkers[i][4].lng = mapMarkers[i][2].position.lng;
+				mapMarkers[i][4].altitude = Number(newContent);
 				mapMarkers[i][2].position = mapMarkers[i][4];					
-			}
-			else if (type === "position") {
+			} else if (type === "update_position") {
 				newContent = newContent.split(",");
 				mapMarkers[i][4].lat = Number(newContent[0]);
 				mapMarkers[i][4].lng = Number(newContent[1]);
+				mapMarkers[i][4].altitude = Number(newContent[2]);
 				mapMarkers[i][2].position = mapMarkers[i][4];					
-			}				
-			else if (type=="title") {
+			} else if (type=="update_title") {
 				mapMarkers[i][2].title = newContent;
-			}
-			else if (type=="zindex") {
+			} else if (type=="update_zindex") {
 				mapMarkers[i][2].zIndex = Number(newContent);
-			}				
-			else if (type=="icon") {
+			} else if (type=="update_icon") {
 				const flagImg = document.createElement("img");
 				flagImg.src = newContent;
 				mapMarkers[i][2].content= flagImg;
-			} 			
+			} else if (type=="update_gmpDraggable") {
+				mapMarkers[i][2].gmpDraggable = newContent;
+			} else if (type=="update_gmpClickable") {
+				mapMarkers[i][2].gmpClickable = newContent;
+			} else if (type=="open_content") {
+				mapMarkers[i][3].open(mapMarkers[i][1], mapMarkers[i][2]);
+			} else if (type=="close_content") {
+				mapMarkers[i][3].close();
+			} else if (type=="clear") {
+				mapMarkers[i][2].setMap(null);
+				mapMarkers.splice(i, 1);
+			}			
 			break;
 		}
 	}
 }
 
-function openMarkerContent(pID) {
+function getMarkerSetting(pID, property) {
 	for (var i=0;i<mapMarkers.length;i++) {
 		if ("point_"+pID==mapMarkers[i][0]) {
-			mapMarkers[i][3].open(mapMarkers[i][1], mapMarkers[i][2]);
-			break;
+			if (property=="latitude") {
+				const position = mapMarkers[i][2].position;
+				return position.lat;
+			} else if (property=="longtitude") {
+				const position = mapMarkers[i][2].position;
+				return position.lng;
+			} else if (property=="altitude") {
+				const position = mapMarkers[i][2].position;
+				return position.altitude;				
+			} else if (property=="latlngalt") {
+				const position = mapMarkers[i][2].position;
+				return [position.lat, position.lng, position.altitude];
+			}			
 		}
 	}
+	return null;
 }
 
-function closeMarkerContent(pID) {
-	for (var i=0;i<mapMarkers.length;i++) {
-		if ("point_"+pID==mapMarkers[i][0]) {
-			mapMarkers[i][3].close();
-			break;
-		}
+function updateMapContent(pMapId, val, type) {
+	if (type=="zoom") {
+		pMapId.setZoom(Number(val));
+	} else if (type === "clear") {
+		for (var i=0;i<mapMarkers.length;i++) {
+			if (pMapId==mapMarkers[i][1]) {
+				mapMarkers[i][2].setMap(null);
+				mapMarkers.splice(i, 1);
+				i--;
+			}
+		}							
+	} else if (type=="heading") {
+		let povData = {
+			heading: Number(val),
+			pitch: pMapId.getPov().pitch
+		};
+		pMapId.setPov(povData);
+	} else if (type === "pitch") {
+		let povData = {
+			heading: pMapId.getPov().heading,
+			pitch: Number(val)
+		};
+		pMapId.setPov(povData);
 	}
+
 }
 
 function centerMap(pMapId, lat, lng) {
@@ -98,34 +141,4 @@ function centerMap(pMapId, lat, lng) {
 function positionMap(pMapId, lat, lng) {
 	const newCenter = new google.maps.LatLng(Number(lat), Number(lng));
 	pMapId.setPosition(newCenter);	
-}
-
-function zoomMap(pMapId, val) {
-	pMapId.setZoom(Number(val));	
-}
-
-function headingMap(pMapId, val) {
-	let povData = {
-		heading: Number(val),
-		pitch: pMapId.getPov().pitch
-	};
-	pMapId.setPov(povData);
-}
-
-function pitchMap(pMapId, val) {
-	let povData = {
-		heading: pMapId.getPov().heading,
-		pitch: Number(val)
-	};
-	pMapId.setPov(povData);
-}
-
-function clearMap(pMapId) {
-	for (var i=0;i<mapMarkers.length;i++) {
-		if (pMapId==mapMarkers[i][1]) {
-			mapMarkers[i][2].setMap(null);
-			mapMarkers.splice(i, 1);
-			i--;
-		}
-	}
 }
