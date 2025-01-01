838e77841ba7d90176327eb19f7d06505b8d500b
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-01 19:23:49 +0800

    Update googlemap.js

--- SpBlocklyJS/googlemap_20241226/googlemap.js
+++ SpBlocklyJS/googlemap_20241226/googlemap.js
@@ -37,48 +37,36 @@ function updateMarkerContent(pID, newContent, type) {
 		if ("point_"+pID==mapMarkers[i][0]) {
 			if (type=="update_content") {
 				mapMarkers[i][3].setContent(newContent);
-			}
-			else if (type=="update_headercontent") {
+			} else if (type=="update_headercontent") {
 				mapMarkers[i][3].setHeaderContent(newContent);
-			}				
-			else if (type === "update_latitude") {
+			} else if (type === "update_latitude") {
 				mapMarkers[i][4].lat = Number(newContent);
 				mapMarkers[i][2].position = mapMarkers[i][4];					
-			} 
-			else if (type === "update_longitude") {
+			} else if (type === "update_longitude") {
 				mapMarkers[i][4].lng = Number(newContent);
 				mapMarkers[i][2].position = mapMarkers[i][4];					
-			}
-			else if (type === "update_position") {
+			} else if (type === "update_position") {
 				newContent = newContent.split(",");
 				mapMarkers[i][4].lat = Number(newContent[0]);
 				mapMarkers[i][4].lng = Number(newContent[1]);
 				mapMarkers[i][2].position = mapMarkers[i][4];					
-			}				
-			else if (type=="update_title") {
+			} else if (type=="update_title") {
 				mapMarkers[i][2].title = newContent;
-			}
-			else if (type=="update_zindex") {
+			} else if (type=="update_zindex") {
 				mapMarkers[i][2].zIndex = Number(newContent);
-			}				
-			else if (type=="update_icon") {
+			} else if (type=="update_icon") {
 				const flagImg = document.createElement("img");
 				flagImg.src = newContent;
 				mapMarkers[i][2].content= flagImg;
-			} 	
-			else if (type=="update_gmpDraggable") {
+			} else if (type=="update_gmpDraggable") {
 				mapMarkers[i][2].gmpDraggable = newContent;
-			}
-			else if (type=="update_gmpClickable") {
+			} else if (type=="update_gmpClickable") {
 				mapMarkers[i][2].gmpClickable = newContent;
-			}
-			else if (type=="open_content") {
+			} else if (type=="open_content") {
 				mapMarkers[i][3].open(mapMarkers[i][1], mapMarkers[i][2]);
-			}
-			else if (type=="close_content") {
+			} else if (type=="close_content") {
 				mapMarkers[i][3].close();
-			}
-			else if (type=="clear") {
+			} else if (type=="clear") {
 				mapMarkers[i][2].setMap(null);
 				mapMarkers.splice(i, 1);
 			}			
@@ -90,22 +78,19 @@ function updateMarkerContent(pID, newContent, type) {
 function updateMapContent(pMapId, val, type) {
 	if (type=="zoomMap") {
 		pMapId.setZoom(Number(val));
-	}
-	else if (type=="headingMap") {
+	} else if (type=="headingMap") {
 		let povData = {
 			heading: Number(val),
 			pitch: pMapId.getPov().pitch
 		};
 		pMapId.setPov(povData);
-	}				
-	else if (type === "pitchMap") {
+	} else if (type === "pitchMap") {
 		let povData = {
 			heading: pMapId.getPov().heading,
 			pitch: Number(val)
 		};
 		pMapId.setPov(povData);					
-	} 
-	else if (type === "clearMap") {
+	} else if (type === "clearMap") {
 		for (var i=0;i<mapMarkers.length;i++) {
 			if (pMapId==mapMarkers[i][1]) {
 				mapMarkers[i][2].setMap(null);
