9e3a7231524db5ab43902cc26e61cd9128e705b3
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-01 20:58:09 +0800

    Update googlemap.js

--- SpBlocklyJS/googlemap_20241226/googlemap.js
+++ SpBlocklyJS/googlemap_20241226/googlemap.js
@@ -78,7 +78,7 @@ function updateMarkerContent(pID, newContent, type) {
 function getMarkerSetting(pID, newContent, property) {
 	for (var i=0;i<mapMarkers.length;i++) {
 		if ("point_"+pID==mapMarkers[i][0]) {
-			console.log(mapMarkers[i][2]);
+			console.log(JSON.stringify(mapMarkers[i][2]));
 			if (property=="latitude") {
 				var position = mapMarkers[i][2].position;
 				return position.lat;
