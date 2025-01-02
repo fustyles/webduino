337c155ac5e5f6e9cd43fefe2a6f5c5e2521e7f7
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-03 00:43:44 +0800

    Update blocks.js

--- LinkIt7697/France/blocks.js
+++ LinkIt7697/France/blocks.js
@@ -575,7 +575,8 @@ Blockly.Blocks['page_googlemap_initial'] = {
 		.setAlign(Blockly.ALIGN_RIGHT)	
 		.setCheck("String")
 		.appendField(Blockly.Msg["GOOGLEMAP_APIKEY"]);	  
-	this.appendStatementInput("statement");
+	this.appendStatementInput("statement")
+	    .appendField(Blockly.Msg["GOOGLEMAP_ADDMAP"]);
 	this.setInputsInline(true);
 	this.setPreviousStatement(true);
 	this.setNextStatement(true);
