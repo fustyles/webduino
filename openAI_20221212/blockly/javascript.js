030da92828af7bb073d34f2c2e0fabb10246bb43
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-11 23:54:26 +0800

    Update javascript.js

--- openAI_20221212/blockly/javascript.js
+++ openAI_20221212/blockly/javascript.js
@@ -32,8 +32,9 @@ Blockly.JavaScript['openai_text_response_clear'] = function (block) {
 
 Blockly.JavaScript['openai_image_initial'] = function (block) {
   var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC); 
+  var model = block.getFieldValue('model');
 
-  var code = 'openai_image_initial('+key+');\n';
+  var code = 'openai_image_initial('+key+', "'+model+'");\n';
   return code; 
 };
 
