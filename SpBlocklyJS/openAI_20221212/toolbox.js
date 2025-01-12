bba9fcc49487a5d511a591ee0ae385c604157ce2
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-12 09:42:15 +0800

    Update toolbox.js

--- SpBlocklyJS/openAI_20221212/toolbox.js
+++ SpBlocklyJS/openAI_20221212/toolbox.js
@@ -60,31 +60,6 @@ var catOpenAI = '<xml>'+
 '      </block>'+
 '    </value>'+
 '  </block>'+
-'  <block type="openai_text_initial">'+
-'    <value name="token">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+
-'    <value name="tokens">'+
-'      <block type="math_number">'+
-'        <field name="NUM">256</field>'+
-'      </block>'+
-'    </value>'+
-'  </block>'+
-'  <block type="openai_text_request">'+
-'    <value name="words">'+
-'      <block type="text">'+
-'        <field name="TEXT"></field>'+
-'      </block>'+
-'    </value>'+
-'  </block>'+  
-'  <block type="openai_text_response">'+
-'  </block>'+
-'  <block type="openai_text_response_get">'+
-'  </block>'+
-'  <block type="openai_text_response_clear">'+
-'  </block>'+
 '  <block type="openai_image_initial">'+
 '    <value name="key">'+
 '      <block type="text">'+
