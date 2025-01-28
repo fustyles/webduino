85b4132072db54ecbd1e0aac398343d2d3e41abf
fustyles <34397289+fustyles@users.noreply.github.com>
2025-01-29 01:16:40 +0800

    Update javascript.js

--- SpBlocklyJS/openAI_20221212/javascript.js
+++ SpBlocklyJS/openAI_20221212/javascript.js
@@ -1,3 +1,67 @@
+Blockly.JavaScript['openai_text_initial'] = function (block) {
+  var token = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC); 
+  var tokens = Blockly.JavaScript.valueToCode(block, 'tokens', Blockly.JavaScript.ORDER_ATOMIC);
+
+  var code = 'openai_text_initial('+token+', '+tokens+');\n';
+  return code; 
+};
+
+Blockly.JavaScript['openai_text_request'] = function (block) {
+  var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
+	
+  var code = 'openai_text_request('+words+');\n';
+  return code; 
+};
+
+Blockly.JavaScript['openai_text_response'] = function (block) {
+  var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
+  var code = 'openai_text_response = async function() {\n' + statements_do + '\n};\n';
+  return code;
+};
+
+Blockly.JavaScript['openai_text_response_get'] = function (block) {
+  var br = block.getFieldValue('br');
+  var code = 'openai_text_response_get(\''+br+'\')';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['openai_text_response_clear'] = function (block) {
+  var code = 'openai_text_response_clear();\n';
+  return code;
+};
+
+Blockly.JavaScript['openai_image_initial'] = function (block) {
+  var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC);
+  var model = block.getFieldValue('model');
+
+  var code = 'openai_image_initial('+key+', "'+model+'");\n';
+  return code; 
+};
+
+Blockly.JavaScript['openai_image_request'] = function (block) {
+  var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
+  var size = block.getFieldValue('size');
+  
+  var code = 'await openai_image_request('+words+',"'+size+'");\n';
+  return code; 
+};
+
+Blockly.JavaScript['openai_image_response'] = function (block) {
+  var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
+  var code = 'openai_image_response = async function() {\n' + statements_do + '\n};\n';
+  return code;
+};
+
+Blockly.JavaScript['openai_image_response_get'] = function (block) {
+  var code = 'openai_image_response_get()';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['openai_image_response_clear'] = function (block) {
+  var code = 'openai_image_response_clear();\n';
+  return code;
+};
+
 Blockly.JavaScript['openai_chat_initial'] = function (block) {
   var token = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC); 
   var role = Blockly.JavaScript.valueToCode(block, 'role', Blockly.JavaScript.ORDER_ATOMIC);   
@@ -10,14 +74,14 @@ Blockly.JavaScript['openai_chat_initial'] = function (block) {
 Blockly.JavaScript['openai_chat_request'] = function (block) {
   var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
 	
-  var code = 'openai_chat_request('+words+');\n';
+  var code = 'await openai_chat_request('+words+');\n';
   return code; 
 };
 
 Blockly.JavaScript['openai_chat_image_request'] = function (block) {
   var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
   var url = Blockly.JavaScript.valueToCode(block, 'url', Blockly.JavaScript.ORDER_ATOMIC)||"";
-  var code = 'openai_chat_image_request('+words+', '+url+');\n';
+  var code = 'await openai_chat_image_request('+words+', '+url+');\n';
   return code; 
 };
 
@@ -65,69 +129,5 @@ Blockly.JavaScript['openai_chat_content_file_remote'] = function (block) {
 Blockly.JavaScript['openai_chat_content_file_remote_insert'] = function (block) {
   var url = Blockly.JavaScript.valueToCode(block, 'url', Blockly.JavaScript.ORDER_ATOMIC)||"";
   var code = 'openai_chat_content_file_remote_insert('+url+');\n';
-  return code;
-};
-
-Blockly.JavaScript['openai_text_initial'] = function (block) {
-  var token = Blockly.JavaScript.valueToCode(block, 'token', Blockly.JavaScript.ORDER_ATOMIC); 
-  var tokens = Blockly.JavaScript.valueToCode(block, 'tokens', Blockly.JavaScript.ORDER_ATOMIC);
-
-  var code = 'openai_text_initial('+token+', '+tokens+');\n';
-  return code; 
-};
-
-Blockly.JavaScript['openai_text_request'] = function (block) {
-  var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
-	
-  var code = 'openai_text_request('+words+');\n';
-  return code; 
-};
-
-Blockly.JavaScript['openai_text_response'] = function (block) {
-  var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = 'openai_text_response = async function() {\n' + statements_do + '\n};\n';
-  return code;
-};
-
-Blockly.JavaScript['openai_text_response_get'] = function (block) {
-  var br = block.getFieldValue('br');
-  var code = 'openai_text_response_get(\''+br+'\')';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['openai_text_response_clear'] = function (block) {
-  var code = 'openai_text_response_clear();\n';
-  return code;
-};
-
-Blockly.JavaScript['openai_image_initial'] = function (block) {
-  var key = Blockly.JavaScript.valueToCode(block, 'key', Blockly.JavaScript.ORDER_ATOMIC);
-  var model = block.getFieldValue('model');
-
-  var code = 'openai_image_initial('+key+', "'+model+'");\n';
-  return code; 
-};
-
-Blockly.JavaScript['openai_image_request'] = function (block) {
-  var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
-  var size = block.getFieldValue('size');
-  
-  var code = 'openai_image_request('+words+',"'+size+'");\n';
-  return code; 
-};
-
-Blockly.JavaScript['openai_image_response'] = function (block) {
-  var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
-  var code = 'openai_image_response = async function() {\n' + statements_do + '\n};\n';
-  return code;
-};
-
-Blockly.JavaScript['openai_image_response_get'] = function (block) {
-  var code = 'openai_image_response_get()';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['openai_image_response_clear'] = function (block) {
-  var code = 'openai_image_response_clear();\n';
   return code;
 };
