ba2920864da63a3a19f0563624ca9a25f45e3c3a
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-09 12:45:01 +0800

    Update javascript.js

--- SpBlocklyJS/dvcbot_20241116/javascript.js
+++ SpBlocklyJS/dvcbot_20241116/javascript.js
@@ -11,6 +11,18 @@ Blockly.JavaScript['dvcbot_response'] = function (block) {
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
+Blockly.JavaScript['dvcbot_response_data'] = function (block) {
+  var br = block.getFieldValue('br');
+  var code = 'dvcbot_response_br(dvcbot_data, \''+br+'\')';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['dvcbot_result1'] = function (block) {
+  var userMessage = Blockly.JavaScript.valueToCode(block, 'userMessage', Blockly.JavaScript.ORDER_ATOMIC); 
+  var code = 'await dvcbot_result('+userMessage+', "s");\n';
+  return code;
+};
+
 Blockly.JavaScript['dvcbot_respons1'] = function (block) {
   var statements_do = Blockly.JavaScript.statementToCode(block, 'do'); 
   
@@ -22,17 +34,5 @@ Blockly.JavaScript['dvcbot_result'] = function (block) {
   var userMessage = Blockly.JavaScript.valueToCode(block, 'userMessage', Blockly.JavaScript.ORDER_ATOMIC); 
   var br = block.getFieldValue('br');
   var code = 'await dvcbot_result('+userMessage+', \''+br+'\')';
-  return [code, Blockly.JavaScript.ORDER_NONE];
-};
-
-Blockly.JavaScript['dvcbot_result1'] = function (block) {
-  var userMessage = Blockly.JavaScript.valueToCode(block, 'userMessage', Blockly.JavaScript.ORDER_ATOMIC); 
-  var code = 'await dvcbot_result('+userMessage+', "s");\n';
-  return code;
-};
-
-Blockly.JavaScript['dvcbot_response_data'] = function (block) {
-  var br = block.getFieldValue('br');
-  var code = 'dvcbot_response_br(dvcbot_data, \''+br+'\')';
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
