9fe4be757b1b53d075bd99efac6c6e76b9e7ba20
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-09 11:54:27 +0800

    Update en.js

--- SpBlocklyJS/dvcbot_20241116/en.js
+++ SpBlocklyJS/dvcbot_20241116/en.js
@@ -9,3 +9,4 @@ Blockly.Msg.DVCBOT_USERMESSAGE_SHOW = "Conversation Message";
 Blockly.Msg.DVCBOT_RESULT_GET_BR_SHOW = "Line Break";
 Blockly.Msg.DVCBOT_RESPONSE_SHOW = "After Receiving Response";
 Blockly.Msg.DVCBOT_RESPONSE_DATA_SHOW = "Response Data";
+Blockly.Msg.DVCBOT_RESPONSE_BR_SHOW = "Line Break Handling";
