c73eb7bf829cd02a18f98ff95764e8861aec3386
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-15 22:17:06 +0800

    Update javascript.js

--- dvcbot_20241116/blockly/javascript.js
+++ dvcbot_20241116/blockly/javascript.js
@@ -11,6 +11,25 @@ Blockly.JavaScript['dvcbot_response'] = function (block) {
   return [code, Blockly.JavaScript.ORDER_NONE];
 };
 
+Blockly.JavaScript['dvcbot_response_data'] = function (block) {
+  var br = block.getFieldValue('br');
+  var code = 'dvcbot_response_br(dvcbot_data, \''+br+'\')';
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['dvcbot_result1'] = function (block) {
+  var userMessage = Blockly.JavaScript.valueToCode(block, 'userMessage', Blockly.JavaScript.ORDER_ATOMIC); 
+  var code = 'await dvcbot_result('+userMessage+', "s");\n';
+  return code;
+};
+
+Blockly.JavaScript['dvcbot_response1'] = function (block) {
+  var statements_do = Blockly.JavaScript.statementToCode(block, 'do'); 
+  
+  var code = 'async function dvcbot_response_get(dvcbot_data) {\n  '+statements_do+'\n}\nwindow.dvcbot_response_get = dvcbot_response_get;\n';
+  return code; 
+};
+
 Blockly.JavaScript['dvcbot_result'] = function (block) {
   var userMessage = Blockly.JavaScript.valueToCode(block, 'userMessage', Blockly.JavaScript.ORDER_ATOMIC); 
   var br = block.getFieldValue('br');
