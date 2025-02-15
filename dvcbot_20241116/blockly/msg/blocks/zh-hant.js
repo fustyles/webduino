de4f352e8e351f1aefc9d0ef792508b6c56d215f
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-15 22:13:13 +0800

    Update zh-hant.js

--- dvcbot_20241116/blockly/msg/blocks/zh-hant.js
+++ dvcbot_20241116/blockly/msg/blocks/zh-hant.js
@@ -6,3 +6,6 @@ Blockly.Msg.DVCBOT_RESPONSE_SHOW = "取得Plugin回傳值";
 Blockly.Msg.DVCBOT_RESULT_SHOW = "取得回應";
 Blockly.Msg.DVCBOT_USERMESSAGE_SHOW = "對話訊息";
 Blockly.Msg.DVCBOT_RESULT_GET_BR_SHOW = "自動換行";
+Blockly.Msg.DVCBOT_RESPONSE_SHOW = "取得回應後";
+Blockly.Msg.DVCBOT_RESPONSE_DATA_SHOW = "回應資料";
+Blockly.Msg.DVCBOT_RESPONSE_BR_SHOW = "換行處理";
