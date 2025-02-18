7d71b34ec13c586750c2d3c29c25f724ba7101a9
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-18 21:05:24 +0800

    Add files via upload

---
+++ gemini_2024029/blockly/blocks.js
@@ -0,0 +1,212 @@
+Blockly.Blocks['gemini_chat_initial'] = {
+  init: function() {
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.GEMINI_CHAT_SHOW);
+  this.appendValueInput("token")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_APIKEY_SHOW);  
+  this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)	  
+      .appendField(Blockly.Msg.GEMINI_MODEL_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+	        ["gemini-1.5-flash","gemini-1.5-flash"],
+		["gemini-1.5-flash-8b","gemini-1.5-flash-8b"],
+	        ["gemini-1.5-pro","gemini-1.5-pro"],
+		["gemini-2.0-flash-lite-preview-02-05","gemini-2.0-flash-lite-preview-02-05"],
+		["gemini-2.0-flash-001","gemini-2.0-flash-001"]
+	  ]), "model");
+  this.appendValueInput("tokens")
+      .setCheck("Number")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_MAX_TOKENS);
+  this.appendValueInput("role")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_ROLE_SHOW);	  
+  this.setInputsInline(false);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(40);
+  this.setHelpUrl("https://aistudio.google.com/app/prompts/new_chat?utm_source=agd&utm_medium=referral&utm_campaign=core-cta&utm_content=");	  
+  }
+};
+
+Blockly.Blocks['gemini_chat_request'] = {
+  init: function() {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW);
+  this.appendValueInput("words")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_REQUEST_SHOW);	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_image_request'] = {
+	  init: function() {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)	  
+	  .appendField(Blockly.Msg.GEMINI_CHAT_VISION_SHOW);
+  this.appendValueInput("words")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_PROMPT_SHOW);
+  this.appendValueInput("url")
+      .setCheck(null)
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_IMAGE_URL_SHOW);	  
+  this.setInputsInline(false);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_response'] = {
+  init: function() {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)
+	  .appendField(Blockly.Msg.GEMINI_CHAT_RESPONSE_SHOW);
+  this.appendStatementInput("do");
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_response_data'] = {
+  init: function() {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW);
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.GEMINI_CHAT_RESPONSE_DATA_SHOW);
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.GEMINI_CHAT_RESPONSE_DATA_BR_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+			["N",""],
+			["Y \<br\>","br"],
+			["Y \\n","n"]		  
+	  ]), "br"); 	  
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_insert'] = {
+  init: function() {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW);
+  this.appendValueInput("words")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_INSERT_SHOW);
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_clear'] = {
+  init: function() {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)
+	  .appendField(Blockly.Msg.GEMINI_CHAT_CLEAR_SHOW);
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_content_file'] = {
+  init: function () {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)   
+	  .appendField(Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_SHOW);
+  this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown([
+			[Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_OPEN_SHOW,"open"],
+			[Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_SAVE_SHOW,"save"]	  
+	  ]), "func"); 	  
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true); 
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_content_file_remote'] = {
+  init: function () {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)   
+	  .appendField(Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_REMOTE_SHOW);
+  this.appendValueInput("url")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_REMOTE_URL_SHOW);	  
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true); 
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_content_file_remote_insert'] = {
+  init: function () {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)   
+	  .appendField(Blockly.Msg.GEMINI_CHAT_INSERT_SHOW);
+  this.appendValueInput("url")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_REMOTE_URL_INSERT_SHOW);	  
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true); 
+  this.setColour(40);
+  }
+};
+
+Blockly.Blocks['gemini_chat_content_file_remote_read'] = {
+  init: function () {
+  this.appendDummyInput()
+	  .appendField(Blockly.Msg.GEMINI_CHAT_SHOW)   
+	  .appendField(Blockly.Msg.GEMINI_CHAT_READ_SHOW);	  
+  this.appendDummyInput()
+      .setAlign(Blockly.ALIGN_RIGHT)  
+      .appendField(new Blockly.FieldDropdown([
+			["PDF", "application/pdf"],
+			["JavaScript", "text/javascript"],
+			["x-JavaScript", "application/x-javascript"],			
+			["Python", "application/x-python, text/x-python"],
+			["TXT", "text/plain"],
+			["HTML", "text/html"],
+			["CSS", "text/css"],
+			["Markdown", "text/md"],
+			["CSV", "text/csv"],
+			["XML", "text/xml"],
+			["RTF", "text/rtf"]
+	  ]), "type"); 
+  this.appendValueInput("url")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_CONTENT_FILE_REMOTE_URL_INSERT_SHOW);
+  this.appendValueInput("words")
+      .setCheck("String")
+      .setAlign(Blockly.ALIGN_RIGHT)
+      .appendField(Blockly.Msg.GEMINI_CHAT_PROMPT_SHOW);	  
+  this.setInputsInline(false);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true); 
+  this.setColour(40);
+  this.setHelpUrl("https://ai.google.dev/gemini-api/docs/document-processing?hl=zh-tw");	  
+  }
+};
