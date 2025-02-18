d0e10d28aaba5250a81557fc508bcb9d09261263
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-18 13:12:29 +0800

    Update javascript.js

--- gemini_2024029/blockly/javascript.js
+++ gemini_2024029/blockly/javascript.js
@@ -4,7 +4,7 @@ Blockly.JavaScript['gemini_chat_initial'] = function (block) {
   var model = block.getFieldValue('model');
   var role = Blockly.JavaScript.valueToCode(block, 'role', Blockly.JavaScript.ORDER_ATOMIC);   
 
-  var code = 'gemini_chat_initial('+token+', "'+model+'", '+tokens+', '+temperature+');\nawait delay(1);\ngemini_chat_insert('+role+', "");\n';
+  var code = 'await delay(1);\ngemini_chat_initial('+token+', "'+model+'", '+tokens+', '+temperature+');\nawait delay(1);\ngemini_chat_insert('+role+', "");\n';
   return code; 
 };
 
