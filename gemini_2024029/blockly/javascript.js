a3b62cb71c3c8a3a0a1d14eccde1b02a425ea2ce
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-20 12:55:46 +0800

    Update javascript.js

--- gemini_2024029/blockly/javascript.js
+++ gemini_2024029/blockly/javascript.js
@@ -9,6 +9,14 @@ Blockly.JavaScript['gemini_chat_initial'] = function (block) {
   return code; 
 };
 
+Blockly.JavaScript['gemini_chat_set'] = function (block) {
+  var property = block.getFieldValue('property');
+  var val = Blockly.JavaScript.valueToCode(block, 'val', Blockly.JavaScript.ORDER_ATOMIC)||"";	
+  
+  var code = 'await gemini_chat_set("'+property+'", '+val+');\n';
+  return code; 
+};
+
 Blockly.JavaScript['gemini_chat_request'] = function (block) {
   var words = Blockly.JavaScript.valueToCode(block, 'words', Blockly.JavaScript.ORDER_ATOMIC)||"";	
   
