d9841826077938d58f1602c42cb0498072a9eb84
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-20 22:09:23 +0800

    Update en.js

--- llm_20241219/blockly/msg/blocks/en.js
+++ llm_20241219/blockly/msg/blocks/en.js
@@ -19,5 +19,6 @@ Blockly.Msg["LLM_CHAT_RESPONSE_SHOW"] = "Execute after getting response text";
 Blockly.Msg["LLM_CHAT_RESPONSE_GET_SHOW"] = "Response Text";
 Blockly.Msg["LLM_CHAT_RESPONSE_GET_BR_SHOW"] = "Auto Line Break";
 Blockly.Msg["LLM_CHAT_RESPONSE_CLEAR_SHOW"] = "Clear Response Text";
+Blockly.Msg["LLM_CHAT_SET_SHOW"] = "Set";
 Blockly.Msg["LLM_CHAT_TEMPERATURE_SHOW"] = "Temperature";
 Blockly.Msg["LLM_CHAT_MAX_TOKENS_SHOW"] = "Maximum Tokens";
