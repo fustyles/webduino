25ed657fc725712ce5d03e2f4a9a7b88b188b881
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-20 12:38:07 +0800

    Update zh-hant.js

--- SpBlocklyJS/gemini_20240209/zh-hant.js
+++ SpBlocklyJS/gemini_20240209/zh-hant.js
@@ -2,6 +2,7 @@ Blockly.Msg["CATGEMINI"] = "Gemini";
 Blockly.Msg["GEMINI_CHAT_SHOW"] = "Gemini 聊天";
 Blockly.Msg["GEMINI_APIKEY_SHOW"] = "金鑰";
 Blockly.Msg["GEMINI_MODEL_SHOW"] = "模型";
+Blockly.Msg["GEMINI_CHAT_SET_SHOW"] = "設定";
 Blockly.Msg["GEMINI_CHAT_RESPONSE_SHOW"] = "取得回應後";
 Blockly.Msg["GEMINI_CHAT_RESPONSE_DATA_SHOW"] = "回應資料";
 Blockly.Msg["GEMINI_CHAT_REQUEST_SHOW"] = "對話內容";
