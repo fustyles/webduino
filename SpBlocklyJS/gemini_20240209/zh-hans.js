ec4fd877dd86a82d8bb9658620d3d966ba5361c8
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-20 12:38:05 +0800

    Update zh-hans.js

--- SpBlocklyJS/gemini_20240209/zh-hans.js
+++ SpBlocklyJS/gemini_20240209/zh-hans.js
@@ -2,6 +2,7 @@ Blockly.Msg["CATGEMINI"] = "Gemini";
 Blockly.Msg["GEMINI_CHAT_SHOW"] = "Gemini 聊天";
 Blockly.Msg["GEMINI_APIKEY_SHOW"] = "金钥";
 Blockly.Msg["GEMINI_MODEL_SHOW"] = "模型";
+Blockly.Msg["GEMINI_CHAT_SET_SHOW"] = "设置";
 Blockly.Msg["GEMINI_CHAT_RESPONSE_SHOW"] = "取得回应后";
 Blockly.Msg["GEMINI_CHAT_RESPONSE_DATA_SHOW"] = "回应资料";
 Blockly.Msg["GEMINI_CHAT_REQUEST_SHOW"] = "对话内容";
