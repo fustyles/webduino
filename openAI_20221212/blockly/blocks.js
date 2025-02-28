36811b8c274a6c0363cb6c659daf5ddfe04f52ee
fustyles <34397289+fustyles@users.noreply.github.com>
2025-02-28 23:24:56 +0800

    Update blocks.js

--- openAI_20221212/blockly/blocks.js
+++ openAI_20221212/blockly/blocks.js
@@ -187,6 +187,7 @@ Blockly.Blocks['openai_chat_initial'] = {
 			["gpt-4o-mini","gpt-4o-mini"],
 			["gpt-o1-mini","o1-mini"],
 			["gpt-o1-2024-12-17","o1-2024-12-17"],
+	      		["gpt-4.5-preview","gpt-4.5-preview"],
 			["Groq_llama-3.2-90b-vision-preview","llama-3.2-90b-vision-preview"],
 			["Groq_deepseek-r1-distill-llama-70b","deepseek-r1-distill-llama-70b"],
 			["Groq_deepseek-r1-distill-llama-70b-specdec","deepseek-r1-distill-llama-70b-specdec"],
