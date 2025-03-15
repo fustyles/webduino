ca8eb6062b3322284a06b624d51ff90a98243f14
fustyles <34397289+fustyles@users.noreply.github.com>
2025-03-15 16:18:06 +0800

    Update blocks.js

--- SpBlocklyJS/gemini_20240209/blocks.js
+++ SpBlocklyJS/gemini_20240209/blocks.js
@@ -10,10 +10,7 @@ Blockly.Blocks['gemini_chat_initial'] = {
       .appendField(Blockly.Msg.GEMINI_MODEL_SHOW)
       .appendField(new Blockly.FieldDropdown([
 		["gemini-2.0-flash-lite-preview-02-05","gemini-2.0-flash-lite-preview-02-05"],
-		["gemini-2.0-flash-001","gemini-2.0-flash-001"],	      
-	        ["gemini-1.5-flash","gemini-1.5-flash"],
-		["gemini-1.5-flash-8b","gemini-1.5-flash-8b"],
-	        ["gemini-1.5-pro","gemini-1.5-pro"]
+		["gemini-2.0-flash-001","gemini-2.0-flash-001"]
 	  ]), "model");
   this.appendValueInput("tokens")
       .setCheck("Number")
