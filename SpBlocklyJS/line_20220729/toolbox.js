2ef56126ee335e24352473cc0d725c92eccbaef9
fustyles <34397289+fustyles@users.noreply.github.com>
2025-04-01 12:56:16 +0800

    Update toolbox.js

--- SpBlocklyJS/line_20220729/toolbox.js
+++ SpBlocklyJS/line_20220729/toolbox.js
@@ -1,49 +1,5 @@
 var catLineNotify ='<xml>'+
 '    <category name="%{BKY_CATLINENOTIFY}" id="catLineNotify" colour="30">'+
-'	  <block type="linenotify">'+
-'		<value name="token_">'+
-'		  <block type="text">'+
-'			<field name="TEXT"></field>'+
-'		  </block>'+
-'		</value>'+
-'		<value name="text_">'+
-'		  <block type="text">'+
-'			<field name="TEXT"></field>'+
-'		  </block>'+
-'		</value>'+
-'		<value name="packageId_">'+
-'		  <block type="math_number">'+
-'			<field name="NUM">1</field>'+
-'		  </block>'+
-'		</value>'+
-'		<value name="stickerId_">'+
-'		  <block type="math_number">'+
-'			<field name="NUM">2</field>'+
-'		  </block>'+
-'		</value>'+
-'		<value name="originalContentUrl_">'+
-'		  <block type="text">'+
-'			<field name="TEXT"></field>'+
-'		  </block>'+
-'		</value>'+
-'		<value name="previewImageUrl_">'+
-'		  <block type="text">'+
-'			<field name="TEXT"></field>'+
-'		  </block>'+
-'		</value>'+
-'	  </block>'+
-'	  <block type="linenotify_image">'+
-'	    <value name="notify_token">'+
-'	      <block type="text">'+
-'	        <field name="TEXT"></field>'+
-'	      </block>'+
-'	    </value>'+
-'	    <value name="notify_id">'+
-'	      <block type="text">'+
-'	        <field name="TEXT"></field>'+
-'	      </block>'+
-'	    </value>'+
-'	  </block>'+
 '	  <block type="linebot_notify">'+
 '	    <value name="bot_token">'+
 '	      <block type="text">'+
