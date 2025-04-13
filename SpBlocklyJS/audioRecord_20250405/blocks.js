c4c0fa8245aad50f6d322d8fcf7cb9a0ffa93288
fustyles <34397289+fustyles@users.noreply.github.com>
2025-04-13 18:23:38 +0800

    Update blocks.js

--- SpBlocklyJS/audioRecord_20250405/blocks.js
+++ SpBlocklyJS/audioRecord_20250405/blocks.js
@@ -5,16 +5,8 @@ Blockly.Blocks['audiorecord_save_initial'] = {
         .appendField(Blockly.Msg.AUDIORECORD_SAVE_SHOW);
     this.appendValueInput("index_")
         .setCheck("Number")
-        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);	
-    this.appendValueInput("start_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_START_ID_SHOW);
-    this.appendValueInput("stop_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_STOP_ID_SHOW);		  	    
-    this.setInputsInline(false);
+        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);			  	    
+    this.setInputsInline(true);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
     this.setColour(180);
@@ -29,15 +21,7 @@ Blockly.Blocks['audiorecord_gemini_initial'] = {
     this.appendValueInput("index_")
         .setCheck("Number")
         .setAlign(Blockly.ALIGN_RIGHT)	    
-        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		
-    this.appendValueInput("start_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_START_ID_SHOW);
-    this.appendValueInput("stop_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_STOP_ID_SHOW);   
+        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		  
     this.appendValueInput("key_")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
@@ -74,15 +58,7 @@ Blockly.Blocks['audiorecord_openai_initial'] = {
     this.appendValueInput("index_")
         .setCheck("Number")
         .setAlign(Blockly.ALIGN_RIGHT)	    
-        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		
-    this.appendValueInput("start_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_START_ID_SHOW);
-    this.appendValueInput("stop_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_STOP_ID_SHOW);   
+        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		  
     this.appendValueInput("key_")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
