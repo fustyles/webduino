721d130b0db4e165b256116c2bc79e763b2b6b77
fustyles <34397289+fustyles@users.noreply.github.com>
2025-04-13 18:23:41 +0800

    Update javascript.js

--- SpBlocklyJS/audioRecord_20250405/javascript.js
+++ SpBlocklyJS/audioRecord_20250405/javascript.js
@@ -1,20 +1,16 @@
 Blockly.JavaScript['audiorecord_save_initial'] = function(block) {
   var audioIndex = Blockly.JavaScript.valueToCode(block, 'index_', Blockly.JavaScript.ORDER_ATOMIC);	
-  var buttonStartID = Blockly.JavaScript.valueToCode(block, 'start_', Blockly.JavaScript.ORDER_ATOMIC);
-  var buttonStopID = Blockly.JavaScript.valueToCode(block, 'stop_', Blockly.JavaScript.ORDER_ATOMIC);
-  var code = 'recording_save_initial('+audioIndex+', '+buttonStartID+', '+buttonStopID+');\n';
+  var code = 'recording_save_initial('+audioIndex+');\n';
   return code;
 };
 
 Blockly.JavaScript['audiorecord_gemini_initial'] = function(block) { 
   var audioIndex = Blockly.JavaScript.valueToCode(block, 'index_', Blockly.JavaScript.ORDER_ATOMIC);	
-  var buttonStartID = Blockly.JavaScript.valueToCode(block, 'start_', Blockly.JavaScript.ORDER_ATOMIC);
-  var buttonStopID = Blockly.JavaScript.valueToCode(block, 'stop_', Blockly.JavaScript.ORDER_ATOMIC);
   var geminiKey = Blockly.JavaScript.valueToCode(block, 'key_', Blockly.JavaScript.ORDER_ATOMIC);
   var geminiPrompt = Blockly.JavaScript.valueToCode(block, 'prompt_', Blockly.JavaScript.ORDER_ATOMIC);
   var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
   
-  var code = 'recording_GeminiSTT_initial('+audioIndex+', '+buttonStartID+', '+buttonStopID+', '+geminiKey+', "gemini-2.0-flash", '+geminiPrompt+');\n'+
+  var code = 'recording_GeminiSTT_initial('+audioIndex+', '+geminiKey+', "gemini-2.0-flash", '+geminiPrompt+');\n'+
              'audioGeminiSTT = async function(geminiResult) {\n  ' + statements_do + '\n  };\n';
   return code;
 };
@@ -26,15 +22,13 @@ Blockly.JavaScript['audiorecord_gemini_get'] = function(block) {
 
 Blockly.JavaScript['audiorecord_openai_initial'] = function(block) { 
   var audioIndex = Blockly.JavaScript.valueToCode(block, 'index_', Blockly.JavaScript.ORDER_ATOMIC);	
-  var buttonStartID = Blockly.JavaScript.valueToCode(block, 'start_', Blockly.JavaScript.ORDER_ATOMIC);
-  var buttonStopID = Blockly.JavaScript.valueToCode(block, 'stop_', Blockly.JavaScript.ORDER_ATOMIC);
   var openaiKey = Blockly.JavaScript.valueToCode(block, 'key_', Blockly.JavaScript.ORDER_ATOMIC);
   var openaiModel = block.getFieldValue('model_');
   var openaiPrompt = Blockly.JavaScript.valueToCode(block, 'prompt_', Blockly.JavaScript.ORDER_ATOMIC);
   var openaiLanguage = block.getFieldValue('language_');
   var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
   
-  var code = 'recording_openAISTT_initial('+audioIndex+', '+buttonStartID+', '+buttonStopID+', '+openaiKey+', "'+openaiModel+'", '+openaiPrompt+', "'+openaiLanguage+'");\n'+
+  var code = 'recording_openAISTT_initial('+audioIndex+', '+openaiKey+', "'+openaiModel+'", '+openaiPrompt+', "'+openaiLanguage+'");\n'+
              'audioOpenAISTT = async function(openaiResult) {\n  ' + statements_do + '\n  };\n';
   return code;
 };
