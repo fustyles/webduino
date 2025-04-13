5a14f75ae0795884139c5ed4e2dcbcf78691495a
fustyles <34397289+fustyles@users.noreply.github.com>
2025-04-13 19:02:20 +0800

    Update blocks.js

--- audioRecord_20250405/blockly/blocks.js
+++ audioRecord_20250405/blockly/blocks.js
@@ -5,15 +5,7 @@ Blockly.Blocks['audiorecord_save_initial'] = {
         .appendField(Blockly.Msg.AUDIORECORD_SAVE_SHOW);
     this.appendValueInput("index_")
         .setCheck("Number")
-        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);	
-    this.appendValueInput("start_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_START_ID_SHOW);
-    this.appendValueInput("stop_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_STOP_ID_SHOW);		  	    
+        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);			  	    
     this.setInputsInline(false);
     this.setPreviousStatement(true);
     this.setNextStatement(true);
@@ -29,15 +21,7 @@ Blockly.Blocks['audiorecord_gemini_initial'] = {
     this.appendValueInput("index_")
         .setCheck("Number")
         .setAlign(Blockly.ALIGN_RIGHT)	    
-        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		
-    this.appendValueInput("start_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_START_ID_SHOW);
-    this.appendValueInput("stop_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_STOP_ID_SHOW);   
+        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		  
     this.appendValueInput("key_")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
@@ -74,15 +58,7 @@ Blockly.Blocks['audiorecord_openai_initial'] = {
     this.appendValueInput("index_")
         .setCheck("Number")
         .setAlign(Blockly.ALIGN_RIGHT)	    
-        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		
-    this.appendValueInput("start_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_START_ID_SHOW);
-    this.appendValueInput("stop_")
-        .setCheck("String")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField(Blockly.Msg.AUDIORECORD_STOP_ID_SHOW);   
+        .appendField(Blockly.Msg.AUDIORECORD_DEVICE_INDEX_SHOW);		  
     this.appendValueInput("key_")
         .setCheck("String")
         .setAlign(Blockly.ALIGN_RIGHT)
@@ -179,5 +155,29 @@ Blockly.Blocks['audiorecord_openai_get'] = {
     this.setInputsInline(true);	  
     this.setOutput(true, null);  
     this.setColour(260);
+  }  
+};
+
+Blockly.Blocks['audiorecord_run'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.AUDIORECORD_SHOW);
+    this.appendDummyInput() 
+        .setAlign(Blockly.ALIGN_RIGHT)	    
+        .appendField(new Blockly.FieldDropdown([
+			[Blockly.Msg.AUDIORECORD_SAVE_SHOW,"save"],
+			[Blockly.Msg.AUDIORECORD_GEMINI_SHOW,"gemini"],
+			[Blockly.Msg.AUDIORECORD_OPENAI_SHOW,"openai"]
+	    ]), "type_"); 	  
+    this.appendDummyInput() 
+        .setAlign(Blockly.ALIGN_RIGHT)	    
+        .appendField(new Blockly.FieldDropdown([
+			[Blockly.Msg.AUDIORECORD_START_SHOW,"start"],
+			[Blockly.Msg.AUDIORECORD_STOP_SHOW,"stop"]
+	    ]), "func_");	   	      
+    this.setInputsInline(true);
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(300);
   }  
 };
