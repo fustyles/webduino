4b81f603f78fe832f5374bc89843065afa6b4ad4
fustyles <34397289+fustyles@users.noreply.github.com>
2025-04-19 12:31:39 +0800

    Add files via upload

--- LinkIt7697/France/javascript.js
+++ LinkIt7697/France/javascript.js
@@ -5465,9 +5465,9 @@ Blockly.Arduino['amb82_mini_myfirmata'] = function(block) {
 	framesize = width +", "+height;
   Blockly.Arduino.definitions_['define_linkit_wifi_include'] ='#include <WiFi.h>\nWiFiSSLClient client;\n#include "VideoStream.h"\nVideoSetting config('+framesize+', CAM_FPS, VIDEO_JPEG, 1);\nchar ssid[] = '+ssid+';\nchar pass[] = '+pass+';\nchar ssid_ap[] = '+ssid_ap+';\nchar pass_ap[] = '+pass_ap+';\nchar channel_ap[] = "2";\nuint32_t img_addr = 0;\nuint32_t img_len = 0;\n';
   Blockly.Arduino.definitions_.define_CONFIG_INIC_IPC_HIGH_TP = '#define CONFIG_INIC_IPC_HIGH_TP\n';
-  Blockly.Arduino.definitions_.define_server80 = 'WiFiServer server81(80);\n';
+  Blockly.Arduino.definitions_.define_server80 = 'WiFiServer server80(80);\n';
   Blockly.Arduino.definitions_.define_server81 = 'WiFiServer server81(81);\n';
-  Blockly.Arduino.definitions_.define_server82 = 'WiFiServer server81(82);\n';
+  Blockly.Arduino.definitions_.define_server82 = 'WiFiServer server82(82);\n';
   Blockly.Arduino.definitions_.define_base64 ='#include "Base64.h"';
 	
   Blockly.Arduino.variables_['getCommand'] = 'String Feedback="",bleData="",Command="",cmd="",p1="",p2="",p3="",p4="",p5="",p6="",p7="",p8="",p9="";\nbyte receiveState=0,cmdState=1,pState=1,questionState=0,equalState=0,semicolonState=0;\n';
