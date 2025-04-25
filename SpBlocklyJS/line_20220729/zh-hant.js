a714e0ae03af30850740dbd0ee217069e53fce3e
fustyles <34397289+fustyles@users.noreply.github.com>
2025-04-25 14:20:30 +0800

    Update zh-hant.js

--- SpBlocklyJS/line_20220729/zh-hant.js
+++ SpBlocklyJS/line_20220729/zh-hant.js
@@ -28,3 +28,4 @@ Blockly.Msg["LINENOTIFY_LOCATION_SHOW"] = "位置";
 Blockly.Msg["LINENOTIFY_LATITUDE_SHOW"] = "緯度";
 Blockly.Msg["LINENOTIFY_LONGITUDE_SHOW"] = "經度";                                     
 Blockly.Msg["LINEBOT_SHOW"] = "機器人"; 
+Blockly.Msg["LINENOTIFY_REPLYTOKEN_SHOW"] = "回覆權杖"; 
