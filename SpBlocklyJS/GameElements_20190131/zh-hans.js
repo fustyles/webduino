b8b0ca753aff9b421b214a5924fd646d2bcfb309
fustyles <34397289+fustyles@users.noreply.github.com>
2025-06-09 12:54:45 +0800

    Update zh-hans.js

--- SpBlocklyJS/GameElements_20190131/zh-hans.js
+++ SpBlocklyJS/GameElements_20190131/zh-hans.js
@@ -647,6 +647,7 @@ Blockly.Msg["ENCODEURI"] = "网址编码";
 Blockly.Msg["DECODEURI"] = "网址解码";
 
 Blockly.Msg["CATPAGEELEMENTS_JSON"] = "Json";
+Blockly.Msg["JSON"] = "JSON";
 Blockly.Msg["JSON_VARIABLE_SET"] = "设定为";
 Blockly.Msg["JSON_TYPE_STRING"] = "文字格式转成JSON格式";
 Blockly.Msg["JSON_VARIABLE_GET"] = "获取";
