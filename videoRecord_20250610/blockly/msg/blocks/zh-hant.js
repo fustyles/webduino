73c38b668e061eeade9544464659e8173ee35ea6
fustyles <34397289+fustyles@users.noreply.github.com>
2025-08-11 08:45:19 +0800

    Update zh-hant.js

--- videoRecord_20250610/blockly/msg/blocks/zh-hant.js
+++ videoRecord_20250610/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg["VIDEORECORD_SHOW"] = "錄影";
 Blockly.Msg["VIDEORECORD_SAVE_SHOW"] = "另存新檔";
-Blockly.Msg["VIDEORECORD_DEVICE_INDEX_SHOW"] = "裝置索引";
+Blockly.Msg["VIDEORECORD_DEVICE_INDEX_SHOW"] = "視訊裝置索引";
 Blockly.Msg["VIDEORECORD_START_ID_SHOW"] = "開始按鈕元素原始代碼";
 Blockly.Msg["VIDEORECORD_STOP_ID_SHOW"] = "停止按鈕元素原始代碼";
 Blockly.Msg["VIDEORECORD_GEMINI_SHOW"] = "Gemini 影片轉文字";
@@ -13,3 +13,4 @@ Blockly.Msg["VIDEORECORD_STOP_SHOW"] = "結束錄影";
 Blockly.Msg["VIDEORECORD_AUDIO_SHOW"] = "錄音";
 Blockly.Msg["VIDEORECORD_URL_SHOW"] = "錄影檔連結";
 Blockly.Msg["VIDEORECORD_NORMAL_SHOW"] = "一般";
+
