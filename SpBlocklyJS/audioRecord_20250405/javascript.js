e1c1fde30e2ee5ea0a6cbfe20401e96bd9d1a204
fustyles <34397289+fustyles@users.noreply.github.com>
2025-08-24 20:35:51 +0800

    Update javascript.js

--- SpBlocklyJS/audioRecord_20250405/javascript.js
+++ SpBlocklyJS/audioRecord_20250405/javascript.js
@@ -10,7 +10,7 @@ Blockly.JavaScript['audiorecord_gemini_initial'] = function(block) {
   var geminiPrompt = Blockly.JavaScript.valueToCode(block, 'prompt_', Blockly.JavaScript.ORDER_ATOMIC);
   var statements_do = Blockly.JavaScript.statementToCode(block, 'do_');
   
-  var code = 'recording_GeminiSTT_initial('+audioIndex+', '+geminiKey+', "gemini-2.0-flash", '+geminiPrompt+');\n'+
+  var code = 'recording_GeminiSTT_initial('+audioIndex+', '+geminiKey+', "gemini-2.5-flash", '+geminiPrompt+');\n'+
              'audioGeminiSTT = async function(geminiResult) {\n  ' + statements_do + '\n  };\n';
   return code;
 };
@@ -64,4 +64,5 @@ Blockly.JavaScript['audiorecord_run'] = function(block) {
 Blockly.JavaScript['audiorecord_audio_get'] = function(block) {
   var code = 'audioUrl';
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
+
 };
