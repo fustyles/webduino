38e65a850078ca32f89a48e58152e1875a81f891
fustyles <34397289+fustyles@users.noreply.github.com>
2025-09-19 18:31:13 +0800

    Update zh-hant.js

--- GameElements_20190131/blockly/msg/blocks/zh-hant.js
+++ GameElements_20190131/blockly/msg/blocks/zh-hant.js
@@ -474,6 +474,7 @@ Blockly.Msg.PASSWORD_VALUE = "值";
 Blockly.Msg.PASSWORD_SET = "設定";
 Blockly.Msg.PASSWORD_GET = "取得";
 Blockly.Msg.PASSWORD_DELETE = "刪除";
+Blockly.Msg.PASSWORD_ONCHANGE = "改變密碼後執行";
 Blockly.Msg.MAXLENGTH = "最大長度";
 Blockly.Msg.FORM_CREATE = "表單 建立";
 Blockly.Msg.FORM_ACTION = "提交網址";
@@ -696,4 +697,5 @@ Blockly.Msg["AUDIO_TEXT"] = "文字";
 Blockly.Msg["AUDIO_PLAY"] = "播放";
 Blockly.Msg["AUDIO_PAUSE"] = "暫停";
 Blockly.Msg["AUDIO_LOAD"] = "重新載入";
+
 
