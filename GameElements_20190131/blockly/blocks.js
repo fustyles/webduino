d2749836d91d7c8b4ae7b55ed86e9c049269eae7
fustyles <34397289+fustyles@users.noreply.github.com>
2025-09-21 23:59:49 +0800

    Update blocks.js

--- GameElements_20190131/blockly/blocks.js
+++ GameElements_20190131/blockly/blocks.js
@@ -4308,8 +4308,7 @@ Blockly.Blocks['div_set'] = {
 	  [Blockly.Msg.PROPERTY_SCROLLX,"scrollX"], 
 	  [Blockly.Msg.PROPERTY_SCROLLY,"scrollY"], 
 	  [Blockly.Msg.PROPERTY_SCROLLLEFT,"scrollLeft"], 
-	  [Blockly.Msg.PROPERTY_SCROLLTOP,"scrollTop"], 
-	  [Blockly.Msg.PROPERTY_TEXTCONTENT,"textContent"],		  
+	  [Blockly.Msg.PROPERTY_SCROLLTOP,"scrollTop"], 	  
 	  [Blockly.Msg.PROPERTY_INNERHTML,"innerHTML"],
 	  [Blockly.Msg.PROPERTY_INNERHTML + " (markdown)","innerHTML_markdown"], 	
 	  [Blockly.Msg.PROPERTY_ZINDEX,"zindex"], 
@@ -8049,6 +8048,7 @@ Blockly.Blocks['audio_delete'] = {
   this.setColour(30);
   }
 };
+
 
 
 
