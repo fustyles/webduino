79389a51c58ffbce3dd60cc223d602361674b3d1
fustyles <34397289+fustyles@users.noreply.github.com>
2025-09-26 11:33:39 +0800

    Update zh-hant.js

--- coco-ssd_20201012/blockly/msg/blocks/zh-hant.js
+++ coco-ssd_20201012/blockly/msg/blocks/zh-hant.js
@@ -1,6 +1,6 @@
 Blockly.Msg.COCOSSD_SHOW = "物件偵測(coco-ssd)";
 Blockly.Msg.COCOSSD_OBJECT_SHOW = "偵測物件";
-Blockly.Msg.COCOSSD_OBJECT_ARRAY_SHOW = "偵測物件陣列";
+Blockly.Msg.COCOSSD_OBJECT_ARRAY_SHOW = "偵測物件資料陣列";
 Blockly.Msg.COCOSSD_OBJECT_ALL_SHOW = "全部";
 Blockly.Msg.COCOSSD_OBJECT_NUMBER_SHOW = "偵測物件數目";
 Blockly.Msg.COCOSSD_INDEX_SHOW = "索引";
@@ -38,4 +38,5 @@ Blockly.Msg.COCOSSD_GEOFENCE = "電子圍籬";
 Blockly.Msg.COCOSSD_XY_IN_TRIANGLE = "在三邊形的範圍內";
 Blockly.Msg.COCOSSD_XY_IN_QUADRILATERAL = "在凸四邊形的範圍內";
 Blockly.Msg.COCOSSD_XY_ARRANGE = "(座標依序排列)";
+
 
