99025d5d10605ea4ddad4e4b7a051d26e3798d80
fustyles <34397289+fustyles@users.noreply.github.com>
2025-09-26 14:38:48 +0800

    Update zh-hant.js

--- SpBlocklyJS/knn-classifier_20201012/zh-hant.js
+++ SpBlocklyJS/knn-classifier_20201012/zh-hant.js
@@ -15,10 +15,11 @@ Blockly.Msg["KNNCLASSIFIER_SRC_SHOW"] = "串流網址";
 Blockly.Msg["KNNCLASSIFIER_CLASS_SHOW"] = "設定分類";
 Blockly.Msg["KNNCLASSIFIER_CLASSNAME_SHOW"] = "名稱";
 Blockly.Msg["KNNCLASSIFIER_CLEARCLASSES_SHOW"] = "清除訓練資料";	   
-Blockly.Msg["KNNCLASSIFIER_SAVEMODEL_SHOW"] = "儲存模型"; 	   
-Blockly.Msg["KNNCLASSIFIER_LOADMODEL_SHOW"] = "載入模型";
-Blockly.Msg["KNNCLASSIFIER_LOADMODELURL_SHOW"] = "載入模型網址";
+Blockly.Msg["KNNCLASSIFIER_SAVEMODEL_SHOW"] = "儲存訓練資料檔"; 	   
+Blockly.Msg["KNNCLASSIFIER_LOADMODEL_SHOW"] = "載入訓練資料檔";
+Blockly.Msg["KNNCLASSIFIER_LOADMODELURL_SHOW"] = "載入訓練資料檔網址";
 Blockly.Msg["KNNCLASSIFIER_PROBABILITY_SHOW"] = "可能性";
 Blockly.Msg["KNNCLASSIFIER_RECOGNITIED_SHOW"] = "分類完成後";
 Blockly.Msg["KNNCLASSIFIER_CLASS_ARRAY_SHOW"] = "取得分類資料陣列";
 Blockly.Msg["KNNCLASSIFIER_CANVAS_GET_SHOW"] = "取得影像代碼";
+
