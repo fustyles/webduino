3035d229babb61091490d050ba202b75c3985ce7
fustyles <34397289+fustyles@users.noreply.github.com>
2025-09-29 18:25:47 +0800

    Add files via upload

--- SpBlocklyJS/tesseract.js_20201012/tesseract.js
+++ SpBlocklyJS/tesseract.js_20201012/tesseract.js
@@ -48,13 +48,8 @@
 		video.height = input_height;
 		video.style.width = input_width+"px";
 		video.style.height = input_height+"px";	
-		video.style.visibility = "visible";
+		video.style.visibility = "hidden";
 
-		var canvas = document.getElementById("gamecanvas_tesseract");  
-		canvas.setAttribute("width", video.width);
-		canvas.setAttribute("height", video.height);
-		canvas.style.width = video.width+"px";
-		canvas.style.height = video.height+"px";
 
 		if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
 			console.log("enumerateDevices() not supported.");
