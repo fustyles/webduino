cefbae2d91469d340d5143da9a09f57329de1555
fustyles <34397289+fustyles@users.noreply.github.com>
2025-11-30 23:08:42 +0800

    Update toolbox.js

--- SpBlocklyJS/MQTT_20220324/toolbox.js
+++ SpBlocklyJS/MQTT_20220324/toolbox.js
@@ -3,7 +3,7 @@ var catMQTT = '<xml>'+
 	  '<block type="fu_mqtt_setup_js">'+
 		'<value name="server">'+
 		  '<block type="text">'+
-			'<field name="TEXT">wss:\/\/broker.mqttgo.io:8084\/mqtt</field>'+
+			'<field name="TEXT">wss:\/\/mqttgo.io:8084\/mqtt</field>'+
 		  '</block>'+
 		'</value>'+
 		'<value name="user">'+
@@ -56,3 +56,4 @@ var catMQTT = '<xml>'+
 	  '</block>'+	
     '</category>'+
 '</xml>';	
+
