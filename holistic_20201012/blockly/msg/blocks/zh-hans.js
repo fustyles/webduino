3994239172902c46324915d5ad75c74acfa51a7b
fustyles <34397289+fustyles@users.noreply.github.com>
2025-12-15 13:43:16 +0800

    Update zh-hans.js

--- holistic_20201012/blockly/msg/blocks/zh-hans.js
+++ holistic_20201012/blockly/msg/blocks/zh-hans.js
@@ -1,4 +1,4 @@
-Blockly.Msg.HOLISTIC_SHOW = "全身侦测";
+Blockly.Msg.HOLISTIC_SHOW = "全身侦测(MP)";
 Blockly.Msg.HOLISTIC_PERSON_SHOW = "索引";
 Blockly.Msg.HOLISTIC_PART_SHOW = "部位";
 Blockly.Msg.HOLISTIC_WIDTH_SHOW = "宽度";
