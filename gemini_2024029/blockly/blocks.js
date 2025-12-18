5fab62e4e3bc589f349ae63cd3e3ad51545e08a0
fustyles <34397289+fustyles@users.noreply.github.com>
2025-12-19 07:41:58 +0800

    Update blocks.js

--- gemini_2024029/blockly/blocks.js
+++ gemini_2024029/blockly/blocks.js
@@ -10,12 +10,13 @@ Blockly.Blocks['gemini_chat_initial'] = {
       .setAlign(Blockly.ALIGN_RIGHT)	  
       .appendField(Blockly.Msg.GEMINI_MODEL_SHOW)
       .appendField(new Blockly.FieldDropdown([
+		["gemini-3-flash-preview","gemini-3-flash-preview"],
+		["gemini-2.5-flash","gemini-2.5-flash"],
+		["gemini-2.5-flash-lite-preview-06-17","gemini-2.5-flash-lite-preview-06-17"],
 		["gemini-2.0-flash","gemini-2.0-flash"],	      
 		["gemini-2.0-flash-lite","gemini-2.0-flash-lite"],
 		["gemini-2.0-flash-lite-preview-02-05","gemini-2.0-flash-lite-preview-02-05"],
-		["gemini-2.0-flash-001","gemini-2.0-flash-001"],
-		["gemini-2.5-flash","gemini-2.5-flash"],
-		["gemini-2.5-flash-lite-preview-06-17","gemini-2.5-flash-lite-preview-06-17"]
+		["gemini-2.0-flash-001","gemini-2.0-flash-001"]		  
 	  ]), "model");
   this.appendValueInput("tokens")
       .setCheck("Number")
@@ -320,5 +321,6 @@ Blockly.Blocks['gemini_youtube'] = {
   this.setColour(90);
   }
 };
+
 
 
