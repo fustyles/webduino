e7a40280ff98c814e296f8dc56893e8163545d95
fustyles <34397289+fustyles@users.noreply.github.com>
2026-06-16 15:04:21 +0800

    Update javascript.js

--- coco-ssd_20201012/blockly/javascript.js
+++ coco-ssd_20201012/blockly/javascript.js
@@ -15,7 +15,7 @@ Blockly.JavaScript['cocossd_list'] = function(block) {
 
 Blockly.JavaScript['cocossd_name'] = function(block) { 
   var value_object_ = block.getFieldValue('object_')
-  return [value_object_, Blockly.JavaScript.ORDER_NONE];
+  return ['"'+value_object_+'"', Blockly.JavaScript.ORDER_NONE];
 };
 
 Blockly.JavaScript['cocossd_object'] = function(block) { 
