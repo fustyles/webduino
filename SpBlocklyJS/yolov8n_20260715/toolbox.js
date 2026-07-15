33fe76c4b5043776561e9ebb5e753dfd345d5e89
fustyles <34397289+fustyles@users.noreply.github.com>
2026-07-15 20:39:56 +0800

    Update toolbox.js

--- SpBlocklyJS/yolov8n_20260715/toolbox.js
+++ SpBlocklyJS/yolov8n_20260715/toolbox.js
@@ -11,7 +11,7 @@ var catYolov8n ='<xml>'+
 '            <field name="TEXT">person,bicycle,car,motorcycle,airplane,bus,train,truck,boat,traffic light,fire hydrant,stop sign,parking meter,bench,bird,cat,dog,horse,sheep,cow,elephant,bear,zebra,giraffe,backpack,umbrella,handbag,tie,suitcase,frisbee,skis,snowboard,sports ball,kite,baseball bat,baseball glove,skateboard,surfboard,tennis racket,bottle,wine glass,cup,fork,knife,spoon,bowl,banana,apple,sandwich,orange,broccoli,carrot,hot dog,pizza,donut,cake,chair,couch,potted plant,bed,dining table,toilet,tv,laptop,mouse,remote,keyboard,cell phone,microwave,oven,toaster,sink,refrigerator,book,clock,vase,scissors,teddy bear,hair drier,toothbrush</field>'+
 '          </block>'+
 '        </value>'+
-'        <field name="scorelimit_">0.5</field>'+
+'        <field name="scorelimit_">0.8</field>'+
 '      </block>'+
 '      <block type="yolov8n_startvideo_media">'+
 '        <value name="width_">'+
