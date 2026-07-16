d05e7b9844540e0695fb325439d3323b45b3cf20
fustyles <34397289+fustyles@users.noreply.github.com>
2026-07-16 19:42:14 +0800

    Add files via upload

--- LinkIt7697/France/blocks.js
+++ LinkIt7697/France/blocks.js
@@ -1,3 +1,351 @@
+
+Blockly.Blocks['yolov8n_esp32cam'] = {
+  init: function() {
+    this.appendDummyInput()
+		.appendField(Blockly.Msg.SERVERMODULE_JAVASCRIPT_SHOW);
+    this.appendDummyInput()
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);		
+    this.appendStatementInput("javascript_initial")
+        .setCheck(null)
+		.appendField(Blockly.Msg.YOLOV8N_INITIAL_SHOW);	
+    this.appendStatementInput("javascript_recognition")
+        .setCheck(null)
+		.appendField(Blockly.Msg.YOLOV8N_RECOGNITION_SHOW);		
+	this.setInputsInline(false);
+    this.setOutput(true, null);
+    this.setColour(Blockly.Msg["HUE_26"])
+	}
+};
+
+Blockly.Blocks['yolov8n_video'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);
+  this.appendValueInput("modelpath_")
+      .setAlign(Blockly.ALIGN_RIGHT)  
+      .setCheck("String")
+      .appendField(Blockly.Msg.YOLOV8N_MODELPATH_SHOW);  
+  this.appendValueInput("classes_")
+      .setAlign(Blockly.ALIGN_RIGHT)    
+      .setCheck("String")
+      .appendField(Blockly.Msg.YOLOV8N_CLASSES_SHOW); 
+  this.appendDummyInput() 
+      .setAlign(Blockly.ALIGN_RIGHT)  
+      .appendField(Blockly.Msg.YOLOV8N_SCORELIMIT_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["0","0"],
+		["0.1","0.1"],
+		["0.2","0.2"],
+		["0.3","0.3"],
+		["0.4","0.4"],	  
+		["0.5","0.5"],
+		["0.6","0.6"],
+		["0.7","0.7"],
+		["0.8","0.8"],
+		["0.9","0.9"]	  
+  ]), "scorelimit_");      
+  this.appendDummyInput() 
+      .setAlign(Blockly.ALIGN_RIGHT)    
+      .appendField(Blockly.Msg.YOLOV8N_RESULT_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["Y","block"],
+		["N","none"]	  
+  ]), "result_"); 
+  this.appendDummyInput()  
+      .setAlign(Blockly.ALIGN_RIGHT)    
+      .appendField(Blockly.Msg.YOLOV8N_FRAME_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["Y","1"],
+		["N","0"]	  
+  ]), "frame_");	  
+  this.appendDummyInput()  
+      .setAlign(Blockly.ALIGN_RIGHT)    
+      .appendField(Blockly.Msg.YOLOV8N_MIRRORIMAGE_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["Y","1"],
+		["N","0"]	  
+  ]), "mirrorimage_"); 
+  this.appendDummyInput()  
+      .setAlign(Blockly.ALIGN_RIGHT)    
+      .appendField(Blockly.Msg.YOLOV8N_OPACITY_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		["1","1"],
+		["0.9","0.9"],
+		["0.8","0.8"],
+		["0.7","0.7"],
+		["0.6","0.6"],	  
+		["0.5","0.5"],
+		["0.4","0.4"],
+		["0.3","0.3"],
+		["0.2","0.2"],
+		["0.1","0.1"],	
+		["0","0"]	   
+  ]), "opacity_");	  
+  this.setInputsInline(false);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_object'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	      
+  this.appendValueInput("class_")
+      .setCheck("String")
+      .appendField(Blockly.Msg.YOLOV8N_DATA_CLASS_SHOW);
+  this.appendValueInput("index_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.YOLOV8N_INDEX_SHOW);      
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.YOLOV8N_DATA_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+		[Blockly.Msg.YOLOV8N_OBJECT_ALL_SHOW,""],
+		[Blockly.Msg.YOLOV8N_DATA_CLASS_SHOW,"class"],
+		[Blockly.Msg.YOLOV8N_DATA_SCORE_SHOW,"score"],
+		["X","x"],
+		["Y","y"],
+		[Blockly.Msg.YOLOV8N_DATA_UPPERX_SHOW,"upperx"],
+		[Blockly.Msg.YOLOV8N_DATA_UPPERY_SHOW,"uppery"],	  
+		[Blockly.Msg.YOLOV8N_DATA_MIDDLEX_SHOW,"middlex"],
+		[Blockly.Msg.YOLOV8N_DATA_MIDDLEY_SHOW,"middley"],
+		[Blockly.Msg.YOLOV8N_DATA_BOTTOMX_SHOW,"bottomx"],
+		[Blockly.Msg.YOLOV8N_DATA_BOTTOMY_SHOW,"bottomy"],	  
+		[Blockly.Msg.YOLOV8N_WIDTH_SHOW,"width"],
+		[Blockly.Msg.YOLOV8N_HEIGHT_SHOW,"height"]
+  ]), "data_");   
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_object_number'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW)      
+      .appendField(Blockly.Msg.YOLOV8N_OBJECT_NUMBER_SHOW);
+  this.appendValueInput("class_")
+      .setCheck("String")
+      .appendField(Blockly.Msg.YOLOV8N_DATA_CLASS_SHOW);	      
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_state'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_STATE_SHOW)
+      .appendField(new Blockly.FieldDropdown([
+        ["Y","1"],
+        ["N","0"]
+  ]), "state_");   
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_video_position'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	  
+  this.appendValueInput("left_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.YOLOV8N_VIDEO_LEFT_SHOW);  
+  this.appendValueInput("top_")
+      .setCheck("Number")
+      .appendField(Blockly.Msg.YOLOV8N_VIDEO_TOP_SHOW);   	  
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_canvas_get'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_CANVAS_GET_SHOW); 
+  this.setInputsInline(true);
+  this.setOutput(true, null); 
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_clear'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_CLEAR_SHOW); 
+  this.setInputsInline(true);	  
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_pause'] = {
+  init: function() {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	  
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_PAUSE_SHOW);
+  this.appendValueInput("time_")
+      .setCheck("Number")    
+      .appendField("(ms)"); 	  
+    this.setInputsInline(true);    
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_recognitied'] = {
+  init: function () {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	  
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.YOLOV8N_RECOGNITIED_SHOW);
+  this.appendDummyInput()
+      .appendField(new Blockly.FieldDropdown([
+		[Blockly.Msg.YOLOV8N_RECOGNITIED_OBJECT_SHOW,"Y"],
+		[Blockly.Msg.YOLOV8N_RECOGNITIED_NOTHING_SHOW,"N"]	
+  ]), "status_");	  
+  this.appendStatementInput("do_");
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_unrecognitied'] = {
+  init: function () {
+  this.appendDummyInput()  
+      .appendField(Blockly.Msg.YOLOV8N_SHOW);	  
+  this.appendDummyInput()
+      .appendField(Blockly.Msg.YOLOV8N_UNRECOGNITIED_SHOW);	  
+  this.appendStatementInput("do_");
+  this.setInputsInline(true);
+  this.setPreviousStatement(true);
+  this.setNextStatement(true);
+  this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_xy_in_triangle'] = {
+  init: function() {
+    this.appendDummyInput()  
+        .appendField(Blockly.Msg.YOLOV8N_SHOW)  
+        .appendField(Blockly.Msg.YOLOV8N_GEOFENCE);	  
+	this.appendValueInput("x")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X")
+		.setCheck("Number");		
+	this.appendValueInput("y")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y")
+		.setCheck("Number");
+	this.appendDummyInput()
+		.setAlign(Blockly.ALIGN_RIGHT)		
+		.appendField(Blockly.Msg["YOLOV8N_XY_IN_TRIANGLE"]);			
+	this.appendValueInput("x1")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X1")
+		.setCheck("Number");		
+	this.appendValueInput("y1")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y1")
+		.setCheck("Number");
+	this.appendValueInput("x2")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X2")
+		.setCheck("Number");		
+	this.appendValueInput("y2")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y2")
+		.setCheck("Number");
+	this.appendValueInput("x3")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X3")
+		.setCheck("Number");		
+	this.appendValueInput("y3")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y3")
+		.setCheck("Number");			
+	this.setInputsInline(false);
+	this.setOutput(true, null); 
+	this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
+Blockly.Blocks['yolov8n_xy_in_quadrilateral'] = {
+  init: function() {
+    this.appendDummyInput()  
+        .appendField(Blockly.Msg.YOLOV8N_SHOW)  
+        .appendField(Blockly.Msg.YOLOV8N_GEOFENCE);		  
+	this.appendValueInput("x")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X")
+		.setCheck("Number");		
+	this.appendValueInput("y")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y")
+		.setCheck("Number");
+	this.appendDummyInput()
+		.setAlign(Blockly.ALIGN_RIGHT)		
+		.appendField(Blockly.Msg["YOLOV8N_XY_IN_QUADRILATERAL"]);			
+	this.appendValueInput("x1")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X1")
+		.setCheck("Number");		
+	this.appendValueInput("y1")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y1")
+		.setCheck("Number");
+	this.appendValueInput("x2")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X2")
+		.setCheck("Number");		
+	this.appendValueInput("y2")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y2")
+		.setCheck("Number");
+	this.appendValueInput("x3")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X3")
+		.setCheck("Number");		
+	this.appendValueInput("y3")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y3")
+		.setCheck("Number");
+	this.appendValueInput("x4")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("X4")
+		.setCheck("Number");		
+	this.appendValueInput("y4")
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField("Y4")
+		.setCheck("Number");
+	this.appendDummyInput()
+		.setAlign(Blockly.ALIGN_RIGHT) 		
+		.appendField(Blockly.Msg["YOLOV8N_XY_ARRANGE"]);
+	this.setInputsInline(false);
+	this.setOutput(true, null); 
+	this.setColour(Blockly.Msg["HUE_26"]);
+  }
+};
+
 Blockly.Blocks['amb82_mini_file_remote_get_sd'] = {
   init: function() {
     this.appendDummyInput()
--- LinkIt7697/France/javascript.js
+++ LinkIt7697/France/javascript.js
@@ -1,3 +1,127 @@
+Blockly.Arduino['yolov8n_esp32cam'] = function(block) {
+	var jsfile = 'yolov8n_esp32cam.js';
+	if (block.parentBlock_) {
+		if (block.parentBlock_.getFieldValue('type')) {
+			if (block.parentBlock_.getFieldValue('type')=="stream") {
+				jsfile = 'yolov8n_esp32cam_stream.js';
+			}
+		}
+		else
+			jsfile = 'yolov8n_esp32cam_stream.js';
+	}
+	
+	var javascript_initial = Blockly.Arduino.statementToCode(block, 'javascript_initial');
+	var javascript_recognition = Blockly.Arduino.statementToCode(block, 'javascript_recognition');
+		
+	var code = "\"<!DOCTYPE html><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><script src='https:\/\/ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js'></script><script src='https:\/\/cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.20.0/dist/tf.min.js'></script><script src='https:\/\/fustyles.github.io/webduino/GameElements_20190131/gameelements.js'></script><script src='https:\/\/fustyles.github.io/webduino/yolov8n_20260715/yolov8n.js'></script><script src='https:\/\/fustyles.github.io/webduino/yolov8n_20260715/"+jsfile+"'></script>";
+	code += servermodule_javascript_include();
+	code += "</head><body><script>const delay=(seconds)=>{return new Promise((resolve)=>{setTimeout(resolve,seconds*1000);});};const main=async()=>{yolov8n_video('block','1','1','1');"+javascript_initial.replace(/"/g,"'").replace(/\n/g,"").replace(/NULL/g,"null").replace(/int /g,"/var ")+" recognitionFinish = async function() {"+javascript_recognition.replace(/"/g,"'").replace(/\n/g,"").replace(/NULL/g,"null").replace(/int /g,"var ")+"}};main();</script></body></html>\"";
+
+  return [code,Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino['yolov8n_video'] = function(block) {
+  var value_modelpath_ = Blockly.Arduino.valueToCode(block, 'modelpath_', Blockly.Arduino.ORDER_ATOMIC);
+  var value_classes_  = Blockly.Arduino.valueToCode(block, 'classes_', Blockly.Arduino.ORDER_ATOMIC); 
+  var value_scorelimit_ = block.getFieldValue('scorelimit_');
+  var value_result_ = block.getFieldValue('result_');
+  var value_frame_ = block.getFieldValue('frame_');
+  var value_mirrorimage_ = block.getFieldValue('mirrorimage_');
+  var value_opacity_ = block.getFieldValue('opacity_');
+  var code = 'yolov8n_video(' + value_modelpath_ + ',' + value_classes_ + ',' + value_scorelimit_ + ',"' + value_result_ + '","' + value_frame_ + '","' + value_mirrorimage_ + '","' + value_opacity_ + '");\n';
+  return code;
+};
+
+Blockly.Arduino['yolov8n_object'] = function(block) { 
+  var value_class_ = Blockly.Arduino.valueToCode(block, 'class_', Blockly.Arduino.ORDER_ATOMIC);
+  var value_index_ = Blockly.Arduino.valueToCode(block, 'index_', Blockly.Arduino.ORDER_ATOMIC);  
+  var value_data_ = block.getFieldValue('data_');
+  var code = 'yolov8n_object(' + value_class_ + ',' + value_index_ + ',"' + value_data_ + '")';
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['yolov8n_object_number'] = function(block) { 
+  var value_class_ = Blockly.Arduino.valueToCode(block, 'class_', Blockly.Arduino.ORDER_ATOMIC);
+  var code = 'yolov8n_object_number(' + value_class_ + ')';
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['yolov8n_state'] = function(block) {
+  var value_state_ = block.getFieldValue('state_');
+  var code = 'yolov8n_state(' + value_state_ + ');\n';
+  return code;
+};
+
+Blockly.Arduino['yolov8n_video_position'] = function(block) { 
+  var value_left_ = Blockly.Arduino.valueToCode(block, 'left_', Blockly.Arduino.ORDER_ATOMIC);
+  var value_top_ = Blockly.Arduino.valueToCode(block, 'top_', Blockly.Arduino.ORDER_ATOMIC);
+  var code = 'yolov8n_video_position(' + value_left_ + ',' + value_top_ + ');\n';
+  return code;
+};
+
+Blockly.Arduino['yolov8n_canvas_get'] = function(block) { 
+  var code = '"yolov8n"';
+  return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['yolov8n_clear'] = function(block) { 
+  var code = 'yolov8n_clear();\n';
+  return code;
+};
+
+Blockly.Arduino['yolov8n_pause'] = function(block) { 
+  var value_time = Blockly.Arduino.valueToCode(block, 'time_', Blockly.Arduino.ORDER_ATOMIC);
+  var code = 'yolov8n_state(0);setTimeout(function(){yolov8n_state(1);}, '+value_time+');\n';
+  return code;
+};
+
+Blockly.Arduino['yolov8n_recognitied'] = function(block) { 
+  var value_status = block.getFieldValue('status_');
+  var statements_do = Blockly.Arduino.statementToCode(block, 'do_');
+  
+  if (value_status=="Y")
+	  var code = 'yolov8n_recognitionFinish = async function() {\n  yolov8n_state(0);\n' + statements_do + '\n  yolov8n_state(1);\n};\n';
+  else
+	  var code = 'yolov8n_unrecognitionFinish = async function() {\n  yolov8n_state(0);\n' + statements_do + '\n  yolov8n_state(1);\n};\n';
+  return code;  
+};
+
+Blockly.Arduino['yolov8n_unrecognitied'] = function(block) { 
+  var statements_do = Blockly.Arduino.statementToCode(block, 'do_');
+  var code = 'yolov8n_unrecognitionFinish = async function() {\n  yolov8n_state(0);\n  ' + statements_do + '\n  yolov8n_state(1);\n};\n';
+  return code;
+};
+
+Blockly.Arduino['yolov8n_xy_in_triangle'] = function(block) {
+	var x = Blockly.Arduino.valueToCode(block, 'x', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y = Blockly.Arduino.valueToCode(block, 'y', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var x1 = Blockly.Arduino.valueToCode(block, 'x1', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y1 = Blockly.Arduino.valueToCode(block, 'y1', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var x2 = Blockly.Arduino.valueToCode(block, 'x2', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y2 = Blockly.Arduino.valueToCode(block, 'y2', Blockly.Arduino.ORDER_ATOMIC)||0;	
+	var x3 = Blockly.Arduino.valueToCode(block, 'x3', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y3 = Blockly.Arduino.valueToCode(block, 'y3', Blockly.Arduino.ORDER_ATOMIC)||0;
+	
+	var code = 'yolov8n_xy_in_triangle('+x+', '+y+', '+x1+', '+y1+', '+x2+', '+y2+', '+x3+', '+y3+')';
+	return [code, Blockly.Arduino.ORDER_NONE];
+};
+
+Blockly.Arduino['yolov8n_xy_in_quadrilateral'] = function(block) {
+	var x = Blockly.Arduino.valueToCode(block, 'x', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y = Blockly.Arduino.valueToCode(block, 'y', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var x1 = Blockly.Arduino.valueToCode(block, 'x1', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y1 = Blockly.Arduino.valueToCode(block, 'y1', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var x2 = Blockly.Arduino.valueToCode(block, 'x2', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y2 = Blockly.Arduino.valueToCode(block, 'y2', Blockly.Arduino.ORDER_ATOMIC)||0;	
+	var x3 = Blockly.Arduino.valueToCode(block, 'x3', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y3 = Blockly.Arduino.valueToCode(block, 'y3', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var x4 = Blockly.Arduino.valueToCode(block, 'x4', Blockly.Arduino.ORDER_ATOMIC)||0;
+	var y4 = Blockly.Arduino.valueToCode(block, 'y4', Blockly.Arduino.ORDER_ATOMIC)||0;
+	
+	var code = 'yolov8n_xy_in_quadrilateral('+x+', '+y+', '+x1+', '+y1+', '+x2+', '+y2+', '+x3+', '+y3+', '+x4+', '+y4+')';
+	return [code, Blockly.Arduino.ORDER_NONE];
+};
+
 Blockly.Arduino['amb82_mini_file_remote_get_sd'] = function(block) {
 	var domain = Blockly.Arduino.valueToCode(block, 'domain_', Blockly.Arduino.ORDER_ATOMIC);	
 	var port = Blockly.Arduino.valueToCode(block, 'port_', Blockly.Arduino.ORDER_ATOMIC);	
