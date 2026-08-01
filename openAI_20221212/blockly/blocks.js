f687ed5d9db336dbf10289be44dbb606b80291a0
fustyles <34397289+fustyles@users.noreply.github.com>
2026-08-01 11:55:16 +0800

    Update blocks.js

--- openAI_20221212/blockly/blocks.js
+++ openAI_20221212/blockly/blocks.js
@@ -195,29 +195,8 @@ Blockly.Blocks['openai_chat_initial'] = {
 			["GPT-5.6_Luna","gpt-5.6-luna"],
 			["GPT-5.5","gpt-5.5"],
 			["GPT-5.5_Pro","gpt-5.5-pro"],
-			["Groq_llama-3.1-8b-instant","llama-3.1-8b-instant"],
-			["Groq_llama-3.3-70b-versatile","llama-3.3-70b-versatile"],
-			["Groq_openai/gpt-oss-120b","openai/gpt-oss-120b"],
-			["Groq_openai/gpt-oss-20b","openai/gpt-oss-20b"],
-			["Groq_whisper-large-v3","whisper-large-v3"],
-			["Groq_whisper-large-v3-turbo","whisper-large-v3-turbo"],
-			["Groq_canopylabs/orpheus-arabic-saudi","canopylabs/orpheus-arabic-saudi"],
-			["Groq_canopylabs/orpheus-v1-english","canopylabs/orpheus-v1-english"],
-			["Groq_meta-llama/llama-4-scout-17b-16e-instruct","meta-llama/llama-4-scout-17b-16e-instruct"],
-			["Groq_meta-llama/llama-prompt-guard-2-22m","meta-llama/llama-prompt-guard-2-22m"],
-			["Groq_meta-llama/llama-prompt-guard-2-86m","meta-llama/llama-prompt-guard-2-86m"],
-			["Groq_moonshotai/kimi-k2-instruct-0905","moonshotai/kimi-k2-instruct-0905"],
-			["Groq_openai/gpt-oss-safeguard-20b","openai/gpt-oss-safeguard-20b"],
-			["Groq_qwen/qwen3-32b","qwen/qwen3-32b"],
-			["xAI_grok-4.20-multi-agent-beta-0309" ,"grok-4.20-multi-agent-beta-0309"],
-			["xAI_grok-4.20-beta-0309-reasoning" ,"grok-4.20-beta-0309-reasoning"],
-			["xAI_grok-4.20-beta-0309-non-reasoning" ,"grok-4.20-beta-0309-non-reasoning"],
-			["xAI_grok-code-fast-1" ,"grok-code-fast-1"],
-			["xAI_grok-4-1-fast-reasoning" ,"grok-4-1-fast-reasoning"],
-			["xAI_grok-4-1-fast-non-reasoning" ,"grok-4-1-fast-non-reasoning"],
-			["xAI_grok-imagine-image-pro" ,"grok-imagine-image-pro"],
-			["xAI_grok-imagine-image" ,"grok-imagine-image"],
-			["xAI_grok-imagine-video" ,"grok-imagine-video"]
+			["xAI_grok-4.5" ,"grok-4.5"],
+			["xAI_grok-imagine-image-quality" ,"grok-imagine-image-quality"]
 	  ]), "model");
   this.appendDummyInput()  
       .appendField(Blockly.Msg["OPENAI_CHAT_TEMPERATURE_SHOW"])
